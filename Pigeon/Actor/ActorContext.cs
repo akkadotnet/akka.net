70bbcca1a7b3585cad4c4ec7230fb68fe38a2dfb
Roger Alsing <rogeralsing@gmail.com>
2013-12-30 19:29:08 +0100

    ActorRefFactory

--- Pigeon/Actor/ActorContext.cs
+++ Pigeon/Actor/ActorContext.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Pigeon.Actor
 {
-    public class ActorContext
+    public class ActorContext : ActorRefFactory
+    {        
+        public string Name { get; set; }
+
+        public ActorRef Self { get; private set; }
+        public ActorRef Parent { get;private set; }
+
+        private IEnumerable<ActorRef> Children { get; set; }
+        public ActorRef Child(string name)
+        {
+            return Children.Where(actorRef => actorRef.Name == name).FirstOrDefault();
+        }
+    }
+
+    public class ActorRefFactory
     {
         public ActorSystem System { get; set; }
-        public string Name { get; set; }
 
         public ActorRef ActorOf<TActor>(string name = null) where TActor : ActorBase
         {
@@ -19,15 +32,6 @@ namespace Pigeon.Actor
         public ActorRef ActorSelection(string remoteActorPath)
         {
             return this.System.ActorSelection(remoteActorPath, null);
-        }
-
-        public ActorRef Self { get; private set; }
-        public ActorRef Parent { get;private set; }
-
-        private IEnumerable<ActorRef> Children { get; set; }
-        public ActorRef Child(string name)
-        {
-            return Children.Where(actorRef => actorRef.Name == name).FirstOrDefault();
         }
     }
 }
