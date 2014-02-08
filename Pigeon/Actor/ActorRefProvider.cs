05f58f52f0b8109ddeb0a1a177125e53de712474
rogeralsing <rogeralsing@gmail.com>
2014-02-08 01:27:15 +0100

    actor ref provider

---
+++ Pigeon/Actor/ActorRefProvider.cs
@@ -0,0 +1,28 @@
+﻿using Pigeon.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pigeon.Actor
+{
+    public class ActorRefProvider
+    {
+        public LocalActorRef ActorOf(ActorCell parentContext,Props props,string name)
+        {
+            if (props.RouterConfig != null)
+            {
+                var cell = new RoutedActorCell(parentContext, props, name);
+                parentContext.NewActor(cell);
+                return cell.Self;
+            }
+            else
+            {
+                var cell = new ActorCell(parentContext, props, name);
+                parentContext.NewActor(cell);
+                return cell.Self;
+            }
+        }
+    }
+}
