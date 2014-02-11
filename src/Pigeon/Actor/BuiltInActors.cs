4e029c36854160a92ec330b35d356963e8eab908
rogeralsing <rogeralsing@gmail.com>
2014-02-11 20:29:02 +0100

    Added scala code for ToDo

--- src/Pigeon/Actor/BuiltInActors.cs
+++ src/Pigeon/Actor/BuiltInActors.cs
@@ -22,6 +22,32 @@ namespace Pigeon.Actor
         }
     }
 
+    /*
+private[akka] class DeadLetterActorRef(_provider: ActorRefProvider,
+                                       _path: ActorPath,
+                                       _eventStream: EventStream) extends EmptyLocalActorRef(_provider, _path, _eventStream) {
+
+  override def !(message: Any)(implicit sender: ActorRef = this): Unit = message match {
+    case null                ⇒ throw new InvalidMessageException("Message is null")
+    case Identify(messageId) ⇒ sender ! ActorIdentity(messageId, Some(this))
+    case d: DeadLetter       ⇒ if (!specialHandle(d.message, d.sender)) eventStream.publish(d)
+    case _ ⇒ if (!specialHandle(message, sender))
+      eventStream.publish(DeadLetter(message, if (sender eq Actor.noSender) provider.deadLetters else sender, this))
+  }
+
+  override protected def specialHandle(msg: Any, sender: ActorRef): Boolean = msg match {
+    case w: Watch ⇒
+      if (w.watchee != this && w.watcher != this)
+        w.watcher.sendSystemMessage(
+          DeathWatchNotification(w.watchee, existenceConfirmed = false, addressTerminated = false))
+      true
+    case _ ⇒ super.specialHandle(msg, sender)
+  }
+
+  @throws(classOf[java.io.ObjectStreamException])
+  override protected def writeReplace(): AnyRef = DeadLetterActorRef.serialized
+} 
+     */
     public class DeadLetterActorRef : ActorRef
     {
         private EventBus eventStream;
@@ -34,6 +60,8 @@ namespace Pigeon.Actor
 
         protected override void TellInternal(object message, ActorRef sender)
         {
+            //TODO: need to special handle some messages here
+            //e.g. identity,
             eventStream.Publish(new DeadLetter(message, sender, this));
         }
 
