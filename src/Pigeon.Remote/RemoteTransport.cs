84cc3746353cc2b5d45cddd25ef5e98f2b1acba3
rogeralsing <rogeralsing@gmail.com>
2014-02-16 20:52:17 +0100

    Added RemoteTransport - prepping for remoting

--- src/Pigeon.Remote/RemoteTransport.cs
+++ src/Pigeon.Remote/RemoteTransport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pigeon.Actor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,44 @@ using System.Threading.Tasks;
 
 namespace Pigeon.Remote
 {
-    public class RemoteTransport
+    public abstract class RemoteTransport
     {
+        private RemoteActorRefProvider provider;
+        private ActorSystem system;
+
+        public ISet<Address> Addresses
+        {
+            get;
+            protected set;
+        }
+
+        public RemoteTransport(ActorSystem system, RemoteActorRefProvider provider)
+        {
+            this.system = system;
+            this.provider = provider;
+        }
+
+        public abstract void Startup();
+
+        public abstract void Send(object message, ActorRef sender, RemoteActorRef recipient);
+
+        public abstract Task<bool> ManagementCommand(object cmd);
+
+        public abstract Address LocalAddressForRemote(Address remote);
+        public Address DefaultAddress
+        {
+            get;
+            protected set;
+        }
+        public bool useUntrustedMode
+        {
+            get;
+            protected set;
+        }
+        public bool logRemoteLifeCycleEvents
+        {
+            get;
+            protected set;
+        }
     }
 }
