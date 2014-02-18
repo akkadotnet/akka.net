1d182875f242dfff05c892db9737bebb63c0f54c
rogeralsing <rogeralsing@gmail.com>
2014-02-18 08:03:12 +0100

    Added tests for RemoteDaemon

---
+++ Pigeon.Remote.Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pigeon.Tests;
+using Pigeon.Actor;
+using Pigeon.Configuration;
+
+namespace Pigeon.Remote.Tests
+{
+    [TestClass]
+    public class RemoteDaemonSpec : AkkaSpec
+    {
+        public class SomeActor : UntypedActor
+        {
+            protected override void OnReceive(object message)
+            {               
+            }
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var config = ConfigurationFactory.ParseString(@"
+akka {  
+    actor {
+        provider = ""Pigeon.Remote.RemoteActorRefProvider, Pigeon.Remote""
+        debug {
+            lifecycle = on
+            receive = on
+        }
+    }
+    remote {
+        server {
+            host = ""127.0.0.1""
+            port = 8091
+        }
+    }
+}
+");
+            var sys = ActorSystem.Create("foo", config);
+            var provider = (RemoteActorRefProvider)sys.Provider;
+            var daemon = provider.RemoteDaemon;
+
+            daemon.Tell(new DaemonMsgCreate(Props.Create(() => new SomeActor()),null,"/user/foo",null));
+            //TODO: complete this test
+        }
+    }
+}
