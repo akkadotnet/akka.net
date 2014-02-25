20424828fc90aba76396df7c4ec00ccbbf7e83f2
rogeralsing <rogeralsing@gmail.com>
2014-02-25 09:48:54 +0100

    added logging in TcpTransport

--- src/Pigeon.Remote/Transport/TcpTransport/TcpTransport.cs
+++ src/Pigeon.Remote/Transport/TcpTransport/TcpTransport.cs
@@ -1,5 +1,6 @@
 ﻿using Pigeon.Actor;
 using Pigeon.Configuration;
+using Pigeon.Event;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace Pigeon.Remote.Transport
     public class TcpTransport : Transport
     {
         private TcpServer server;
+        private LoggingAdapter log;
         public TcpTransport(ActorSystem system, Config config):base(system,config)
         {            
             var protocol = "akka."+config.GetString("transport-protocol");
@@ -18,12 +20,14 @@ namespace Pigeon.Remote.Transport
             var host = config.GetString("hostname");
             var port = config.GetInt("port");
             this.Address = new Address(protocol, system.Name, host, port);
+            this.log = Logging.GetLogger(system, this);
             server = new TcpServer(system, host, port);
         }
 
         public override Address Listen()
         {
             server.Start();
+            log.Info("is listening @ {0}", Address);
             return Address;
         }
 
