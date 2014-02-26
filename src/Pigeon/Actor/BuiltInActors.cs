0ed6887b80aed9afb6966a1b7cdfd29b57033640
rogeralsing <rogeralsing@gmail.com>
2014-02-26 20:36:31 +0100

    DeadLetterActorRef

--- src/Pigeon/Actor/BuiltInActors.cs
+++ src/Pigeon/Actor/BuiltInActors.cs
@@ -23,18 +23,6 @@ namespace Akka.Actor
     }
 
     /*
-private[akka] class DeadLetterActorRef(_provider: ActorRefProvider,
-                                       _path: ActorPath,
-                                       _eventStream: EventStream) extends EmptyLocalActorRef(_provider, _path, _eventStream) {
-
-  override def !(message: Any)(implicit sender: ActorRef = this): Unit = message match {
-    case null                ⇒ throw new InvalidMessageException("Message is null")
-    case Identify(messageId) ⇒ sender ! ActorIdentity(messageId, Some(this))
-    case d: DeadLetter       ⇒ if (!specialHandle(d.message, d.sender)) eventStream.publish(d)
-    case _ ⇒ if (!specialHandle(message, sender))
-      eventStream.publish(DeadLetter(message, if (sender eq Actor.noSender) provider.deadLetters else sender, this))
-  }
-
   override protected def specialHandle(msg: Any, sender: ActorRef): Boolean = msg match {
     case w: Watch ⇒
       if (w.watchee != this && w.watcher != this)
@@ -43,9 +31,6 @@ private[akka] class DeadLetterActorRef(_provider: ActorRefProvider,
       true
     case _ ⇒ super.specialHandle(msg, sender)
   }
-
-  @throws(classOf[java.io.ObjectStreamException])
-  override protected def writeReplace(): AnyRef = DeadLetterActorRef.serialized
 } 
      */
     public class DeadLetterActorRef : ActorRef
