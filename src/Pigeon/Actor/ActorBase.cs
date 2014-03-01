f54fb4582b14ab4815bde6d71aafb7ae30da2cdf
rogeralsing <rogeralsing@gmail.com>
2014-03-01 20:26:24 +0100

    Documentation

--- src/Pigeon/Actor/ActorBase.cs
+++ src/Pigeon/Actor/ActorBase.cs
@@ -13,8 +13,15 @@ namespace Akka.Actor
     {
 
     }
+    /// <summary>
+    /// Class ActorBase.
+    /// </summary>
     public abstract partial class ActorBase
     {
+        /// <summary>
+        /// Gets the sending ActorRef of the current message
+        /// </summary>
+        /// <value>The sender ActorRef</value>
         protected ActorRef Sender
         {
             get
@@ -22,10 +29,18 @@ namespace Akka.Actor
                 return Context.Sender;
             }
         }
+        /// <summary>
+        /// Gets the self ActorRef
+        /// </summary>
+        /// <value>Self ActorRef</value>
         protected LocalActorRef Self { get; private set; }
 
-       
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActorBase"/> class.
+        /// </summary>
+        /// <exception cref="System.Exception">Do not create actors using 'new', always create them using an ActorContext/System</exception>
         protected ActorBase()
         {
             if (ActorCell.Current == null)
@@ -36,28 +51,53 @@ namespace Akka.Actor
             ((ActorCell)Context).Start();
         }
 
+        /// <summary>
+        /// Processor for user defined messages.
+        /// </summary>
+        /// <param name="message">The message.</param>
         protected abstract void OnReceive(object message);
 
+        /// <summary>
+        /// The last message is unhandled
+        /// </summary>
         private bool lastMessageIsUnhandled = false;
+        /// <summary>
+        /// Gets a function that will tell if the last message was unhandled or not.
+        /// </summary>
+        /// <returns>Func{System.ObjectSystem.Boolean}.</returns>
         public Func<object,bool> GetUnhandled()
         {
             lastMessageIsUnhandled = false;
 
             return IsUnhandled;
-        }   
+        }
+        /// <summary>
+        /// Determines whether the specified message is unhandled.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns><c>true</c> if the specified message is unhandled; otherwise, <c>false</c>.</returns>
         private bool IsUnhandled(object message)
         {
             return lastMessageIsUnhandled;
         }
 
+        /// <summary>
+        /// Marks the message as unhandled.
+        /// </summary>
+        /// <param name="message">The message.</param>
         protected void Unhandled(object message)
         {
             lastMessageIsUnhandled = true;
             Context.System.EventStream.Publish(new UnhandledMessage(message, Sender, Self));
         }
 
-        
 
+
+        /// <summary>
+        /// Gets the context.
+        /// </summary>
+        /// <value>The context.</value>
+        /// <exception cref="System.NotSupportedException">There is no active ActorContext, this is most likely due to use of async operations from within this actor.</exception>
         protected static IActorContext Context
         {
             get
@@ -69,11 +109,18 @@ namespace Akka.Actor
                 return context;
             }
         }
+        /// <summary>
+        /// Becomes the specified receive function.
+        /// </summary>
+        /// <param name="receive">The receive.</param>
         protected void Become(Receive receive)
         {
             Context.Become(receive);
         }
 
+        /// <summary>
+        /// Unbecomes the current receive function.
+        /// </summary>
         protected void Unbecome()
         {
             Context.Unbecome();
