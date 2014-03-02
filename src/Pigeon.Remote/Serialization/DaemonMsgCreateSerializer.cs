0246c0e8799edf56650fde6b3b93754179d13284
rogeralsing <rogeralsing@gmail.com>
2014-03-02 09:39:11 +0100

    Remote serializers

---
+++ src/Pigeon.Remote/Serialization/DaemonMsgCreateSerializer.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Actor;
+using Akka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.Remote.Serialization
+{
+    public class DaemonMsgCreateSerializer : Serializer
+    {
+        public DaemonMsgCreateSerializer(ActorSystem system) : base(system) { }
+
+        public override int Identifier
+        {
+            get { return 3; }
+        }
+
+        public override bool IncludeManifest
+        {
+            get { return false; }
+        }
+
+        public override byte[] ToBinary(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object FromBinary(byte[] bytes, Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Pigeon.Remote/Serialization/MessageContainerSerializer.cs
@@ -0,0 +1,36 @@
+﻿using Akka.Actor;
+using Akka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.Remote.Serialization
+{
+    public class MessageContainerSerializer : Serializer
+    {
+
+        public MessageContainerSerializer(ActorSystem system) : base(system) { }
+
+        public override int Identifier
+        {
+            get { return 6; }
+        }
+
+        public override bool IncludeManifest
+        {
+            get { return false; }
+        }
+
+        public override byte[] ToBinary(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object FromBinary(byte[] bytes, Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
