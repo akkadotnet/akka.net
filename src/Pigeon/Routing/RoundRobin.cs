691fc010b2761fe86540ca57a2cd9d72bba3c5a5
rogeralsing <rogeralsing@gmail.com>
2014-03-08 19:36:30 +0100

    More clean up

--- src/Pigeon/Routing/RoundRobin.cs
+++ src/Pigeon/Routing/RoundRobin.cs
@@ -6,10 +6,22 @@ using Akka.Configuration;
 
 namespace Akka.Routing
 {
+    /// <summary>
+    ///     Class RoundRobinRoutingLogic.
+    /// </summary>
     public class RoundRobinRoutingLogic : RoutingLogic
     {
+        /// <summary>
+        ///     The next
+        /// </summary>
         private int next = -1;
 
+        /// <summary>
+        ///     Selects the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="routees">The routees.</param>
+        /// <returns>Routee.</returns>
         public override Routee Select(object message, Routee[] routees)
         {
             if (routees == null || routees.Length == 0)
@@ -20,50 +32,97 @@ namespace Akka.Routing
         }
     }
 
+    /// <summary>
+    ///     Class RoundRobinGroup.
+    /// </summary>
     public class RoundRobinGroup : Group
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoundRobinGroup" /> class.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
         public RoundRobinGroup(Config config)
             : base(config.GetStringList("routees.paths"))
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoundRobinGroup" /> class.
+        /// </summary>
+        /// <param name="paths">The paths.</param>
         public RoundRobinGroup(params string[] paths)
             : base(paths)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoundRobinGroup" /> class.
+        /// </summary>
+        /// <param name="paths">The paths.</param>
         public RoundRobinGroup(IEnumerable<string> paths) : base(paths)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoundRobinGroup" /> class.
+        /// </summary>
+        /// <param name="routees">The routees.</param>
         public RoundRobinGroup(IEnumerable<ActorRef> routees) : base(routees)
         {
         }
 
+        /// <summary>
+        ///     Creates the router.
+        /// </summary>
+        /// <returns>Router.</returns>
         public override Router CreateRouter()
         {
             return new Router(new RoundRobinRoutingLogic());
         }
     }
 
+    /// <summary>
+    ///     Class RoundRobinPool.
+    /// </summary>
     public class RoundRobinPool : Pool
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoundRobinPool" /> class.
+        /// </summary>
+        /// <param name="nrOfInstances">The nr of instances.</param>
+        /// <param name="resizer">The resizer.</param>
+        /// <param name="supervisorStrategy">The supervisor strategy.</param>
+        /// <param name="routerDispatcher">The router dispatcher.</param>
+        /// <param name="usePoolDispatcher">if set to <c>true</c> [use pool dispatcher].</param>
         public RoundRobinPool(int nrOfInstances, Resizer resizer, SupervisorStrategy supervisorStrategy,
             string routerDispatcher, bool usePoolDispatcher = false)
             : base(nrOfInstances, resizer, supervisorStrategy, routerDispatcher, usePoolDispatcher)
         {
         }
 
+        /// <summary>
+        ///     Gets the routees.
+        /// </summary>
+        /// <param name="system">The system.</param>
+        /// <returns>IEnumerable{Routee}.</returns>
         public override IEnumerable<Routee> GetRoutees(ActorSystem system)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        ///     Creates the router actor.
+        /// </summary>
+        /// <returns>RouterActor.</returns>
         public override RouterActor CreateRouterActor()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        ///     Creates the router.
+        /// </summary>
+        /// <returns>Router.</returns>
         public override Router CreateRouter()
         {
             throw new NotImplementedException();
--- src/Pigeon/Routing/RouterMsg.cs
+++ src/Pigeon/Routing/RouterMsg.cs
@@ -3,26 +3,49 @@ using System.Linq;
 
 namespace Akka.Routing
 {
+    /// <summary>
+    /// Class RouterMessage.
+    /// </summary>
     public static class RouterMessage
     {
+        /// <summary>
+        /// The get routees
+        /// </summary>
         public static readonly GetRoutees GetRoutees = new GetRoutees();
     }
 
+    /// <summary>
+    /// Class RouterManagementMesssage.
+    /// </summary>
     public abstract class RouterManagementMesssage
     {
     }
 
+    /// <summary>
+    /// Class GetRoutees. This class cannot be inherited.
+    /// </summary>
     public sealed class GetRoutees : RouterManagementMesssage
     {
     }
 
+    /// <summary>
+    /// Class Routees. This class cannot be inherited.
+    /// </summary>
     public sealed class Routees
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Routees"/> class.
+        /// </summary>
+        /// <param name="routees">The routees.</param>
         public Routees(IEnumerable<Routee> routees)
         {
             Members = routees.ToArray();
         }
 
+        /// <summary>
+        /// Gets the members.
+        /// </summary>
+        /// <value>The members.</value>
         public IEnumerable<Routee> Members { get; private set; }
     }
 }
