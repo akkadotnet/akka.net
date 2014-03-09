f84a5da7e0f8b5485ee61424979c1007a671b96d
rogeralsing <rogeralsing@gmail.com>
2014-03-09 18:19:00 +0100

    Documenting remote daemon

--- src/Pigeon.Remote/RemoteDaemon.cs
+++ src/Pigeon.Remote/RemoteDaemon.cs
@@ -5,8 +5,18 @@ using Akka.Dispatch.SysMsg;
 
 namespace Akka.Remote
 {
+    /// <summary>
+    /// Class DaemonMsgCreate.
+    /// </summary>
     public class DaemonMsgCreate
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DaemonMsgCreate"/> class.
+        /// </summary>
+        /// <param name="props">The props.</param>
+        /// <param name="deploy">The deploy.</param>
+        /// <param name="path">The path.</param>
+        /// <param name="supervisor">The supervisor.</param>
         public DaemonMsgCreate(Props props, Deploy deploy, string path, ActorRef supervisor)
         {
             Props = props;
@@ -15,25 +25,58 @@ namespace Akka.Remote
             Supervisor = supervisor;
         }
 
+        /// <summary>
+        /// Gets the props.
+        /// </summary>
+        /// <value>The props.</value>
         public Props Props { get; private set; }
 
+        /// <summary>
+        /// Gets the deploy.
+        /// </summary>
+        /// <value>The deploy.</value>
         public Deploy Deploy { get; private set; }
 
+        /// <summary>
+        /// Gets the path.
+        /// </summary>
+        /// <value>The path.</value>
         public string Path { get; private set; }
 
+        /// <summary>
+        /// Gets the supervisor.
+        /// </summary>
+        /// <value>The supervisor.</value>
         public ActorRef Supervisor { get; private set; }
     }
 
+    /// <summary>
+    /// Class RemoteDaemon.
+    /// </summary>
     public class RemoteDaemon : VirtualPathContainer
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RemoteDaemon"/> class.
+        /// </summary>
+        /// <param name="system">The system.</param>
+        /// <param name="path">The path.</param>
+        /// <param name="parent">The parent.</param>
         public RemoteDaemon(ActorSystem system, ActorPath path, InternalActorRef parent)
             : base(system.Provider, path, parent)
         {
             System = system;
         }
 
+        /// <summary>
+        /// Gets the system.
+        /// </summary>
+        /// <value>The system.</value>
         public ActorSystem System { get; private set; }
 
+        /// <summary>
+        /// Called when [receive].
+        /// </summary>
+        /// <param name="message">The message.</param>
         protected void OnReceive(object message)
         {
             if (message is DaemonMsgCreate)
@@ -42,11 +85,20 @@ namespace Akka.Remote
             }
         }
 
+        /// <summary>
+        /// Tells the internal.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="sender">The sender.</param>
         protected override void TellInternal(object message, ActorRef sender)
         {
             OnReceive(message);
         }
 
+        /// <summary>
+        /// Handles the daemon MSG create.
+        /// </summary>
+        /// <param name="message">The message.</param>
         private void HandleDaemonMsgCreate(DaemonMsgCreate message)
         {
             //TODO: find out what format "Path" should have
@@ -61,16 +113,22 @@ namespace Akka.Remote
             actor.Tell(new Watch(actor, this));
         }
 
+        /// <summary>
+        /// Gets the child.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>ActorRef.</returns>
         public override ActorRef GetChild(IEnumerable<string> name)
         {
+            string[] parts = name.ToArray();
             //TODO: I have no clue what the scala version does
-            if (!name.Any())
+            if (!parts.Any())
                 return this;
 
-            string n = name.First();
+            string n = parts.First();
             if (string.IsNullOrEmpty(n))
                 return this;
-            string[] parts = name.ToArray();
+            
             for (int i = parts.Length; i >= 0; i--)
             {
                 string joined = string.Join("/", parts, 0, i);
