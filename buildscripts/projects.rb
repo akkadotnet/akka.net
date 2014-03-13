e913cfe551b11dff32fd737e4d379df25469d23f
Aaron Stannard <aaron@markedup.com>
2014-03-13 12:16:02 -0700

    updated nuget packaging to support changes in dependencies

--- buildscripts/projects.rb
+++ buildscripts/projects.rb
@@ -16,17 +16,13 @@ Projects = {
         :authors => "Roger Alsing",
         :test_dir => "Pigeon.Test",
         :dependencies => {
-            :fast_json => {
-                :package => "fastJSON",
-                :version => "2.0.27.1"
+            :newtonsoft_json => {
+                :package => "Newtonsoft.Json",
+                :version => "6.0.1"
            },
-           :protobuf_net => {
-                :package => "protobuf-net",
-                :version => "2.0.0.668"
-           },
+         },
          :nuget_tags => "akka actors actor model pigeon concurrency",
        },
-      },
     :pigeon_fsharp => {
         :id => "Akka.FSharp",
         :dir => "Pigeon.FSharp",
@@ -50,14 +46,10 @@ Projects = {
         :copyright => " (c) Roger Alsing 2013-2014",
         :authors => "Roger Alsing",
         :nuget_tags => "akka distributed actors remote actor model pigeon concurrency",
-        :references => {
-            :google_protobuf => {
-                :file => "Google.ProtocolBuffers",
-                :target => "lib\\net45\\"
-           },
-           :google_protobuf_serialization => {
-                :file => "Google.ProtocolBuffers.Serialization",
-                :target => "lib\\net45\\"
+        :dependencies => {
+            :google_protocolbuffers => {
+                :package => "Google.ProtocolBuffers",
+                :version => "2.4.1.521"
            },
        },
    },
--- src/Pigeon/Properties/AssemblyInfo.cs
+++ src/Pigeon/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Pigeon")]
+[assembly: AssemblyTitle("Akka")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("Pigeon")]
+[assembly: AssemblyProduct("Akka")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
