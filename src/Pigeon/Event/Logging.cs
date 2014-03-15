f675a40fde0780c6948468e40de4ba46e00fdc9a
rogeralsing <rogeralsing@gmail.com>
2014-03-15 18:14:20 +0100

    changing Logging output

--- src/Pigeon/Event/Logging.cs
+++ src/Pigeon/Event/Logging.cs
@@ -298,8 +298,7 @@ namespace Akka.Event
         /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
         public override string ToString()
         {
-            return string.Format("[{0}][{1}][{2}] {3} [Thread {4}]", LogLevel().ToString().Replace("Level",""),Timestamp , LogSource, Message,
-                Thread.ManagedThreadId);
+            return string.Format("[{0}][{1}][Thread {2}][{3}] {4}", LogLevel().ToString().Replace("Level", "").ToUpperInvariant(), Timestamp, Thread.ManagedThreadId.ToString().PadLeft(4,'0'), LogSource, Message);
         }
     }
 
