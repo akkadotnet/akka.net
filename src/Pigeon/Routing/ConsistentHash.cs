20b9282207c0851850ffc7dc3f21c95c64c665a8
rogeralsing <rogeralsing@gmail.com>
2014-03-15 08:20:47 +0100

    Routers

--- src/Pigeon/Routing/ConsistentHash.cs
+++ src/Pigeon/Routing/ConsistentHash.cs
@@ -39,6 +39,11 @@ namespace Akka.Routing
 
     public class ConsistentHashingGroup : Group
     {
+        [Obsolete("For serialization only",true)]
+        public ConsistentHashingGroup()
+        {
+        }
+
         public ConsistentHashingGroup(Config config)
             : base(config.GetStringList("routees.paths"))
         {
@@ -57,6 +62,43 @@ namespace Akka.Routing
         public ConsistentHashingGroup(IEnumerable<ActorRef> routees) : base(routees)
         {
         }
+
+        public override Router CreateRouter(ActorSystem system)
+        {
+            return new Router(new ConsistentHashingRoutingLogic());
+        }
+    }
+
+    public class ConsistentHashingPool : Pool
+    {
+
+                /// <summary>
+        /// Initializes a new instance of the <see cref="ConsistentHashingPool"/> class.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        public ConsistentHashingPool(Config config) : base(config)
+        {
+            
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsistentHashingPool"/> class.
+        /// </summary>
+        /// <param name="nrOfInstances">The nr of instances.</param>
+        /// <param name="resizer">The resizer.</param>
+        /// <param name="supervisorStrategy">The supervisor strategy.</param>
+        /// <param name="routerDispatcher">The router dispatcher.</param>
+        /// <param name="usePoolDispatcher">if set to <c>true</c> [use pool dispatcher].</param>
+        public ConsistentHashingPool(int nrOfInstances, Resizer resizer,SupervisorStrategy supervisorStrategy, string routerDispatcher, bool usePoolDispatcher = false)
+            : base(nrOfInstances, resizer, supervisorStrategy, routerDispatcher, usePoolDispatcher)
+        {
+            
+        }
+
+        /// <summary>
+        /// Simple form of ConsistentHashingPool constructor
+        /// </summary>
+        /// <param name="nrOfInstances">The nr of instances.</param>
+        public ConsistentHashingPool(int nrOfInstances) : base(nrOfInstances, null, Pool.DefaultStrategy, null) { }
 
         public override Router CreateRouter(ActorSystem system)
         {
