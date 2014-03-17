68861d9a43307babd74c114af28e738a347fbd65
thinkbeforecoding <jeremie.chassaing@thinkbeforecoding.com>
2014-03-17 18:26:43 +0100

    Add tests for the computation expression

---
+++ test/Pigeon.FSharp.Tests/ComputationExpression.fs
@@ -0,0 +1,34 @@
+﻿namespace Akka.FSharp.Tests
+
+open Akka.FSharp
+
+
+
+[<TestClass>]
+type ComputationExpression() =
+    let send m =
+        function
+        | Func f -> f m
+        | r -> r
+
+    let value =
+        function
+        | Return (v:'a) -> v
+        | _ -> Microsoft.VisualStudio.TestTools.UnitTesting.Assert.Fail("Expected a value, found a continuation.")
+               Unchecked.defaultof<'a>
+
+    [<TestMethod>]
+    member x.``return should return value``() =
+        actor { return 1}
+        |> value
+        |> equals 1
+
+
+    [<TestMethod>]
+    member x.``let should bind message``() =
+        actor { let! m = IO<int>.Input
+                return m } 
+        |> send 42
+        |> value
+        |> equals 42
+
---
+++ test/Pigeon.FSharp.Tests/Tests.fs
@@ -0,0 +1,11 @@
+﻿[<AutoOpen>]
+module Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+type TestClassAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute
+type TestMethodAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute
+
+
+let equals (expected: 'a) (value: 'a) = Assert.AreEqual(expected, value) 
+let success = ()
