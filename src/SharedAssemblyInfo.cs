222dff780d488a623374aea23bd244374235479e
Aaron Stannard <aaron@markedup.com>
2014-03-17 17:42:17 -0700

    bumped version to v0.1.4.4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.4.3")]
-[assembly: AssemblyFileVersion("0.1.4.3")]
+[assembly: AssemblyVersion("0.1.4.4")]
+[assembly: AssemblyFileVersion("0.1.4.4")]
