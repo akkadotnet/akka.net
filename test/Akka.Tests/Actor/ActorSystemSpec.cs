06169eebce9f95a570fe72f2642663edc4a3e5fb
stefanalfbo <stefan@alfbo.se>
2014-03-21 22:10:58 +0100

    Add ActorSystemSpec with an initial test AnActorSystemMustRejectInvalidNames

---
+++ test/Akka.Tests/Actor/ActorSystemSpec.cs
@@ -0,0 +1,25 @@
+﻿using Akka.Actor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Akka.Tests.Actor
+{
+    [TestClass]
+    public class ActorSystemSpec : AkkaSpec
+    {
+        [TestMethod]
+        public void AnActorSystemMustRejectInvalidNames()
+        {
+            (new List<string> { 
+                  "hallo_welt",
+                  "-hallowelt",
+                  "hallo*welt",
+                  "hallo@welt",
+                  "hallo#welt",
+                  "hallo$welt",
+                  "hallo%welt",
+                  "hallo/welt"}).ForEach(n => intercept<ArgumentException>(() => ActorSystem.Create(n)));
+        }
+    }
+}
