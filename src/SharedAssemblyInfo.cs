b2819d3c3b055863fbb02f82e371d88ddb726e78
Aaron Stannard <aaron@markedup.com>
2014-03-22 14:59:29 -0700

    made the properties of the UnhandledMessage event public, so they can be logged when received via the event stream

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.2.0")]
+[assembly: AssemblyFileVersion("0.2.2.0")]
