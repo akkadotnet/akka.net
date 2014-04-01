be8ac7aac7f23f1c4efcbc0b05a2f1fb98f04fcd
Aaron Stannard <aaron@markedup.com>
2014-04-01 15:13:15 -0700

    working on adding proper remoting configuration and TcpTransport

---
+++ src/Akka.Remote/Transport/Tcp/TcpTransport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Akka.Configuration;
+
+namespace Akka.Remote.Transport.Tcp
+{
+    class TcpTransportException : RemoteTransportException
+    {
+        public TcpTransportException(string message, Exception cause = null) : base(message, cause)
+        {
+        }
+    }
+
+    class TcpTransportSettings
+    {
+        private Config _config;
+
+        public TcpTransportSettings(Config config)
+        {
+            _config = config;
+        }
+    }
+}
---
+++ test/Akka.Remote.Tests/RemoteConfigSpec.cs
@@ -0,0 +1,28 @@
+﻿using Akka.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Akka.Remote.Tests
+{
+    [TestClass]
+    public class RemoteConfigSpec : AkkaSpec
+    {
+
+        #region Setup / Configuration
+        protected override string GetConfig()
+        {
+            return @"
+                akka.actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                akka.remote.tcp-transport.port = 0
+            ";
+        }
+        #endregion
+
+        [TestMethod]
+        public void Remoting_should_contain_correct_configuration_values_in_ReferenceConf()
+        {
+            var remoteSettings = ((RemoteActorRefProvider) sys.Provider).RemoteSettings;
+
+            Assert.IsFalse(remoteSettings.LogReceive);
+        }
+    }
+}
