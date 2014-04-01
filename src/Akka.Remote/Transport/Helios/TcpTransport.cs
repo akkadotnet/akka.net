74b7c4a0df5f6bc29016e7833fcc3d57d0711a36
Aaron Stannard <aaron@markedup.com>
2014-04-01 16:44:28 -0700

    refactored the HeliosSettings

---
+++ src/Akka.Remote/Transport/Helios/TcpTransport.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Configuration;
+using Akka.Configuration;
+using Akka.Dispatch;
+
+namespace Akka.Remote.Transport.Helios
+{
+    abstract class TransportMode { }
+
+    class Tcp : TransportMode
+    {
+        public override string ToString()
+        {
+            return "tcp";
+        }
+    }
+
+    class Udp : TransportMode
+    {
+        public override string ToString()
+        {
+            return "udp";
+        }
+    }
+
+    class TcpTransportException : RemoteTransportException
+    {
+        public TcpTransportException(string message, Exception cause = null) : base(message, cause)
+        {
+        }
+    }
+
+    class HeliosTransportSettings
+    {
+        private readonly Config _config;
+
+        public HeliosTransportSettings(Config config)
+        {
+            _config = config;
+            Init();
+        }
+
+        private void Init()
+        {
+            //TransportMode
+            var protocolString = _config.GetString("transport-protocol");
+            if (protocolString.Equals("tcp")) TransportMode = new Tcp();
+            else if (protocolString.Equals("udp")) TransportMode = new Udp();
+            else throw new ConfigurationException(string.Format("Unknown transport {0}", protocolString));
+            EnableSsl = _config.GetBoolean("enable-ssl");
+            ConnectTimeout = _config.GetMillisDuration("connection-timeout");
+            WriteBufferHighWaterMark = OptionSize("write-buffer-high-water-mark");
+            WriteBufferLowWaterMark = OptionSize("write-buffer-low-water-mark");
+            SendBufferSize = OptionSize("send-buffer-size");
+            ReceiveBufferSize = OptionSize("receive-buffer-size");
+            var size = OptionSize("maximum-frame-size");
+            if(size == null || size < 32000) throw new ConfigurationException("Setting 'maximum-frame-size' must be at least 32000 bytes");
+            MaxFrameSize = (int)size;
+            Backlog = _config.GetInt("backlog");
+            TcpNoDelay = _config.GetBoolean("tcp-nodelay");
+            TcpKeepAlive = _config.GetBoolean("tcp-keepalive");
+            TcpReuseAddr = _config.GetBoolean("tcp-reuse-addr");
+            var configHost = _config.GetString("hostname");
+            Hostname = string.IsNullOrEmpty(configHost) ? IPHelper.DefaultIp : configHost;
+            ServerSocketWorkerPoolSize = ComputeWps(_config.GetConfig("server-socket-worker-pool"));
+            ClientSocketWorkerPoolSize = ComputeWps(_config.GetConfig("client-socket-worker-pool"));
+        }
+
+        public TransportMode TransportMode { get; private set; }
+
+        public bool EnableSsl { get; private set; }
+
+        public TimeSpan ConnectTimeout { get; private set; }
+
+        public long? WriteBufferHighWaterMark { get; private set; }
+
+        public long? WriteBufferLowWaterMark { get; private set; }
+
+        public long? SendBufferSize { get; private set; }
+
+        public long? ReceiveBufferSize { get; private set; }
+
+        public int MaxFrameSize { get; private set; }
+
+        public int Backlog { get; private set; }
+
+        public bool TcpNoDelay { get; private set; }
+
+        public bool TcpKeepAlive { get; private set; }
+
+        public bool TcpReuseAddr { get; private set; }
+
+        public string Hostname { get; private set; }
+
+        public int ServerSocketWorkerPoolSize { get; private set; }
+
+        public int ClientSocketWorkerPoolSize { get; private set; }
+
+        #region Internal methods
+
+        private long? OptionSize(string s)
+        {
+            var bytes = _config.GetByteSize(s);
+            if (bytes == null || bytes == 0) return null;
+            if(bytes < 0) throw new ConfigurationException(string.Format("Setting {0} must be 0 or positive", s));
+            return bytes;
+        }
+
+        private int ComputeWps(Config config)
+        {
+            return ThreadPoolConfig.ScaledPoolSize(config.GetInt("pool-size-min"), config.GetDouble("pool-size-factor"),
+                config.GetInt("pool-size-max"));
+        }
+
+        #endregion
+    }
+}
---
+++ src/Akka/Dispatch/ThreadPoolBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Akka.Configuration;
+
+namespace Akka.Dispatch
+{
+    class ThreadPoolBuilder
+    {
+    }
+
+    class ThreadPoolConfig
+    {
+        private readonly Config _config;
+
+        public ThreadPoolConfig(Config config)
+        {
+            _config = config;
+        }
+
+        public int PoolSizeMin
+        {
+            get { return _config.GetInt("pool-size-min"); }
+        }
+
+        public double PoolSizeFactor
+        {
+            get { return _config.GetDouble("pool-size-factor"); }
+        }
+
+        public int PoolSizeMax
+        {
+            get { return _config.GetInt("pool-size-max"); }
+        }
+
+        #region Static methods
+
+        public static int ScaledPoolSize(int floor, double scalar, int ceiling)
+        {
+            return Math.Min(Math.Max((int) (Environment.ProcessorCount*scalar), floor), ceiling);
+        }
+
+        #endregion
+    }
+}
