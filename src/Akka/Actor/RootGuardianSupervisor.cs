59c5d83c616f007e2c1518a48a26c749d43d59f3
HCanber <hakan_github@canberger.se>
2014-06-29 09:24:15 +0200

    Add RootGuardianSupervisor, aka theOneWhoWalksTheBubblesOfSpaceTime & RootGuardianActorRef

---
+++ src/Akka/Actor/RootGuardianSupervisor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Dispatch.SysMsg;
+using Akka.Event;
+using Akka.Util;
+
+namespace Akka.Actor
+{
+
+    /// <summary>
+    /// Top-level anchor for the supervision hierarchy of this actor system.
+    /// Note: This class is called theOneWhoWalksTheBubblesOfSpaceTime in Akka
+    /// </summary>
+    public class RootGuardianSupervisor : MinimalActorRef
+    {
+        private readonly LoggingAdapter _log;
+        private readonly TaskCompletionSource<Status> _terminationPromise;
+        private readonly ActorPath _path;
+        private readonly Switch _stopped=new Switch(false);
+        private readonly ActorRefProvider _provider;
+
+        public RootGuardianSupervisor(RootActorPath root, ActorRefProvider provider, TaskCompletionSource<Status> terminationPromise, LoggingAdapter log)
+        {
+            _log = log;
+            _terminationPromise = terminationPromise;
+            _provider = provider;
+            _path = root/"_Root-guardian-supervisor";
+        }
+
+        protected override void TellInternal(object message, ActorRef sender)
+        {
+            var systemMessage = message as SystemMessage;
+            if(systemMessage!=null)
+            {
+                SendSystemMessage(systemMessage);
+            }
+            else
+            {
+                _stopped.IfOff(() =>
+                {
+                    if(message == null) throw new InvalidMessageException();
+                    _log.Error("{0} received unexpected message [{1}]", _path, message);
+                });
+            }
+        }
+
+        private void SendSystemMessage(SystemMessage systemMessage)
+        {
+            _stopped.IfOff(() =>
+            {
+                var failed = systemMessage as Failed;
+                if(failed != null)
+                {
+                    var cause = failed.Cause;
+                    var child = failed.Child;
+                    _log.Error(cause, "guardian {0} failed, shutting down!", child);
+                    CauseOfTermination = cause;
+                    ((InternalActorRef) child).Stop();
+                    return;
+                }
+                var supervise = systemMessage as Supervise;
+                if(supervise != null)
+                {
+                    // TODO register child in some map to keep track of it and enable shutdown after all dead
+                    return;
+                }
+                var deathWatchNotification = systemMessage as DeathWatchNotification;
+                if(deathWatchNotification != null)
+                {
+                    Stop();
+                    return;
+                }
+                _log.Error("{0} received unexpected system message [{1}]",_path,systemMessage);
+            });
+        }
+
+        public Exception CauseOfTermination { get; private set; }
+        public override void Stop()
+        {
+            _stopped.SwitchOn(() =>
+            {
+                var causeOfTermination = CauseOfTermination;
+                var status = causeOfTermination == null ? (Status) new Status.Success(null) : new Status.Failure(causeOfTermination);
+                _terminationPromise.SetResult(status);
+            });
+        }
+       
+        public override ActorPath Path
+        {
+            get { return _path; }
+        }
+
+        public override ActorRefProvider Provider
+        {
+            get { return _provider; }
+        }
+    }
+}
---
+++ test/Akka.Tests/Actor/RootGuardianActorRef_Tests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using Akka.Actor;
+using Akka.Dispatch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Akka.Tests.Actor
+{
+    [TestClass]
+    public class RootGuardianActorRef_Tests
+    {
+        static RootActorPath _rootActorPath = new RootActorPath(new Address("akka", "test"));
+        static DummyActorRef _deadLetters = new DummyActorRef(_rootActorPath / "deadLetters");
+        ReadOnlyDictionary<string, InternalActorRef> _emptyExtraNames = new ReadOnlyDictionary<string, InternalActorRef>(new Dictionary<string, InternalActorRef>());
+
+        [TestMethod]
+        public void Path_Should_be_the_same_path_as_specified()
+        {
+            var rootGuardianActorRef = new RootGuardianActorRef(_rootActorPath, null, _deadLetters, _emptyExtraNames);
+            Assert.AreEqual(_rootActorPath, rootGuardianActorRef.Path);
+        }
+
+        [TestMethod]
+        public void Parent_Should_be_itself()
+        {
+            var rootGuardianActorRef = new RootGuardianActorRef(_rootActorPath, null, _deadLetters, _emptyExtraNames);
+            var parent = rootGuardianActorRef.Parent;
+            Assert.AreSame(rootGuardianActorRef, parent);
+        }
+
+
+        [TestMethod]
+        public void Getting_temp_child_Should_return_tempContainer()
+        {
+            var rootGuardianActorRef = new RootGuardianActorRef(_rootActorPath, null, _deadLetters, _emptyExtraNames);
+            var tempContainer = new DummyActorRef(_rootActorPath / "temp");
+            rootGuardianActorRef.SetTempContainer(tempContainer);
+            var actorRef = rootGuardianActorRef.GetSingleChild("temp");
+            Assert.AreSame(tempContainer, actorRef);
+        }
+
+        [TestMethod]
+        public void Getting_deadLetters_child_Should_return_tempContainer()
+        {
+            var rootGuardianActorRef = new RootGuardianActorRef(_rootActorPath, null, _deadLetters, _emptyExtraNames);
+            var actorRef = rootGuardianActorRef.GetSingleChild("deadLetters");
+            Assert.AreSame(_deadLetters, actorRef);
+        }
+
+
+        [TestMethod]
+        public void Getting_a_child_that_exists_in_extraNames_Should_return_the_child()
+        {
+            var extraNameChild = new DummyActorRef(_rootActorPath / "extra");
+            var extraNames = new Dictionary<string, InternalActorRef> { { "extra", extraNameChild } };
+            var rootGuardianActorRef = new RootGuardianActorRef(_rootActorPath,null, _deadLetters, extraNames);
+            var actorRef = rootGuardianActorRef.GetSingleChild("extra");
+            Assert.AreSame(extraNameChild, actorRef);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Getting_an_unknown_child_that_exists_in_extraNames_Should_return_nobody()
+        {
+            var rootGuardianActorRef = new RootGuardianActorRef(_rootActorPath, null, _deadLetters, _emptyExtraNames);
+            var actorRef = rootGuardianActorRef.GetSingleChild("unknown-child");
+            Assert.AreSame(ActorRef.Nobody, actorRef);
+        }
+
+
+        private class DummyActorRef : MinimalActorRef
+        {
+            private readonly ActorPath _path;
+
+            public DummyActorRef(ActorPath path)
+            {
+                _path = path;
+            }
+
+            public override ActorPath Path
+            {
+                get { return _path; }
+            }
+
+            public override ActorRefProvider Provider
+            {
+                get { throw new System.NotImplementedException(); }
+            }
+        }
+
+    
+    }
+}
