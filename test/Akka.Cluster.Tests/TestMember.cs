63ab5611348172d698f115ea497959f0d9b8c6be
David Smith <dsmith@travelrepublic.co.uk>
2014-07-09 16:04:41 +0100

    Implement cluster member

---
+++ test/Akka.Cluster.Tests/TestMember.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Akka.Actor;
+
+namespace Akka.Cluster.Tests
+{
+    static class TestMember
+    {
+        public static Member Create(Address address, MemberStatus status)
+        {
+            return Create(address, status, new HashSet<string>());
+        }
+
+        public static Member Create(Address address, MemberStatus status, HashSet<string> roles)
+        {
+            return new Member(new UniqueAddress(address, 0), status, roles);
+        }
+    }
+}
