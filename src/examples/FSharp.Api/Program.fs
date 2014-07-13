6cb6178fa9322858daf0574db1e56dc51805b1d3
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2014-07-13 23:15:21 +0300

    F# API - supervisor strategy handler functions, minor modifications in Actor interface (added spawn method for actors to create child actors). Bunch of F# API examples attached - MapReduce & Supervisioning

--- src/examples/FSharp.Api/Program.fs
+++ src/examples/FSharp.Api/Program.fs
@@ -1,61 +1,7 @@
-﻿open Akka.Actor
-open Akka.FSharp
-open Akka.Configuration
-open System
+﻿open System
 
-type SomeActorMessages =
-    | Greet of string
-    | Hi
-
-type SomeActor() =    
-    inherit Actor()
-
-    override x.OnReceive message =
-        match message with
-        | :? SomeActorMessages as m ->  
-            match m with
-            | Greet(name) -> Console.WriteLine("Hello {0}",name)
-            | Hi -> Console.WriteLine("Hello from F#!")
-        | _ -> failwith "unknown message"
-
-let system =  
-    ConfigurationFactory.Default() 
-    |> System.create "FSharpActors"
-
-let actor = 
-    spawn system "MyActor"
-    <| fun mailbox ->
-        let rec again name =
-            actor {
-                let! message = mailbox.Receive()
-                match message with
-                | Greet(n) when n = name ->
-                    printfn "Hello again, %s" name
-                    return! again name
-                | Greet(n) -> 
-                    printfn "Hello %s" n
-                    return! again n
-                | Hi -> 
-                    printfn "Hello from F#!"
-                    return! again name }
-        and loop() =
-            actor {
-                let! message = mailbox.Receive()
-                match message with
-                | Greet(name) -> 
-                    printfn "Hello %s" name
-                    return! again name
-                | Hi ->
-                    printfn "Hello from F#!"
-                    return! loop() } 
-        loop()
-
-actor <! Greet "roger"
-actor <! Hi
-actor <! Greet "roger"
-actor <! Hi
-actor <! Greet "jeremie"
-actor <! Hi
-actor <! Greet "jeremie"
-
-System.Console.ReadKey() |> ignore
+[<EntryPoint>]
+let main args =
+    Supervisioning.main()
+    Console.ReadLine()
+    0
