db0c39aea4deffeb74a48148e008e75e818b3e20
rogeralsing <rogeralsing@gmail.com>
2014-07-13 15:08:25 +0200

    FluentConfig demo

---
+++ src/core/Akka.Remote/Configuration/FluentConfigRemote.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka
+{
+    public static class FluentConfigRemote
+    {
+        /*
+remote {
+		log-received-messages = on
+		log-sent-messages = on
+        #log-remote-lifecycle-events = on
+
+        #this is the new upcoming remoting support, which enables multiple transports
+       helios.tcp {
+            transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+		    applied-adapters = []
+		    transport-protocol = tcp
+		    port = 8081
+		    hostname = 0.0.0.0 #listens on ALL ips for this machine
+            public-hostname = localhost #but only accepts connections on localhost (usually 127.0.0.1)
+        }
+        log-remote-lifecycle-events = INFO
+    }*/
+        public static FluentConfig Remoting(this FluentConfig self, string hostname,int port)
+        {
+            string remoteConfig = @"
+akka.actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+akka.remote.helios.tcp.transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+akka.remote.helios.tcp.applied-adapters = []
+akka.remote.helios.tcp.transport-protocol = tcp
+akka.remote.helios.tcp.port = {0}
+akka.remote.helios.tcp.hostname = 0.0.0.0 #listens on ALL ips for this machine
+akka.remote.helios.tcp.public-hostname = {1} #but only accepts connections on localhost (usually 127.0.0.1)
+";
+            self.AsInstanceOf<FluentConfigInternals>().AppendLine(string.Format(remoteConfig,port,hostname));
+
+            return self;
+        }
+        public static FluentConfig LogRemoteLifecycleEvents(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.remote.log-remote-lifecycle-events = on");
+            }
+            return self;
+        }
+        public static FluentConfig LogReceivedMessages(this FluentConfig self,bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.remote.log-received-messages = on");
+            }
+            return self;
+        }
+        public static FluentConfig LogSentMessages(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.remote.log-sent-messages = on");
+            }
+            return self;
+        }
+
+    }
+}
---
+++ src/core/Akka/Configuration/FluentConfig.cs
@@ -0,0 +1,115 @@
+﻿using Akka.Configuration;
+using Akka.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka
+{
+    public class FluentConfig : FluentConfigInternals
+    {
+        private StringBuilder _hoconConfiguration = new StringBuilder();
+
+        void FluentConfigInternals.AppendLine(string configString)
+        {
+            _hoconConfiguration.AppendLine(configString);
+        }
+
+        public static FluentConfig Begin()
+        {
+            return new FluentConfig();
+        }
+
+        public Config Build()
+        {
+            return ConfigurationFactory.ParseString(_hoconConfiguration.ToString());
+        }
+    }
+
+    public interface FluentConfigInternals
+    {
+        void AppendLine(string configString);
+    }
+
+    public static class FluentConfigLogging
+    {
+        /*
+    log-config-on-start = on
+    stdout-loglevel = DEBUG
+    loglevel = ERROR
+    actor {
+        provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+        
+        debug {  
+          receive = on 
+          autoreceive = on
+          lifecycle = on
+          event-stream = on
+          unhandled = on
+        }
+*/
+        public static FluentConfig LogConfigOnStart(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.log-config-on-start = on");
+            }
+            return self;
+        }
+        public static FluentConfig StdOutLogLevel(this FluentConfig self, LogLevel logLevel)
+        {
+            self.AsInstanceOf<FluentConfigInternals>().AppendLine(string.Format("akka.stdout-loglevel = {0}", logLevel.StringFor()));
+
+            return self;
+        }
+
+        public static FluentConfig LogLevel(this FluentConfig self, LogLevel logLevel)
+        {
+            self.AsInstanceOf<FluentConfigInternals>().AppendLine(string.Format("akka.loglevel = {0}", logLevel.StringFor()));
+
+            return self;
+        }
+        public static FluentConfig DebugReceive(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.actor.debug.receive = on");
+            }
+            return self;
+        }
+        public static FluentConfig DebugAutoReceive(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.actor.debug.autoreceive = on");
+            }
+            return self;
+        }
+        public static FluentConfig DebugLifecycle(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.actor.debug.lifecycle = on");
+            }
+            return self;
+        }
+        public static FluentConfig DebugEventStream(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.actor.debug.event-stream = on");
+            }
+            return self;
+        }
+        public static FluentConfig DebugUnhandled(this FluentConfig self, bool on)
+        {
+            if (on)
+            {
+                self.AsInstanceOf<FluentConfigInternals>().AppendLine("akka.actor.debug.unhandled = on");
+            }
+            return self;
+        }
+    }
+}
