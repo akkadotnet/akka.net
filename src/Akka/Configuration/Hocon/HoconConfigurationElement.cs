52c255bd6af3481513005625b4e76bb0a474bdeb
Maciek Misztal <mmisztal1980@gmail.com>
2014-07-15 23:48:21 +0200

    Added a configuration section capable of deserializing CData/Hocon + Supporting infrastructure. Added test.

---
+++ src/Akka/Configuration/Hocon/HoconConfigurationElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace Akka.Configuration.Hocon
+{
+    public class HoconConfigurationElement : CDataConfigurationElement
+    {
+        [ConfigurationProperty(ContentPropertyName, IsRequired = true, IsKey = true)]
+        public string Content
+        {
+            get { return (string)base[ContentPropertyName]; }
+            set { base[ContentPropertyName] = value; }
+        }
+    }
+}
---
+++ test/Akka.Tests/Configuration/ConfigurationSpec.cs
@@ -0,0 +1,20 @@
+﻿using Akka.Configuration.Hocon;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+
+namespace Akka.Tests.Configuration
+{
+    [TestClass]
+    public class ConfigurationSpec : AkkaSpec
+    {
+        [TestMethod]
+        public void DeserializesHoconConfigurationFromNetConfigFile()
+        {
+            var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
+            Assert.IsNotNull(section);
+            Assert.IsFalse(string.IsNullOrEmpty(section.Hocon.Content));
+            var akkaConfig = section.AkkaConfig;
+            Assert.IsNotNull(akkaConfig);
+        }
+    }
+}
