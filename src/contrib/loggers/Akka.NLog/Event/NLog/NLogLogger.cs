679ec7726175a5998ed2bacd6ebf4b9f72c613bb
rogeralsing <rogeralsing@gmail.com>
2014-08-17 08:32:51 +0200

    fixing code standards

--- src/contrib/loggers/Akka.NLog/Event/NLog/NLogLogger.cs
+++ src/contrib/loggers/Akka.NLog/Event/NLog/NLogLogger.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace Akka.NLog.Event.NLog
 {
-    public class NLogLogger : UntypedActor
+    public class NLogLogger : ReceiveActor
     {
         private readonly LoggingAdapter log = Logging.GetLogger(Context);
 
@@ -15,19 +15,17 @@ namespace Akka.NLog.Event.NLog
             logStatement(logger);
         }
 
-        protected override void OnReceive(object message)
+        protected NLogLogger()
         {
-            message
-                .Match()
-                .With<Error>(m => WithNLog(m.LogSource, m, logger => logger.Error("{0}", m.Message)))
-                .With<Warning>(m => WithNLog(m.LogSource, m, logger => logger.Warn("{0}", m.Message)))
-                .With<Info>(m => WithNLog(m.LogSource, m, logger => logger.Info("{0}", m.Message)))
-                .With<Debug>(m => WithNLog(m.LogSource, m, logger => logger.Debug("{0}", m.Message)))
-                .With<InitializeLogger>(m =>
-                {
-                    log.Info("NLogLogger started");
-                    Sender.Tell(new LoggerInitialized());
-                });
+            Receive<Error>(m => WithNLog(m.LogSource, m, logger => logger.Error("{0}", m.Message)));
+            Receive<Warning>(m => WithNLog(m.LogSource, m, logger => logger.Warn("{0}", m.Message)));
+            Receive<Info>(m => WithNLog(m.LogSource, m, logger => logger.Info("{0}", m.Message)));
+            Receive<Debug>(m => WithNLog(m.LogSource, m, logger => logger.Debug("{0}", m.Message)));
+            Receive<InitializeLogger>(m =>
+            {
+                log.Info("NLogLogger started");
+                Sender.Tell(new LoggerInitialized());
+            });
         }
     }
 }
