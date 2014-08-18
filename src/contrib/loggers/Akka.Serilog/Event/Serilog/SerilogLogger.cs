bece4c2c9bb32a6fd0d2aedc5ab1c78d072867c4
Damian Reeves <DamReev@gmail.com>
2014-08-18 09:14:34 -0400

    Adding SerilogLogger to allow logging via Serilog.

---
+++ src/contrib/loggers/Akka.Serilog/Event/Serilog/SerilogLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Event;
+using Serilog;
+
+namespace Akka.Serilog.Event.Serilog
+{
+    public class SerilogLogger:ReceiveActor
+    {
+        private readonly LoggingAdapter log = Logging.GetLogger(Context);
+
+        private void WithSerilog(Action<ILogger> logStatement)
+        {
+            var logger = Log.Logger.ForContext(GetType());
+            logStatement(logger);
+        }
+        protected SerilogLogger()
+        {
+            Receive<Error>(m => WithSerilog(logger => logger.Error("{0}", m.Message)));
+            Receive<Warning>(m => WithSerilog(logger => logger.Warning("{0}", m.Message)));
+            Receive<Info>(m => WithSerilog(logger => logger.Information("{0}", m.Message)));
+            Receive<Debug>(m => WithSerilog(logger => logger.Debug("{0}", m.Message)));
+            Receive<InitializeLogger>(m =>
+            {
+                log.Info("SerilogLogger started");
+                Sender.Tell(new LoggerInitialized());
+            });
+        }
+    }
+}
