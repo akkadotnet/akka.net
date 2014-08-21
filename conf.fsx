654bd22c8e79f095de2ce2e0c1205ddc925f6592
Matt Nischan <nishmaster@gmail.com>
2014-08-20 22:55:34 -0500

    Add preliminary build support on Mono for #62.

--- conf.fsx
+++
@@ -1,6 +0,0 @@
-open Fake
-module FB = Fake.Boot
-FB.Prepare {
-    FB.Config.Default "src" with
-        NuGetDependencies = [{ PackageId = "xunit.runners"; Version = FB.SemanticVersion "1.9.2" }]
-}
