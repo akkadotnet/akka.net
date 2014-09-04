ce716b3130a4341b891654430f087dcb75ab3732
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2014-09-04 20:27:42 +0300

    Act created

---
+++ src/core/Akka.Tests/Actor/ActorDslSpec.cs
@@ -0,0 +1,102 @@
+﻿using Akka.Actor;
+using Akka.Actor.Dsl;
+using Akka.TestKit;
+using Xunit;
+
+namespace Akka.Tests.Actor
+{
+    public class ActorDslSpec : AkkaSpec
+    {
+        [Fact]
+        public void A_ligthweight_creator_must_support_creating_regular_actors()
+        {
+            var a = Sys.ActorOf(Props.Create(() => new Act(c =>
+                c.Receive<string>(msg => msg == "hello", (msg, ctx) => TestActor.Tell("hi")))));
+
+            a.Tell("hello");
+            ExpectMsg("hi");
+        }
+
+        [Fact]
+        public void A_lightweight_creator_must_support_become_stacked()
+        {
+            var a = Sys.ActorOf(c => c.Become((msg, ctx) =>
+            {
+                if (msg == "info")
+                    TestActor.Tell("A");
+                else if (msg == "switch")
+                    c.BecomeStacked((msg2, ctx2) =>
+                    {
+                        if (msg2 == "info")
+                            TestActor.Tell("B");
+                        else if (msg2 == "switch")
+                            c.UnbecomeStacked();
+                    });
+                else if (msg == "lobotomize")
+                    c.UnbecomeStacked();
+            }));
+
+            a.Tell("info");
+            ExpectMsg("A");
+
+            a.Tell("switch");
+            a.Tell("info");
+            ExpectMsg("B");
+
+            a.Tell("switch");
+            a.Tell("info");
+            ExpectMsg("A");
+        }
+
+        [Fact]
+        public void A_ligthweight_creator_must_support_actor_setup_and_teardown()
+        {
+            const string started = "started";
+            const string stopped = "stopped";
+
+            var a = Sys.ActorOf(c =>
+            {
+                c.OnPreStart = _ => TestActor.Tell(started);
+                c.OnPostStop = _ => TestActor.Tell(stopped);
+            });
+
+            Sys.Stop(a);
+            ExpectMsg(started);
+            ExpectMsg(stopped);
+        }
+
+        [Fact]
+        public void A_ligthweight_creator_must_support_restart()
+        {
+            //TODO: requires event filters
+        }
+
+        [Fact]
+        public void A_ligthweight_creator_must_support_supervising()
+        {
+            //TODO: requires event filters
+        }
+
+        [Fact]
+        public void A_ligthweight_creator_must_support_nested_declarations()
+        {
+            var a = Sys.ActorOf(act =>
+            {
+                var b = act.ActorOf(act2 =>
+                {
+                    act2.OnPreStart = context => context.Parent.Tell("hello from " + context.Self.Path);
+                }, "barney");
+                act.ReceiveAny((x, _) => TestActor.Tell(x));
+            }, "fred");
+
+            ExpectMsg("hello from akka://" + Sys.Name + "/user/fred/barney");
+            LastSender.ShouldBe(a);
+        }
+
+        [Fact]
+        public void A_ligthweight_creator_must_support_stash()
+        {
+            //TODO: requires proven and tested stash implementation
+        }
+    }
+}
---
+++ src/core/Akka/Actor/Dsl/Act.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Akka.Actor.Dsl
+{
+    public interface IActorDsl
+    {
+        Action<Exception, IActorContext> OnPostRestart { get; set; }
+        Action<Exception, object, IActorContext> OnPreRestart { get; set; }
+        Action<IActorContext> OnPostStop { get; set; }
+        Action<IActorContext> OnPreStart { get; set; }
+        SupervisorStrategy Strategy { get; set; }
+
+        void Receive<T>(Action<T, IActorContext> handler);
+        void Receive<T>(Predicate<T> shouldHandle, Action<T, IActorContext> handler);
+        void Receive<T>(Action<T, IActorContext> handler, Predicate<T> shouldHandle);
+        void ReceiveAny(Action<object, IActorContext> handler);
+
+        /// <summary>
+        /// Become new behavior with discard of the old one. Equivalent of: Context.Become(_, discardOld: true).
+        /// </summary>
+        void Become(Action<object, IActorContext> handler);
+
+        /// <summary>
+        /// Become new behavior without discarding the old one. Equivalent of: Context.Become(_, discardOld: false).
+        /// </summary>
+        void BecomeStacked(Action<object, IActorContext> handler);
+
+        /// <summary>
+        /// Reverts <see cref="BecomeStacked"/> behavior.
+        /// </summary>
+        void UnbecomeStacked();
+
+        ActorRef ActorOf(Action<IActorDsl> config, string name = null);
+    }
+
+    public sealed class Act : ReceiveActor, IActorDsl
+    {
+        public Action<Exception, IActorContext> OnPostRestart { get; set; }
+        public Action<Exception, object, IActorContext> OnPreRestart { get; set; }
+        public Action<IActorContext> OnPostStop { get; set; }
+        public Action<IActorContext> OnPreStart { get; set; }
+        public SupervisorStrategy Strategy { get; set; }
+
+        public Act(Action<IActorDsl> config)
+        {
+            config(this);
+        }
+
+        public void Receive<T>(Action<T, IActorContext> handler)
+        {
+            Receive<T>(msg => handler(msg, Context));
+        }
+
+        public void Receive<T>(Action<T, IActorContext> handler, Predicate<T> shouldHandle)
+        {
+            Receive(msg => handler(msg, Context), shouldHandle);
+        }
+        public void Receive<T>(Predicate<T> shouldHandle, Action<T, IActorContext> handler)
+        {
+            Receive(shouldHandle, msg => handler(msg, Context));
+        }
+
+        public void ReceiveAny(Action<object, IActorContext> handler)
+        {
+            ReceiveAny(msg => handler(msg, Context));
+        }
+
+        public void Become(Action<object, IActorContext> handler)
+        {
+            Become(msg => handler(msg, Context), true);
+        }
+
+        public void BecomeStacked(Action<object, IActorContext> handler)
+        {
+            Become(msg => handler(msg, Context), false);
+        }
+        public void UnbecomeStacked()
+        {
+            base.Unbecome();
+        }
+
+        public ActorRef ActorOf(Action<IActorDsl> config, string name = null)
+        {
+            var props = Props.Create(() => new Act(config));
+            return Context.ActorOf(props, name);
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            if (OnPreRestart != null)
+            {
+                OnPreRestart(reason, message, Context);
+            }
+            else
+            {
+                base.PreRestart(reason, message);
+            }
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            if (OnPostRestart != null)
+            {
+                OnPostRestart(reason, Context);
+            }
+            else
+            {
+                base.PostRestart(reason);
+            }
+        }
+
+        protected override void PostStop()
+        {
+            if (OnPostStop != null)
+            {
+                OnPostStop(Context);
+            }
+            else
+            {
+                base.PostStop();
+            }
+        }
+
+        protected override void PreStart()
+        {
+            if (OnPreStart != null)
+            {
+                OnPreStart(Context);
+            }
+            else
+            {
+                base.PreStart();
+            }
+        }
+
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            return Strategy ?? base.SupervisorStrategy();
+        }
+    }
+
+    public static class ActExtensions
+    {
+        public static ActorRef ActorOf(this ActorRefFactory factory, Action<IActorDsl> config, string name = null)
+        {
+            return factory.ActorOf(Props.Create(() => new Act(config)), name);
+        }
+    }
+}
