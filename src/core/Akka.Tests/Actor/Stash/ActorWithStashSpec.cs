2c91e16ff3140bb700288cb0060a0b4147dddb5f
HCanber <hakan_github@canberger.se>
2014-09-30 19:51:57 +0200

    Add ActorWithStashSpec

--- src/core/Akka.Tests/Actor/Stash/ActorWithStashSpec.cs
+++ src/core/Akka.Tests/Actor/Stash/ActorWithStashSpec.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.Remoting.Contexts;
 using System.Threading;
 using Akka.Actor;
 using Akka.Actor.Internal;
+using Akka.Event;
 using Akka.TestKit;
 using Akka.TestKit.TestActors;
 using Akka.Tests.TestUtils;
@@ -38,21 +40,89 @@ namespace Akka.Tests.Actor.Stash
         [Fact]
         public void An_actor_Must_throw_an_exception_if_the_same_message_is_stashed_twice()
         {
-            _state.ExpectedException=new TestLatch(Sys);
+            _state.ExpectedException = new TestLatch(Sys);
             var stasher = ActorOf<StashingTwiceActor>("stashing-actor");
             stasher.Tell("hello");
             _state.ExpectedException.Ready(TimeSpan.FromSeconds(3));
         }
 
         [Fact]
+        public void An_actor_must_unstash_all_messages_on_PostStop()
+        {
+            var stasher = ActorOf<StashEverythingActor>("stasher");
+            Watch(stasher);
+            //This message will be stashed by stasher
+            stasher.Tell("message");
+
+            //When stasher is stopped it should unstash message during poststop to mailbox
+            //the mailbox will be emptied and the messages will be sent to deadletters
+            EventFilter.DeadLetter<string>(s=>s=="message", source: stasher.Path.ToString()).ExpectOne(() =>
+            {
+                Sys.Stop(stasher);
+                ExpectTerminated(stasher);
+            });
+        }
+
+        [Fact]
         public void An_actor_Must_process_stashed_messages_after_restart()
         {
-            SupervisorStrategy strategy=new OneForOneStrategy(2,TimeSpan.FromSeconds(1),e=>Directive.Restart);
+            SupervisorStrategy strategy = new OneForOneStrategy(2, TimeSpan.FromSeconds(1), e => Directive.Restart);
             var boss = ActorOf(() => new Supervisor(strategy));
             var restartLatch = CreateTestLatch();
             var hasMsgLatch = CreateTestLatch();
+            var slaveProps = Props.Create(() => new SlaveActor(restartLatch, hasMsgLatch, "stashme"));
 
-           throw new Exception("Incomplete");
+            //Send the props to supervisor, which will create an actor and return the ActorRef
+            var slave = boss.AskAndWait<ActorRef>(slaveProps, TestKitSettings.DefaultTimeout);
+
+            //send a message that will be stashed
+            slave.Tell("stashme");
+
+            //this will crash the slave.
+            slave.Tell("crash");
+
+            //During preRestart restartLatch is opened
+            //After that the cell will unstash "stashme", it should be received by the slave and open hasMsgLatch
+            restartLatch.Ready(TimeSpan.FromSeconds(10));
+            hasMsgLatch.Ready(TimeSpan.FromSeconds(10));
+        }
+
+        [Fact]
+        public void An_actor_that_clears_the_stash_on_preRestart_Must_not_receive_previously_stashed_messages()
+        {
+            SupervisorStrategy strategy = new OneForOneStrategy(2, TimeSpan.FromSeconds(1), e => Directive.Restart);
+            var boss = ActorOf(() => new Supervisor(strategy));
+            var restartLatch = CreateTestLatch();
+            var slaveProps = Props.Create(() => new ActorsThatClearsStashOnPreRestart(restartLatch));
+
+            //Send the props to supervisor, which will create an actor and return the ActorRef
+            var slave = boss.AskAndWait<ActorRef>(slaveProps, TestKitSettings.DefaultTimeout);
+
+            //send messages that will be stashed
+            slave.Tell("stashme 1");
+            slave.Tell("stashme 2");
+            slave.Tell("stashme 3");
+
+            //this will crash the slave.
+            slave.Tell("crash");
+
+            //send a message that should not be stashed
+            slave.Tell("this should bounce back");
+
+            //During preRestart restartLatch is opened
+            //After that the cell will clear the stash
+            //So when the cell tries to unstash, it will not unstash messages. If it would TestActor
+            //would receive all stashme messages instead of "this should bounce back"
+            restartLatch.Ready(TimeSpan.FromSeconds(1110));
+            ExpectMsg("this should bounce back");
+        }
+
+        [Fact]
+        public void An_actor_must_rereceive_unstashed_Terminated_messages()
+        {
+            ActorOf(Props.Create(() => new TerminatedMessageStashingActor(TestActor)), "terminated-message-stashing-actor");
+            ExpectMsg("terminated1");
+            ExpectMsg("terminated2");
         }
 
         private class UnboundedStashActor : BlackHoleActor, WithUnboundedStash
@@ -60,11 +130,11 @@ namespace Akka.Tests.Actor.Stash
             public IStash Stash { get; set; }
         }
 
-        private class StashingActor : ReceiveActor, WithUnboundedStash
+        private class StashingActor : TestReceiveActor, WithUnboundedStash
         {
             public StashingActor()
             {
-                Receive<string>(m => m == "hello", m =>
+                Receive("hello", m =>
                 {
                     _state.S = "hello";
                     Stash.UnstashAll();
@@ -75,7 +145,7 @@ namespace Akka.Tests.Actor.Stash
 
             private void Greeted()
             {
-                Receive<string>(s => s == "bye", _ =>
+                Receive("bye", _ =>
                 {
                     _state.S = "bye";
                     _state.Finished.Await();
@@ -86,11 +156,20 @@ namespace Akka.Tests.Actor.Stash
             public IStash Stash { get; set; }
         }
 
-        private class StashingTwiceActor : ReceiveActor, WithUnboundedStash
+        private class StashEverythingActor : ReceiveActor, WithUnboundedStash
+        {
+            public StashEverythingActor()
+            {
+                ReceiveAny(m=>Stash.Stash());
+            }
+            public IStash Stash { get; set; }
+        }
+
+        private class StashingTwiceActor : TestReceiveActor, WithUnboundedStash
         {
             public StashingTwiceActor()
             {
-                Receive<string>(m => m == "hello", m =>
+                Receive("hello", m =>
                 {
                     Stash.Stash();
                     try
@@ -107,7 +186,7 @@ namespace Akka.Tests.Actor.Stash
 
             private void Greeted()
             {
-                Receive<string>(s => s == "bye", _ =>
+                Receive("bye", _ =>
                 {
                     _state.S = "bye";
                     _state.Finished.Await();
@@ -118,6 +197,82 @@ namespace Akka.Tests.Actor.Stash
             public IStash Stash { get; set; }
         }
 
+        private class SlaveActor : TestReceiveActor, WithUnboundedStash
+        {
+            private readonly TestLatch _restartLatch;
+
+            public SlaveActor(TestLatch restartLatch, TestLatch hasMsgLatch, string expectedUnstashedMessage)
+            {
+                _restartLatch = restartLatch;
+                Receive("crash", _ => { throw new Exception("Received \"crash\""); });
+
+                // when restartLatch is not yet open, stash all messages != "crash"                
+                Receive<object>(_ => !restartLatch.IsOpen, m => Stash.Stash());
+
+                // when restartLatch is open, must receive the unstashed message
+                Receive(expectedUnstashedMessage, _ => hasMsgLatch.Open());
+            }
+
+            protected override void PreRestart(Exception reason, object message)
+            {
+                if(!_restartLatch.IsOpen) _restartLatch.Open();
+
+                base.PreRestart(reason, message);
+            }
+            public IStash Stash { get; set; }
+        }
+
+        private class ActorsThatClearsStashOnPreRestart : TestReceiveActor, WithUnboundedStash
+        {
+            private readonly TestLatch _restartLatch;
+
+            public ActorsThatClearsStashOnPreRestart(TestLatch restartLatch)
+            {
+                _restartLatch = restartLatch;
+                Receive("crash", _ => { throw new Exception("Received \"crash\""); });
+
+                // when restartLatch is not yet open, stash all messages != "crash"                
+                Receive<object>(_ => !restartLatch.IsOpen, m => Stash.Stash());
+
+                // when restartLatch is open we send all messages back
+                ReceiveAny(m => Sender.Tell(m));
+            }
+            protected override void PreRestart(Exception reason, object message)
+            {
+                if(!_restartLatch.IsOpen) _restartLatch.Open();
+                Stash.ClearStash();
+                base.PreRestart(reason, message);
+            }
+            public IStash Stash { get; set; }
+
+        }
+
+        private class TerminatedMessageStashingActor : TestReceiveActor, WithUnboundedStash
+        {
+            public TerminatedMessageStashingActor(ActorRef probe)
+            {
+                var watchedActor=Context.Watch(Context.ActorOf<BlackHoleActor>("watched-actor"));
+                var stashed = false;
+                Context.Stop(watchedActor);
+
+                Receive<Terminated>(w => w.ActorRef == watchedActor, w =>
+                {
+                    if(!stashed)
+                    {
+                        Stash.Stash();        //Stash the Terminated message
+                        stashed = true;
+                        Stash.UnstashAll();   //Unstash the Terminated message
+                        probe.Tell("terminated1");
+                    }
+                    else
+                    {
+                        probe.Tell("terminated2");
+                    }
+                });
+            }
+            public IStash Stash { get; set; }
+
+        }
 
         private class StateObj
         {
