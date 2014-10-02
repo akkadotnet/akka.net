1c03b5f0e3f3c87ccdcc865d26e1c28f26312cc9
Maciek Misztal <mmisztal1980@gmail.com>
2014-10-02 23:16:50 +0200

    Fixed issue #447 - where NLog loggers wouldn't be created based on the LogClass.

--- src/contrib/loggers/Akka.NLog/NLogLogger.cs
+++ src/contrib/loggers/Akka.NLog/NLogLogger.cs
@@ -2,6 +2,7 @@
 using Akka.Event;
 using NLog;
 using System;
+using NLogger = global::NLog.Logger;
 
 namespace Akka.Logger.NLog
 {
@@ -9,18 +10,18 @@ namespace Akka.Logger.NLog
     {
         private readonly LoggingAdapter _log = Logging.GetLogger(Context);
 
-        private static void WithNLog(Action<global::NLog.Logger> logStatement)
+        private static void Log(LogEvent logEvent, Action<NLogger> logStatement)
         {
-            var logger = LogManager.GetCurrentClassLogger();
+            var logger = LogManager.GetLogger(logEvent.LogClass.FullName);
             logStatement(logger);
         }
 
         public NLogLogger()
         {
-            Receive<Error>(m => WithNLog(logger => logger.Error("{0}", m.Message)));
-            Receive<Warning>(m => WithNLog(logger => logger.Warn("{0}", m.Message)));
-            Receive<Info>(m => WithNLog(logger => logger.Info("{0}", m.Message)));
-            Receive<Debug>(m => WithNLog(logger => logger.Debug("{0}", m.Message)));
+            Receive<Error>(m => Log(m, logger => logger.Error("{0}", m.Message)));
+            Receive<Warning>(m => Log(m, logger => logger.Warn("{0}", m.Message)));
+            Receive<Info>(m => Log(m, logger => logger.Info("{0}", m.Message)));
+            Receive<Debug>(m => Log(m, logger => logger.Debug("{0}", m.Message)));
             Receive<InitializeLogger>(m =>
             {
                 _log.Info("NLogLogger started");
