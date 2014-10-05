18a1d7a48b0a3bd71abcac2eb2a56864b44d177a
HCanber <hakan_github@canberger.se>
2014-10-05 12:30:13 +0200

    Simplify AbstractStash. Add IStash.ClearStash(). Remove IStashSupport
    Originally AbstractStash was internal so this is really no breaking change.
    IStashSupport isn't really needed. In Akka JVM the trait StashSupport is internal, and most of its code is in our AbstractStash

--- src/core/Akka/Actor/Stash/IStash.cs
+++ src/core/Akka/Actor/Stash/IStash.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Akka.Actor
 {
@@ -8,23 +9,32 @@ namespace Akka.Actor
     public interface IStash
     {
         /// <summary>
-        /// Stashes the current message
+        /// Stashes the current message (the message that the actor received last)
         /// </summary>
         void Stash();
 
         /// <summary>
-        /// Unstash the oldest message in the stash
+        /// Unstash the oldest message in the stash and prepends it to the actor's mailbox.
+        /// The message is removed from the stash.
         /// </summary>
         void Unstash();
 
         /// <summary>
-        /// Unstashes all messages
+        /// Unstashes all messages by prepending them to the actor's mailbox.
+        /// The stash is guaranteed to be empty afterwards.
         /// </summary>
         void UnstashAll();
 
         /// <summary>
-        /// Unstashes all messages selected by the predicate function
+        /// Unstashes all messages selected by the predicate function. Other messages are discarded.
+        /// The stash is guaranteed to be empty afterwards.
         /// </summary>
         void UnstashAll(Func<Envelope, bool> predicate);
+
+        /// <summary>
+        /// Returns all messages and clears the stash.
+        /// The stash is guaranteed to be empty afterwards.
+        /// </summary>
+        IEnumerable<Envelope> ClearStash();
     }
 }
--- src/core/Akka/Actor/Stash/Internal/AbstractStash.cs
+++ src/core/Akka/Actor/Stash/Internal/AbstractStash.cs
@@ -6,132 +6,107 @@ using Akka.Util.Internal;
 
 namespace Akka.Actor.Internal
 {
-    /// <summary>
+    /// <summary>INTERNAL
     /// Abstract base class for stash support
+    /// <remarks>Note! Part of internal API. Breaking changes may occur without notice. Use at own risk.</remarks>
     /// </summary>
-    public abstract class AbstractStash : IStash, IStashSupport
+    public abstract class AbstractStash : IStash
     {
-        protected IActorContext Context;
-        protected ActorRef Self;
-        protected LinkedList<Envelope> TheStash;
-        protected ActorCell ActorCell;
-        protected DequeBasedMailbox Mailbox;
+        private LinkedList<Envelope> _theStash;
+        private readonly ActorCell _actorCell;
+        private readonly int _capacity;
 
+        /// <summary>INTERNAL
+        /// Abstract base class for stash support
+        /// <remarks>Note! Part of internal API. Breaking changes may occur without notice. Use at own risk.</remarks>
+        /// </summary>
         protected AbstractStash(IActorContext context, int capacity = 100)
         {
-            var actorCell = context.AsInstanceOf<ActorCell>();
+            var actorCell = (ActorCell)context;
             var mailbox = actorCell.Mailbox as DequeBasedMailbox;
-            if (mailbox == null)
+            if(mailbox == null)
             {
-                string message = @"DequeBasedMailbox required, got: " + actorCell.Mailbox.GetType().Name +  @"
+                string message = @"DequeBasedMailbox required, got: " + actorCell.Mailbox.GetType().Name + @"
 An (unbounded) deque-based mailbox can be configured as follows:
     my-custom-mailbox {
         mailbox-type = ""Akka.Dispatch.UnboundedDequeBasedMailbox""
     }";
                 throw new NotSupportedException(message);
             }
-            Mailbox = mailbox;
-            Context = context;
-            Self = Context.Self;
-            TheStash = new LinkedList<Envelope>();
-            ActorCell = actorCell;
-            Capacity = capacity;
+            _theStash = new LinkedList<Envelope>();
+            _actorCell = actorCell;
+
+            // TODO: capacity needs to come from dispatcher or mailbox confg
+            // https://github.com/akka/akka/blob/master/akka-actor/src/main/scala/akka/actor/Stash.scala#L126
+            _capacity = capacity;
         }
 
+        private DequeBasedMailbox Mailbox { get { return (DequeBasedMailbox)_actorCell.Mailbox; } }
 
         public void Stash()
         {
-            StashInternal();
+            var currMsg = _actorCell.CurrentMessage;
+            var sender = _actorCell.Sender;
+            if(_theStash.Count > 0)
+            {
+                var lastEnvelope = _theStash.Last.Value;
+                if(lastEnvelope.Message.Equals(currMsg) && lastEnvelope.Sender == sender)
+                    throw new IllegalActorStateException(string.Format("Can't stash the same message {0} more than once", currMsg));
+            }
+            if(_capacity <= 0 || _theStash.Count < _capacity)
+                _theStash.AddLast(new Envelope() { Message = currMsg, Sender = sender });
+            else throw new StashOverflowException(string.Format("Couldn't enqueue message {0} to stash of {1}", currMsg, _actorCell.Self));
         }
 
         public void Unstash()
         {
-            UnstashInternal();
+            if(_theStash.Count > 0)
+            {
+                try
+                {
+                    EnqueueFirst(_theStash.Head());
+                }
+                finally
+                {
+                    _theStash.RemoveFirst();
+                }
+            }
         }
 
         public void UnstashAll()
         {
-            UnstashAllInternal();
+            UnstashAll(envelope => true);
         }
 
         public void UnstashAll(Func<Envelope, bool> predicate)
         {
-            UnstashAllInternal(predicate);
-        }
-
-        /// <summary>
-        /// TODO: capacity needs to come from dispatcher or mailbox confg
-        /// https://github.com/akka/akka/blob/master/akka-actor/src/main/scala/akka/actor/Stash.scala#L126
-        /// </summary>
-        public int Capacity { get; protected set; }
-
-        public void StashInternal()
-        {
-            var currMsg = ActorCell.CurrentMessage;
-            var sender = ActorCell.Sender;
-            if(TheStash.Count > 0 && TheStash.Last.Value.Message.Equals(currMsg) && TheStash.Last.Value.Sender == sender)
-                throw new IllegalActorStateException(string.Format("Can't stash the same message {0} more than once", currMsg));
-            if (Capacity <= 0 || TheStash.Count < Capacity)
-                TheStash.AddLast(new Envelope() {Message = currMsg, Sender = sender});
-            else throw new StashOverflowException(string.Format("Couldn't enqueue message {0} to stash of {1}", currMsg, Self));
-        }
-
-        public virtual void PrependInternal(IEnumerable<Envelope> others)
-        {
-            foreach (var other in others)
-            {
-                TheStash.AddFirst(other);
-            }
-        }
-
-        public virtual void UnstashInternal()
-        {
-            if (TheStash.Count > 0)
+            if(_theStash.Count > 0)
             {
                 try
                 {
-                    EnqueueFirst(TheStash.Head());
-                }
-                finally
-                {
-                    TheStash.RemoveFirst();
-                }
-            }
-        }
-
-        public virtual void UnstashAllInternal(Func<Envelope,bool> predicate)
-        {
-            if (TheStash.Count > 0)
-            {
-                try
-                {
-                    foreach (var item in TheStash.Where(predicate))
+                    foreach(var item in _theStash.Where(predicate))
                     {
                         EnqueueFirst(item);
                     }
                 }
                 finally
                 {
-                    TheStash = new LinkedList<Envelope>();
+                    _theStash = new LinkedList<Envelope>();
                 }
             }
         }
-
-        public virtual void UnstashAllInternal()
+     
+        public IEnumerable<Envelope> ClearStash()
         {
-            UnstashAllInternal(envelope => true);
-        }
-
-        public virtual IEnumerable<Envelope> ClearStash()
-        {
-            var stashed = TheStash;
-            TheStash = new LinkedList<Envelope>();
+            var stashed = _theStash;
+            _theStash = new LinkedList<Envelope>();
             return stashed;
         }
 
-        public virtual void EnqueueFirst(Envelope msg)
+        private void EnqueueFirst(Envelope msg)
         {
             Mailbox.EnqueueFirst(msg);
         }
     }
 }
+
