a35df71e084435f57527477ef0967dae6b370463
HCanber <hakan_github@canberger.se>
2014-10-08 17:20:00 +0200

    Log to stdout in colors
    StandardOutLogger.UseColors = false turns it off.
    Colors can be customized using: StandardOutLogger.DebugColor = ConsoleColor.Green

--- src/core/Akka/Event/StandardOutLogger.cs
+++ src/core/Akka/Event/StandardOutLogger.cs
@@ -1,9 +1,11 @@
-﻿using Akka.Actor;
+﻿using System.Reflection;
+using Akka.Actor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Akka.Util;
 
 namespace Akka.Event
 {
@@ -12,14 +14,18 @@ namespace Akka.Event
     /// </summary>
     public class StandardOutLogger : MinimalActorRef
     {
-        private ActorPath _path = new RootActorPath(Address.AllSystems, "/StandardOutLogger");
+        private readonly ActorPath _path = new RootActorPath(Address.AllSystems, "/StandardOutLogger");
 
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="StandardOutLogger" /> class.
-        /// </summary>
-        public StandardOutLogger()
+        static StandardOutLogger()
         {
+            DebugColor = ConsoleColor.Gray;
+            InfoColor = ConsoleColor.White;
+            WarningColor = ConsoleColor.Yellow;
+            ErrorColor = ConsoleColor.Red;
+            UseColors = true;
         }
+
+
 
         /// <summary>
         ///     Gets the provider.
@@ -44,9 +50,47 @@ namespace Akka.Event
         /// <exception cref="System.ArgumentNullException">message</exception>
         protected override void TellInternal(object message, ActorRef sender)
         {
-            if (message == null)
+            if(message == null)
                 throw new ArgumentNullException("message");
-            Console.WriteLine(message);
+            var logEvent = message as LogEvent;
+            if(logEvent != null)
+                PrintLogEvent(logEvent);
+            else
+                Console.WriteLine(message);
+        }
+
+
+
+
+        public static ConsoleColor DebugColor { get; set; }
+        public static ConsoleColor InfoColor { get; set; }
+        public static ConsoleColor WarningColor { get; set; }
+        public static ConsoleColor ErrorColor { get; set; }
+        public static bool UseColors { get; set; }
+
+        public static void PrintLogEvent(LogEvent logEvent)
+        {
+            ConsoleColor? color = null;
+            if(UseColors)
+            {
+                var logLevel = logEvent.LogLevel();
+                switch(logLevel)
+                {
+                    case LogLevel.DebugLevel:
+                        color = DebugColor;
+                        break;
+                    case LogLevel.InfoLevel:
+                        color = InfoColor;
+                        break;
+                    case LogLevel.WarningLevel:
+                        color = WarningColor;
+                        break;
+                    case LogLevel.ErrorLevel:
+                        color = ErrorColor;
+                        break;
+                }
+            }
+            StandardOutWriter.WriteLine(logEvent.ToString(), color);
         }
     }
 }
