9aac0c0599cf296bd0855ec12df531ffa49cf24e
Aaron Stannard <aaron@markedup.com>
2014-10-21 15:28:28 -0700

    added ClusterDeployer and relevant specs per #486
    Also changed a bunch of Akka.Cluster test configurations to use
    akka.remote.helios instead of akka.remote.netty

--- src/core/Akka.Cluster/ClusterActorRefProvider.cs
+++ src/core/Akka.Cluster/ClusterActorRefProvider.cs
@@ -1,8 +1,12 @@
-﻿using Akka.Actor;
+﻿using System;
+using Akka.Actor;
 using Akka.Actor.Internals;
 using Akka.Cluster.Configuration;
+using Akka.Cluster.Routing;
+using Akka.Configuration;
 using Akka.Event;
 using Akka.Remote;
+using Akka.Routing;
 
 namespace Akka.Cluster
 {
@@ -15,10 +19,12 @@ namespace Akka.Cluster
     /// </summary>
     public class ClusterActorRefProvider : RemoteActorRefProvider
     {
-        public ClusterActorRefProvider(string systemName, Settings settings, EventStream eventStream /*DynamicAcccess*/) : base(systemName, settings, eventStream)
+        public ClusterActorRefProvider(string systemName, Settings settings, EventStream eventStream /*DynamicAcccess*/)
+            : base(systemName, settings, eventStream)
         {
             var clusterConfig = ClusterConfigFactory.Default();
             settings.InjectTopLevelFallback(clusterConfig);
+            Deployer = new ClusterDeployer(settings);
         }
 
         public override void Init(ActorSystemImpl system)
@@ -43,6 +49,65 @@ namespace Akka.Cluster
                 RemoteSettings.WatchHeartbeatExpectedResponseAfter), "remote-watcher");
         }
 
-        //TODO: Deployment stuff
+        
+    }
+
+    /// <summary>
+    /// Cluster-aware scope of a <see cref="Deploy"/>
+    /// </summary>
+    public class ClusterScope : Scope
+    {
+        private ClusterScope() { }
+
+        public static readonly ClusterScope Instance = new ClusterScope();
+    }
+
+    /// <summary>
+    /// INTERNAL API
+    /// 
+    /// Deployer of cluster-aware routers
+    /// </summary>
+    internal class ClusterDeployer : RemoteDeployer
+    {
+        public ClusterDeployer(Settings settings)
+            : base(settings)
+        {
+        }
+
+        public override Deploy ParseConfig(string key, Config config)
+        {
+            var deploy = base.ParseConfig(key, config);
+            if (deploy == null) return null;
+
+            if (deploy.Config.GetBoolean("cluster.enabled"))
+            {
+                if(deploy.Scope != Deploy.NoScopeGiven)
+                    throw new ConfigurationException(string.Format("Cluster deployment can't be combined with scope [{0}]", deploy.Scope));
+                //TODO: add handling for RemoteRouterConfig
+
+                if (deploy.RouterConfig is Pool)
+                {
+                    return
+                        deploy.Copy(scope: ClusterScope.Instance)
+                            .WithRouterConfig(new ClusterRouterPool(deploy.RouterConfig as Pool,
+                                ClusterRouterPoolSettings.FromConfig(deploy.Config)));
+                }
+                else if (deploy.RouterConfig is Group)
+                {
+                    return
+                        deploy.Copy(scope: ClusterScope.Instance)
+                            .WithRouterConfig(new ClusterRouterGroup(deploy.RouterConfig as Group,
+                                ClusterRouterGroupSettings.FromConfig(deploy.Config)));
+                }
+                else
+                {
+                    throw new ArgumentException(string.Format("Cluster-aware router can only wrap Pool or Group, got [{0}]", deploy.RouterConfig.GetType()));
+                }
+            }
+            else
+            {
+                return deploy;
+            }
+        }
     }
 }
