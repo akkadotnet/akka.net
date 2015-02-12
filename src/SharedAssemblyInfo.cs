cb871b8686efe517c9490ab30dce4a1588a48099
Aaron Stannard <aaron@petabridge.com>
2015-02-11 16:36:43 -0800

    updated SharedAssemblyInfo.cs

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 [assembly: AssemblyCompanyAttribute("Akka.NET Team")]
-[assembly: AssemblyCopyrightAttribute("Copyright © 2013-2014 Akka.NET Team")]
+[assembly: AssemblyCopyrightAttribute("Copyright © 2013-2015 Akka.NET Team")]
 [assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersionAttribute("0.7.1.0")]
-[assembly: AssemblyFileVersionAttribute("0.7.1.0")]
+[assembly: AssemblyVersionAttribute("0.8.0.0")]
+[assembly: AssemblyFileVersionAttribute("0.8.0.0")]
