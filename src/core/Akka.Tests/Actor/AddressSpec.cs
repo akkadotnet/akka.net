9f6a681ad3e594de10759b7e545a7efbe2cf44a6
Stefan Sedich <stefan.sedich@gmail.com>
2015-03-25 18:24:32 +1000

    Initial cleanup of Address and fix so that the hostname is lower-cased when constructing the Address instance

---
+++ src/core/Akka.Tests/Actor/AddressSpec.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+using Akka.TestKit;
+using Xunit;
+
+namespace Akka.Tests.Actor
+{
+    public class AddressSpec
+    {
+        [Fact]
+        public void Host_is_lowercased_when_created()
+        {
+            var address = new Address("akka", "test", "HOSTNAME");
+            address.Host.ShouldBe("hostname");
+        }
+    }
+}
