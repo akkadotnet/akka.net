d3963d5ee36d6a7a75bce05485b5d4e5226fcea2
jwconway <james.conway@tombola.com>
2015-04-07 22:43:47 +0100

    Added a Unity Dependency resolver
    https://unity.codeplex.com/

---
+++ src/contrib/dependencyInjection/Akka.DI.Unity/UnityDependencyResolver.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.DI.Core;
+using Microsoft.Practices.Unity;
+
+namespace Akka.DI.Unity
+{
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+	    private IUnityContainer container;
+	    private ConcurrentDictionary<string, Type> typeCache;
+	    private ActorSystem system;
+
+	    public UnityDependencyResolver(IUnityContainer container, ActorSystem system)
+		{
+			if (system == null) throw new ArgumentNullException("system");
+            if (container == null) throw new ArgumentNullException("container");
+            this.container = container;
+		    typeCache = new ConcurrentDictionary<string, Type>(StringComparer.InvariantCultureIgnoreCase);
+		    this.system = system;
+            this.system.AddDependencyResolver(this);
+		}
+
+	    public Type GetType(string actorName)
+	    {
+		    typeCache.TryAdd(actorName, actorName.GetTypeValue());
+
+            return typeCache[actorName];
+	    }
+
+	    public Func<ActorBase> CreateActorFactory(string actorName)
+	    {
+			return () =>
+			{
+				var actorType = GetType(actorName);
+				return (ActorBase)container.Resolve(actorType);
+			};
+	    }
+
+	    public Props Create<TActor>() where TActor : ActorBase
+	    {
+		    return system.GetExtension<DIExt>().Props(typeof(TActor).Name);
+	    }
+    }
+
+	internal static class Extensions
+    {
+        public static Type GetTypeValue(this string typeName)
+        {
+            var firstTry = Type.GetType(typeName);
+            Func<Type> searchForType = () =>
+            {
+                return
+                AppDomain.
+                    CurrentDomain.
+                    GetAssemblies().
+                    SelectMany(x => x.GetTypes()).
+                    Where(t => t.Name.Equals(typeName)).
+                    FirstOrDefault();
+            };
+            return firstTry ?? searchForType();
+        }
+    }
+}
