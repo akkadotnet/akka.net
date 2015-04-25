e143615382e8f628e03f2ea73cc32ff228ada5e0
Nikita Tsukanov <keks9n@gmail.com>
2015-04-25 16:10:22 +0300

    Proposed fix for #886

---
+++ src/core/Akka/Util/Internal/TaskExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.Util.Internal
+{
+    public static class TaskExtensions
+    {
+        public static Task<TResult> CastTask<TTask, TResult>(this Task<TTask> task)
+        {
+            if (task.IsCompleted)
+                return Task.FromResult((TResult) (object)task.Result);
+            var tcs = new TaskCompletionSource<TResult>();
+            if (task.IsFaulted)
+                tcs.SetException(task.Exception);
+            else
+                task.ContinueWith(_ =>
+                {
+                    if (task.IsFaulted)
+                        tcs.SetException(task.Exception);
+                    try
+                    {
+                        tcs.SetResult((TResult) (object) task.Result);
+                    }
+                    catch (Exception e)
+                    {
+                        tcs.SetException(e);
+                    }
+                }, TaskContinuationOptions.ExecuteSynchronously);
+            return tcs.Task;
+        }
+    }
+}
