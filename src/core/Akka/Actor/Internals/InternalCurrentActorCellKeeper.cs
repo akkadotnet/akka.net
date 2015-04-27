fb9a114196dd17ade3553d9ebeb8c50b43a38c61
Nikita Tsukanov <keks9n@gmail.com>
2015-04-27 22:13:31 +0300

    Use CallContext for current ActorCell ref #907 #912

--- src/core/Akka/Actor/Internals/InternalCurrentActorCellKeeper.cs
+++ src/core/Akka/Actor/Internals/InternalCurrentActorCellKeeper.cs
@@ -6,6 +6,8 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization;
 
 namespace Akka.Actor.Internal
 {
@@ -14,14 +16,39 @@ namespace Akka.Actor.Internal
     /// </summary>
     public static class InternalCurrentActorCellKeeper
     {
-        [ThreadStatic]
-        private static ActorCell _current;
+	    private static readonly string Key = Guid.NewGuid().ToString();
 
-
+		[Serializable]
+	    class ActorCellSlot : ISerializable
+	    {
+		    public ActorCell Cell;
+			//Hack for xUnit
+		    public void GetObjectData(SerializationInfo info, StreamingContext context)
+		    {
+			    var type = typeof (Exception);
+			    info.AssemblyName = type.Assembly.FullName;
+			    info.FullTypeName = type.FullName;
+				new Exception("Why are you serializing me?").GetObjectData(info, context);
+		    }
+	    }
         /// <summary>INTERNAL!
         /// <remarks>Note! Part of internal API. Breaking changes may occur without notice. Use at own risk.</remarks>
         /// </summary>
-        public static ActorCell Current { get { return _current; } set { _current = value; } }
+        public static ActorCell Current {
+	        get
+	        {
+		        var slot = CallContext.LogicalGetData(Key) as ActorCellSlot;
+		        return slot != null ? slot.Cell : null;
+	        }
+	        set
+	        {
+		        var slot = CallContext.LogicalGetData(Key) as ActorCellSlot;
+		        if (slot == null)
+			        CallContext.LogicalSetData(Key, new ActorCellSlot {Cell = value});
+		        else
+			        slot.Cell = value;
+	        }
+        }
     }
 }
 
