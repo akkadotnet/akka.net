9640b4b110d737bce5d5058db16d33996c42b7d2
Joshua Benjamin <joshuabnjmn@gmail.com>
2015-05-07 22:59:11 -0700

    added xml doc to vector helpers. Added multinode tests to the build

--- src/core/Akka/Util/Vector.cs
+++ src/core/Akka/Util/Vector.cs
@@ -4,8 +4,19 @@ using System.Linq;
 
 namespace Akka.Util
 {
+    /// <summary>
+    /// Utility methods for operating on arrays.
+    /// </summary>
     public static class Vector
     {
+        /// <summary>
+        /// Returns a function that returns an array that contains the results of some element 
+        /// computation a number of times.
+        /// </summary>
+        /// <typeparam name="T">The type of element</typeparam>
+        /// <param name="number">the number of elements desired</param>
+        /// <returns>A func that will take the element computation and return an Array of size n, 
+        /// where each element contains the result of computing elem. </returns>
         public static Func<Func<T>, IList<T>> Fill<T>(int number)
         {
             return func => Enumerable
