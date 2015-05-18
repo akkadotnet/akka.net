12baf23946762807c542f407b8bc22a2788ca487
Sean Gilliam <sean.gilliam@gmail.com>
2015-05-18 16:59:08 -0500

    Missing xmldoc comments and typo fix
    - added missing xmldoc comments to the contrib loggers
    - file rename Slf4jLogger => Slf4NetLogger to match the class name

--- src/contrib/loggers/Akka.Logger.Serilog/SerilogLogMessageFormatter.cs
+++ src/contrib/loggers/Akka.Logger.Serilog/SerilogLogMessageFormatter.cs
@@ -13,15 +13,32 @@ using Serilog.Parsing;
 
 namespace Akka.Logger.Serilog
 {
+    /// <summary>
+    /// This class contains methods used to convert Serilog templated messages
+    /// into normal text messages.
+    /// </summary>
     public class SerilogLogMessageFormatter : ILogMessageFormatter
     {
         private readonly MessageTemplateCache _templateCache;
-            
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerilogLogMessageFormatter"/> class.
+        /// </summary>
         public SerilogLogMessageFormatter()
         {
             _templateCache = new MessageTemplateCache(new MessageTemplateParser());
         }
 
+        /// <summary>
+        /// Converts the specified template string to a text string using the specified
+        /// token array to match replacements.
+        /// </summary>
+        /// <param name="format">The template string used in the conversion.</param>
+        /// <param name="args">The array that contains values to replace in the template.</param>
+        /// <returns>
+        /// A text string where the template placeholders have been replaced with
+        /// their corresponding values.
+        /// </returns>
         public string Format(string format, params object[] args)
         {
             var template = _templateCache.Parse(format);
