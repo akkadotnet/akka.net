617a4ff07c82d702c27368b704f82f87530d9d30
rogeralsing <rogeralsing@gmail.com>
2015-05-21 21:31:57 +0200

    Updated the TaskBasedScheduler tests for DedicatedThreadScheduler instead.

--- src/core/Akka.Tests/Actor/Scheduler/TaskBasedScheduler_TellScheduler_Cancellation_Tests.cs
+++ src/core/Akka.Tests/Actor/Scheduler/TaskBasedScheduler_TellScheduler_Cancellation_Tests.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="TaskBasedScheduler_TellScheduler_Cancellation_Tests.cs" company="Akka.NET Project">
+// <copyright file="DedicatedThreadScheduler_TellScheduler_Cancellation_Tests.cs" company="Akka.NET Project">
 //     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
 //     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/akka.net>
 // </copyright>
@@ -12,13 +12,13 @@ using Xunit;
 namespace Akka.Tests.Actor.Scheduler
 {
     // ReSharper disable once InconsistentNaming
-    public class TaskBasedScheduler_TellScheduler_Cancellation_Tests : AkkaSpec
+    public class DedicatedThreadScheduler_TellScheduler_Cancellation_Tests : AkkaSpec
     {
         [Fact]
         public void When_ScheduleTellOnce_using_canceled_Cancelable_Then_their_actions_should_not_be_invoked()
         {
             // Prepare, set up actions to be fired
-            ITellScheduler scheduler = new TaskBasedScheduler();
+            ITellScheduler scheduler = new DedicatedThreadScheduler(Sys);
 
             var canceled = Cancelable.CreateCanceled();
             scheduler.ScheduleTellOnce(0, TestActor, "Test", ActorRefs.NoSender, canceled);
@@ -32,7 +32,7 @@ namespace Akka.Tests.Actor.Scheduler
         public void When_ScheduleTellRepeatedly_using_canceled_Cancelable_Then_their_actions_should_not_be_invoked()
         {
             // Prepare, set up actions to be fired
-            ITellScheduler scheduler = new TaskBasedScheduler();
+            ITellScheduler scheduler = new DedicatedThreadScheduler(Sys);
 
             var canceled = Cancelable.CreateCanceled();
             scheduler.ScheduleTellRepeatedly(0, 2, TestActor, "Test", ActorRefs.NoSender, canceled);
@@ -46,7 +46,7 @@ namespace Akka.Tests.Actor.Scheduler
         public void When_ScheduleTellOnce_and_then_canceling_before_they_occur_Then_their_actions_should_not_be_invoked()
         {
             // Prepare, set up actions to be fired
-            IScheduler scheduler = new TaskBasedScheduler();
+            IScheduler scheduler = new DedicatedThreadScheduler(Sys);
 
             var cancelable = new Cancelable(scheduler);
             scheduler.ScheduleTellOnce(100, TestActor, "Test", ActorRefs.NoSender, cancelable);
@@ -61,7 +61,7 @@ namespace Akka.Tests.Actor.Scheduler
         public void When_ScheduleTellRepeatedly_and_then_canceling_before_they_occur_Then_their_actions_should_not_be_invoked()
         {
             // Prepare, set up actions to be fired
-            IScheduler scheduler = new TaskBasedScheduler();
+            IScheduler scheduler = new DedicatedThreadScheduler(Sys);
 
             var cancelable = new Cancelable(scheduler);
             scheduler.ScheduleTellRepeatedly(100, 2, TestActor, "Test", ActorRefs.NoSender, cancelable);
@@ -76,7 +76,7 @@ namespace Akka.Tests.Actor.Scheduler
         public void When_canceling_existing_running_repeaters_Then_their_future_actions_should_not_be_invoked()
         {
             // Prepare, set up actions to be fired
-            IScheduler scheduler = new TaskBasedScheduler();
+            IScheduler scheduler = new DedicatedThreadScheduler(Sys);
 
             var cancelable = new Cancelable(scheduler);
             scheduler.ScheduleTellRepeatedly(0, 150, TestActor, "Test", ActorRefs.NoSender, cancelable);
@@ -91,7 +91,7 @@ namespace Akka.Tests.Actor.Scheduler
         public void When_canceling_existing_running_repeaters_by_scheduling_the_cancellation_ahead_of_time_Then_their_future_actions_should_not_be_invoked()
         {
             // Prepare, set up actions to be fired
-            IScheduler scheduler = new TaskBasedScheduler();
+            IScheduler scheduler = new DedicatedThreadScheduler(Sys);
 
             var cancelableOdd = new Cancelable(scheduler);
             scheduler.ScheduleTellRepeatedly(1, 150, TestActor, "Test", ActorRefs.NoSender, cancelableOdd);
