46489375d8b27d1699712b46daa7f8620afeb963
Willie Ferguson <willie.ferguson@gmail.com>
2015-06-12 10:38:02 +0200

    Akka IO UDP driver port

---
+++ src/core/Akka/IO/DatagramChannel.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Akka.IO
+{
+    public class DatagramChannel : SocketChannel
+    {
+        private DatagramChannel(Socket socket) : base(socket)
+        {
+            
+        }
+
+        public static DatagramChannel Open()
+        {
+            return new DatagramChannel(new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp));
+        }
+
+        public override bool IsOpen()
+        {
+            return true;
+        }
+
+        public int Send(ByteBuffer buffer, IPEndPoint target)
+        {
+            try
+            {
+                var data = new byte[buffer.Remaining];
+                buffer.Get(data);
+                return Socket.SendTo(data, target);
+            }
+            catch (SocketException ex)
+            {
+                if (ex.SocketErrorCode == SocketError.WouldBlock)
+                {
+                    buffer.Flip();
+                    return 0;
+                }
+                throw new IOException(ex.Message, ex);
+            }
+        }
+
+        public EndPoint Receive(ByteBuffer buffer)
+        {
+            try
+            {
+                var ep = Socket.LocalEndPoint;
+                var data = new byte[buffer.Remaining];
+                var length = Socket.ReceiveFrom(data, ref ep);
+                buffer.Put(data, 0, length);
+                return ep;
+            }
+            catch (SocketException ex)
+            {
+                if (ex.SocketErrorCode == SocketError.WouldBlock)
+                    return null;
+                throw new IOException(ex.Message, ex);
+            }
+            
+        }
+    }
+}
---
+++ src/core/Akka/IO/UdpConnectedManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Akka.Actor;
+
+namespace Akka.IO
+{
+    // INTERNAL API
+    class UdpConnectedManager : SelectionHandler.SelectorBasedManager
+    {
+        private readonly UdpConnectedExt _udpConn;
+
+        public UdpConnectedManager(UdpConnectedExt udpConn)
+            : base(udpConn.Settings, udpConn.Settings.NrOfSelectors)
+        {
+            _udpConn = udpConn;
+        }
+
+        protected override bool Receive(object m)
+        {
+            return WorkerForCommandHandler(message =>
+            {
+                var c = message as UdpConnected.Connect;
+                if (c != null)
+                {
+                    var commander = Sender;
+                    return registry => Props.Create(() => new UdpConnection(_udpConn, registry, commander, c));
+                }
+                throw new Exception();
+            })(m);
+        }
+
+    }
+}
