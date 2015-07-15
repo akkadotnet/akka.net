76db7704953d6eaa269cb192b904d437f2e2a227
Sean Gilliam <sean.gilliam@gmail.com>
2015-07-15 12:46:41 -0500

    [misc] fixed copyright headers for Akka.IO
    The recently merged Akka.IO had incorrect or missing copyright headers.
    This PR fixes that.

--- src/contrib/transports/Akka.Remote.AkkaIOTransport/TransportManager.cs
+++ src/contrib/transports/Akka.Remote.AkkaIOTransport/TransportManager.cs
@@ -1,9 +1,10 @@
 //-----------------------------------------------------------------------
-// <copyright file="ActorBase.cs" company="Akka.NET Project">
+// <copyright file="TransportManager.cs" company="Akka.NET Project">
 //     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
 //     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/akka.net>
 // </copyright>
 //-----------------------------------------------------------------------
+
 using System;
 using System.Net;
 using System.Threading.Tasks;
