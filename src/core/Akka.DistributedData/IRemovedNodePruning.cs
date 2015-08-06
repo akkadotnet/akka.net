07df36402ce5188b005019431215d00950e599cb
Anthony Brown <bruinbrown@gmail.com>
2015-08-06 23:35:22 +0100

    Added interface for removing data when a node leaves

---
+++ src/core/Akka.DistributedData/IRemovedNodePruning.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Cluster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public interface IRemovedNodePruning<T> : IReplicatedData<T>
+    {
+        bool NeedPruningFrom(UniqueAddress removedNode);
+
+        T Prune(UniqueAddress removedNode, UniqueAddress collapseInto);
+
+        T PruningCleanup(UniqueAddress removedNode);
+    }
+}
