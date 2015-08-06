044e7855709b077b82a81847d5205fc1cf62e9ad
Anthony Brown <bruinbrown@gmail.com>
2015-08-06 23:58:38 +0100

    Added key for the data store

---
+++ src/core/Akka.DistributedData/Key.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public abstract class Key<T> where T : IReplicatedData
+    {
+        private readonly string _id;
+
+        public string Id
+        {
+            get { return _id; }
+        }
+
+        public Key(string id)
+        {
+            _id = id;
+        }
+
+        public sealed override bool Equals(object obj)
+        {
+            var other = obj as Key<T>;
+            if(other != null)
+            {
+                return other.Id == Id;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return _id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return _id;
+        }
+    }
+}
