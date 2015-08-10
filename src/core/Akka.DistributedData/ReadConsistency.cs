11634754862e21d3e40b5d0286950329e61af115
Anthony Brown <bruinbrown@gmail.com>
2015-08-10 11:37:49 +0100

    Read and write consistencies for requests

---
+++ src/core/Akka.DistributedData/ReadConsistency.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public interface IReadConsistency
+    {
+        TimeSpan Timeout { get; }
+    }
+
+    public class ReadLocal : IReadConsistency
+    {
+        static readonly ReadLocal _instance = new ReadLocal();
+
+        static ReadLocal Instance { get { return _instance; } }
+        
+        public TimeSpan Timeout
+        {
+            get { return TimeSpan.Zero; }
+        }
+    }
+
+    public class ReadFrom : IReadConsistency
+    {
+        readonly int _n;
+        readonly TimeSpan _timeout;
+
+        public int N
+        {
+            get { return _n; }
+        }
+
+        public TimeSpan Timeout
+        {
+            get { return _timeout; }
+        }
+
+        public ReadFrom(int n, TimeSpan timeout)
+        {
+            _n = n;
+            _timeout = timeout;
+        }
+    }
+
+    public class ReadMajority : IReadConsistency
+    {
+        readonly TimeSpan _timeout;
+
+        public TimeSpan Timeout
+        {
+            get { return _timeout; }
+        }
+
+        public ReadMajority(TimeSpan timeout)
+        {
+            _timeout = timeout;
+        }
+    }
+
+    public class ReadAll : IReadConsistency
+    {
+        readonly TimeSpan _timeout;
+
+        public TimeSpan Timeout
+        {
+            get { return _timeout; }
+        }
+
+        public ReadAll(TimeSpan timeout)
+        {
+            _timeout = timeout;
+        }
+    }
+
+}
---
+++ src/core/Akka.DistributedData/WriteConsistency.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public interface IWriteConsistency
+    {
+        TimeSpan Timeout { get; }
+    }
+
+    public class WriteLocal : IWriteConsistency
+    {
+        static readonly WriteLocal _instance = new WriteLocal();
+
+        static WriteLocal Instance { get { return _instance; } }
+
+        public TimeSpan Timeout
+        {
+            get { return TimeSpan.Zero; }
+        }
+    }
+
+    public class WriteTo : IReadConsistency
+    {
+        readonly int _n;
+        readonly TimeSpan _timeout;
+
+        public int N
+        {
+            get { return _n; }
+        }
+
+        public TimeSpan Timeout
+        {
+            get { return _timeout; }
+        }
+
+        public WriteTo(int n, TimeSpan timeout)
+        {
+            if(n < 2)
+            {
+                throw new ArgumentException("WriteTo requires n > 2, Use WriteLocal for n=1");
+            }
+            _n = n;
+            _timeout = timeout;
+        }
+    }
+
+    public class WriteMajority : IReadConsistency
+    {
+        readonly TimeSpan _timeout;
+
+        public TimeSpan Timeout
+        {
+            get { return _timeout; }
+        }
+
+        public WriteMajority(TimeSpan timeout)
+        {
+            _timeout = timeout;
+        }
+    }
+
+    public class WriteAll : IReadConsistency
+    {
+        readonly TimeSpan _timeout;
+
+        public TimeSpan Timeout
+        {
+            get { return _timeout; }
+        }
+
+        public WriteAll(TimeSpan timeout)
+        {
+            _timeout = timeout;
+        }
+    }
+
+}
