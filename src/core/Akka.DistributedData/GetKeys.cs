a8b7ab671d029b9447deaaf2612a9f3b024eac0f
Anthony Brown <bruinbrown@gmail.com>
2015-08-11 00:30:35 +0100

    Messages to retrieve the keys stored in the DB and associated response

---
+++ src/core/Akka.DistributedData/GetKeys.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    internal class GetKeysIds
+    {
+    }
+
+    internal sealed class GetKeysIdsResult
+    {
+        private IImmutableSet<string> _keys;
+
+        internal IImmutableSet<string> Keys
+        {
+            get { return _keys; }
+        }
+
+        internal GetKeysIdsResult(IImmutableSet<string> keys)
+        {
+
+        }
+    }
+}
