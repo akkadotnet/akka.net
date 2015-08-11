9840226ee689f29e020a52a8ecdab93a494f00e5
Anthony Brown <bruinbrown@gmail.com>
2015-08-11 12:33:24 +0100

    Added messages related to deleting data

---
+++ src/core/Akka.DistributedData/Delete.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public class Delete<T> : ICommand<T>
+    {
+        readonly Key<T> _key;
+        readonly IWriteConsistency _consistency;
+
+        public Key<T> Key
+        {
+            get { return _key; }
+        }
+
+        public IWriteConsistency Consistency
+        {
+            get { return _consistency; }
+        }
+
+        public Delete(Key<T> key, IWriteConsistency consistency)
+        {
+            _key = key;
+            _consistency = consistency;
+        }
+    }
+
+    public interface IDeleteResponse<T> where T : IReplicatedData
+    {
+        Key<T> Key { get; }
+    }
+
+    public class DeleteSuccess<T> : IDeleteResponse<T>
+    {
+        readonly Key<T> _key;
+
+        public Key<T> Key
+        {
+            get { return _key; }
+        }
+
+        public DeleteSuccess(Key<T> key)
+        {
+            _key = key;
+        }
+    }
+
+    public class ReplicationDeletedFailure<T> : IDeleteResponse<T>
+    {
+        readonly Key<T> _key;
+
+        public Key<T> Key
+        {
+            get { return _key; }
+        }
+
+        public ReplicationDeletedFailure(Key<T> key)
+        {
+            _key = key;
+        }
+    }
+
+    public class DataDeleted<T> : Exception, IDeleteResponse<T>
+    {
+        readonly Key<T> _key;
+
+        public Key<T> Key
+        {
+            get { return _key; }
+        }
+
+        public DataDeleted(Key<T> key)
+        {
+            _key = key;
+        }
+
+        public override string ToString()
+        {
+ 	        return String.Format("DataDeleted {0}", _key.Id);
+        }
+    }
+}
