bc3685f29a1544243f8cae642747415551e30395
Anthony Brown <bruinbrown@gmail.com>
2015-08-11 12:39:24 +0100

    Flush changes message

---
+++ src/core/Akka.DistributedData/FlushChanges.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public class FlushChanges
+    {
+        static readonly FlushChanges _instance;
+
+        static FlushChanges Instance
+        {
+            get { return _instance; }
+        }
+    }
+}
