11c77e3b9e7b25cd33cd849e5066d4be458871e9
Anthony Brown <bruinbrown@gmail.com>
2015-08-11 12:37:19 +0100

    Messages for retrieving the number of replicas

---
+++ src/core/Akka.DistributedData/ReplicaCount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public class GetReplicaCount
+    {
+        static readonly GetReplicaCount _instance = new GetReplicaCount();
+
+        static GetReplicaCount Instance
+        {
+            get { return _instance; }
+        }
+    }
+
+    public class ReplicaCount
+    {
+        readonly int _n;
+
+        public int N
+        {
+            get { return _n; }
+        }
+
+        public ReplicaCount(int n)
+        {
+            _n = n;
+        }
+    }
+}
