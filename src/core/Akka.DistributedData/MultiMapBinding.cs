fef8f620c4d84d3ead461b4a6a7df229a160ec41
Anthony Brown <bruinbrown@gmail.com>
2015-08-12 23:57:56 +0100

    Added extension methods to help with dealing with MultiMaps

---
+++ src/core/Akka.DistributedData/MultiMapBinding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public static class MultiMapBinding
+    {
+        public static void AddBinding<T,U>(this IDictionary<T, ISet<U>> dict, T key, U value)
+        {
+            if(!dict.ContainsKey(key))
+            {
+                dict.Add(key, (ISet<U>)new HashSet<U>());
+            }
+            dict[key].Add(value);
+        }
+
+        public static void RemoveBinding<T,U>(this IDictionary<T, ISet<U>> dict, T key, U value)
+        {
+            if(dict.ContainsKey(key))
+            {
+                dict[key].Remove(value);
+            }
+        }
+    }
+}
