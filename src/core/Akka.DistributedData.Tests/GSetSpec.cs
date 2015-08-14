539797adf77379d6df6ad105664f01a2c408795c
Anthony Brown <bruinbrown@gmail.com>
2015-08-14 22:52:02 +0100

    Added an implementation of a GSet

---
+++ src/core/Akka.DistributedData.Tests/GSetSpec.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Akka.DistributedData.Tests
+{
+    public class GSetSpec
+    {
+        const string user1 = "{\"username\":\"john\",\"password\":\"coltrane\"}";
+        const string user2 = "{\"username\":\"sonny\",\"password\":\"rollins\"}";
+        const string user3 = "{\"username\":\"charlie\",\"password\":\"parker\"}";
+        const string user4 = "{\"username\":\"charles\",\"password\":\"mingus\"}";
+
+        [Fact]
+        public void AGSetMustBeAbleToAddAUser()
+        {
+            var c1 = new GSet<string>();
+            var c2 = c1.Add(user1);
+            var c3 = c2.Add(user2);
+            var c4 = c3.Add(user3);
+            var c5 = c4.Add(user4);
+
+            Assert.Equal(true, c5.Contains(user1));
+            Assert.Equal(true, c5.Contains(user2));
+            Assert.Equal(true, c5.Contains(user3));
+            Assert.Equal(true, c5.Contains(user4));
+        }
+
+        [Fact]
+        public void AGSetMustBeAbleToMergeWithUniqueSets()
+        {
+            var c11 = new GSet<string>();
+            var c12 = c11.Add(user1);
+            var c13 = c12.Add(user2);
+
+            var c21 = new GSet<string>();
+            var c22 = c21.Add(user3);
+            var c23 = c22.Add(user4);
+
+            var merged1 = c13.Merge(c23);
+            Assert.True(merged1.Contains(user1));
+            Assert.True(merged1.Contains(user2));
+            Assert.True(merged1.Contains(user3));
+            Assert.True(merged1.Contains(user4));
+
+            var merged2 = c23.Merge(c13);
+            Assert.True(merged2.Contains(user1));
+            Assert.True(merged2.Contains(user2));
+            Assert.True(merged2.Contains(user3));
+            Assert.True(merged2.Contains(user4));
+        }
+
+        [Fact]
+        public void AGSetMustBeAbleToMergeWithNonUniqueSets()
+        {
+            var c11 = new GSet<string>();
+            var c12 = c11.Add(user1);
+            var c13 = c12.Add(user2);
+            var c14 = c13.Add(user3);
+
+            var c21 = new GSet<string>();
+            var c22 = c21.Add(user3);
+            var c23 = c22.Add(user4);
+            var c24 = c23.Add(user2);
+
+            var merged1 = c13.Merge(c23);
+            Assert.True(merged1.Contains(user1));
+            Assert.True(merged1.Contains(user2));
+            Assert.True(merged1.Contains(user3));
+            Assert.True(merged1.Contains(user4));
+
+            var merged2 = c23.Merge(c13);
+            Assert.True(merged2.Contains(user1));
+            Assert.True(merged2.Contains(user2));
+            Assert.True(merged2.Contains(user3));
+            Assert.True(merged2.Contains(user4));
+        }
+    }
+}
---
+++ src/core/Akka.DistributedData/GSet.cs
@@ -0,0 +1,61 @@
+﻿using Akka.DistributedData.Proto;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.DistributedData
+{
+    public sealed class GSet<T> : AbstractReplicatedData<GSet<T>>, IReplicatedDataSerialization
+    {
+        readonly IImmutableSet<T> _elements;
+
+        public IImmutableSet<T> Elements
+        {
+            get { return _elements; }
+        }
+
+        public GSet()
+            : this(ImmutableHashSet<T>.Empty)
+        { }
+
+        public GSet(IImmutableSet<T> elements)
+        {
+            _elements = elements;
+        }
+
+        public override GSet<T> Merge(GSet<T> other)
+        {
+            return new GSet<T>(Elements.Union(other.Elements));
+        }
+
+        public bool Contains(T element)
+        {
+            return _elements.Contains(element);
+        }
+
+        public bool IsEmpty()
+        {
+            return _elements.Count == 0;
+        }
+
+        public int Count()
+        {
+            return _elements.Count;
+        }
+
+        public GSet<T> Add(T element)
+        {
+            return new GSet<T>(_elements.Add(element));
+        }
+    }
+
+    public sealed class GSetKey<T> : Key<GSet<T>>
+    {
+        public GSetKey(string id)
+            : base(id)
+        { }
+    }
+}
