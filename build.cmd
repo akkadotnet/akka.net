01f778c747690b58e2f97d1bced768a4e837ad1b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-09-17 14:47:50 +0200

    Upgrade to Paket

--- build.cmd
+++ build.cmd
@@ -1,20 +1,14 @@
 @echo off
+cls
 
-pushd %~dp0
-
-src\.nuget\NuGet.exe update -self
-
-src\.nuget\NuGet.exe install FAKE -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages -ExcludeVersion -Version 4.1.0
-
-src\.nuget\NuGet.exe install xunit.runner.console -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages\FAKE -ExcludeVersion -Version 2.0.0
-src\.nuget\NuGet.exe install nunit.runners -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages\FAKE -ExcludeVersion -Version 2.6.4
-
-if not exist src\packages\SourceLink.Fake\tools\SourceLink.fsx (
-  src\.nuget\nuget.exe install SourceLink.Fake -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages -ExcludeVersion
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
 )
-rem cls
 
-set encoding=utf-8
-src\packages\FAKE\tools\FAKE.exe build.fsx %*
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
 
-popd
+packages\build\FAKE\tools\FAKE.exe build.fsx %*
