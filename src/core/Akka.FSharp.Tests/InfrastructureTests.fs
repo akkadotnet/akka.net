4cad0add410440cc34678a4645375aacf2ae8bf7
maxim.salamatko <maxim.salamatko@fastdev.se>
2015-10-04 15:04:28 +0400

    implemented IComparable  genericless variant

---
+++ src/core/Akka.FSharp.Tests/InfrastructureTests.fs
@@ -0,0 +1,35 @@
+﻿
+//-----------------------------------------------------------------------
+// <copyright file="InfrastructureTests.fs" company="Akka.NET Project">
+//     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+module Akka.FSharp.Tests.InfrastructureTests
+
+open Akka.FSharp
+open Akka.Actor
+open System
+open Xunit
+
+
+[<Fact>]
+let ``IActorRef should be possible to use as a Key`` () =
+    let timeoutConfig =
+       """
+       akka { 
+           actor {
+               ask-timeout = 5s
+           }
+       }
+       """
+       |> Configuration.parse 
+
+    let getWhateverHandler (mailbox : Actor<_>) _ = 
+        mailbox.Sender() <! "SomethingToReturn"
+
+    let system = System.create "my-system" timeoutConfig
+    let aref = spawn system "UnitActor" (actorOf2 getWhateverHandler)
+    Set.empty.Add(aref).Count 
+    |> equals 1
