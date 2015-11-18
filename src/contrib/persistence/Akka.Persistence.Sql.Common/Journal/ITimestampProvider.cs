e30e2dae1fa575bf14840931cb5b8b2e1cf73f86
Sean Gilliam <sean.gilliam@gmail.com>
2015-11-18 16:27:57 -0600

    [misc] added missing copyright headers
    This PR adds copyright headers to source files that are missing them.

--- src/contrib/persistence/Akka.Persistence.Sql.Common/Journal/ITimestampProvider.cs
+++ src/contrib/persistence/Akka.Persistence.Sql.Common/Journal/ITimestampProvider.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="ITimestampProvider.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 
 namespace Akka.Persistence.Sql.Common.Journal
 {
--- src/contrib/persistence/Akka.Persistence.Sqlite/Snapshot/QueryBuilder.cs
+++ src/contrib/persistence/Akka.Persistence.Sqlite/Snapshot/QueryBuilder.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="QueryBuilder.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Data;
 using System.Data.Common;
 using System.Data.SQLite;
