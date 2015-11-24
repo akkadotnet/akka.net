7c45b63c195eec15f0e9094152fb3955354b9324
Aaron Stannard <aaron@petabridge.com>
2015-11-24 14:11:16 -0800

    Revert "Merge pull request #1451 from forki/paket"
    This reverts commit 3d1f1baeba2f0e0d88cbb57d5a2035e215e7be73, reversing
    changes made to 0bd5dcc92fadb3a27f0a0b6376bdfc6cec9273b2.

--- build.cmd
+++ build.cmd
@@ -2,6 +2,23 @@
 
 pushd %~dp0
 
+SETLOCAL
+SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+
+IF EXIST %CACHED_NUGET% goto copynuget
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:copynuget
+IF EXIST src\.nuget\nuget.exe goto restore
+md src\.nuget
+copy %CACHED_NUGET% src\.nuget\nuget.exe > nul
+
+:restore
+
+src\.nuget\NuGet.exe update -self
+
 cls
 
 .paket\paket.bootstrapper.exe
