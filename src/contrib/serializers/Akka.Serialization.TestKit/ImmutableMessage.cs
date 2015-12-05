89120863806cd03905d98be31a6ead9e524191e8
rogeralsing <rogeralsing@gmail.com>
2015-12-05 16:13:46 +0100

    add wire back as contrib package
    Added Serializer spec

---
+++ src/contrib/serializers/Akka.Serialization.TestKit/ImmutableMessage.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Akka.Tests.Serialization
+{
+    public class ImmutableMessage
+    {
+        public string Foo { get; private set; }
+        public string Bar { get; private set; }
+
+        public ImmutableMessage()
+        {
+
+        }
+
+        protected bool Equals(ImmutableMessage other)
+        {
+            return string.Equals(Bar, other.Bar) && string.Equals(Foo, other.Foo);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((ImmutableMessage)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Bar != null ? Bar.GetHashCode() : 0) * 397) ^ (Foo != null ? Foo.GetHashCode() : 0);
+            }
+        }
+
+        public ImmutableMessage(Tuple<string, string> nonConventionalArg)
+        {
+            Foo = nonConventionalArg.Item1;
+            Bar = nonConventionalArg.Item2;
+        }
+    }
+}
