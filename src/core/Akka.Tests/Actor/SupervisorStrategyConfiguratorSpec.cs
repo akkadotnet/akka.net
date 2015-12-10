07f911b27cff2daa82f390406a720a8229987e48
Alex Koshelev <alexanderk@pressreader.com>
2015-12-09 20:21:36 -0800

    Support akka.actor.guardian-supervisor-strategy.
    Fixed issue #1524. LocalActorRefProvider now supports option 'akka.actor.guardian-supervisor-strategy'

---
+++ src/core/Akka.Tests/Actor/SupervisorStrategyConfiguratorSpec.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Actor;
+using Xunit;
+
+namespace Akka.Tests.Actor
+{
+    public class DefaultSupervisorStrategySpec
+    {
+        [Fact]
+        public void DefaultSupervisorStrategy_returns_default_strategy_as_a_guardian_strategy()
+        {
+            var defaultStrategyConfigurator = new DefaultSupervisorStrategy();
+
+            var result = defaultStrategyConfigurator.Create();
+
+            Assert.Equal(SupervisorStrategy.DefaultStrategy, result);
+        }
+    }
+
+    public class StoppingSupervisorStrategySpec
+    {
+        [Fact]
+        public void StoppingSupervisorStrategy_returns_stopping_strategy_as_a_guardian_strategy()
+        {
+            var defaultStrategyConfigurator = new StoppingSupervisorStrategy();
+
+            var result = defaultStrategyConfigurator.Create();
+
+            Assert.Equal(SupervisorStrategy.StoppingStrategy, result);
+        }
+    }
+}
