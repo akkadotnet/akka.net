5209f6b390207ff94f5bafcfc9a52e5c080d4475
Aaron Stannard <aaron@petabridge.com>
2015-12-14 14:31:42 -0800

    added dispatcher throughput specs for ThreadPoolDispatcher and ForkJoinDispatcher

---
+++ src/core/Akka.Tests.Performance/Dispatch/ThreadPoolDispatcherThroughputSpec.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Configuration;
+using Akka.Dispatch;
+
+namespace Akka.Tests.Performance.Dispatch
+{
+    public class ThreadPoolDispatcherColdThroughputSpec : ColdDispatcherThroughputSpecBase
+    {
+        protected override MessageDispatcherConfigurator Configurator()
+        {
+            return new ThreadPoolDispatcherConfigurator(ConfigurationFactory.Empty, null);
+        }
+    }
+
+    public class ThreadPoolDispatcherWarmThroughputSpec : WarmDispatcherThroughputSpecBase
+    {
+        protected override MessageDispatcherConfigurator Configurator()
+        {
+            return new ThreadPoolDispatcherConfigurator(ConfigurationFactory.Empty, null);
+        }
+    }
+}
