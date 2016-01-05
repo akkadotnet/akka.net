8cd7303b45fe710df9ca5418f4c122ca86001846
danielmarbach <daniel.marbach@openplace.net>
2016-01-05 23:15:06 +0100

    API Approvals for Core

---
+++ src/core/Akka.API.Tests/CoreAPISpecConfig.cs
@@ -0,0 +1,6 @@
+﻿using ApprovalTests.Reporters;
+#if(DEBUG)
+[assembly: UseReporter(typeof(DiffReporter), typeof(AllFailingTestsClipboardReporter))]
+#else
+[assembly: UseReporter(typeof(DiffReporter))]
+#endif
