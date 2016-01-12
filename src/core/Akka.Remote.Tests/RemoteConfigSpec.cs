5ca3c0e1363ec50605a83576cea6f8fab7fd0cd5
Aaron Stannard <aaron@petabridge.com>
2016-01-12 12:16:54 -0800

    fixes issue with Helios message order at the cost of some throughput

--- src/core/Akka.Remote.Tests/RemoteConfigSpec.cs
+++ src/core/Akka.Remote.Tests/RemoteConfigSpec.cs
@@ -94,8 +94,8 @@ namespace Akka.Remote.Tests
             Assert.True(s.TcpKeepAlive);
             Assert.True(s.TcpReuseAddr);
             Assert.True(string.IsNullOrEmpty(c.GetString("hostname")));
-            Assert.Equal(2, s.ServerSocketWorkerPoolSize);
-            Assert.Equal(2, s.ClientSocketWorkerPoolSize);
+            Assert.Equal(1, s.ServerSocketWorkerPoolSize);
+            Assert.Equal(1, s.ClientSocketWorkerPoolSize);
         }
 
         [Fact]
@@ -106,17 +106,17 @@ namespace Akka.Remote.Tests
             // server-socket-worker-pool
             {
                 var pool = c.GetConfig("server-socket-worker-pool");
-                Assert.Equal(2, pool.GetInt("pool-size-min"));
+                Assert.Equal(1, pool.GetInt("pool-size-min"));
                 Assert.Equal(1.0d, pool.GetDouble("pool-size-factor"));
-                Assert.Equal(2, pool.GetInt("pool-size-max"));
+                Assert.Equal(1, pool.GetInt("pool-size-max"));
             }
 
             //client-socket-worker-pool
             {
                 var pool = c.GetConfig("client-socket-worker-pool");
-                Assert.Equal(2, pool.GetInt("pool-size-min"));
+                Assert.Equal(1, pool.GetInt("pool-size-min"));
                 Assert.Equal(1.0d, pool.GetDouble("pool-size-factor"));
-                Assert.Equal(2, pool.GetInt("pool-size-max"));
+                Assert.Equal(1, pool.GetInt("pool-size-max"));
             }
         }
 
