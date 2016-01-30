b20cfc06ab368ec65e99374f799e0004364cd482
rogeralsing <rogeralsing@gmail.com>
2016-01-30 19:26:30 +0100

    WIP Actor DSL
    aaa

---
+++ src/core/Akka/Actor/ActorDsl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using Akka.Configuration;
+using Akka.Routing;
+
+namespace Akka.Actor
+{
+    public static class ActorDsl
+    {
+        public static ActorSystem ActorSystem(string name, Config config)
+        {
+            return Actor.ActorSystem.Create(name, config);
+        }
+
+        public static ActorSystem ActorSystem(string name)
+        {
+            return Actor.ActorSystem.Create(name);
+        }
+
+        public static Props Props<T>() where T : ActorBase, new()
+        {
+            return Actor.Props.Create<T>();
+        }
+
+        public static Props Props<T>(Expression<Func<T>> factory) where T : ActorBase
+        {
+            return Actor.Props.Create(factory);
+        }
+
+        public static FromConfig FromConfig()
+        {
+            return Routing.FromConfig.Instance;
+        }
+
+        public static Identify Identify(object messageId) => new Identify(messageId);
+        public static PoisonPill PoisonPill() => Actor.PoisonPill.Instance;
+
+        public static Status.Success Success(object status)
+        {
+            return new Status.Success(status);
+        }
+
+        public static Status.Failure Failure(Exception cause)
+        {
+            return new Status.Failure(cause);
+        }
+    }
+}
