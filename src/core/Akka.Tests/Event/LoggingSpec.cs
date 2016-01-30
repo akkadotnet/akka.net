94cfd151334b127754ed1b9921d1cf052ad36ef4
rogeralsing <rogeralsing@gmail.com>
2016-01-30 19:15:50 +0100

    named log format PoC
    Optimized
    
    Added log templates
    
    added some log event specs
    
    using fix
    
    Shared log template
    
    api
    
    api

---
+++ src/core/Akka.Tests/Event/LoggingSpec.cs
@@ -0,0 +1,43 @@
+﻿using Akka.Event;
+using Akka.TestKit;
+using Xunit;
+
+namespace Akka.Tests.Event
+{
+    public class LoggingSpec : AkkaSpec
+    {
+        public LoggingSpec() : base(GetConfig())
+        {
+            
+        }
+
+        private static string GetConfig()
+        {
+            return @"
+akka.stdout-logtemplate = ""TestConfig [{LogLevel}][{Timestamp}][Thread {ThreadId}][{LogSource}] {Message}""
+";
+        }        
+
+        [Fact]
+        public void Default_stdout_logging_template_is_read_from_config()
+        {
+            Sys.Settings.StdoutLogTemplate.ShouldBe("TestConfig [{LogLevel}][{Timestamp}][Thread {ThreadId}][{LogSource}] {Message}");
+        }
+
+        [Fact]
+        public void LogEvent_can_be_rendered_with_custom_templates()
+        {
+            var debug = new Debug("abc",typeof(string),123);
+            var res = debug.ToString("{LogSource}{Message}");
+            res.ShouldBe("abc123");
+        }
+
+        [Fact]
+        public void LogEvent_can_be_rendered_with_formatters()
+        {
+            var debug = new Debug("abc", typeof(string), 123);
+            var res = debug.ToString("{Timestamp:yyyy}");
+            res.Length.ShouldBe(4);
+        }
+    }
+}
