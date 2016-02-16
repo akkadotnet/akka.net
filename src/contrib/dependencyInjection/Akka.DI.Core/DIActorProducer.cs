a2e7c0d031ebe8dce3842480213db580ae6c8fe4
Sean Gilliam <sean.gilliam@gmail.com>
2016-02-16 15:18:28 -0600

    [api-docs] Added more DI documentation
    This PR fixes missing or boilerplate documentation related to dependency
    injection.

--- src/contrib/dependencyInjection/Akka.DI.Core/DIActorProducer.cs
+++ src/contrib/dependencyInjection/Akka.DI.Core/DIActorProducer.cs
@@ -11,7 +11,7 @@ using Akka.Actor;
 namespace Akka.DI.Core
 {
     /// <summary>
-    /// Dependency Injection Backed IndirectActorProducer
+    /// This class represents an actor creation strategy that uses dependency injection (DI) to resolve and instantiate actors based on their type.
     /// </summary>
     public class DIActorProducer : IIndirectActorProducer
     {
@@ -20,8 +20,15 @@ namespace Akka.DI.Core
 
         readonly Func<ActorBase> actorFactory;
 
-        public DIActorProducer(IDependencyResolver dependencyResolver,
-                               Type actorType)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DIActorProducer"/> class.
+        /// </summary>
+        /// <param name="dependencyResolver">The resolver used to resolve the given actor type.</param>
+        /// <param name="actorType">The type of actor that this producer creates.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Either the <paramref name="dependencyResolver"/> or the <paramref name="actorType"/> was null.
+        /// </exception>
+        public DIActorProducer(IDependencyResolver dependencyResolver, Type actorType)
         {
             if (dependencyResolver == null) throw new ArgumentNullException("dependencyResolver");
             if (actorType == null) throw new ArgumentNullException("actorType");
@@ -30,30 +37,31 @@ namespace Akka.DI.Core
             this.actorType = actorType;
             this.actorFactory = dependencyResolver.CreateActorFactory(actorType);
         }
+
         /// <summary>
-        /// The System.Type of the Actor specified in the constructor parameter actorName
+        /// Retrieves the type of the actor to produce.
         /// </summary>
         public Type ActorType
         {
             get { return this.actorType; }
         }
+
         /// <summary>
-        /// Creates an instance of the Actor based on the Type specified in the constructor parameter actorName
+        /// Creates an actor based on the container's implementation specific actor factory.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>An actor created by the container.</returns>
         public ActorBase Produce()
         {
             return actorFactory();
         }
 
         /// <summary>
-        /// This method is used to signal the container that it can release it's reference to the actor.
+        /// Signals the container that it can release its reference to the actor.
         /// </summary>
-        /// <param name="actor">The actor to remove from the container</param>
+        /// <param name="actor">The actor to remove from the container.</param>
         public void Release(ActorBase actor)
         {
             dependencyResolver.Release(actor);
         }
     }
 }
-
--- src/contrib/dependencyInjection/Akka.DI.Core/Extensions.cs
+++ src/contrib/dependencyInjection/Akka.DI.Core/Extensions.cs
@@ -12,15 +12,18 @@ using Akka.Actor;
 namespace Akka.DI.Core
 {
     /// <summary>
-    /// Extension methods used to simplify working with the Akka.DI.Core
+    /// This class contains extension methods used to simplify working with dependency injection (DI) inside an <see cref="ActorSystem"/>.
     /// </summary>
     public static class Extensions
     {
         /// <summary>
-        /// Method used to register the IDependencyResolver to the ActorSystem
+        /// Registers a dependency resolver with a given actor system.
         /// </summary>
-        /// <param name="system">Instance of the ActorSystem</param>
-        /// <param name="dependencyResolver">Concrete Instance of IDependencyResolver i.e. Akka.DI.AutoFac.AutoFacDependencyResolver</param>
+        /// <param name="system">The actor system in which to register the given dependency resolver.</param>
+        /// <param name="dependencyResolver">The dependency resolver being registered to the actor system.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Either the <paramref name="system"/> or the <paramref name="dependencyResolver"/> was null.
+        /// </exception>
         public static void AddDependencyResolver(this ActorSystem system, IDependencyResolver dependencyResolver)
         {
             if (system == null) throw new ArgumentNullException("system");
@@ -29,16 +32,31 @@ namespace Akka.DI.Core
             DIExtension.DIExtensionProvider.Get(system).Initialize(dependencyResolver);
         }
 
+        /// <summary>
+        /// Creates an adapter used to generate <see cref="Akka.Actor.Props"/> configuration objects using the DI extension using a given actor system.
+        /// </summary>
+        /// <param name="system">The actor system that contains the DI extension.</param>
+        /// <returns>An adapter used to generate <see cref="Akka.Actor.Props"/> configuration objects using the DI extension.</returns>
         public static DIActorSystemAdapter DI(this ActorSystem system)
         {
             return new DIActorSystemAdapter(system);
         }
 
+        /// <summary>
+        /// Creates an adapter used to generate <see cref="Akka.Actor.Props"/> configuration objects using the DI extension using a given actor context.
+        /// </summary>
+        /// <param name="context">The actor context associated with a system that contains the DI extension.</param>
+        /// <returns>An adapter used to generate <see cref="Akka.Actor.Props"/> configuration objects using the DI extension.</returns>
         public static DIActorContextAdapter DI(this IActorContext context)
         {
             return new DIActorContextAdapter(context);
         }
 
+        /// <summary>
+        /// Retrieves the <see cref="Type"/> with a given name from the current <see cref="AppDomain"/>.
+        /// </summary>
+        /// <param name="typeName">The string representation of the type to retrieve.</param>
+        /// <returns>The <see cref="Type"/> with the given name.</returns>
         public static Type GetTypeValue(this string typeName)
         {
             var firstTry = Type.GetType(typeName);
@@ -52,4 +70,3 @@ namespace Akka.DI.Core
         }
     }
 }
-
