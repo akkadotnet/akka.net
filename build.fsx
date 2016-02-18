6a7eefd8b329f5418def5369c4ca980fad576fe5
Danthar <Deathraven163@gmail.com>
2016-02-18 20:57:21 +0100

    Removed Serilog plugin from the main repo.
    Removed Serilog plugin from the main repo.

--- build.fsx
+++ build.fsx
@@ -222,7 +222,6 @@ Target "CopyOutput" <| fun _ ->
       "core/Akka.Persistence.TestKit"
       "contrib/loggers/Akka.Logger.slf4net"
       "contrib/loggers/Akka.Logger.NLog" 
-      "contrib/loggers/Akka.Logger.Serilog" 
       "contrib/loggers/Akka.Logger.CommonLogging"
       "contrib/loggers/Akka.Logger.log4net" 
       "contrib/dependencyinjection/Akka.DI.Core"
