f99518477ae1ea6cad200bcb3b7a187de505b098
Danthar <Deathraven163@gmail.com>
2016-02-25 21:14:40 +0100

    Manually patch for ApiApprover
    Fixed localization issue which causes problems when building on a system
    with non US locale

--- src/core/Akka.API.Tests/App_Packages/ApiApprover.3.0.1/PublicApiGenerator.cs
+++ src/core/Akka.API.Tests/App_Packages/ApiApprover.3.0.1/PublicApiGenerator.cs
@@ -17,6 +17,7 @@ using Microsoft.CSharp;
 using Mono.Cecil;
 using Mono.Cecil.Rocks;
 using TypeAttributes = System.Reflection.TypeAttributes;
+using System.Globalization;
 
 // ReSharper disable CheckNamespace
 // ReSharper disable BitwiseOperatorOnEnumWithoutFlags
@@ -537,7 +538,7 @@ namespace ApiApprover
                 }
 
                 var name = parameter.HasConstant
-                    ? string.Format("{0} = {1}", parameter.Name, FormatParameterConstant(parameter))
+                    ? string.Format(CultureInfo.InvariantCulture, "{0} = {1}", parameter.Name, FormatParameterConstant(parameter))
                     : parameter.Name;
                 var expression = new CodeParameterDeclarationExpression(type, name)
                 {
