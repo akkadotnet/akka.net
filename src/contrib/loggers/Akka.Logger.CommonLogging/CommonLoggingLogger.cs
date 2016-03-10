af36e1a606d4d8f9197baf2d6c39bd63b0f993d2
Chris Constantin <chris.constantin@aligned.io>
2016-03-10 11:21:45 -0800

    Port  LoggerMailbox and ILoggerMessageQueueSemantics

--- src/contrib/loggers/Akka.Logger.CommonLogging/CommonLoggingLogger.cs
+++ src/contrib/loggers/Akka.Logger.CommonLogging/CommonLoggingLogger.cs
@@ -8,6 +8,7 @@
 using System;
 
 using Akka.Actor;
+using Akka.Dispatch;
 using Akka.Event;
 
 using Common.Logging;
@@ -20,7 +21,7 @@ namespace Akka.Logger.CommonLogging
     /// recognized: <see cref="Debug"/>, <see cref="Info"/>,
     /// <see cref="Warning"/> and <see cref="Error"/>.
     /// </summary>
-    public class CommonLoggingLogger : ReceiveActor
+    public class CommonLoggingLogger : ReceiveActor, IRequiresMessageQueue<ILoggerMessageQueueSemantics>
     {
         private readonly ILoggingAdapter _log = Context.GetLogger();
 
---
+++ src/core/Akka/Event/ILoggerMessageQueueSemantics.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="ILoggerMessageQueueSemantics.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Akka.Dispatch;
+
+namespace Akka.Event
+{
+    public interface ILoggerMessageQueueSemantics : ISemantics
+    {
+    }
+}
