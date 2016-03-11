379898ddf2de9a2c7e236f1bfa32d3e66233f6c5
Aaron Stannard <aaron@petabridge.com>
2016-03-11 10:15:18 -0800

    broke out the shared Akka.Cluster multi-node testing utilities into its own Akka.Cluster.TestKit

--- src/core/Akka.Cluster/Properties/AssemblyInfo.cs
+++ src/core/Akka.Cluster/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Akka.Cluster.Tests")]
 [assembly: InternalsVisibleTo("Akka.Cluster.Tests.MultiNode")]
+[assembly: InternalsVisibleTo("Akka.Cluster.TestKit")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
