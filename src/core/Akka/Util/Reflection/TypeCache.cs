4e6f0e770c3fefe4cbc049757a08b40148ca8cd5
JeffCyr <jeff.cyr.github@gmail.com>
2016-03-13 11:24:56 -0400

    Default MailboxType optimization

---
+++ src/core/Akka/Util/Reflection/TypeCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Akka.Util.Reflection
+{
+    public static class TypeCache
+    {
+        private static readonly ConcurrentDictionary<string, Type> TypeMap = new ConcurrentDictionary<string, Type>();
+
+        public static Type GetType(string typeName)
+        {
+            return TypeMap.GetOrAdd(typeName, item => Type.GetType(item));
+        }
+    }
+}
