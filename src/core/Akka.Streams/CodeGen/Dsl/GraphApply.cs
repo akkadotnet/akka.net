8932981fad254ac33ebb9f2630d0265196c70d17
Chris Constantin <chris.constantin@aligned.io>
2016-03-21 10:12:16 -0700

    Update Graph Dsl to use combineMaterializers func

--- src/core/Akka.Streams/CodeGen/Dsl/GraphApply.cs
+++ src/core/Akka.Streams/CodeGen/Dsl/GraphApply.cs
@@ -1,8 +1,5 @@
 ﻿// --- auto generated: 05.02.2016 09:30:42 --- //
 using System;
-using System.Linq;
-using System.Reactive.Streams;
-using Akka.Streams.Implementation;
 using Akka.Streams.Dsl.Internal;
 
 namespace Akka.Streams.Dsl
@@ -12,9 +9,9 @@ namespace Akka.Streams.Dsl
 		/// <summary>
 		/// Creates a new <see cref="IGraph{TShape, TMat}"/> by passing a <see cref="GraphDsl.Builder{TMat}"/> to the given create function.
 		/// </summary>
-		public static IGraph<TShape, TMat> Create<TShape, TMat>(Func<GraphDsl.Builder<TMat>, TShape> buildBlock) where TShape: Shape
+		public static IGraph<TShape, TMat> Create<TShape, TMat>(Func<Builder<TMat>, TShape> buildBlock) where TShape: Shape
 		{
-			var builder = new GraphDsl.Builder<TMat>();
+			var builder = new Builder<TMat>();
 			var shape = buildBlock(builder);
 			var module = builder.Module.Nest().ReplaceShape(shape);
 
@@ -25,11 +22,11 @@ namespace Akka.Streams.Dsl
 		/// Creates a new <see cref="IGraph{TShape, TMat}"/> by importing the given graph <paramref name="g1"/> 
 		/// and passing its <see cref="Shape"/> along with the <see cref="GraphDsl.Builder{TMat}"/> to the given create function.
 		/// </summary>
-		public static IGraph<TShapeOut, TMat> Create<TShapeOut, TMat, TShape1>(IGraph<TShape1, TMat> g1, Func<GraphDsl.Builder<TMat>, TShape1, TShapeOut> buildBlock) 
+		public static IGraph<TShapeOut, TMat> Create<TShapeOut, TMat, TShape1>(IGraph<TShape1, TMat> g1, Func<Builder<TMat>, TShape1, TShapeOut> buildBlock) 
 			where TShapeOut: Shape
 			where TShape1: Shape
 		{
-			var builder = new GraphDsl.Builder<TMat>();
+			var builder = new Builder<TMat>();
 			var shape1 = builder.Add(g1);
 			var shape = buildBlock(builder, shape1);
 			var module = builder.Module.Nest().ReplaceShape(shape);
@@ -44,16 +41,16 @@ namespace Akka.Streams.Dsl
 		public static IGraph<TShapeOut, TMatOut> Create<TShapeOut, TMatOut, TMat0, TMat1, TShape0, TShape1>(
 			IGraph<TShape0, TMat0> g0, IGraph<TShape1, TMat1> g1, 
 			Func<TMat0, TMat1, TMatOut> combineMaterializers,
-			Func<GraphDsl.Builder<TMatOut>, TShape0, TShape1, TShapeOut> buildBlock) 
+			Func<Builder<TMatOut>, TShape0, TShape1, TShapeOut> buildBlock) 
 			where TShapeOut: Shape
 			where TShape0: Shape
 			where TShape1: Shape
 			
 		{
-			var builder = new GraphDsl.Builder<TMatOut>();
+			var builder = new Builder<TMatOut>();
 			
-			var shape0 = builder.Add(g0);
-			var shape1 = builder.Add(g1);
+			var shape0 = builder.Add<TShape0, TMat0, Func<TMat1, TMatOut>>(g0, m0 => (m1 => combineMaterializers(m0, m1)));
+			var shape1 = builder.Add<TShape1, Func<TMat1, TMatOut>, TMat1, TMatOut>(g1, (f, m1) => f(m1));
 			
 			var shape = buildBlock(builder, shape0, shape1);
 			var module = builder.Module.Nest().ReplaceShape(shape);
@@ -68,18 +65,18 @@ namespace Akka.Streams.Dsl
 		public static IGraph<TShapeOut, TMatOut> Create<TShapeOut, TMatOut, TMat0, TMat1, TMat2, TShape0, TShape1, TShape2>(
 			IGraph<TShape0, TMat0> g0, IGraph<TShape1, TMat1> g1, IGraph<TShape2, TMat2> g2, 
 			Func<TMat0, TMat1, TMat2, TMatOut> combineMaterializers,
-			Func<GraphDsl.Builder<TMatOut>, TShape0, TShape1, TShape2, TShapeOut> buildBlock) 
+			Func<Builder<TMatOut>, TShape0, TShape1, TShape2, TShapeOut> buildBlock) 
 			where TShapeOut: Shape
 			where TShape0: Shape
 			where TShape1: Shape
 			where TShape2: Shape
 			
 		{
-			var builder = new GraphDsl.Builder<TMatOut>();
+			var builder = new Builder<TMatOut>();
 			
-			var shape0 = builder.Add(g0);
-			var shape1 = builder.Add(g1);
-			var shape2 = builder.Add(g2);
+			var shape0 = builder.Add<TShape0, TMat0, Func<TMat1, TMat2, TMatOut>>(g0, m0 => ((m1, m2) => combineMaterializers(m0, m1, m2)));
+			var shape1 = builder.Add<TShape1, Func<TMat1, TMatOut>, TMat1, TMatOut>(g1, (f, m1) => f(m1));
+			var shape2 = builder.Add<TShape2, Func<TMat2, TMatOut>, TMat2, TMatOut>(g2, (f, m2) => f(m2));
 			
 			var shape = buildBlock(builder, shape0, shape1, shape2);
 			var module = builder.Module.Nest().ReplaceShape(shape);
@@ -94,7 +91,7 @@ namespace Akka.Streams.Dsl
 		public static IGraph<TShapeOut, TMatOut> Create<TShapeOut, TMatOut, TMat0, TMat1, TMat2, TMat3, TShape0, TShape1, TShape2, TShape3>(
 			IGraph<TShape0, TMat0> g0, IGraph<TShape1, TMat1> g1, IGraph<TShape2, TMat2> g2, IGraph<TShape3, TMat3> g3, 
 			Func<TMat0, TMat1, TMat2, TMat3, TMatOut> combineMaterializers,
-			Func<GraphDsl.Builder<TMatOut>, TShape0, TShape1, TShape2, TShape3, TShapeOut> buildBlock) 
+			Func<Builder<TMatOut>, TShape0, TShape1, TShape2, TShape3, TShapeOut> buildBlock) 
 			where TShapeOut: Shape
 			where TShape0: Shape
 			where TShape1: Shape
@@ -102,12 +99,12 @@ namespace Akka.Streams.Dsl
 			where TShape3: Shape
 			
 		{
-			var builder = new GraphDsl.Builder<TMatOut>();
+			var builder = new Builder<TMatOut>();
 			
-			var shape0 = builder.Add(g0);
-			var shape1 = builder.Add(g1);
-			var shape2 = builder.Add(g2);
-			var shape3 = builder.Add(g3);
+			var shape0 = builder.Add<TShape0, TMat0, Func<TMat1, TMat2, TMat3, TMatOut>>(g0, m0 => ((m1, m2, m3) => combineMaterializers(m0, m1, m2, m3)));
+			var shape1 = builder.Add<TShape1, Func<TMat1, TMatOut>, TMat1, TMatOut>(g1, (f, m1) => f(m1));
+			var shape2 = builder.Add<TShape2, Func<TMat2, TMatOut>, TMat2, TMatOut>(g2, (f, m2) => f(m2));
+			var shape3 = builder.Add<TShape3, Func<TMat3, TMatOut>, TMat3, TMatOut>(g3, (f, m3) => f(m3));
 			
 			var shape = buildBlock(builder, shape0, shape1, shape2, shape3);
 			var module = builder.Module.Nest().ReplaceShape(shape);
@@ -122,7 +119,7 @@ namespace Akka.Streams.Dsl
 		public static IGraph<TShapeOut, TMatOut> Create<TShapeOut, TMatOut, TMat0, TMat1, TMat2, TMat3, TMat4, TShape0, TShape1, TShape2, TShape3, TShape4>(
 			IGraph<TShape0, TMat0> g0, IGraph<TShape1, TMat1> g1, IGraph<TShape2, TMat2> g2, IGraph<TShape3, TMat3> g3, IGraph<TShape4, TMat4> g4, 
 			Func<TMat0, TMat1, TMat2, TMat3, TMat4, TMatOut> combineMaterializers,
-			Func<GraphDsl.Builder<TMatOut>, TShape0, TShape1, TShape2, TShape3, TShape4, TShapeOut> buildBlock) 
+			Func<Builder<TMatOut>, TShape0, TShape1, TShape2, TShape3, TShape4, TShapeOut> buildBlock) 
 			where TShapeOut: Shape
 			where TShape0: Shape
 			where TShape1: Shape
@@ -131,13 +128,13 @@ namespace Akka.Streams.Dsl
 			where TShape4: Shape
 			
 		{
-			var builder = new GraphDsl.Builder<TMatOut>();
+			var builder = new Builder<TMatOut>();
 			
-			var shape0 = builder.Add(g0);
-			var shape1 = builder.Add(g1);
-			var shape2 = builder.Add(g2);
-			var shape3 = builder.Add(g3);
-			var shape4 = builder.Add(g4);
+			var shape0 = builder.Add<TShape0, TMat0, Func<TMat1, TMat2, TMat3, TMat4, TMatOut>>(g0, m0 => ((m1, m2, m3, m4) => combineMaterializers(m0, m1, m2, m3, m4)));
+			var shape1 = builder.Add<TShape1, Func<TMat1, TMatOut>, TMat1, TMatOut>(g1, (f, m1) => f(m1));
+			var shape2 = builder.Add<TShape2, Func<TMat2, TMatOut>, TMat2, TMatOut>(g2, (f, m2) => f(m2));
+			var shape3 = builder.Add<TShape3, Func<TMat3, TMatOut>, TMat3, TMatOut>(g3, (f, m3) => f(m3));
+			var shape4 = builder.Add<TShape4, Func<TMat4, TMatOut>, TMat4, TMatOut>(g4, (f, m4) => f(m4));
 			
 			var shape = buildBlock(builder, shape0, shape1, shape2, shape3, shape4);
 			var module = builder.Module.Nest().ReplaceShape(shape);
--- src/core/Akka.Streams/Graph.cs
+++ src/core/Akka.Streams/Graph.cs
@@ -5,8 +5,7 @@ namespace Akka.Streams
     /// <summary>
     /// </summary>
     /// <typeparam name="TShape">Type-level accessor for the shape parameter of this graph.</typeparam>
-    /// <typeparam name="TMaterializer"></typeparam>
-    public interface IGraph<out TShape, TMaterializer> where TShape : Shape
+    public interface IGraph<out TShape> where TShape : Shape
     {
         /// <summary>
         /// The shape of a graph is all that is externally visible: its inlets and outlets.
@@ -17,7 +16,14 @@ namespace Akka.Streams
         /// INTERNAL API: Every materializable element must be backed by a stream layout module
         /// </summary>
         IModule Module { get; }
+    }
 
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TShape">Type-level accessor for the shape parameter of this graph.</typeparam>
+    /// <typeparam name="TMaterializer"></typeparam>
+    public interface IGraph<out TShape, TMaterializer> : IGraph<TShape> where TShape : Shape
+    {
         IGraph<TShape, TMaterializer> WithAttributes(Attributes attributes);
         IGraph<TShape, TMaterializer> Named(string name);
     }
