697ba65e8e0113c5bb607139df5da712df2f96c4
Chris Constantin <chris.constantin@aligned.io>
2016-03-24 19:01:14 -0700

    Fixes InterpreterStressSpec

--- src/core/Akka.Streams.Tests/Implementation/Fusing/InterpreterStressSpec.cs
+++ src/core/Akka.Streams.Tests/Implementation/Fusing/InterpreterStressSpec.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using System.Linq;
-using System.Linq.Expressions;
 using Akka.Streams.Implementation.Fusing;
 using Akka.Streams.Stage;
 using Akka.Streams.Supervision;
@@ -24,9 +23,6 @@ namespace Akka.Streams.Tests.Implementation.Fusing
         private readonly ITestOutputHelper _helper;
 
         private readonly Map<int,int> _map = new Map<int, int>(x=>x+1, Deciders.StoppingDecider);
-        private readonly Drop<int> _dropOne = new Drop<int>(1); 
-        private readonly Take<int> _takeOne = new Take<int>(1);
-        private readonly Take<int> _takeHalfOfRepetition = new Take<int>(Repetition/2);
 
         public InterpreterStressSpec(ITestOutputHelper helper = null) : base(helper)
         {
@@ -37,7 +33,8 @@ namespace Akka.Streams.Tests.Implementation.Fusing
         [Fact]
         public void Interpreter_must_work_with_a_massive_chain_of_maps()
         {
-            var ops = Enumerable.Range(1, ChainLength).Select(_=>_map).ToArray();
+            var ops = Enumerable.Range(1, ChainLength).Select(_ => _map)
+                .Cast<IStage<int, int>>().ToArray();
             WithOneBoundedSetup(ops, (lastEvents, upstream, downstream) =>
                 {
                     lastEvents().Should().BeEmpty();
@@ -48,15 +45,15 @@ namespace Akka.Streams.Tests.Implementation.Fusing
                     while (i < Repetition)
                     {
                         downstream.RequestOne();
-                        lastEvents().Should().Equal(new RequestOne());
+                        lastEvents().Should().BeEquivalentTo(new RequestOne());
 
                         upstream.OnNext(i);
-                        lastEvents().Should().Equal(new OnNext(i + ChainLength));
+                        lastEvents().Should().BeEquivalentTo(new OnNext(i + ChainLength));
                         i++;
                     }
 
                     upstream.OnComplete();
-                    lastEvents().Should().Equal(new OnComplete());
+                    lastEvents().Should().BeEquivalentTo(new OnComplete());
 
                     tstamp.Stop();
                     var time = tstamp.Elapsed.TotalSeconds;
@@ -69,7 +66,7 @@ namespace Akka.Streams.Tests.Implementation.Fusing
         public void Interpreter_must_work_with_a_massive_chain_of_maps_with_early_complete()
         {
             var ops = Enumerable.Range(1, HalfLength).Select(_ => _map).ToList<IStage<int, int>>();
-            ops.Add(_takeHalfOfRepetition);
+            ops.Add(new Take<int>(Repetition/2));
             ops.AddRange(Enumerable.Range(1, HalfLength).Select(_ => _map));
 
             WithOneBoundedSetup(ops.ToArray(), (lastEvents, upstream, downstream) =>
@@ -79,66 +76,69 @@ namespace Akka.Streams.Tests.Implementation.Fusing
                 tstamp.Start();
 
                 var i = 0;
-                while (i < (Repetition / 2) - 1)
+                while (i < (Repetition/2) - 1)
                 {
                     downstream.RequestOne();
-                    lastEvents().Should().Equal(new RequestOne());
+                    lastEvents().Should().BeEquivalentTo(new RequestOne());
 
                     upstream.OnNext(i);
-                    lastEvents().Should().Equal(new OnNext(i + ChainLength));
+                    lastEvents().Should().BeEquivalentTo(new OnNext(i + ChainLength));
                     i++;
                 }
 
                 downstream.RequestOne();
-                lastEvents().Should().Equal(new RequestOne());
+                lastEvents().Should().BeEquivalentTo(new RequestOne());
 
                 upstream.OnNext(0);
-                lastEvents().Should().Equal(new OnNext(0 + ChainLength), new Cancel(), new OnComplete());
+                lastEvents().Should().BeEquivalentTo(new OnNext(0 + ChainLength), new Cancel(), new OnComplete());
 
                 tstamp.Stop();
                 var time = tstamp.Elapsed.TotalSeconds;
                 // Not a real benchmark, just for sanity check
-                _helper?.WriteLine($"Chain finished in {time} seconds {ChainLength * Repetition} maps in total and {(ChainLength * Repetition) / (time * 1000 * 1000)} million maps/s");
+                _helper?.WriteLine(
+                    $"Chain finished in {time} seconds {ChainLength*Repetition} maps in total and {(ChainLength*Repetition)/(time*1000*1000)} million maps/s");
             });
         }
 
         [Fact]
         public void Interpreter_must_work_with_a_massive_chain_of_takes()
         {
-            var ops = Enumerable.Range(1, ChainLength / 10).Select(_ => _takeOne).ToArray();
+            var ops = Enumerable.Range(1, ChainLength / 10).Select(_ => new Take<int>(1))
+                .Cast<IStage<int, int>>().ToArray();
             WithOneBoundedSetup(ops, (lastEvents, upstream, downstream) =>
             {
                 lastEvents().Should().BeEmpty();
 
                 downstream.RequestOne();
-                lastEvents().Should().Equal(new RequestOne());
+                lastEvents().Should().BeEquivalentTo(new RequestOne());
 
                 upstream.OnNext(0);
-                lastEvents().Should().Equal(new OnNext(0), new Cancel(), new OnComplete());
+                lastEvents().Should().BeEquivalentTo(new OnNext(0), new Cancel(), new OnComplete());
             });
         }
 
         [Fact]
         public void Interpreter_must_work_with_a_massive_chain_of_drops()
         {
-            var ops = Enumerable.Range(1, ChainLength / 1000).Select(_ => _dropOne).ToArray();
+            var ops = Enumerable.Range(1, ChainLength / 1000).Select(_ => new Drop<int>(1))
+                .Cast<IStage<int, int>>().ToArray();
             WithOneBoundedSetup(ops, (lastEvents, upstream, downstream) =>
             {
                 lastEvents().Should().BeEmpty();
 
                 downstream.RequestOne();
-                lastEvents().Should().Equal(new RequestOne());
+                lastEvents().Should().BeEquivalentTo(new RequestOne());
 
                 var i = 0;
                 while (i < (ChainLength / 1000))
                 {
                     upstream.OnNext(0);
-                    lastEvents().Should().Equal(new RequestOne());
+                    lastEvents().Should().BeEquivalentTo(new RequestOne());
                     i++;
                 }
 
                 upstream.OnNext(0);
-                lastEvents().Should().Equal(new OnNext(0));
+                lastEvents().Should().BeEquivalentTo(new OnNext(0));
             });
 
         }
@@ -147,17 +147,18 @@ namespace Akka.Streams.Tests.Implementation.Fusing
         public void Interpreter_must_work_with_a_massive_chain_of_batches_of_overflowing_to_the_heap()
         {
             var batch = new Batch<int, int>(0, _ => 0, i => i, (agg, i) => agg + i);
-            var ops = Enumerable.Range(1, ChainLength/10).Select(_ => batch).ToArray();
+            var ops = Enumerable.Range(1, ChainLength/10).Select(_ => batch)
+                .Cast<IGraphStageWithMaterializedValue>().ToArray();
 
             WithOneBoundedSetup<int, int>(ops, (lastEvents, upstream, downstream) =>
             {
-                lastEvents().Should().Equal(new RequestOne());
+                lastEvents().Should().BeEquivalentTo(new RequestOne());
 
                 var i = 0;
                 while (i < Repetition)
                 {
                     upstream.OnNext(1);
-                    lastEvents().Should().Equal(new RequestOne());
+                    lastEvents().Should().BeEquivalentTo(new RequestOne());
                     i++;
                 }
             });
