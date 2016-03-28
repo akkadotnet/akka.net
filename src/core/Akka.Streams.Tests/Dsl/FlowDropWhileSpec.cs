aa9c3f875d82532c3ba8c5870861827153bdb9b2
Silv3rcircl3 <piechura@gmx.net>
2016-03-28 23:20:03 +0200

    Ported FlowDropWhileSpec

---
+++ src/core/Akka.Streams.Tests/Dsl/FlowDropWhileSpec.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using Akka.Streams.Dsl;
+using Akka.Streams.Supervision;
+using Akka.Streams.TestKit;
+using Akka.Streams.TestKit.Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Streams.Tests.Dsl
+{
+    public class FlowDropWhileSpec : AkkaSpec
+    {
+        private ActorMaterializerSettings Settings { get; }
+        private ActorMaterializer Materializer { get; }
+
+        public FlowDropWhileSpec(ITestOutputHelper helper) : base(helper)
+        {
+            Settings = ActorMaterializerSettings.Create(Sys).WithInputBuffer(2, 16);
+            Materializer = ActorMaterializer.Create(Sys, Settings);
+        }
+
+        [Fact]
+        public void A_DropWhile_must_drop_while_predicate_is_true()
+        {
+            this.AssertAllStagesStopped(() =>
+            {
+                Source.From(Enumerable.Range(1, 4))
+                    .DropWhile(x => x < 3)
+                    .RunWith(this.SinkProbe<int>(), Materializer)
+                    .Request(2)
+                    .ExpectNext(3, 4)
+                    .ExpectComplete();
+            }, Materializer);
+        }
+
+        [Fact]
+        public void A_DropWhile_must_complete_the_future_for_an_empty_stream()
+        {
+            this.AssertAllStagesStopped(() =>
+            {
+                Source.Empty<int>()
+                    .DropWhile(x => x < 2)
+                    .RunWith(this.SinkProbe<int>(), Materializer)
+                    .Request(1)
+                    .ExpectComplete();
+            }, Materializer);
+        }
+
+        [Fact]
+        public void A_DropWhile_must_continue_if_error()
+        {
+            this.AssertAllStagesStopped(() =>
+            {
+                var testException = new Exception("test");
+                Source.From(Enumerable.Range(1, 4)).DropWhile(x =>
+                {
+                    if (x < 3)
+                        return true;
+                    throw testException;
+                })
+                    .WithAttributes(new Attributes(new ActorAttributes.SupervisionStrategy(Deciders.ResumingDecider)))
+                    .RunWith(this.SinkProbe<int>(), Materializer)
+                    .Request(1)
+                    .ExpectComplete();
+            }, Materializer);
+        }
+    }
+}
