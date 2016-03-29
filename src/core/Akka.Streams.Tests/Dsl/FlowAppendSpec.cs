d104f6d30fecc4433160e8c421bd3e462c1eb2d9
Silv3rcircl3 <piechura@gmx.net>
2016-03-29 16:25:34 +0200

    Ported FlowAppendSpec

---
+++ src/core/Akka.Streams.Tests/Dsl/FlowAppendSpec.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Streams;
+using Akka.Streams.Dsl;
+using Akka.Streams.TestKit;
+using Akka.Streams.TestKit.Tests;
+using Akka.TestKit;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+using static Akka.Streams.Tests.Dsl.FlowAppendSpec.River;
+
+namespace Akka.Streams.Tests.Dsl
+{
+    public class FlowAppendSpec : AkkaSpec
+    {
+        private ActorMaterializer Materializer { get; }
+
+        public FlowAppendSpec(ITestOutputHelper helper) : base (helper)
+        {
+            var settings = ActorMaterializerSettings.Create(Sys);
+            Materializer = ActorMaterializer.Create(Sys, settings);
+        }
+
+        [Fact]
+        public void Flow_should_append_Flow()
+        {
+            RiverOf<string>((subscriber, otherFlow, elements) =>
+            {
+                var flow = Flow.Create<int>().Via(otherFlow);
+                Source.From(elements).Via(flow).To(Sink.FromSubscriber<string, Unit>(subscriber)).Run(Materializer);
+            }, this);
+        }
+
+        [Fact]
+        public void Flow_should_append_Sink()
+        {
+            RiverOf<string>((subscriber, otherFlow, elements) =>
+            {
+                Source.From(elements).To(otherFlow.To(Sink.FromSubscriber<string, Unit>(subscriber))).Run(Materializer);
+            }, this);
+        }
+
+        [Fact]
+        public void Source_should_append_Flow()
+        {
+            RiverOf<string>((subscriber, otherFlow, elements) =>
+            {
+                Source.From(elements).Via(otherFlow).To(Sink.FromSubscriber<string, Unit>(subscriber)).Run(Materializer);
+            }, this);
+        }
+
+        [Fact]
+        public void Source_should_append_Sink()
+        {
+            RiverOf<string>((subscriber, otherFlow, elements) =>
+            {
+                Source.From(elements).To(otherFlow.To(Sink.FromSubscriber<string, Unit>(subscriber))).Run(Materializer);
+            }, this);
+        }
+
+        internal static class River
+        {
+            private static readonly Flow<int, string, Unit> OtherFlow = Flow.Create<int>().Map(i => i.ToString());
+            
+            public static void RiverOf<T>(Action<ISubscriber<T>, Flow<int, string, Unit>, IEnumerable<int>> flowConstructor, TestKitBase kit)
+            {
+                var subscriber = TestSubscriber.CreateManualProbe<T>(kit);
+
+                var elements = Enumerable.Range(1, 10).ToList();
+                flowConstructor(subscriber, OtherFlow, elements);
+
+                var subscription = subscriber.ExpectSubscription();
+                subscription.Request(elements.Count);
+                elements.ForEach(el=>subscriber.ExpectNext().Should().Be(el.ToString()));
+                subscription.Request(1);
+                subscriber.ExpectComplete();
+            }
+        }
+    }
+}
