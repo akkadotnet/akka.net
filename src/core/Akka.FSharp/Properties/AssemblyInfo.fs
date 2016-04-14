7bda483099a1f51e778c75750536aae7df626eb2
Aaron Stannard <aaron@petabridge.com>
2016-04-13 17:49:06 -0700

    added benchmark to illustrate potential memory leak with StandardOutWriter eliminated allocations inside StandardOutWriter

--- src/core/Akka.FSharp/Properties/AssemblyInfo.fs
+++ src/core/Akka.FSharp/Properties/AssemblyInfo.fs
@@ -1,11 +1,4 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="AssemblyInfo.fs" company="Akka.NET Project">
-//     Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
-//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
-// </copyright>
-//-----------------------------------------------------------------------
-
-namespace System
+﻿namespace System
 open System
 open System.Reflection
 open System.Runtime.InteropServices
