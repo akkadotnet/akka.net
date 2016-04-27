0ef7e24665bc33034046814994e4f93c3c3f6659
Marc Piechura <piechura@gmx.net>
2016-04-27 08:41:51 +0200

    Updated Lightbend company name

--- src/core/Akka.Persistence.Tests/LoadPluginSpec.cs
+++ src/core/Akka.Persistence.Tests/LoadPluginSpec.cs
@@ -1,6 +1,6 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="LoadPluginSpec.cs" company="Akka.NET Project">
-//     Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
 //     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
 // </copyright>
 //-----------------------------------------------------------------------
