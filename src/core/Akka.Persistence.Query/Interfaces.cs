d2790b18b77239b2bbdd1a2a9c0a35457c315afe
Chris Constantin <chris.constantin@aligned.io>
2016-04-29 07:01:14 -0700

    Update ICurrent/AllPersistenceIdsQuery to expose methods instead of properties

--- src/core/Akka.Persistence.Query/Interfaces.cs
+++ src/core/Akka.Persistence.Query/Interfaces.cs
@@ -121,7 +121,7 @@ namespace Akka.Persistence.Query
         /// is completed immediately when it reaches the end of the "result set". Persistent
         /// actors that are created after the query is completed are not included in the stream.
         /// </summary>
-        Source<string, Unit> CurrentPersistenceIds { get; }
+        Source<string, Unit> CurrentPersistenceIds();
     }
 
     public interface IAllPersistenceIdsQuery : IReadJournal
@@ -135,6 +135,6 @@ namespace Akka.Persistence.Query
         /// Corresponding query that is completed when it reaches the end of the currently
         /// currently used `persistenceIds` is provided by <see cref="ICurrentPersistenceIdsQuery.CurrentPersistenceIds"/>.
         /// </summary>
-        Source<string, Unit> AllPersistenceIds { get; }
+        Source<string, Unit> AllPersistenceIds();
     }
 }
