6f6da00decf54c1cbc9c58fbe99b9f1d55e2ecfe
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2016-05-02 19:24:08 +0200

    Added Tagged class for persistent journals

---
+++ src/core/Akka.Persistence/Journal/Tagged.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Akka.Persistence.Journal
+{
+    /// <summary>
+    /// The journal may support tagging of events that are used by the
+    /// `EventsByTag` query and it may support specifying the tags via an
+    /// <see cref="IEventAdapter"/> that wraps the events
+    /// in a <see cref="Tagged"/> with the given <see cref="Tags"/>. The journal may support other
+    /// ways of doing tagging. Please consult the documentation of the specific
+    /// journal implementation for more information.
+    /// The journal will unwrap the event and store the <see cref="Payload"/>.
+    /// </summary>
+    public struct Tagged
+    {
+        public readonly object Payload;
+        public readonly IImmutableSet<string> Tags;
+
+        public Tagged(object payload, IEnumerable<string> tags)
+        {
+            Payload = payload;
+            Tags = tags.ToImmutableHashSet();
+        }
+
+        public Tagged(object payload, IImmutableSet<string> tags)
+        {
+            Payload = payload;
+            Tags = tags;
+        }
+    }
+}
