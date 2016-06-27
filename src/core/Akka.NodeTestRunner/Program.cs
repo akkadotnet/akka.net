8ff0156e84bec3abe69dec84db169cedeeba8bf6
Aaron Stannard <aaron@petabridge.com>
2016-06-27 13:30:44 -0700

    increased max wait timeout

--- src/core/Akka.NodeTestRunner/Program.cs
+++ src/core/Akka.NodeTestRunner/Program.cs
@@ -24,7 +24,7 @@ namespace Akka.NodeTestRunner
         /// If it takes longer than this value for the <see cref="Sink"/> to get back to us
         /// about a particular test passing or failing, throw loudly.
         /// </summary>
-        private static readonly TimeSpan MaxProcessWaitTimeout = TimeSpan.FromMinutes(1.5);
+        private static readonly TimeSpan MaxProcessWaitTimeout = TimeSpan.FromMinutes(5);
         private static IActorRef _logger;
 
         static int Main(string[] args)
