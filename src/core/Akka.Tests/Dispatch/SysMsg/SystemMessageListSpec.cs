91c0b3718510470f02a879882b7e26aabc1f7463
Marc Piechura <piechura@gmx.net>
2016-07-07 08:58:52 +0200

    fixed lightbend url

--- src/core/Akka.Tests/Dispatch/SysMsg/SystemMessageListSpec.cs
+++ src/core/Akka.Tests/Dispatch/SysMsg/SystemMessageListSpec.cs
@@ -1,11 +1,10 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="SystemMessageListSpec.cs" company="Akka.NET Project">
-//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.typesafe.com>
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
 //     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System.Collections.Generic;
 using Akka.Dispatch.SysMsg;
 using Akka.TestKit;
 using Xunit;
