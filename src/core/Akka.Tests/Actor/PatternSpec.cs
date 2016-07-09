dcb8f150dbe3d5fbc6fcae027869533c27fd6acb
Esun Kim <veblush@gmail.com>
2016-07-09 14:16:04 +0900

    Add new a test instead of modifying one.

--- src/core/Akka.Tests/Actor/PatternSpec.cs
+++ src/core/Akka.Tests/Actor/PatternSpec.cs
@@ -22,7 +22,6 @@ namespace Akka.Tests.Actor
         {
             //arrange
             var target = Sys.ActorOf<TargetActor>();
-            Sys.EventStream.Subscribe(TestActor, typeof(DeadLetter));
 
             //act
             var result = target.GracefulStop(TimeSpan.FromSeconds(5));
@@ -30,7 +29,6 @@ namespace Akka.Tests.Actor
 
             //assert
             Assert.True(result.Result);
-            ExpectNoMsg(); // there should be no deadletter.
         }
 
         [Fact]
@@ -64,6 +62,21 @@ namespace Akka.Tests.Actor
                 var result = task.Result;
             });
             latch.Open();
+        }
+
+        [Fact]
+        public async Task GracefulStop_must_not_send_unnecessary_Deadletter_bug_2157()
+        {
+            //arrange
+            var target = Sys.ActorOf<TargetActor>();
+            Sys.EventStream.Subscribe(TestActor, typeof(DeadLetter));
+
+            //act
+            var stopped = await target.GracefulStop(TimeSpan.FromSeconds(5));
+
+            //assert
+            Assert.True(stopped);
+            ExpectNoMsg(TimeSpan.Zero);
         }
 
         #region Actors
