1e8349fe30f48b7ffc01d0c2657bb28c738f2e3e
ravengerUA <yurisikorsky@gmail.com>
2016-07-15 15:30:23 +0300

    Mono MapToIPv4 fix

--- src/core/Akka.Remote/Transport/AkkaIO/AddressConverters.cs
+++ src/core/Akka.Remote/Transport/AkkaIO/AddressConverters.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using Akka.Actor;
 
 namespace Akka.Remote.Transport.AkkaIO
@@ -30,6 +31,39 @@ namespace Akka.Remote.Transport.AkkaIO
             if (address == null || address.Host == null || !address.Port.HasValue)
                 throw new ArgumentException("Invalid address", "address");
             return new DnsEndPoint(address.Host, address.Port.Value, AddressFamily.InterNetwork);
+        }
+    }
+
+    public static class IPExtensions
+    {
+        internal static object GetInstanceField(Type type, object instance, string fieldName)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+            FieldInfo field = type.GetField(fieldName, bindFlags);
+            return field.GetValue(instance);
+        }
+
+        public static IPAddress MapToIPv4(this IPAddress ipa)
+        {
+            ushort[] m_Numbers = GetInstanceField(typeof(IPAddress), ipa, "m_Numbers") as ushort[];
+
+            if (m_Numbers == null)
+                throw new Exception("IPAddress.m_Numbers not found");
+
+            if (ipa.AddressFamily == AddressFamily.InterNetwork)
+                return ipa;
+
+            if (ipa.AddressFamily != AddressFamily.InterNetworkV6)
+                throw new Exception("Only AddressFamily.InterNetworkV6 can be converted to IPv4");
+
+            // Cast the ushort values to a uint and mask with unsigned literal before bit shifting.
+            // Otherwise, we can end up getting a negative value for any IPv4 address that ends with
+            // a byte higher than 127 due to sign extension of the most significant 1 bit.
+            long address = (((m_Numbers[6] & 0x0000FF00u) >> 8) | ((m_Numbers[6] & 0x000000FFu) << 8)) |
+                    ((((m_Numbers[7] & 0x0000FF00u) >> 8) | ((m_Numbers[7] & 0x000000FFu) << 8)) << 16);
+
+            return new IPAddress(address);
         }
     }
 }
