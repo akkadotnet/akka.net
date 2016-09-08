6d531163625df78911287a7e5e3f3c51bd48b768
Richard Dobson <richard.dobson@starkwood.co.uk>
2016-09-08 12:53:31 +0100

    fix "0.05 s" parsing
    fix the regular expression to account for more than a single digit after
    the decimal point
    
    fix test
    
    change brackets to Allman style
    
    change brackets to Allman style
    
    update failing test

--- src/core/Akka.Tests/Configuration/HoconTests.cs
+++ src/core/Akka.Tests/Configuration/HoconTests.cs
@@ -805,7 +805,8 @@ ip = ""::1""
         }
 
         [Fact]
-        public void Can_parse_non_abbreviated_timespan() {
+        public void Can_parse_non_abbreviated_timespan()
+        {
             var hocon = "timespan = 10 seconds";
 
             var res = ConfigurationFactory.ParseString(hocon).GetTimeSpan("timespan");
@@ -813,11 +814,21 @@ ip = ""::1""
         }
 
         [Fact]
-        public void Can_parse_abbreviated_timespan() {
+        public void Can_parse_abbreviated_timespan() 
+        {
             var hocon = "timespan = 10 s";
 
             var res = ConfigurationFactory.ParseString(hocon).GetTimeSpan("timespan");
             Assert.Equal(10, res.TotalSeconds);
+        }
+
+        [Fact]
+        public void Can_parse_abbreviated_timespan2()
+        {
+            var hocon = "timespan = 0.05 s";
+
+            var res = ConfigurationFactory.ParseString(hocon).GetTimeSpan("timespan");
+            Assert.Equal(50, res.TotalMilliseconds);
         }
     }
 }
--- src/core/Akka/Configuration/Hocon/HoconValue.cs
+++ src/core/Akka/Configuration/Hocon/HoconValue.cs
@@ -366,12 +366,14 @@ namespace Akka.Configuration.Hocon
         {
             string res = GetString();
 
-            var match = Regex.Match(res, @"^(?<value>([0-9]+(\.[0-9])?))\s*(?<unit>(nanoseconds|nanosecond|nanos|nano|ns|microseconds|microsecond|micros|micro|us|milliseconds|millisecond|millis|milli|ms|seconds|second|s|minutes|minute|m|hours|hour|h|days|day|d))$");
-            if (match.Success) {
+            var match = Regex.Match(res, @"^(?<value>([0-9]+(\.[0-9]+)?))\s*(?<unit>(nanoseconds|nanosecond|nanos|nano|ns|microseconds|microsecond|micros|micro|us|milliseconds|millisecond|millis|milli|ms|seconds|second|s|minutes|minute|m|hours|hour|h|days|day|d))$");
+            if (match.Success) 
+            {
                 var u = match.Groups["unit"].Value;
                 var v = ParsePositiveValue(match.Groups["value"].Value);
 
-                switch (u) {
+                switch (u) 
+                {
                     case "nanoseconds":
                     case "nanosecond":
                     case "nanos":
