c12af10bb773e801587319f8be8a350ba7a8084f
boriskreminskimoldev <boris.kreminski@moldev.com>
2016-12-09 23:37:32 -0500

    Messages are logged recursively with custom logger
    Refactored output to exclude the receiver part (can be logged via
    LogEvent)

--- src/core/Akka/Actor/ActorCell.DefaultMessages.cs
+++ src/core/Akka/Actor/ActorCell.DefaultMessages.cs
@@ -171,7 +171,7 @@ namespace Akka.Actor
             if (System.Settings.AddLoggingReceive && _actor is ILogReceive)
             {
                 //TODO: akka alters the receive handler for logging, but the effect is the same. keep it this way?
-                var msg = "received " + (wasHandled ? "handled" : "unhandled") + " message " + message + " from " + Self.Path + " to " + Sender.Path;
+                var msg = "received " + (wasHandled ? "handled" : "unhandled") + " message " + message + " from " + Sender.Path;
                 Publish(new Debug(Self.Path.ToString(), _actor.GetType(), msg));
             }
         }
