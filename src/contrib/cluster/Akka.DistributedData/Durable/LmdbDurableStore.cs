9023f54f85d08423db93e6c1961675734b9f2d3f
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2017-01-13 20:34:14 +0100

    init ddata durable envelopes

---
+++ src/contrib/cluster/Akka.DistributedData/Durable/LmdbDurableStore.cs
@@ -0,0 +1,221 @@
+﻿#region copyright
+// -----------------------------------------------------------------------
+//  <copyright file="LmdbDurableStore.cs" company="Akka.NET project">
+//      Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//      Copyright (C) 2013-2017 Akka.NET project <https://github.com/akkadotnet>
+//  </copyright>
+// -----------------------------------------------------------------------
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.DistributedData.Internal;
+using Akka.Event;
+using Akka.IO;
+using Akka.Serialization;
+using Google.ProtocolBuffers;
+using LightningDB;
+
+namespace Akka.DistributedData.Durable
+{
+    /// <summary>
+    /// An actor implementing the durable store for the Distributed Data <see cref="Replicator"/>
+    /// has to implement the protocol with the messages defined here.
+    /// 
+    /// At startup the <see cref="Replicator"/> creates the durable store actor and sends the
+    /// <see cref="Load"/> message to it. It must then reply with 0 or more <see cref="LoadData"/> messages
+    /// followed by one <see cref="LoadAllCompleted"/> message to the `sender` (the <see cref="Replicator"/>).
+    /// 
+    /// If the <see cref="LoadAll"/> fails it can throw <see cref="LoadFailed"/> and the <see cref="Replicator"/> supervisor
+    /// will stop itself and the durable store.
+    /// 
+    /// When the <see cref="Replicator"/> needs to store a value it sends a <see cref="Store"/> message
+    /// to the durable store actor, which must then reply with the <see cref="StoreReply.SuccessMessage"/> or
+    /// <see cref="StoreReply.FailureMessage"/> to the <see cref="StoreReply.ReplyTo"/>.
+    /// </summary>
+    public sealed class LmdbDurableStore : ReceiveActor
+    {
+        private sealed class WriteBehind
+        {
+            public static readonly WriteBehind Instance = new WriteBehind();
+            private WriteBehind() { }
+        }
+
+        public static Actor.Props Props(Config config) => Actor.Props.Create(() => new LmdbDurableStore(config));
+
+        private readonly TimeSpan _writeBehindInterval;
+        private readonly Dictionary<string, IReplicatedData> _pending = new Dictionary<string, IReplicatedData>();
+        private readonly LightningEnvironment _environment;
+        private readonly LightningDatabase _db;
+        private readonly ILoggingAdapter _log;
+        private readonly Serializer _serializer;
+        private ByteBuffer _keyBuffer;
+        private ByteBuffer _valueBuffer;
+
+        public LmdbDurableStore(Config config)
+        {
+            _log = Context.GetLogger();
+            _writeBehindInterval = config.GetString("lmdb.write-behind-interval") == "off" 
+                ? TimeSpan.Zero : config.GetTimeSpan("lmdb.write-behind-interval");
+
+            var mapSize = config.GetByteSize("lmdb.map-size");
+            var dirPath = config.GetString("lmdb.dir");
+            if (dirPath.EndsWith("ddata"))
+            {
+                dirPath = $"path-{Context.System.Name}-{Self.Path.Parent.Name}-{Cluster.Cluster.Get(Context.System).SelfAddress.Port.Value}";
+            }
+
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            _environment = new LightningEnvironment(dirPath, new EnvironmentConfiguration
+            {
+                MapSize = mapSize.Value,
+                MaxDatabases = 1
+            });
+            _environment.Open(EnvironmentOpenFlags.NoLock);
+
+            using (var tx = _environment.BeginTransaction())
+            {
+                _db = tx.OpenDatabase("ddata", new DatabaseConfiguration {Flags = DatabaseOpenFlags.Create});
+            }
+
+            _keyBuffer = ByteBuffer.Allocate(1024);
+            _valueBuffer = ByteBuffer.Allocate(100 * 1014);
+            _serializer = Context.System.Serialization.FindSerializerForType(typeof(DurableDataEnvelope));
+
+            Init();
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+            DoWriteBehind();
+            _db.Dispose();
+            _environment.Dispose();
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            base.PostRestart(reason);
+            // Load is only done on first start, not on restart
+            Become(Active);
+        }
+
+        private void Active()
+        {
+            Receive<Store>(store =>
+            {
+                var reply = store.Reply;
+
+                try
+                {
+                    if (_writeBehindInterval == TimeSpan.Zero)
+                    {
+                        DbPut(null, store.Key, store.Data);
+                    }
+                    else
+                    {
+                        if (_pending.Count > 0)
+                            Context.System.Scheduler.ScheduleTellOnce(_writeBehindInterval, Self, WriteBehind.Instance, ActorRefs.NoSender);
+                        _pending[store.Key] = store.Data;
+                    }
+
+                    reply?.ReplyTo.Tell(reply.SuccessMessage);
+                }
+                catch (Exception cause)
+                {
+                    _log.Error(cause, "Failed to store [{0}]", store.Key);
+                    reply?.ReplyTo.Tell(reply.FailureMessage);
+                }
+            });
+            Receive<WriteBehind>(_ => DoWriteBehind());
+        }
+
+        private void Init()
+        {
+            Receive<LoadAll>(loadAll =>
+            {
+                var t0 = System.DateTime.UtcNow;
+                using (var tx = _environment.BeginTransaction(TransactionBeginFlags.ReadOnly))
+                {
+                    LightningCursor cursor = null;
+                    try
+                    {
+                        var n = 0;
+                        var builder = ImmutableDictionary<string, IReplicatedData>.Empty.ToBuilder();
+                        cursor = tx.CreateCursor(_db);
+                        foreach (var entry in cursor)
+                        {
+                            n++;
+                            var key = Encoding.UTF8.GetString(entry.Key);
+                            var envelope = (DurableDataEnvelope)_serializer.FromBinary(entry.Value, typeof(DurableDataEnvelope));
+                            builder.Add(new KeyValuePair<string, IReplicatedData>(key, envelope.Data));
+                        }
+                        //TODO
+
+                        Sender.Tell(LoadAllCompleted.Instance);
+
+                        if (_log.IsDebugEnabled)
+                            _log.Debug("Load all of [{0}] entries took [{1}]", n, DateTime.UtcNow - t0);
+
+                        Become(Active);
+                    }
+                    catch (Exception e)
+                    {
+                        //TODO
+                    }
+                    finally
+                    {
+                        cursor?.Dispose();
+                    }
+                }
+            });
+        }
+
+        private void DbPut(LightningTransaction tx, string key, IReplicatedData data)
+        {
+            try
+            {
+                Encoding.UTF8.GetBytes(key, 0, key.Length, _keyBuffer.Array(), 0);
+                var value = _serializer.ToBinary(new DurableDataEnvelope(data));
+                _valueBuffer.Put(value);
+                tx.Put(_db,);
+            }
+            finally
+            {
+                
+            }
+        }
+
+        private void DoWriteBehind()
+        {
+            if (_pending.Count > 0)
+            {
+                var t0 = DateTime.UtcNow;
+                using (var tx = _environment.BeginTransaction())
+                {
+                    try
+                    {
+
+                    }
+                    catch (Exception cause)
+                    {
+
+                    }
+                    finally
+                    {
+                        _pending.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/contrib/cluster/Akka.DistributedData/Durable/Messages.cs
@@ -0,0 +1,124 @@
+﻿#region copyright
+// -----------------------------------------------------------------------
+//  <copyright file="Messages.cs" company="Akka.NET project">
+//      Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//      Copyright (C) 2013-2017 Akka.NET project <https://github.com/akkadotnet>
+//  </copyright>
+// -----------------------------------------------------------------------
+#endregion
+
+using System;
+using System.Collections.Immutable;
+using Akka.Actor;
+
+namespace Akka.DistributedData.Durable
+{
+    /// <summary>
+    /// Request to store an entry. It optionally contains a <see cref="StoreReply"/>, which
+    /// should be used to signal success or failure of the operation to the contained
+    /// <see cref="StoreReply.ReplyTo"/> actor.
+    /// </summary>
+    public sealed class Store
+    {
+        public readonly string Key;
+        public readonly IReplicatedData Data;
+        public readonly StoreReply Reply;
+
+        public Store(string key, IReplicatedData data, StoreReply reply = null)
+        {
+            Key = key;
+            Data = data;
+            Reply = reply;
+        }
+    }
+
+    public sealed class StoreReply
+    {
+        public readonly object SuccessMessage;
+        public readonly object FailureMessage;
+        public readonly IActorRef ReplyTo;
+
+        public StoreReply(object successMessage, object failureMessage, IActorRef replyTo)
+        {
+            SuccessMessage = successMessage;
+            FailureMessage = failureMessage;
+            ReplyTo = replyTo;
+        }
+    }
+
+    /// <summary>
+    /// Request to load all entries.
+    /// 
+    /// It must reply with 0 or more `LoadData` messages
+    /// followed by one `LoadAllCompleted` message to the `sender` (the `Replicator`).
+    /// 
+    /// If the `LoadAll` fails it can throw `LoadFailed` and the `Replicator` supervisor
+    /// will stop itself and the durable store.
+    /// </summary>
+    public sealed class LoadAll : IEquatable<LoadAll>
+    {
+        public static readonly LoadAll Instance = new LoadAll();
+        private LoadAll() { }
+        public bool Equals(LoadAll other) => true;
+        public override bool Equals(object obj) => obj is LoadAll;
+    }
+
+    public sealed class LoadData
+    {
+        public readonly ImmutableDictionary<string, IReplicatedData> Data;
+
+        public LoadData(ImmutableDictionary<string, IReplicatedData> data)
+        {
+            Data = data;
+        }
+    }
+
+    public sealed class LoadAllCompleted : IEquatable<LoadAllCompleted>
+    {
+        public static readonly LoadAllCompleted Instance = new LoadAllCompleted();
+        private LoadAllCompleted() { }
+        public bool Equals(LoadAllCompleted other) => true;
+        public override bool Equals(object obj) => obj is LoadAllCompleted;
+    }
+
+    public sealed class LoadFailed
+    {
+        public readonly string Message;
+        public readonly Exception Cause;
+
+        public LoadFailed(string message, Exception cause)
+        {
+            Message = message;
+            Cause = cause;
+        }
+    }
+
+    public sealed class DurableDataEnvelope : IReplicatorMessage, IEquatable<DurableDataEnvelope>
+    {
+        public readonly IReplicatedData Data;
+
+        public DurableDataEnvelope(IReplicatedData data)
+        {
+            Data = data;
+        }
+
+        public override int GetHashCode()
+        {
+            return Data.GetHashCode();
+        }
+
+        public bool Equals(DurableDataEnvelope other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(Data, other.Data);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is DurableDataEnvelope && Equals((DurableDataEnvelope) obj);
+        }
+    }
+}
