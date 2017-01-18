3e62e78c049a114bfd0574954902e905a880612f
Aaron Stannard <aaron@petabridge.com>
2017-01-18 15:12:03 -0800

    updatded DData to use SharedAssemblyInfo

--- src/contrib/cluster/Akka.DistributedData/Properties/AssemblyInfo.cs
+++ src/contrib/cluster/Akka.DistributedData/Properties/AssemblyInfo.cs
@@ -8,10 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Akka.DistributedData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Akka.DistributedData")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,17 +18,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f22db348-9ab3-4c6a-b1e8-9b835308d367")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("Akka.DistributedData.Tests")]
