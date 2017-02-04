80a3692a05437f185d2c06254eeb59de1db9b820
Sergey Kostrukov <sergey@kostrukov.com>
2017-02-04 20:54:30 +0800

    Cluster LeaveAsync() with CancellationToken

--- src/core/Akka/Util/Internal/TaskExtensions.cs
+++ src/core/Akka/Util/Internal/TaskExtensions.cs
@@ -6,12 +6,13 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Akka.Util.Internal
 {
     /// <summary>
-    /// TBD
+    /// Extension methods for operations on <see cref="Task"/> task types.
     /// </summary>
     public static class TaskExtensions
     {
@@ -47,6 +48,27 @@ namespace Akka.Util.Internal
                         }
                 }, TaskContinuationOptions.ExecuteSynchronously);
             return tcs.Task;
+        }
+
+        /// <summary>
+        /// Returns the task which completes with result of original task if cancellation token not canceled it before completion.
+        /// </summary>
+        /// <param name="task">The original task.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The task which completes with result of original task or with cancelled state.</returns>
+        public static Task WithCancellation(this Task task, CancellationToken cancellationToken)
+        {
+            if (task.IsCompleted || !cancellationToken.CanBeCanceled)
+                return task;
+
+            var tcs = new TaskCompletionSource<object>();
+            var r = cancellationToken.Register(() => { tcs.SetCanceled(); }, false);
+
+            return Task.WhenAny(task, tcs.Task)
+                // Dispose subscription to cancellation token
+                .ContinueWith(t => { r.Dispose(); }, TaskContinuationOptions.ExecuteSynchronously)
+                // Check cancellation, to return task in cancelled state instead of completed
+                .ContinueWith(t => { }, cancellationToken, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
         }
     }
 }
