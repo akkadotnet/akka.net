ac2351cd3b2adf6f870082914ea4c06846dc69dd
Ismael Hamed <ismael.hamed@outlook.com>
2017-03-05 13:59:59 +0100

    Replaced async/await with a continuation task in order to fix mono build

--- src/contrib/cluster/Akka.Cluster.Sharding.Tests/ConstantRateEntityRecoveryStrategySpec.cs
+++ src/contrib/cluster/Akka.Cluster.Sharding.Tests/ConstantRateEntityRecoveryStrategySpec.cs
@@ -33,7 +33,7 @@ namespace Akka.Cluster.Sharding.Tests
             var entities = ImmutableHashSet.Create<EntityId>("1", "2", "3", "4", "5");
             var startTime = DateTime.UtcNow;
             var resultWithTimes = strategy.RecoverEntities(entities)
-                .Select(async bucketTask => new KeyValuePair<IImmutableSet<string>, TimeSpan>(await bucketTask, DateTime.UtcNow - startTime))
+                .Select(scheduledRecovery => scheduledRecovery.ContinueWith(t => new KeyValuePair<IImmutableSet<string>, TimeSpan>(t.Result, DateTime.UtcNow - startTime)))
                 .ToArray();
 
             var result = Task.WhenAll(resultWithTimes).Result.OrderBy(pair => pair.Value).ToArray();
