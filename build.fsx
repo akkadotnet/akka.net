69069245731f3967eb96afd9d8fcfc9267335a90
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-03-14 16:01:44 -0400

    increase timeout for NBench target in build.fsx

--- build.fsx
+++ build.fsx
@@ -365,7 +365,7 @@ Target "NBench" <| fun _ ->
         let result = ExecProcess(fun info -> 
             info.FileName <- nbenchTestPath
             info.WorkingDirectory <- (Path.GetDirectoryName (FullName nbenchTestPath))
-            info.Arguments <- args) (System.TimeSpan.FromMinutes 25.0) (* Reasonably long-running task. *)
+            info.Arguments <- args) (System.TimeSpan.FromMinutes 45.0) (* Reasonably long-running task. *)
         if result <> 0 then failwithf "NBench.Runner failed. %s %s" nbenchTestPath args
     
     nbenchTestAssemblies |> Seq.iter (runNBench)
