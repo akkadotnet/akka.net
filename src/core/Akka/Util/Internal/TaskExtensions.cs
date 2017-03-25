69baba62956b555795edf6aad29fc924c6ddaa0a
Sergey Kostrukov <sergey@kostrukov.com>
2017-03-24 20:15:59 -0500

    Cluster LeaveAsync() with CancellationToken support (#2501)
    * Cluster LeaveAsync() with CancellationToken
    
    * LeaveAsync() doesn't trigger another leave after Leave()
    
    * APi changes

--- src/core/Akka/Util/Internal/TaskExtensions.cs
+++ src/core/Akka/Util/Internal/TaskExtensions.cs
@@ -6,13 +6,14 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Akka.Util.Internal
 {
     /// <summary>
     /// INTERNAL API
-    /// 
+    ///
     /// Extensions for working with <see cref="Task"/> types
     /// </summary>
     public static class TaskExtensions
@@ -49,6 +50,27 @@ namespace Akka.Util.Internal
                         }
                 }, TaskContinuationOptions.ExecuteSynchronously);
             return tcs.Task;
+        }
+
+        /// <summary>
+        /// Returns the task which completes with result of original task if cancellation token not canceled it before completion.
+        /// </summary>
+        /// <param name="task">The original task.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The task which completes with result of original task or with cancelled state.</returns>
+        public static Task WithCancellation(this Task task, CancellationToken cancellationToken)
+        {
+            if (task.IsCompleted || !cancellationToken.CanBeCanceled)
+                return task;
+
+            var tcs = new TaskCompletionSource<object>();
+            var r = cancellationToken.Register(() => { tcs.SetCanceled(); }, false);
+
+            return Task.WhenAny(task, tcs.Task)
+                // Dispose subscription to cancellation token
+                .ContinueWith(t => { r.Dispose(); }, TaskContinuationOptions.ExecuteSynchronously)
+                // Check cancellation, to return task in cancelled state instead of completed
+                .ContinueWith(t => { }, cancellationToken, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
         }
     }
 }
