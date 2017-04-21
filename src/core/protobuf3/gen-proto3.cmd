e8a68796fd906a645d99f872a639843549417092
ravengerUA <yurisikorsky@gmail.com>
2017-04-21 07:48:34 +0300

    remote protobuf3 folder

--- src/core/protobuf3/gen-proto3.cmd
+++
@@ -1,4 +0,0 @@
-rem get binaries from https://github.com/google/protobuf/releases e.g. protoc-3.2.0-win32.zip for windows
-
-protoc.exe --csharp_out ..\Akka.Remote\Proto\ ContainerFormats.proto
-protoc.exe --csharp_out ..\Akka.Remote\Proto\ WireFormats.proto 
