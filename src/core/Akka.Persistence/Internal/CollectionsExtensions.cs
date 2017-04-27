e6745521333cc8a68c1dca79c47995f3f97e88a0
ravengerUA <yurisikorsky@gmail.com>
2017-04-27 21:39:53 +0300

    Removed racy conditions

---
+++ src/core/Akka.Persistence/Internal/CollectionsExtensions.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CollectionsExtensions.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Immutable;
+
+namespace Akka.Persistence.Internal
+{
+    internal static class CollectionsExtensions
+    {
+        public static ImmutableArray<T> AddFirst<T>(this ImmutableArray<T> @this, T item)
+        {
+            var builder = ImmutableArray.CreateBuilder<T>();
+            builder.Add(item);
+            builder.AddRange(@this);
+            return builder.ToImmutable();
+        }
+    }
+}
