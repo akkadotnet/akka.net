1194349e2eec3b99da6fb55e972c19b3ab6b7558
Marc Piechura <piechura@gmx.net>
2017-05-07 12:08:02 +0200

    fix copyright header

--- src/core/Akka.Streams/IMaterializerLoggingProvider.cs
+++ src/core/Akka.Streams/IMaterializerLoggingProvider.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="IMaterializer.cs" company="Akka.NET Project">
+// <copyright file="IMaterializerLoggingProvider.cs" company="Akka.NET Project">
 //     Copyright (C) 2015-2017 Lightbend Inc. <http://www.lightbend.com>
 //     Copyright (C) 2013-2017 Akka.NET project <https://github.com/akkadotnet/akka.net>
 // </copyright>
