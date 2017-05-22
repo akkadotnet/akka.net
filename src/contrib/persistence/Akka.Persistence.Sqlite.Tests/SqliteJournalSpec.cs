04fe76e9a848cc9544a6957dbcfe277dd9df0d15
Alex Valuyskiy <alexvaluyskiy@users.noreply.github.com>
2017-05-22 12:42:02 +0300

    Added NetStandard1.6 support for Sqlite plugin (#2681)
    * Sqlite support for NetStandard
    
    * update Microsoft.Data.SQLite
    
    * Fix sqlite in the examples

--- src/contrib/persistence/Akka.Persistence.Sqlite.Tests/SqliteJournalSpec.cs
+++ src/contrib/persistence/Akka.Persistence.Sqlite.Tests/SqliteJournalSpec.cs
@@ -17,7 +17,7 @@ namespace Akka.Persistence.Sqlite.Tests
         private static AtomicCounter counter = new AtomicCounter(0);
 
         public SqliteJournalSpec(ITestOutputHelper output)
-            : base(CreateSpecConfig("FullUri=file:memdb-journal-" + counter.IncrementAndGet() + ".db?mode=memory&cache=shared;"), "SqliteJournalSpec", output)
+            : base(CreateSpecConfig("Filename=file:memdb-journal-" + counter.IncrementAndGet() + ".db;Mode=Memory;Cache=Shared"), "SqliteJournalSpec", output)
         {
             SqlitePersistence.Get(Sys);
 
