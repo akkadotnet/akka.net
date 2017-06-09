2f367cedda3953a0e1f4c93d7b853fee2427c311
ravengerUA <yurisikorsky@gmail.com>
2017-06-09 23:41:38 +0300

    Fix racing conditions

--- src/contrib/persistence/Akka.Persistence.Sqlite.Tests/Batching/BatchingSqliteEventsByTagSpec.cs
+++ src/contrib/persistence/Akka.Persistence.Sqlite.Tests/Batching/BatchingSqliteEventsByTagSpec.cs
@@ -16,13 +16,13 @@ namespace Akka.Persistence.Sqlite.Tests.Batching
 {
     public class BatchingSqliteEventsByTagSpec : EventsByTagSpec
     {
-        public static readonly AtomicCounter Counter = new AtomicCounter(200);
+        public static readonly AtomicCounter Counter = new AtomicCounter(0);
         public static Config Config(int id) => ConfigurationFactory.ParseString($@"
             akka.loglevel = INFO
             akka.persistence.journal.plugin = ""akka.persistence.journal.sqlite""
             akka.persistence.journal.sqlite {{
                 event-adapters {{
-                  color-tagger  = ""Akka.Persistence.Sql.TestKit.ColorTagger, Akka.Persistence.Sql.TestKit""
+                  color-tagger  = ""Akka.Persistence.TCK.Query.ColorFruitTagger, Akka.Persistence.TCK""
                 }}
                 event-adapter-bindings = {{
                   ""System.String"" = color-tagger
@@ -32,7 +32,7 @@ namespace Akka.Persistence.Sqlite.Tests.Batching
                 table-name = event_journal
                 metadata-table-name = journal_metadata
                 auto-initialize = on
-                connection-string = ""Datasource=memdb-journal-batch-{id}.db;Mode=Memory;Cache=Shared""
+                connection-string = ""Datasource=memdb-journal-batch-eventsbytag-{id}.db;Mode=Memory;Cache=Shared""
                 refresh-interval = 1s
             }}
             akka.test.single-expect-default = 10s")
