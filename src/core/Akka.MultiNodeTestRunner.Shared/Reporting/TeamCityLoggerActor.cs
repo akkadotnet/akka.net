41470ff0ff621afc7f62b7f99971184f72125185
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-06-14 10:01:43 -0400

    Add TeamCity formatting option to MultiNode Test Runner (#2720)
    * wip
    
    * parse teamcity flag
    
    * teamcity flag
    
    * use TeamCity.ServiceMessages to format teamCity output when passed flag
    
    * class name instead of file path
    
    * shift process exit into  test scope
    
    * building actors
    
    * heirarchy for teamcity actors
    
    * removing teamcity code form program.cs
    
    * removed teamcity code in Program.cs
    
    * extracted teamcity code into a sink

---
+++ src/core/Akka.MultiNodeTestRunner.Shared/Reporting/TeamCityLoggerActor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace Akka.MultiNodeTestRunner.Shared.Reporting
+{
+    public class TeamCityLoggerActor : ReceiveActor
+    {
+        private readonly bool _unMuted = false;
+        public TeamCityLoggerActor(bool unMuted)
+        {
+            _unMuted = unMuted;
+            
+            ReceiveAny(o =>
+            {
+                if (_unMuted)
+                {
+                    Console.WriteLine(o.ToString());
+                }
+            });
+        }
+    }
+}
