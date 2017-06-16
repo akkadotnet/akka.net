0d40f343c9c656dd3b256cbe04f4dc165f6431a8
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-06-16 08:30:14 -0400

    started a spec for encoding/decoding

---
+++ src/core/Akka.Remote.TestKit.Tests/MsgEncoderSpec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetty.Transport.Channels.Embedded;
+using Xunit;
+
+namespace Akka.Remote.TestKit.Tests
+{
+    public class MsgEncoderSpec
+    {
+
+    }
+}
