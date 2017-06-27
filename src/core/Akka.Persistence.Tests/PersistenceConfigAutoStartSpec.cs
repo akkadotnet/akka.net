537dbd42e844b24b6034eb10fe5bcceb8bcb2eb2
Aaron Stannard <aaron@petabridge.com>
2017-06-27 11:33:22 -0500

    added copyright header

--- src/core/Akka.Persistence.Tests/PersistenceConfigAutoStartSpec.cs
+++ src/core/Akka.Persistence.Tests/PersistenceConfigAutoStartSpec.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//-----------------------------------------------------------------------
+// <copyright file="PersistenceConfigAutoStartSpec.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
 using Akka.Actor;
 using Akka.Configuration;
 using Akka.Persistence.Journal;
