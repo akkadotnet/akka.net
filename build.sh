185d6421e66a333c45f36d06f7998f766db0054f
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-06-28 10:46:30 -0400

    lock nuget down to v4.1.0

--- build.sh
+++ build.sh
@@ -10,11 +10,8 @@ popd  > /dev/null
 
 if ! [ -f $SCRIPT_PATH/src/.nuget/nuget.exe ] 
     then
-        wget "https://www.nuget.org/nuget.exe" -P $SCRIPT_PATH/src/.nuget/
+        wget "https://dist.nuget.org/win-x86-commandline/v4.1.0/nuget.exe" -P $SCRIPT_PATH/src/.nuget/
 fi
-
-mono $SCRIPT_PATH/src/.nuget/nuget.exe update -self
-
 
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 if ([ -h "${SCRIPT_PATH}" ]) then
@@ -24,8 +21,6 @@ pushd . > /dev/null
 cd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd`;
 popd  > /dev/null
-
-mono $SCRIPT_PATH/src/.nuget/nuget.exe update -self
 
 mono $SCRIPT_PATH/src/.nuget/nuget.exe install FAKE -OutputDirectory $SCRIPT_PATH/src/packages -ExcludeVersion -Version 4.61.3
 
