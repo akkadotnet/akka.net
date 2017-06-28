f9838d51522aa83f2005ddc7f4932502d54e3171
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-06-28 11:58:31 -0400

    commit NuGet.exe v4.1.0

--- build.cmd
+++ build.cmd
@@ -8,7 +8,7 @@ SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
 IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/v4.1.0/nuget.exe' -OutFile '%CACHED_NUGET%'"
 
 :copynuget
 IF EXIST src\.nuget\nuget.exe goto restore
@@ -18,8 +18,6 @@ copy %CACHED_NUGET% src\.nuget\nuget.exe > nul
 :restore
 
 pushd %~dp0
-
-src\.nuget\NuGet.exe update -self
 
 src\.nuget\NuGet.exe install FAKE -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages -ExcludeVersion -Version 4.16.1
 
