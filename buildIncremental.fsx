16886ddbb6ea89cb5079562672b21d146763561c
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-07-06 09:19:18 -0400

    remove tracking part of checkout -b

--- buildIncremental.fsx
+++ buildIncremental.fsx
@@ -47,7 +47,7 @@ module IncrementalTests =
         let srcDir = __SOURCE_DIRECTORY__
         let localBranches = getLocalBranches srcDir
         if not (localBranches |> Seq.exists (fun b -> b = "v1.3")) then
-            checkoutTracked srcDir "v1.3" "origin/v1.3"
+            checkoutNewBranch srcDir "v1.3" "origin/v1.3"
         let forkPoint = runSimpleGitCommand srcDir "merge-base --fork-point v1.3"
         let currentHash = getCurrentHash()
         getChangedFiles srcDir forkPoint currentHash
