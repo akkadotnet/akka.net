edddec26b3cd244c3c16bf9b280e2e2c825127a6
alexvaluyskiy <yurisikorsky@gmail.com>
2017-07-14 17:12:13 +0300

    refactor offsets

--- src/core/Akka.Persistence.Query/Offset.cs
+++ src/core/Akka.Persistence.Query/Offset.cs
@@ -25,16 +25,6 @@ namespace Akka.Persistence.Query
         /// as the `offset` parameter in a subsequent query.
         /// </summary>
         public static Offset Sequence(long value) => new Sequence(value);
-
-        /// <summary>
-        /// Corresponds to an ordered unique identifier of the events. Note that the corresponding
-        /// offset of each event is provided in the <see cref="EventEnvelope"/>,
-        /// which makes it possible to resume the stream at a later point from a given offset.
-        /// The `offset` is exclusive, i.e.the event with the exact same sequence number will not be included
-        /// in the returned stream.This means that you can use the offset that is returned in <see cref="EventEnvelope"/>
-        /// as the `offset` parameter in a subsequent query.
-        /// </summary>
-        public static Offset TimeBasedGuid(Guid value) => new TimeBasedGuid(value);
     }
 
     /// <summary>
@@ -45,7 +35,7 @@ namespace Akka.Persistence.Query
     /// in the returned stream. This means that you can use the offset that is returned in <see cref="EventEnvelope"/>
     /// as the `offset` parameter in a subsequent query.
     /// </summary>
-    public sealed class Sequence : Offset
+    public sealed class Sequence : Offset, IComparable<Sequence>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Sequence"/> class.
@@ -57,6 +47,8 @@ namespace Akka.Persistence.Query
 
         public long Value { get; }
 
+        public int CompareTo(Sequence other) => Value.CompareTo(other.Value);
+
         private bool Equals(Sequence other) => Value == other.Value;
 
         public override bool Equals(object obj)
@@ -64,38 +56,6 @@ namespace Akka.Persistence.Query
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
             return obj is Sequence && Equals((Sequence)obj);
-        }
-
-        public override int GetHashCode() => Value.GetHashCode();
-    }
-
-    /// <summary>
-    /// Corresponds to an ordered unique identifier of the events. Note that the corresponding
-    /// offset of each event is provided in the <see cref="EventEnvelope"/>,
-    /// which makes it possible to resume the stream at a later point from a given offset.
-    /// The `offset` is exclusive, i.e.the event with the exact same sequence number will not be included
-    /// in the returned stream.This means that you can use the offset that is returned in <see cref="EventEnvelope"/>
-    /// as the `offset` parameter in a subsequent query.
-    /// </summary>
-    public sealed class TimeBasedGuid : Offset
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TimeBasedGuid"/> class.
-        /// </summary>
-        public TimeBasedGuid(Guid value)
-        {
-            Value = value;
-        }
-
-        public Guid Value { get; }
-
-        private bool Equals(TimeBasedGuid other) => Value.Equals(other.Value);
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            return obj is TimeBasedGuid && Equals((TimeBasedGuid)obj);
         }
 
         public override int GetHashCode() => Value.GetHashCode();
