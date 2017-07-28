0004d950e81d366ee42c4d5e7112436edc5a91bb
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-07-28 10:22:36 -0400

    add environment vars to disable startup experience with dotnetcli

--- build.ps1
+++ build.ps1
@@ -72,6 +72,8 @@ Function Remove-PathVariable([string]$VariableToRemove)
 $FoundDotNetCliVersion = $null;
 if (Get-Command dotnet -ErrorAction SilentlyContinue) {
     $FoundDotNetCliVersion = dotnet --version;
+	$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+    $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
 }
 
 if($FoundDotNetCliVersion -ne $DotNetVersion) {
