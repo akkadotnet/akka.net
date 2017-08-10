9ae472f3a5541b90069dcbe76282d57feeecf229
zbynek001 <zbynek001@gmail.com>
2017-08-10 16:48:04 +0200

    default sharding configs added to tests

--- src/contrib/cluster/Akka.Cluster.Sharding.Tests.MultiNode/ClusterShardingFailureSpec.cs
+++ src/contrib/cluster/Akka.Cluster.Sharding.Tests.MultiNode/ClusterShardingFailureSpec.cs
@@ -66,6 +66,8 @@ namespace Akka.Cluster.Sharding.Tests
                         timeout = 5s
                     }
                 "))
+                .WithFallback(Sharding.ClusterSharding.DefaultConfig())
+                .WithFallback(Tools.Singleton.ClusterSingletonManager.DefaultConfig())
                 .WithFallback(MultiNodeClusterSpec.ClusterConfig());
 
             TestTransport = true;
