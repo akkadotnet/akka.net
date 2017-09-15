ff312a2d541355f119988410af3107ec50fea62c
ravengerUA <yurisikorsky@gmail.com>
2017-09-15 19:48:17 +0300

    More tests and docs

--- docs/articles/streams/error-handling.md
+++ docs/articles/streams/error-handling.md
@@ -10,6 +10,7 @@ In many cases you may want to avoid complete stream failure, this can be done in
 
 - `Recover` to emit a final element then complete the stream normally on upstream failure
 - `RecoverWithRetries` to create a new upstream and start consuming from that on failure
+- Restarting sections of the stream after a backoff
 - Using a supervision strategy for stages that support it
 
 In addition to these built in tools for error handling, a common pattern is to wrap the stream inside an actor, and have the actor restart the entire stream on failure.
@@ -82,6 +83,46 @@ six
 seven
 eight
 ```
+
+## Delayed restarts with a backoff stage
+
+Just as Akka provides the [backoff supervision pattern for actors](xref:supervision#delayed-restarts-with-the-backoffsupervisor-pattern), Akka streams
+also provides a `RestartSource`, `RestartSink` and `RestartFlow` for implementing the so-called *exponential backoff 
+supervision strategy*, starting a stage again when it fails, each time with a growing time delay between restarts.
+
+This pattern is useful when the stage fails or completes because some external resource is not available
+and we need to give it some time to start-up again. One of the prime examples when this is useful is
+when a WebSocket connection fails due to the HTTP server it's running on going down, perhaps because it is overloaded. 
+By using an exponential backoff, we avoid going into a tight reconnect look, which both gives the HTTP server some time
+to recover, and it avoids using needless resources on the client side.
+
+The following snippet shows how to create a backoff supervisor using `Akka.Streams.Dsl.RestartSource` 
+which will supervise the given `Source`. The `Source` in this case is a 
+stream of Server Sent Events, produced by akka-http. If the stream fails or completes at any point, the request will
+be made again, in increasing intervals of 3, 6, 12, 24 and finally 30 seconds (at which point it will remain capped due
+to the `maxBackoff` parameter):
+
+```C#
+// TODO
+```
+
+Using a `randomFactor` to add a little bit of additional variance to the backoff intervals
+is highly recommended, in order to avoid multiple streams re-start at the exact same point in time,
+for example because they were stopped due to a shared resource such as the same server going down
+and re-starting after the same configured interval. By adding additional randomness to the
+re-start intervals the streams will start in slightly different points in time, thus avoiding
+large spikes of traffic hitting the recovering server or other resource that they all need to contact.
+
+The above `RestartSource` will never terminate unless the `Sink` it's fed into cancels. It will often be handy to use
+it in combination with a `KillSwitch`, so that you can terminate it when needed:
+
+```C#
+// TODO
+```
+
+Sinks and flows can also be supervised, using `Akka.Streams.Dsl.RestartSink` and `Akka.Streams.Dsl.RestartFlow`].
+The `RestartSink` is restarted when it cancels, while the `RestartFlow` is restarted when either the in port cancels,
+the out port completes, or the out port sends an error.
 
 ## Supervision Strategies
 
--- src/core/Akka.Streams.Tests/Dsl/RestartSpec.cs
+++ src/core/Akka.Streams.Tests/Dsl/RestartSpec.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,6 +21,10 @@ namespace Akka.Streams.Tests.Dsl
         {
             Materializer = Sys.Materializer();
         }
+
+        //
+        // Source
+        //
 
         [Fact]
         public void A_restart_with_backoff_source_should_run_normally()
@@ -191,6 +196,181 @@ namespace Akka.Streams.Tests.Dsl
             // Wait to ensure it isn't restarted
             Thread.Sleep(300);
             created.Current.Should().Be(1);
+        }
+
+        //
+        // Sink
+        //
+
+        [Fact]
+        public void A_restart_with_backoff_sink_should_run_normally()
+        {
+            var created = new AtomicCounter(0);
+            var tcs = new TaskCompletionSource<IEnumerable<string>>();
+            var probe = this.SourceProbe<string>().ToMaterialized(RestartSink.WithBackoff(() =>
+            {
+                created.IncrementAndGet();
+                return default(Sink<string, NotUsed>);
+                //return Sink.Seq<string>().MapMaterializedValue(task => tcs.TrySetResult(task.Result));
+            }, TimeSpan.FromMilliseconds(10), TimeSpan.FromMilliseconds(20), 0), Keep.Left).Run(Materializer);
+
+            probe.SendNext("a");
+            probe.SendNext("b");
+            probe.SendNext("c");
+            probe.SendComplete();
+
+            tcs.Task.Result.Should().ContainInOrder("a", "b", "c");
+            created.Current.Should().Be(1);
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_sink_should_backoff_before_restart()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_sink_should_reset_exponential_backoff_back_to_minimum_when_source_runs_for_at_least_minimum_backoff_without_completing()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_sink_should_not_restart_the_sink_when_completed_while_backing_off()
+        {
+            // TODO
+        }
+
+        //
+        // Flow
+        //
+
+        private Tuple<AtomicCounter, TestPublisher.Probe<string>, TestSubscriber.Probe<string>, TestPublisher.Probe<string>, TestSubscriber.Probe<string>> SetupFlow(TimeSpan minBackoff, TimeSpan maxBackoff)
+        {
+            var created = new AtomicCounter(0);
+            var probe1 = this.SourceProbe<string>().ToMaterialized(this.SinkProbe<string>(), Keep.Both).Run(Materializer);
+            var flowInSource = probe1.Item1;
+            var flowInProbe = probe1.Item2;
+            var probe2 = this.SourceProbe<string>().ToMaterialized(BroadcastHub.Sink<string>(), Keep.Both).Run(Materializer);
+            var flowOutProbe = probe2.Item1;
+            var flowOutSource = probe2.Item2;
+
+            var probe3 = this.SourceProbe<string>().ViaMaterialized(RestartFlow.WithBackoff(() =>
+                {
+                    created.IncrementAndGet();
+                    var snk = Flow.Create<string>().TakeWhile(s =>
+                    {
+                        return s != "cancel";
+                    }).To(Sink.ForEach<string>(c => flowInSource.SendNext(c))
+                        .MapMaterializedValue(task => task.ContinueWith(
+                            t1 =>
+                            {
+                                if (!t1.IsFaulted || !t1.IsCanceled)
+                                    flowInSource.SendNext("in error");
+                                else
+                                    flowInSource.SendNext("in complete");
+                            })));
+
+                    var src = flowOutSource.TakeWhile(s =>
+                    {
+                        return s != "complete";
+                    }).Select(c =>
+                    {
+                        if (c == "error")
+                            throw new ArgumentException("failed");
+                        return c;
+                    }).WatchTermination((s1, task) =>
+                    {
+                        flowInSource.SendNext("out complete");
+                        return s1;
+                    });
+
+                    return Flow.FromSinkAndSource(snk, src);
+                }, minBackoff, maxBackoff, 0), Keep.Left)
+                .ToMaterialized(this.SinkProbe<string>(), Keep.Both).Run(Materializer);
+            var source = probe3.Item1;
+            var sink = probe3.Item2;
+
+            return Tuple.Create(created, source, flowInProbe, flowOutProbe, sink);
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_flow_should_run_normally()
+        {
+            var created = new AtomicCounter(0);
+            var tuple = this.SourceProbe<string>().ViaMaterialized(RestartFlow.WithBackoff(() =>
+            {
+                created.IncrementAndGet();
+                return Flow.Create<string>();;
+            }, TimeSpan.FromMilliseconds(10), TimeSpan.FromMilliseconds(20), 0), Keep.Left).ToMaterialized(this.SinkProbe<string>(), Keep.Both).Run(Materializer);
+            var source = tuple.Item1;
+            var sink = tuple.Item2;
+
+            source.SendNext("a");
+            sink.RequestNext("a");
+            source.SendNext("b");
+            sink.RequestNext("b");
+
+            created.Current.Should().Be(1);
+            source.SendComplete();
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_flow_should_restart_on_cancellation()
+        {
+            var tuple = SetupFlow(TimeSpan.FromMilliseconds(10), TimeSpan.FromMilliseconds(20));
+            var created = tuple.Item1;
+            var source = tuple.Item2;
+            var flowInProbe = tuple.Item3;
+            var flowOutProbe = tuple.Item4;
+            var sink = tuple.Item5;
+
+            source.SendNext("a");
+            flowInProbe.RequestNext("a");
+            flowOutProbe.SendNext("b");
+            sink.RequestNext("b");
+
+            source.SendNext("cancel");
+            flowInProbe.Request(2);
+            ImmutableList.Create(flowInProbe.ExpectNext(), flowInProbe.ExpectNext()).Should().Contain(ImmutableList.Create("in complete", "out complete"));
+
+            // and it should restart
+            source.SendNext("c");
+            flowInProbe.RequestNext("c");
+            flowOutProbe.SendNext("d");
+            sink.RequestNext("d");
+
+            created.Current.Should().Be(2);
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_flow_should_restart_on_completion()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_flow_should_restart_on_failure()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_flow_should_backoff_before_restart()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_flow_should_continue_running_flow_out_port_after_in_has_been_sent_completion()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void A_restart_with_backoff_flow_should_continue_running_flow_in_port_after_out_has_been_cancelled()
+        {
+            // TODO
         }
     }
 }
