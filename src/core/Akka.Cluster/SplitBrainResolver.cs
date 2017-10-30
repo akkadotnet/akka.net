218d38f0a65cdd2ecf83b76e44e48e83390559f4
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2017-10-30 21:23:28 +0100

    initial implementation of split brain resolver

--- src/core/Akka.Cluster/SplitBrainResolver.cs
+++ src/core/Akka.Cluster/SplitBrainResolver.cs
@@ -6,8 +6,12 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using Akka.Actor;
 using Akka.Configuration;
+using Akka.Event;
 
 namespace Akka.Cluster
 {
@@ -26,25 +30,112 @@ namespace Akka.Cluster
         public Props DowningActorProps => SplitBrainStrategy.Props(_splitBrainResolverConfig);
     }
 
-    internal abstract class SplitBrainStrategy : ActorBase
+    internal abstract class SplitBrainStrategy : UntypedActor
     {
+        #region internal classes
+
+        protected sealed class StabilityReached
+        {
+            public static readonly StabilityReached Instance = new StabilityReached();
+            private StabilityReached() { }
+        }
+
+        #endregion
+
         public static Actor.Props Props(Config config)
         {
+            var stabilityTimeout = config.GetTimeSpan("stable-after");
             var activeStrategy = config.GetString("active-strategy");
             switch (activeStrategy)
             {
-                case "static-quorum": return Actor.Props.Create(() => new StaticQuorum(StaticQuorum.Settings.Create(config.GetConfig("static-quorum")))).WithDeploy(Deploy.Local);
-                case "keep-majority": return Actor.Props.Create(() => new KeepMajority(KeepMajority.Settings.Create(config.GetConfig("keep-majority")))).WithDeploy(Deploy.Local);
-                case "keep-oldest": return Actor.Props.Create(() => new KeepOldest(KeepOldest.Settings.Create(config.GetConfig("keep-oldest")))).WithDeploy(Deploy.Local);
-                case "keep-referee": return Actor.Props.Create(() => new KeepReferee(KeepReferee.Settings.Create(config.GetConfig("keep-referee")))).WithDeploy(Deploy.Local);
+                case "static-quorum": return Actor.Props.Create(() => new StaticQuorum(config.GetConfig("static-quorum"), stabilityTimeout)).WithDeploy(Deploy.Local);
+                case "keep-majority": return Actor.Props.Create(() => new KeepMajority(config.GetConfig("keep-majority"), stabilityTimeout)).WithDeploy(Deploy.Local);
+                case "keep-oldest": return Actor.Props.Create(() => new KeepOldest(config.GetConfig("keep-oldest"), stabilityTimeout)).WithDeploy(Deploy.Local);
+                case "keep-referee": return Actor.Props.Create(() => new KeepReferee(config.GetConfig("keep-referee"), stabilityTimeout)).WithDeploy(Deploy.Local);
                 default: throw new ConfigurationException($"Unrecognized value [{activeStrategy}] of `akka.cluster.split-brain-resolver.active-strategy`. Supported options are: static-quorum | keep-majority | keep-oldest | keep-referee.");
             }
         }
 
-        protected override bool Receive(object message)
+        protected readonly Cluster Cluster;
+        protected readonly TimeSpan StabilityTimeout;
+
+        private ImmutableSortedSet<Member> _reachable = ImmutableSortedSet<Member>.Empty;
+        private ImmutableSortedSet<Member> _unreachable = ImmutableSortedSet<Member>.Empty;
+        private ICancelable _stabilityTask;
+        private ILoggingAdapter _log;
+
+        protected SplitBrainStrategy(TimeSpan stabilityTimeout)
         {
-            throw new NotImplementedException();
+            StabilityTimeout = stabilityTimeout;
+            Cluster = Cluster.Get(Context.System);
         }
+
+        protected ILoggingAdapter Log => _log ?? (_log = Context.GetLogger());
+
+        protected abstract string Role { get; }
+        protected abstract void ApplyStrategy(ImmutableSortedSet<Member> reachable, ImmutableSortedSet<Member> unreachable);
+
+        protected override void PreStart()
+        {
+            base.PreStart();
+            Cluster.Subscribe(Self, typeof(ClusterEvent.IMemberEvent), typeof(ClusterEvent.IReachabilityEvent));
+        }
+
+        protected override void PostStop()
+        {
+            Cluster.Unsubscribe(Self);
+            base.PostStop();
+        }
+
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case ClusterEvent.CurrentClusterState state:
+                    ResetStabilityTimeout();
+                    _reachable = state.Members.Where(m => m.Status == MemberStatus.Up && HasRole(m)).ToImmutableSortedSet(Member.AgeOrdering);
+                    _unreachable = state.Unreachable.Where(HasRole).ToImmutableSortedSet(Member.AgeOrdering);
+                    return;
+                case ClusterEvent.IMemberEvent memberEvent:
+                    ResetStabilityTimeout();
+                    switch (memberEvent)
+                    {
+                        case ClusterEvent.MemberUp up when HasRole(up.Member):
+                            _reachable = _reachable.Add(up.Member);
+                            break;
+                        case ClusterEvent.MemberRemoved removed when HasRole(removed.Member):
+                            _reachable = _reachable.Remove(removed.Member);
+                            _unreachable = _unreachable.Remove(removed.Member);
+                            break;
+                    }
+                    return;
+                case ClusterEvent.IReachabilityEvent reachabilityEvent:
+                    ResetStabilityTimeout();
+                    switch (reachabilityEvent)
+                    {
+                        case ClusterEvent.ReachableMember reachable when HasRole(reachable.Member):
+                            _reachable = _reachable.Add(reachable.Member);
+                            _unreachable = _unreachable.Remove(reachable.Member);
+                            break;
+                        case ClusterEvent.UnreachableMember unreachable when HasRole(unreachable.Member):
+                            _reachable = _reachable.Remove(unreachable.Member);
+                            _unreachable = _unreachable.Add(unreachable.Member);
+                            break;
+                    }
+                    return;
+                case StabilityReached _:
+                    ApplyStrategy(_reachable, _unreachable);
+                    return;
+            }
+        }
+
+        private void ResetStabilityTimeout()
+        {
+            _stabilityTask?.Cancel();
+            _stabilityTask = Context.System.Scheduler.ScheduleTellOnceCancelable(StabilityTimeout, Self, StabilityReached.Instance, ActorRefs.NoSender);
+        }
+
+        private bool HasRole(Member member) => string.IsNullOrEmpty(Role) || member.HasRole(Role);
     }
 
     internal sealed class StaticQuorum : SplitBrainStrategy
@@ -81,9 +172,25 @@ namespace Akka.Cluster
 
         #endregion
 
-        public StaticQuorum(Settings settings)
+        private readonly Settings _settings;
+        protected override string Role => _settings.Role;
+
+        public StaticQuorum(Settings settings, TimeSpan stabilityTimeout) : base(stabilityTimeout)
         {
-            throw new NotImplementedException();
+            _settings = settings;
+        }
+
+        public StaticQuorum(Config config, TimeSpan stabilityTimeout) : this(Settings.Create(config), stabilityTimeout)
+        {
+        }
+
+        protected override void ApplyStrategy(ImmutableSortedSet<Member> reachable, ImmutableSortedSet<Member> unreachable)
+        {
+            if (reachable.Count < _settings.QuorumSize)
+            {
+                Log.Info("Static quorum failed - only {0} of min. {1} of expected nodes were reachable. Shutting down cluster node.", reachable.Count, _settings.QuorumSize);
+                Cluster.Down(Cluster.SelfAddress);
+            }
         }
     }
 
@@ -114,9 +221,25 @@ namespace Akka.Cluster
 
         #endregion
 
-        public KeepMajority(Settings settings)
+        private readonly Settings _settings;
+        protected override string Role => _settings.Role;
+
+        public KeepMajority(Settings settings, TimeSpan stabilityTimeout) : base(stabilityTimeout)
         {
-            throw new NotImplementedException();
+            _settings = settings;
+        }
+
+        public KeepMajority(Config config, TimeSpan stabilityTimeout) : this(Settings.Create(config), stabilityTimeout)
+        {
+        }
+
+        protected override void ApplyStrategy(ImmutableSortedSet<Member> reachable, ImmutableSortedSet<Member> unreachable)
+        {
+            if (reachable.Count < unreachable.Count)
+            {
+                Log.Info("Failed to reach majority of the nodes. Reachable members: {0}. Unreachable members: {1}. Shutting down cluster node.", reachable.Count, unreachable.Count);
+                Cluster.Down(Cluster.SelfAddress);
+            }
         }
     }
 
@@ -155,9 +278,41 @@ namespace Akka.Cluster
 
         #endregion
 
-        public KeepOldest(Settings settings)
+        private readonly Settings _settings;
+        protected override string Role => _settings.Role;
+
+        public KeepOldest(Settings settings, TimeSpan stabilityTimeout) : base(stabilityTimeout)
         {
-            throw new NotImplementedException();
+            _settings = settings;
+        }
+
+        public KeepOldest(Config config, TimeSpan stabilityTimeout) : this(Settings.Create(config), stabilityTimeout)
+        {
+        }
+
+        protected override void ApplyStrategy(ImmutableSortedSet<Member> reachable, ImmutableSortedSet<Member> unreachable)
+        {
+            var oldestReachable = reachable.FirstOrDefault();
+            var oldestUnreachable = unreachable.FirstOrDefault();
+
+            if (oldestReachable == null) Down(Cluster.SelfAddress);
+            else if (oldestUnreachable != null && oldestUnreachable.IsOlderThan(oldestReachable))
+            {
+                if (_settings.DownIfAlone && unreachable.Count == 1)
+                {
+                    // the oldest node is the only one unreachable
+                    Down(oldestUnreachable.Address);
+                    return;
+                }
+                
+                Down(Cluster.SelfAddress);
+            }
+        }
+
+        private void Down(Address address)
+        {
+            Log.Info("Oldest node was not reached, shutting down {0}", address);
+            Cluster.Down(address);
         }
     }
 
@@ -188,9 +343,27 @@ namespace Akka.Cluster
 
         #endregion
 
-        public KeepReferee(Settings settings)
+        private readonly Settings _settings;
+        protected override string Role => null;
+
+        public KeepReferee(Settings settings, TimeSpan stabilityTimeout) : base(stabilityTimeout)
         {
-            throw new NotImplementedException();
+            _settings = settings;
+        }
+
+        public KeepReferee(Config config, TimeSpan stabilityTimeout) : this(Settings.Create(config), stabilityTimeout)
+        {
+        }
+
+        protected override void ApplyStrategy(ImmutableSortedSet<Member> reachable, ImmutableSortedSet<Member> unreachable)
+        {
+            var refereeReachable = reachable.Select(m => m.Address).Contains(_settings.ReferreeAddress);
+
+            if (!refereeReachable || reachable.Count < _settings.DownAllIfLessThanNodes)
+            {
+                Log.Info("A referee node address {0} was not reached or a reachable number of nodes ({1}) didn't pass required threshold ({2}). Shutting down cluster node.", _settings.ReferreeAddress, reachable.Count, _settings.DownAllIfLessThanNodes);
+                Cluster.Down(Cluster.SelfAddress);
+            }
         }
     }
 }
