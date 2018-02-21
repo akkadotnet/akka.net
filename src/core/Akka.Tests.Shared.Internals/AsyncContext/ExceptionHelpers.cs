3f0c716c90af571d134c9c3b1b034c3b9de65a8b
Aaron Stannard <aaron@petabridge.com>
2018-02-21 15:27:37 -0600

    removed copyright from Nito.AsyncEx files (#3335)

--- src/core/Akka.Tests.Shared.Internals/AsyncContext/ExceptionHelpers.cs
+++ src/core/Akka.Tests.Shared.Internals/AsyncContext/ExceptionHelpers.cs
@@ -1,11 +1,4 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="ExceptionHelpers.cs" company="Akka.NET Project">
-//     Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
-//     Copyright (C) 2013-2018 .NET Foundation <https://github.com/akkadotnet/akka.net>
-// </copyright>
-//-----------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Runtime.ExceptionServices;
 
 internal static class ExceptionHelpers
--- src/core/Akka.Tests.Shared.Internals/AsyncContext/SingleDisposable (of T).cs
+++ src/core/Akka.Tests.Shared.Internals/AsyncContext/SingleDisposable (of T).cs
@@ -1,11 +1,4 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="SingleDisposable (of T).cs" company="Akka.NET Project">
-//     Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
-//     Copyright (C) 2013-2018 .NET Foundation <https://github.com/akkadotnet/akka.net>
-// </copyright>
-//-----------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Threading;
 using Nito.Disposables.Internals;
 
