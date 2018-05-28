08467316769d9f76e716796d4146d83513dbcc2e
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2018-05-28 20:59:53 +0200

    initial commit with BenchmarkDotNet benchmarks

---
+++ src/benchmark/Akka.Benchmarks/Configurations/Configs.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Configs.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2018 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+
+namespace Akka.Benchmarks.Configurations
+{
+    public class MicroBenchmarkConfig : ManualConfig
+    {
+        public MicroBenchmarkConfig()
+        {
+            this.Add(new MemoryDiagnoser());
+            this.Add(MarkdownExporter.GitHub);
+        }
+    }
+    public class MonitoringConfig : ManualConfig
+    {
+        public MonitoringConfig()
+        {
+            this.Add(MarkdownExporter.GitHub);
+        }
+    }
+}
