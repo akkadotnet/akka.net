28ee736ec43574786ff4310d22ab41a5d69e4216
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2018-07-05 22:31:57 +0200

    reintroduced FunctionRefs

--- src/core/Akka/Actor/RepointableActorRef.cs
+++ src/core/Akka/Actor/RepointableActorRef.cs
@@ -293,8 +293,7 @@ namespace Akka.Actor
                     return ActorRefs.Nobody;
                 default:
                     var nameAndUid = ActorCell.SplitNameAndUid(next);
-                    IChildStats stats;
-                    if (Lookup.TryGetChildStatsByName(nameAndUid.Name, out stats))
+                    if (Lookup.TryGetChildStatsByName(nameAndUid.Name, out var stats))
                     {
                         var crs = stats as ChildRestartStats;
                         var uid = nameAndUid.Uid;
@@ -305,6 +304,10 @@ namespace Akka.Actor
                             else
                                 return crs.Child;
                         }
+                    }
+                    else if (Lookup is ActorCell cell && cell.TryGetFunctionRef(nameAndUid.Name, nameAndUid.Uid, out var functionRef))
+                    {
+                        return functionRef;
                     }
                     return ActorRefs.Nobody;
             }
@@ -342,7 +345,7 @@ namespace Akka.Actor
         private readonly IInternalActorRef _supervisor;
         private readonly object _lock = new object();
 
-       /* Both queues must be accessed via lock */
+        /* Both queues must be accessed via lock */
         private readonly LinkedList<Envelope> _messageQueue = new LinkedList<Envelope>();
         private LatestFirstSystemMessageList _sysMsgQueue = SystemMessageList.LNil;
 
