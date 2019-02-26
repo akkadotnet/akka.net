5c204b628a1b029b0c09ca9ccbf31d8bea57cc46
Aaron Stannard <aaron@petabridge.com>
2019-02-26 16:04:44 -0600

    Added reproduction spec for #3724

---
+++ src/core/Akka.Cluster.Tests/Serialization/BugFix3724Spec.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.TestKit;
+using Akka.Util.Internal;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Cluster.Tests.Serialization
+{
+    /// <summary>
+    /// https://github.com/akkadotnet/akka.net/issues/3724
+    ///
+    /// Used to validate that `akka.actor.serialize-messages = on` works while
+    /// using Akka.Cluster
+    /// </summary>
+    public class BugFix3724Spec : AkkaSpec
+    {
+        private readonly Address _selfAddress;
+        private readonly Cluster _cluster;
+
+        public BugFix3724Spec(ITestOutputHelper helper)
+            : base(@"akka.actor.provider = cluster
+                     akka.actor.serialize-messages = on", helper)
+        {
+            _cluster = Cluster.Get(Sys);
+            _selfAddress = Sys.AsInstanceOf<ExtendedActorSystem>().Provider.DefaultAddress;
+        }
+
+        [Fact(DisplayName = "Should be able to use 'akka.actor.serialize-messages' while running Akka.Cluster")]
+        public void Should_serialize_all_AkkaCluster_messages()
+        {
+            _cluster.Subscribe(TestActor, ClusterEvent.SubscriptionInitialStateMode.InitialStateAsEvents, typeof(ClusterEvent.MemberUp));
+            Within(TimeSpan.FromSeconds(10), () =>
+            {
+                EventFilter.Exception<Exception>().Expect(0, () =>
+                {
+                    // wait for a singleton cluster to fully form and publish a member up event
+                    _cluster.Join(_selfAddress);
+                    var up = ExpectMsg<ClusterEvent.MemberUp>();
+                    up.Member.Address.Should().Be(_selfAddress);
+                });
+            });
+        }
+    }
+}
