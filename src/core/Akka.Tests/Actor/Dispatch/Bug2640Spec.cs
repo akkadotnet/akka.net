1784991c390c8844dd3775e56d78905104bd2aff
Aaron Stannard <aaron@petabridge.com>
2019-03-20 08:52:17 -0500

    fixed race condition with Bug2640Spec for #2640
    needed to give the system more messages to process so we guarantee hitting all four dispatcher threads when running the test suite in parallel.

--- src/core/Akka.Tests/Actor/Dispatch/Bug2640Spec.cs
+++ src/core/Akka.Tests/Actor/Dispatch/Bug2640Spec.cs
@@ -91,10 +91,11 @@ namespace Akka.Tests.Actor.Dispatch
 
             Dictionary<int, Thread> threads = null;
             Watch(actor);
-            for (var i = 0; i < 100; i++)
+            var msgCount = 10000;
+            for (var i = 0; i < msgCount; i++)
                 actor.Tell(GetThread.Instance);
 
-            threads = ReceiveN(100).Cast<Thread>().GroupBy(x => x.ManagedThreadId)
+            threads = ReceiveN(msgCount).Cast<Thread>().GroupBy(x => x.ManagedThreadId)
                 .ToDictionary(x => x.Key, grouping => grouping.First());
             threads.Count.Should().Be(4, "Expected 4 distinct threads in this example");
 
