d3d1576694d4b07a0fc916b3d88abc04b198bdca
Aaron Stannard <aaron@petabridge.com>
2019-04-29 12:19:51 -0500

    porting over OutboundConnection timeout spec

--- src/core/Akka.Remote.Tests/Transport/AkkaProtocolSpec.cs
+++ src/core/Akka.Remote.Tests/Transport/AkkaProtocolSpec.cs
@@ -15,6 +15,7 @@ using Akka.Remote.Serialization;
 using Akka.Remote.Transport;
 using Akka.TestKit;
 using Akka.Util.Internal;
+using FluentAssertions;
 using Google.Protobuf;
 using Xunit;
 using Xunit.Abstractions;
@@ -22,20 +23,20 @@ using SerializedMessage = Akka.Remote.Serialization.Proto.Msg.Payload;
 
 namespace Akka.Remote.Tests.Transport
 {
-    
+
     public class AkkaProtocolSpec : AkkaSpec
     {
         #region Setup / Config
 
-        Address localAddress = new Address("test", "testsystem", "testhost", 1234);
-        Address localAkkaAddress = new Address("akka.test", "testsystem", "testhost", 1234);
+        private readonly Address _localAddress = new Address("test", "testsystem", "testhost", 1234);
+        private readonly Address _localAkkaAddress = new Address("akka.test", "testsystem", "testhost", 1234);
 
-        Address remoteAddress = new Address("test", "testsystem2", "testhost2", 1234);
-        Address remoteAkkaAddress = new Address("akka.test", "testsystem2", "testhost2", 1234);
+        private readonly Address _remoteAddress = new Address("test", "testsystem2", "testhost2", 1234);
+        private readonly Address _remoteAkkaAddress = new Address("akka.test", "testsystem2", "testhost2", 1234);
 
         private AkkaPduCodec codec;
 
-        SerializedMessage testMsg =
+        private readonly SerializedMessage testMsg =
             new SerializedMessage { SerializerId = 0, Message = ByteString.CopyFromUtf8("foo") };
 
         private ByteString testEnvelope;
@@ -44,18 +45,46 @@ namespace Akka.Remote.Tests.Transport
         private IHandleEvent testHeartbeat;
         private IHandleEvent testPayload;
         private IHandleEvent testDisassociate(DisassociateInfo info) { return new InboundPayload(codec.ConstructDisassociate(info)); }
-        private IHandleEvent testAssociate(int uid) { return new InboundPayload(codec.ConstructAssociate(new HandshakeInfo(remoteAkkaAddress, uid))); }
-        private TimeSpan DefaultTimeout { get { return Dilated(TestKitSettings.DefaultTimeout); } }
+        private IHandleEvent testAssociate(int uid) { return new InboundPayload(codec.ConstructAssociate(new HandshakeInfo(_remoteAkkaAddress, uid))); }
+        private TimeSpan DefaultTimeout => Dilated(TestKitSettings.DefaultTimeout);
 
         public AkkaProtocolSpec(ITestOutputHelper helper)
-            : base(@"akka.test.default-timeout = 1.5 s", helper)
+            : base(@"
+                    akka.actor.provider = remote
+                    akka.test.default-timeout = 1.5 s", helper)
         {
             codec = new AkkaPduProtobuffCodec(Sys);
-            testEnvelope = codec.ConstructMessage(localAkkaAddress, TestActor, testMsg);
+            testEnvelope = codec.ConstructMessage(_localAkkaAddress, TestActor, testMsg);
             testMsgPdu = codec.ConstructPayload(testEnvelope);
 
             testHeartbeat = new InboundPayload(codec.ConstructHeartbeat());
             testPayload = new InboundPayload(testMsgPdu);
+
+            config = ConfigurationFactory.ParseString(
+                @"akka{
+                    remote {
+
+                    transport-failure-detector {
+                      implementation-class = ""Akka.Remote.PhiAccrualFailureDetector, Akka.Remote""
+                      threshold = 7.0
+                      max-sample-size = 100
+                      min-std-deviation = 100 ms
+                      acceptable-heartbeat-pause = 3 s
+                      heartbeat-interval = 1 s
+                    }
+
+                    backoff-interval = 1 s
+
+                    require-cookie = off
+
+                    secure-cookie = ""abcde""
+
+                    shutdown-timeout = 5 s
+
+                    startup-timeout = 5 s
+
+                    use-passive-connections = on
+                }}").WithFallback(Sys.Settings.Config);
         }
 
         public class Collaborators
@@ -80,8 +109,8 @@ namespace Akka.Remote.Tests.Transport
         public Collaborators GetCollaborators()
         {
             var registry = new AssociationRegistry();
-            var transport = new TestTransport(localAddress, registry);
-            var handle = new TestAssociationHandle(localAddress, remoteAddress, transport, true);
+            var transport = new TestTransport(_localAddress, registry);
+            var handle = new TestAssociationHandle(_localAddress, _remoteAddress, transport, true);
             transport.WriteBehavior.PushConstant(true);
             return new Collaborators(registry, transport, handle, new TestFailureDetector());
         }
@@ -89,16 +118,10 @@ namespace Akka.Remote.Tests.Transport
         public class TestFailureDetector : FailureDetector
         {
             internal volatile bool isAvailable = true;
-            public override bool IsAvailable
-            {
-                get { return isAvailable; }
-            }
+            public override bool IsAvailable => isAvailable;
 
             internal volatile bool called = false;
-            public override bool IsMonitoring
-            {
-                get { return called; }
-            }
+            public override bool IsMonitoring => called;
 
             public override void HeartBeat()
             {
@@ -106,31 +129,7 @@ namespace Akka.Remote.Tests.Transport
             }
         }
 
-        private Config config = ConfigurationFactory.ParseString(
-        @"akka{
-            remote {
-
-            transport-failure-detector {
-              implementation-class = ""Akka.Remote.PhiAccrualFailureDetector, Akka.Remote""
-              threshold = 7.0
-              max-sample-size = 100
-              min-std-deviation = 100 ms
-              acceptable-heartbeat-pause = 3 s
-              heartbeat-interval = 1 s
-            }
-
-            backoff-interval = 1 s
-
-            require-cookie = off
-
-            secure-cookie = ""abcde""
-
-            shutdown-timeout = 5 s
-
-            startup-timeout = 5 s
-
-            use-passive-connections = on
-        }}");
+        private Config config;
 
         #endregion
 
@@ -140,7 +139,7 @@ namespace Akka.Remote.Tests.Transport
         public void ProtocolStateActor_must_register_itself_as_reader_on_injected_handles()
         {
             var collaborators = GetCollaborators();
-            Sys.ActorOf(ProtocolStateActor.InboundProps(new HandshakeInfo(localAddress, 42), collaborators.Handle,
+            Sys.ActorOf(ProtocolStateActor.InboundProps(new HandshakeInfo(_localAddress, 42), collaborators.Handle,
                 new ActorAssociationEventListener(TestActor), new AkkaProtocolSettings(config), codec,
                 collaborators.FailureDetector));
 
@@ -152,7 +151,7 @@ namespace Akka.Remote.Tests.Transport
         {
             var collaborators = GetCollaborators();
             var reader =
-                Sys.ActorOf(ProtocolStateActor.InboundProps(new HandshakeInfo(localAddress, 42), collaborators.Handle,
+                Sys.ActorOf(ProtocolStateActor.InboundProps(new HandshakeInfo(_localAddress, 42), collaborators.Handle,
                     new ActorAssociationEventListener(TestActor), new AkkaProtocolSettings(config), codec,
                     collaborators.FailureDetector));
 
@@ -189,7 +188,7 @@ namespace Akka.Remote.Tests.Transport
             var collaborators = GetCollaborators();
 
             var reader =
-                Sys.ActorOf(ProtocolStateActor.InboundProps(new HandshakeInfo(localAddress, 42), collaborators.Handle,
+                Sys.ActorOf(ProtocolStateActor.InboundProps(new HandshakeInfo(_localAddress, 42), collaborators.Handle,
                     new ActorAssociationEventListener(TestActor), new AkkaProtocolSettings(config), codec,
                     collaborators.FailureDetector));
 
@@ -214,7 +213,7 @@ namespace Akka.Remote.Tests.Transport
 
             var statusPromise = new TaskCompletionSource<AssociationHandle>();
             var reader =
-                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(localAddress, 42), remoteAddress,
+                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(_localAddress, 42), _remoteAddress,
                     statusPromise, collaborators.Transport,
                     new AkkaProtocolSettings(config), codec, collaborators.FailureDetector));
 
@@ -233,11 +232,11 @@ namespace Akka.Remote.Tests.Transport
             statusPromise.Task.Result.Match()
                 .With<AkkaProtocolHandle>(h =>
                 {
-                    Assert.Equal(remoteAkkaAddress, h.RemoteAddress);
-                    Assert.Equal(localAkkaAddress, h.LocalAddress);
+                    Assert.Equal(_remoteAkkaAddress, h.RemoteAddress);
+                    Assert.Equal(_localAkkaAddress, h.LocalAddress);
                     Assert.Equal(33, h.HandshakeInfo.Uid);
                 })
-                .Default(msg => Assert.True(false,"Did not receive expected AkkaProtocolHandle from handshake"));
+                .Default(msg => Assert.True(false, "Did not receive expected AkkaProtocolHandle from handshake"));
         }
 
         [Fact]
@@ -248,7 +247,7 @@ namespace Akka.Remote.Tests.Transport
 
             var statusPromise = new TaskCompletionSource<AssociationHandle>();
             var reader =
-                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(localAddress, 42), remoteAddress,
+                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(_localAddress, 42), _remoteAddress,
                     statusPromise, collaborators.Transport,
                     new AkkaProtocolSettings(config), codec, collaborators.FailureDetector));
 
@@ -260,10 +259,10 @@ namespace Akka.Remote.Tests.Transport
             statusPromise.Task.Result.Match()
                 .With<AkkaProtocolHandle>(h =>
                 {
-                    Assert.Equal(remoteAkkaAddress, h.RemoteAddress);
-                    Assert.Equal(localAkkaAddress, h.LocalAddress);
+                    Assert.Equal(_remoteAkkaAddress, h.RemoteAddress);
+                    Assert.Equal(_localAkkaAddress, h.LocalAddress);
                 })
-                .Default(msg => Assert.True(false,"Did not receive expected AkkaProtocolHandle from handshake"));
+                .Default(msg => Assert.True(false, "Did not receive expected AkkaProtocolHandle from handshake"));
             var wrappedHandle = statusPromise.Task.Result.AsInstanceOf<AkkaProtocolHandle>();
 
             wrappedHandle.ReadHandlerSource.SetResult(new ActorHandleEventListener(TestActor));
@@ -289,7 +288,7 @@ namespace Akka.Remote.Tests.Transport
 
             var statusPromise = new TaskCompletionSource<AssociationHandle>();
             var reader =
-                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(localAddress, 42), remoteAddress,
+                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(_localAddress, 42), _remoteAddress,
                     statusPromise, collaborators.Transport,
                     new AkkaProtocolSettings(config), codec, collaborators.FailureDetector));
 
@@ -301,10 +300,10 @@ namespace Akka.Remote.Tests.Transport
             statusPromise.Task.Result.Match()
                 .With<AkkaProtocolHandle>(h =>
                 {
-                    Assert.Equal(remoteAkkaAddress, h.RemoteAddress);
-                    Assert.Equal(localAkkaAddress, h.LocalAddress);
+                    Assert.Equal(_remoteAkkaAddress, h.RemoteAddress);
+                    Assert.Equal(_localAkkaAddress, h.LocalAddress);
                 })
-                .Default(msg => Assert.True(false,"Did not receive expected AkkaProtocolHandle from handshake"));
+                .Default(msg => Assert.True(false, "Did not receive expected AkkaProtocolHandle from handshake"));
             var wrappedHandle = statusPromise.Task.Result.AsInstanceOf<AkkaProtocolHandle>();
 
             wrappedHandle.ReadHandlerSource.SetResult(new ActorHandleEventListener(TestActor));
@@ -330,7 +329,7 @@ namespace Akka.Remote.Tests.Transport
 
             var statusPromise = new TaskCompletionSource<AssociationHandle>();
             var stateActor =
-                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(localAddress, 42), remoteAddress,
+                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(_localAddress, 42), _remoteAddress,
                     statusPromise, collaborators.Transport,
                     new AkkaProtocolSettings(config), codec, collaborators.FailureDetector));
 
@@ -342,8 +341,8 @@ namespace Akka.Remote.Tests.Transport
             statusPromise.Task.Result.Match()
                 .With<AkkaProtocolHandle>(h =>
                 {
-                    Assert.Equal(remoteAkkaAddress, h.RemoteAddress);
-                    Assert.Equal(localAkkaAddress, h.LocalAddress);
+                    Assert.Equal(_remoteAkkaAddress, h.RemoteAddress);
+                    Assert.Equal(_localAkkaAddress, h.LocalAddress);
                 })
                 .Default(msg => Assert.True(false, "Did not receive expected AkkaProtocolHandle from handshake"));
             var wrappedHandle = statusPromise.Task.Result.AsInstanceOf<AkkaProtocolHandle>();
@@ -374,7 +373,7 @@ namespace Akka.Remote.Tests.Transport
 
             var statusPromise = new TaskCompletionSource<AssociationHandle>();
             var stateActor =
-                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(localAddress, 42), remoteAddress,
+                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(_localAddress, 42), _remoteAddress,
                     statusPromise, collaborators.Transport,
                     new AkkaProtocolSettings(config), codec, collaborators.FailureDetector));
 
@@ -386,10 +385,10 @@ namespace Akka.Remote.Tests.Transport
             statusPromise.Task.Result.Match()
                 .With<AkkaProtocolHandle>(h =>
                 {
-                    Assert.Equal(remoteAkkaAddress, h.RemoteAddress);
-                    Assert.Equal(localAkkaAddress, h.LocalAddress);
+                    Assert.Equal(_remoteAkkaAddress, h.RemoteAddress);
+                    Assert.Equal(_localAkkaAddress, h.LocalAddress);
                 })
-                .Default(msg => Assert.True(false,"Did not receive expected AkkaProtocolHandle from handshake"));
+                .Default(msg => Assert.True(false, "Did not receive expected AkkaProtocolHandle from handshake"));
             var wrappedHandle = statusPromise.Task.Result.AsInstanceOf<AkkaProtocolHandle>();
 
             stateActor.Tell(new Disassociated(DisassociateInfo.Unknown), Self);
@@ -408,6 +407,33 @@ namespace Akka.Remote.Tests.Transport
             });
         }
 
+        [Fact]
+        public void ProtocolStateActor_must_give_up_outbound_after_connection_timeout()
+        {
+            var collaborators = GetCollaborators();
+            collaborators.Handle.Writeable = false;
+            collaborators.Transport.AssociateBehavior.PushConstant(collaborators.Handle);
+
+            var statusPromise = new TaskCompletionSource<AssociationHandle>();
+
+            var conf2 = ConfigurationFactory.ParseString("akka.remote.dot-netty.tcp.connection-timeout = 500 ms")
+                .WithFallback(config);
+
+            var stateActor =
+                Sys.ActorOf(ProtocolStateActor.OutboundProps(new HandshakeInfo(_localAddress, 42), _remoteAddress,
+                    statusPromise, collaborators.Transport,
+                    new AkkaProtocolSettings(conf2), codec, collaborators.FailureDetector));
+
+            Watch(stateActor);
+
+            // inner exception will be a TimeoutException
+            Intercept<AggregateException>(() =>
+            {
+                statusPromise.Task.Wait(TimeSpan.FromSeconds(5)).Should().BeTrue();
+            });
+            ExpectTerminated(stateActor);
+        }
+
         #endregion
 
         #region Internal helper methods
@@ -418,8 +444,8 @@ namespace Akka.Remote.Tests.Transport
             var rValue = false;
             if (associationRegistry.LogSnapshot().Last() is WriteAttempt)
             {
-                var attempt = (WriteAttempt) associationRegistry.LogSnapshot().Last();
-                if (attempt.Sender.Equals(localAddress) && attempt.Recipient.Equals(remoteAddress))
+                var attempt = (WriteAttempt)associationRegistry.LogSnapshot().Last();
+                if (attempt.Sender.Equals(_localAddress) && attempt.Recipient.Equals(_remoteAddress))
                 {
                     codec.DecodePdu(attempt.Payload)
                         .Match()
@@ -437,12 +463,12 @@ namespace Akka.Remote.Tests.Transport
             var rValue = false;
             if (associationRegistry.LogSnapshot().Last() is WriteAttempt)
             {
-                var attempt = (WriteAttempt) associationRegistry.LogSnapshot().Last();
-                if (attempt.Sender.Equals(localAddress) && attempt.Recipient.Equals(remoteAddress))
+                var attempt = (WriteAttempt)associationRegistry.LogSnapshot().Last();
+                if (attempt.Sender.Equals(_localAddress) && attempt.Recipient.Equals(_remoteAddress))
                 {
                     codec.DecodePdu(attempt.Payload)
                         .Match()
-                        .With<Associate>(h => rValue = h.Info.Origin.Equals(localAddress) && h.Info.Uid == uid)
+                        .With<Associate>(h => rValue = h.Info.Origin.Equals(_localAddress) && h.Info.Uid == uid)
                         .Default(msg => rValue = false);
                 }
             }
@@ -456,8 +482,8 @@ namespace Akka.Remote.Tests.Transport
             var rValue = false;
             if (associationRegistry.LogSnapshot().Last() is WriteAttempt)
             {
-                var attempt = (WriteAttempt) associationRegistry.LogSnapshot().Last();
-                if (attempt.Sender.Equals(localAddress) && attempt.Recipient.Equals(remoteAddress))
+                var attempt = (WriteAttempt)associationRegistry.LogSnapshot().Last();
+                if (attempt.Sender.Equals(_localAddress) && attempt.Recipient.Equals(_remoteAddress))
                     codec.DecodePdu(attempt.Payload)
                         .Match()
                         .With<Disassociate>(h => rValue = true)
