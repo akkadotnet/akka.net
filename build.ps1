24fc0b7300107d1e2cff61878e99e82bc332635a
Aaron Stannard <aaron@petabridge.com>
2019-05-16 16:11:38 -0500

    added workaround for https://github.com/dotnet/cli/issues/9114

--- build.ps1
+++ build.ps1
@@ -90,6 +90,7 @@ if($FoundDotNetCliVersion -ne $DotNetVersion) {
     $env:PATH = "$InstallPath;$env:PATH"
     $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
     $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
+    $env:DOTNET_ROOT = $InstallPath\dotnet.exe #workaround for https://github.com/dotnet/cli/issues/9114
 }
 
 ###########################################################################
