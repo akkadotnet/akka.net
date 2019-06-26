e7e00034aa6dd57b3a85f205cca615826febab85
zbynek001 <zbynek001@gmail.com>
2019-06-26 21:09:52 +0200

    catch NotSerializableException from deserialization, Migrated from #20641 (#3782)
    * to be able to introduce new messages and still support rolling upgrades,
      i.e. a cluster of mixed versions
    * note that it's only catching NotSerializableException, which we already
      use for unknown serializer ids and class manifests
    * note that it is not catching for system messages, since that could result
      in infinite resending
    
    Do not tear down connections on IllegalArgumentException from serializer, Migrated from #24910

--- src/contrib/cluster/Akka.Cluster.Sharding/Serialization/ClusterShardingMessageSerializer.cs
+++ src/contrib/cluster/Akka.Cluster.Sharding/Serialization/ClusterShardingMessageSerializer.cs
@@ -11,6 +11,7 @@ using System.Collections.Immutable;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Runtime.Serialization;
 using Akka.Actor;
 using Akka.Cluster.Sharding.Serialization.Proto.Msg;
 using Akka.Remote.Serialization.Proto.Msg;
@@ -175,7 +176,7 @@ namespace Akka.Cluster.Sharding.Serialization
             if (_fromBinaryMap.TryGetValue(manifest, out var factory))
                 return factory(bytes);
 
-            throw new ArgumentException($"Unimplemented deserialization of message with manifest [{manifest}] in [{this.GetType()}]");
+            throw new SerializationException($"Unimplemented deserialization of message with manifest [{manifest}] in [{this.GetType()}]");
         }
 
         /// <summary>
