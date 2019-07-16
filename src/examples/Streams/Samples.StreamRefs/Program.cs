ea08c642c212bcca58cdf279b05d5d1aaf80512b
Aaron Stannard <aaron@petabridge.com>
2019-07-16 13:10:48 -0500

    simpler example

--- src/examples/Streams/Samples.StreamRefs/Program.cs
+++ src/examples/Streams/Samples.StreamRefs/Program.cs
@@ -31,34 +31,37 @@ namespace Samples.StreamRefs
             var actorSystem = ActorSystem.Create("StreamRefs");
 
             // 
-            var source = Source.Tick(TimeSpan.FromMilliseconds(40), TimeSpan.FromMilliseconds(1000),
-                    ThreadLocalRandom.Current.Next(0, 10000))
+            var source = Source.Tick(TimeSpan.FromMilliseconds(40), TimeSpan.FromMilliseconds(10),
+                   1)
+                .Select(x => ThreadLocalRandom.Current.Next(0, 1000))
                 .PreMaterialize(actorSystem.Materializer());
 
             var cancelHandle = source.Item1;
             var runHandle = source.Item2;
             var sourceRef = Akka.Streams.Dsl.StreamRefs.SourceRef<int>();
 
-            var graph = GraphDsl.Create(sourceRef, (builder, shape) =>
-            {
-                var graphSource = builder.Add(runHandle);
-                var broadcast = builder.Add(new Broadcast<int>(2));
+            //var graph = GraphDsl.Create(sourceRef, (builder, shape) =>
+            //{
+            //    var graphSource = builder.Add(runHandle);
+            //    var broadcast = builder.Add(new Broadcast<int>(2));
 
-                builder.From(graphSource).Via(broadcast);
-                builder.From(broadcast.Out(0)).To(shape);
+            //    builder.From(graphSource).Via(broadcast);
+            //    builder.From(broadcast.Out(0)).To(shape);
 
-                var countingFlow = Flow.Create<int>().Async().GroupedWithin(int.MaxValue, TimeSpan.FromSeconds(1));
+            //    var countingFlow = Flow.Create<int>().Async().GroupedWithin(int.MaxValue, TimeSpan.FromSeconds(1));
 
-                builder.From(broadcast.Out(1)).Via(countingFlow).To(Sink.ForEach<IEnumerable<int>>(i =>
-                {
-                    var arr = i.ToArray();
-                    actorSystem.Log.Info("Emitted {0} events totaling {1}", arr.Length, arr.Sum());
-                }));
+            //    builder.From(broadcast.Out(1)).Via(countingFlow).To(Sink.ForEach<IEnumerable<int>>(i =>
+            //    {
+            //        var arr = i.ToArray();
+            //        actorSystem.Log.Info("Emitted {0} events totaling {1}", arr.Length, arr.Sum());
+            //    }));
 
-                return ClosedShape.Instance;
-            });
+            //    return ClosedShape.Instance;
+            //});
 
-            var actorRef = await actorSystem.Materializer().Materialize(graph);
+            //var actorRef = await actorSystem.Materializer().Materialize(graph);
+
+            var actorRef = await runHandle.RunWith(sourceRef, actorSystem.Materializer());
 
             await actorRef.Source.RunForeach(i =>
             {
