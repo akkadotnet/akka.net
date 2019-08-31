37be6a5624367d43bf46ae04334387fcdc47ce94
Valdis Zobēla <valdis@zobela.eu>
2019-08-31 20:38:48 +0300

    TestJournal and TestSnaphostStore (#3881)
    * TestJournal with Write interception and various failure strategies
    * Akka.Persistence.TestKit implementation

---
+++ src/core/Akka.Persistence.TestKit/Journal/IJournalBehaviorSetter.cs
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------
+// <copyright file="IJournalBehaviorSetter.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2019 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2019 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Akka.Persistence.TestKit
+{
+    using System.Threading.Tasks;
+
+    public interface IJournalBehaviorSetter
+    {
+        Task SetInterceptorAsync(IJournalInterceptor interceptor);
+    }
+}
