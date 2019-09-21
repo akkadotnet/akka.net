bbefb52eba227f0c075d6bfef2c109bb90f752a4
Valdis Zobēla <valdis@zobela.eu>
2019-09-21 19:17:50 +0300

    moved classes into separate files

---
+++ src/core/Akka.MultiNodeTestRunner.AzureDevOps/SpecEvent.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="SpecEvent.cs" company="Akka.NET Project">
+//      Copyright (C) 2009-2019 Lightbend Inc. <http://www.lightbend.com>
+//      Copyright (C) 2013-2019 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Akka.MultiNodeTestRunner.Shared.AzureDevOps
+{
+    using System;
+
+    public class SpecEvent<T>
+    {
+        public SpecEvent(DateTime time, T value)
+        {
+            Time = time;
+            Value = value;
+        }
+
+        public DateTime Time { get; }
+        public T Value { get; }
+
+        public void Deconstruct(out DateTime time, out T value)
+        {
+            time = Time;
+            value = Value;
+        }
+    }
+}
--- src/core/Akka.MultiNodeTestRunner.AzureDevOps/TrxSinkActor.cs
+++ src/core/Akka.MultiNodeTestRunner.AzureDevOps/TrxSinkActor.cs
@@ -9,7 +9,6 @@ namespace Akka.MultiNodeTestRunner.Shared.AzureDevOps
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Xml.Linq;
@@ -18,46 +17,6 @@ namespace Akka.MultiNodeTestRunner.Shared.AzureDevOps
     using Reporting;
     using Sinks;
 
-    public class SpecEvent<T>
-    {
-        public SpecEvent(DateTime time, T value)
-        {
-            Time = time;
-            Value = value;
-        }
-
-        public DateTime Time { get; }
-        public T Value { get; }
-
-        public void Deconstruct(out DateTime time, out T value)
-        {
-            time = Time;
-            value = Value;
-        }
-    }
-
-    public class SpecSession
-    {
-        private readonly List<SpecEvent<NodeCompletedSpecWithSuccess>> _successes = new List<SpecEvent<NodeCompletedSpecWithSuccess>>();
-        private readonly List<SpecEvent<NodeCompletedSpecWithFail>> _fails = new List<SpecEvent<NodeCompletedSpecWithFail>>();
-        private readonly List<SpecEvent<LogMessageFragmentForNode>> _messages = new List<SpecEvent<LogMessageFragmentForNode>>();
-
-        public SpecEvent<BeginNewSpec> Begin { get; private set; }
-        public IReadOnlyList<SpecEvent<NodeCompletedSpecWithSuccess>> Successes => _successes;
-        public IReadOnlyList<SpecEvent<NodeCompletedSpecWithFail>> Fails => _fails;
-        public IReadOnlyList<SpecEvent<LogMessageFragmentForNode>> Messages => _messages;
-        public SpecEvent<EndSpec> End { get; private set; }
-
-        public void OnBegin(BeginNewSpec value) => Begin = new SpecEvent<BeginNewSpec>(DateTime.UtcNow, value);
-        public void OnSuccess(NodeCompletedSpecWithSuccess value) => _successes.Add(new SpecEvent<NodeCompletedSpecWithSuccess>(DateTime.UtcNow, value));
-        public void OnFailure(NodeCompletedSpecWithFail value) => _fails.Add(new SpecEvent<NodeCompletedSpecWithFail>(DateTime.UtcNow, value));
-        public void OnMessage(LogMessageFragmentForNode value) => _messages.Add(new SpecEvent<LogMessageFragmentForNode>(DateTime.UtcNow, value));
-        public void OnEnd(EndSpec value) => End = new SpecEvent<EndSpec>(DateTime.UtcNow, value);
-    }
-
-    /// <summary>
-    /// Sink actor with built-in multiplexor to forward testing progress using several reporters.
-    /// </summary>
     public class TrxSinkActor : TestCoordinatorEnabledMessageSink
     {
         public TrxSinkActor(string suiteName, string userName, string computerName, bool useTestCoordinator)
