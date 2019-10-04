c40b90b7baae649b1b78557f947acfc6366d961e
Aaron Stannard <aaron@petabridge.com>
2019-10-04 14:44:41 -0500

    showed fix to #3943

--- src/core/Akka.Cluster.Tests/Bugfix3943Specs.cs
+++ src/core/Akka.Cluster.Tests/Bugfix3943Specs.cs
@@ -16,7 +16,7 @@ namespace Akka.Cluster.Tests
             {
                 "akka : {\r\n    stdout-loglevel : INFO\r\n    loglevel : INFO\r\n    log-config-on-start : on\r\n    loggers : [\"Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog\"]\r\n    actor : {\r\n      debug : {\r\n        receive : on\r\n        autoreceive : on\r\n        lifecycle : on\r\n        event-stream : on\r\n        unhandled : on\r\n      }\r\n    }\r\n  }",
                 "akka : {\r\n    remote : {\r\n      dot-netty : {\r\n        tcp : {\r\n          log-transport : true\r\n          transport-class : \"Akka.Remote.Transport.DotNetty.TcpTransport, Akka.Remote\"\r\n          transport-protocol : tcp\r\n          hostname : 0.0.0.0\r\n          public-hostname : localhost\r\n          port : 9000\r\n        }\r\n      }\r\n    }\r\n  }",
-                "akka : {\r\n    actor : {\r\n      provider : \"Akka.Cluster.ClusterActorRefProvider, Akka.Cluster\"\r\n    }\r\n    cluster : {\r\n      log-info : on\r\n      seed-nodes : [\"akka.tcp://System@localhost:9000\"]\r\n      roles : []\r\n      role : []\r\n    }\r\n  }"
+                "akka : {\r\n    actor : {\r\n      provider : \"Akka.Cluster.ClusterActorRefProvider, Akka.Cluster\"\r\n    }\r\n    cluster : {\r\n      log-info : on\r\n      seed-nodes : [\"akka.tcp://System@localhost:9000\"]\r\n      roles : []\r\n   }\r\n  }"
             };
 
        [Fact]
