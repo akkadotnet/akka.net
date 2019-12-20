2b7f153f496037d5a83b51a9688f3fdfa04513ba
IgorFedchenko <igor@fedchenko.pro>
2019-12-20 13:27:47 +0300

    Created project

---
+++ src/contrib/cluster/Akka.Cluster.Metrics/IMetricsCollector.cs
@@ -0,0 +1,16 @@
+// //-----------------------------------------------------------------------
+// // <copyright file="IMetricsCollector.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2019 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2019 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+using System;
+
+namespace Akka.Cluster.Metrics
+{
+    public interface IMetricsCollector : IDisposable
+    {
+        
+    }
+}
