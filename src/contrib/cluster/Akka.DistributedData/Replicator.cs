bde5efdca506b481c888586f1deda50548f15c97
Aaron Stannard <aaron@petabridge.com>
2020-02-03 22:03:09 -0600

    fixed return value

--- src/contrib/cluster/Akka.DistributedData/Replicator.cs
+++ src/contrib/cluster/Akka.DistributedData/Replicator.cs
@@ -695,7 +695,7 @@ namespace Akka.DistributedData
             var envelope = GetData(key);
             if (envelope != null)
             {
-                if (Equals(envelope, writeEnvelope)) return writeEnvelope;
+                if (Equals(envelope, writeEnvelope)) return envelope;
                 if (envelope.Data is DeletedData) return DeletedEnvelope; // already deleted
 
                 try
