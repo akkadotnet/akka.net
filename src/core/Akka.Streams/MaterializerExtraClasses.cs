ae794adcc2017d49681602e6ab3604f9ab42a6e1
Jonathan Nagy <nagytech@users.noreply.github.com>
2020-02-06 15:32:44 +1100

    Checking in some progress

---
+++ src/core/Akka.Streams/MaterializerExtraClasses.cs
@@ -0,0 +1,14 @@
+// //-----------------------------------------------------------------------
+// // <copyright file="MaterializerExtraClasses.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2020 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2020 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+namespace Akka.Streams
+{
+    public class MaterializerExtraClasses
+    {
+        
+    }
+}
---
+++ src/core/Akka/Actor/IClassicActorSystemProvider.cs
@@ -0,0 +1,14 @@
+// //-----------------------------------------------------------------------
+// // <copyright file="IClassicActorSystemProvider.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2020 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2020 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+namespace Akka.Actor
+{
+    public interface IClassicActorSystemProvider
+    {
+        
+    }
+}
