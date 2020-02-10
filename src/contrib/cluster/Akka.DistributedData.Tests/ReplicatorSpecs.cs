d0fb9e99fdf8cee42941f24cdda37ec320edb025
Aaron Stannard <aaron@petabridge.com>
2020-02-10 12:54:24 -0600

    introduced standalone reproduction spec for #4198

--- src/contrib/cluster/Akka.DistributedData.Tests/ReplicatorSpecs.cs
+++ src/contrib/cluster/Akka.DistributedData.Tests/ReplicatorSpecs.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Akka.Actor;
@@ -50,8 +51,9 @@ namespace Akka.DistributedData.Tests
         private readonly ReadMajority _readMajority;
         private readonly ReadAll _readAll;
 
-        private readonly ORDictionaryKey<string, Flag> KeyH = new ORDictionaryKey<string, Flag>("H");
-        private readonly GSetKey<string> KeyI = new GSetKey<string>("I");
+        private readonly PNCounterDictionaryKey<string> _keyC = new PNCounterDictionaryKey<string>("C");
+        private readonly ORDictionaryKey<string, Flag> _keyH = new ORDictionaryKey<string, Flag>("H");
+        private readonly GSetKey<string> _keyI = new GSetKey<string>("I");
 
         public ReplicatorSpecs(ITestOutputHelper helper) : base(SpecConfig, helper)
         {
@@ -124,27 +126,27 @@ namespace Akka.DistributedData.Tests
             var changedProbe = CreateTestProbe(_sys2);
 
             // subscribe to updates for KeyH, then update it with a replication factor of two
-            _replicator2.Tell(Dsl.Subscribe(KeyH, changedProbe.Ref));
-            _replicator2.Tell(Dsl.Update(KeyH, ORDictionary<string, Flag>.Empty, _writeTwo, x => x.SetItem(Cluster.Cluster.Get(_sys2), "a", Flag.False)));
+            _replicator2.Tell(Dsl.Subscribe(_keyH, changedProbe.Ref));
+            _replicator2.Tell(Dsl.Update(_keyH, ORDictionary<string, Flag>.Empty, _writeTwo, x => x.SetItem(Cluster.Cluster.Get(_sys2), "a", Flag.False)));
 
             // receive local update
-            changedProbe.ExpectMsg<Changed>(g => Equals(g.Key, KeyH)).Get(KeyH).Entries.SequenceEqual(ImmutableDictionary.CreateRange(new[]
+            changedProbe.ExpectMsg<Changed>(g => Equals(g.Key, _keyH)).Get(_keyH).Entries.SequenceEqual(ImmutableDictionary.CreateRange(new[]
             {
                 new KeyValuePair<string, Flag>("a", Flag.False),
             })).ShouldBeTrue();
 
             // push update from node 1
-            _replicator1.Tell(Dsl.Update(KeyH, ORDictionary<string, Flag>.Empty, _writeTwo, x => x.SetItem(Cluster.Cluster.Get(_sys1), "a", Flag.True)));
+            _replicator1.Tell(Dsl.Update(_keyH, ORDictionary<string, Flag>.Empty, _writeTwo, x => x.SetItem(Cluster.Cluster.Get(_sys1), "a", Flag.True)));
 
             // expect replication of update on node 2
-            changedProbe.ExpectMsg<Changed>(g => Equals(g.Key, KeyH)).Get(KeyH).Entries.SequenceEqual(ImmutableDictionary.CreateRange(new[]
+            changedProbe.ExpectMsg<Changed>(g => Equals(g.Key, _keyH)).Get(_keyH).Entries.SequenceEqual(ImmutableDictionary.CreateRange(new[]
             {
                 new KeyValuePair<string, Flag>("a", Flag.True)
             })).ShouldBeTrue();
 
             // add new value to dictionary from node 2
-            _replicator2.Tell(Dsl.Update(KeyH, ORDictionary<string, Flag>.Empty, _writeTwo, x => x.SetItem(Cluster.Cluster.Get(_sys2), "b", Flag.True)));
-            changedProbe.ExpectMsg<Changed>(g => Equals(g.Key, KeyH)).Get(KeyH).Entries.SequenceEqual(ImmutableDictionary.CreateRange(new[]
+            _replicator2.Tell(Dsl.Update(_keyH, ORDictionary<string, Flag>.Empty, _writeTwo, x => x.SetItem(Cluster.Cluster.Get(_sys2), "b", Flag.True)));
+            changedProbe.ExpectMsg<Changed>(g => Equals(g.Key, _keyH)).Get(_keyH).Entries.SequenceEqual(ImmutableDictionary.CreateRange(new[]
             {
                 new KeyValuePair<string, Flag>("a", Flag.True),
                 new KeyValuePair<string, Flag>("b", Flag.True)
@@ -170,12 +172,12 @@ namespace Akka.DistributedData.Tests
             var probes = new[] { p1, p2, p3 };
 
             // subscribe to updates on all 3 nodes
-            _replicator1.Tell(Dsl.Subscribe(KeyI, p1.Ref));
-            _replicator2.Tell(Dsl.Subscribe(KeyI, p2.Ref));
-            _replicator3.Tell(Dsl.Subscribe(KeyI, p3.Ref));
+            _replicator1.Tell(Dsl.Subscribe(_keyI, p1.Ref));
+            _replicator2.Tell(Dsl.Subscribe(_keyI, p2.Ref));
+            _replicator3.Tell(Dsl.Subscribe(_keyI, p3.Ref));
 
             // update item on 2
-            _replicator2.Tell(Dsl.Update(KeyI, GSet<string>.Empty, _writeTwo, a => a.Add("a")));
+            _replicator2.Tell(Dsl.Update(_keyI, GSet<string>.Empty, _writeTwo, a => a.Add("a")));
 
             Sys.Log.Info("Pushed change from sys2 for I");
 
@@ -183,16 +185,72 @@ namespace Akka.DistributedData.Tests
             Within(TimeSpan.FromSeconds(5), () =>
             {
                 foreach(var p in probes)
-                    p.ExpectMsg<Changed>(c => c.Get(KeyI).Elements.ShouldBe(ImmutableHashSet.Create("a")));
+                    p.ExpectMsg<Changed>(c => c.Get(_keyI).Elements.ShouldBe(ImmutableHashSet.Create("a")));
             });
 
             // create duplicate write on node 1
             Sys.Log.Info("Pushing change from sys1 for I");
-            _replicator1.Tell(Dsl.Update(KeyI, GSet<string>.Empty, _writeTwo, a => a.Add("a")));
+            _replicator1.Tell(Dsl.Update(_keyI, GSet<string>.Empty, _writeTwo, a => a.Add("a")));
             
 
             // no probe should receive an update
             p2.ExpectNoMsg(TimeSpan.FromSeconds(1));
+        }
+
+        /// <summary>
+        /// Reproduction spec for https://github.com/akkadotnet/akka.net/issues/4198
+        /// </summary>
+        [Fact]
+        public async Task Bugfix_4198_PNCounterDictionary_Merge()
+        {
+            await InitCluster();
+            await PNCounterDictionary_Should_Merge();
+        }
+
+        private async Task PNCounterDictionary_Should_Merge()
+        {
+            var p1 = CreateTestProbe(_sys1);
+            var p2 = CreateTestProbe(_sys2);
+            var p3 = CreateTestProbe(_sys3);
+
+            var probes = new[] { 
+                (p1, _replicator1, Cluster.Cluster.Get(_sys1)), 
+                (p2, _replicator2, Cluster.Cluster.Get(_sys2)), 
+                (p3, _replicator3, Cluster.Cluster.Get(_sys3))
+            };
+
+            AwaitAssert(() =>
+            {
+                _replicator1.Tell(Dsl.GetReplicaCount, p1);
+                p1.ExpectMsg(new ReplicaCount(3));
+            });
+
+            // kick off writes
+            foreach (var i in Enumerable.Repeat(0, 10))
+            {
+                foreach (var (probe, replicator, cluster) in probes)
+                {
+                    replicator.Tell(Dsl.Update(_keyC, PNCounterDictionary<string>.Empty,
+                        new WriteAll(_timeOut), x => x.Increment(cluster, "x")
+                            .Increment(cluster, "y")), probe);
+                    probe.ExpectMsg(new UpdateSuccess(_keyC, null));
+                }
+            }
+
+            // perform read to ensure values are consistent
+
+            Within(TimeSpan.FromSeconds(5), () =>
+            {
+                AwaitAssert(() => {
+                    foreach (var (probe, replicator, _) in probes)
+                    {
+                        replicator.Tell(Dsl.Get(_keyC, new ReadAll(_timeOut)), probe);
+                        probe.ExpectMsg<GetSuccess>().Get(_keyC).Entries["x"].ShouldBe(new BigInteger(30.0d));
+                    }
+                });
+            });
+
+            Sys.Log.Info("Done");
         }
 
         protected override void BeforeTermination()
