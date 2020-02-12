a6aaa5adfe133aed45d593a4cc49dae09db437bd
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-02-13 04:06:38 +0700

    Fix Hocon.WithFallback failed to merge correctly when using Hocon with subtitutions.

--- src/core/Akka/Dispatch/CachingConfig.cs
+++ src/core/Akka/Dispatch/CachingConfig.cs
@@ -10,7 +10,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Akka.Annotations;
 using Hocon;
-using Akka.Configuration.Hocon;
 
 namespace Akka.Dispatch
 {
@@ -122,25 +121,23 @@ namespace Akka.Dispatch
 
         #endregion
 
-        private readonly Config _config;
         private readonly ConcurrentDictionary<string, IPathEntry> _entryMap;
 
         /// <summary>
         /// TBD
         /// </summary>
         /// <param name="config">TBD</param>
-        public CachingConfig(Config config) : base(Empty)
+        public CachingConfig(Config config) : base(config)
         {
-            if (config is CachingConfig cachingConfig)
-            {
-                _config = new Config(cachingConfig._config);
-                _entryMap = cachingConfig._entryMap;
-            }
+            if (config is CachingConfig caching)
+                _entryMap = caching._entryMap;
             else
-            {
-                _config = new Config(config);
                 _entryMap = new ConcurrentDictionary<string, IPathEntry>();
-            }
+        }
+
+        public CachingConfig(HoconRoot root) : base(root)
+        {
+            _entryMap = new ConcurrentDictionary<string, IPathEntry>();
         }
 
         private IPathEntry GetPathEntry(string path)
@@ -149,11 +146,11 @@ namespace Akka.Dispatch
             {
                 try
                 {
-                    if (_config.HasPath(path)) //found something
+                    if (base.HasPath(path)) //found something
                     {
                         try
                         {
-                            var configValue = _config.GetValue(path);
+                            var configValue = base.GetValue(path);
                             if (configValue == null) //empty
                                 pathEntry = EmptyPathEntry;
                             else if (configValue.Type == HoconType.String) //is a string value
@@ -189,150 +186,13 @@ namespace Akka.Dispatch
         }
 
         /// <inheritdoc />
-        public override Config WithFallback(Config fallback)
-        {
-            if (fallback.IsNullOrEmpty())
-                return this; // no-op
-
-            if (ReferenceEquals(fallback, _config))
-                throw new ArgumentException("Config can not have itself as fallback", nameof(fallback));
-
-            if (_config.IsEmpty)
-                return new CachingConfig(fallback);
-
-            return new CachingConfig(_config.WithFallback(fallback));
-        }
-
-        /// <inheritdoc />
         public override bool HasPath(string path)
         {
             var entry = GetPathEntry(path);
             if (entry.Valid)
                 return entry.Exists;
             else //run the real code in order to get exceptions
-                return _config.HasPath(path);
-        }
-
-        /// <inheritdoc />
-        public override bool IsEmpty
-        {
-            get { return _config.IsEmpty; }
-        }
-
-        /// <inheritdoc />
-        public override IEnumerable<KeyValuePair<string, HoconField>> AsEnumerable()
-        {
-            return _config.AsEnumerable();
-        }
-
-        /// <inheritdoc />
-        public override Config GetConfig(string path)
-        {
-            return _config.GetConfig(path);
-        }
-
-        /// <inheritdoc />
-        public override Config GetConfig(HoconPath path)
-        {
-            return _config.GetConfig(path);
-        }
-
-        #region Value getter functions
-
-        /// <inheritdoc />
-        public override bool GetBoolean(string path)
-        {
-            return _config.GetBoolean(path);
-        }
-
-        /// <inheritdoc />
-        public override bool GetBoolean(HoconPath path)
-        {
-            return _config.GetBoolean(path);
-        }
-
-        /// <inheritdoc />
-        public override bool GetBoolean(string path, bool @default = false)
-        {
-            return _config.GetBoolean(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override bool GetBoolean(HoconPath path, bool @default = false)
-        {
-            return _config.GetBoolean(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override int GetInt(string path)
-        {
-            return _config.GetInt(path);
-        }
-
-        /// <inheritdoc />
-        public override int GetInt(HoconPath path)
-        {
-            return _config.GetInt(path);
-        }
-
-        /// <inheritdoc />
-        public override int GetInt(string path, int @default = 0)
-        {
-            return _config.GetInt(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override int GetInt(HoconPath path, int @default = 0)
-        {
-            return _config.GetInt(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override long GetLong(string path)
-        {
-            return _config.GetLong(path);
-        }
-
-        /// <inheritdoc />
-        public override long GetLong(HoconPath path)
-        {
-            return _config.GetLong(path);
-        }
-
-        /// <inheritdoc />
-        public override long GetLong(string path, long @default = 0)
-        {
-            return _config.GetLong(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override long GetLong(HoconPath path, long @default = 0)
-        {
-            return _config.GetLong(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override double GetDouble(string path)
-        {
-            return _config.GetDouble(path);
-        }
-
-        /// <inheritdoc />
-        public override double GetDouble(HoconPath path)
-        {
-            return _config.GetDouble(path);
-        }
-
-        /// <inheritdoc />
-        public override double GetDouble(string path, double @default = 0)
-        {
-            return _config.GetDouble(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override double GetDouble(HoconPath path, double @default = 0)
-        {
-            return _config.GetDouble(path, @default);
+                return base.HasPath(path);
         }
 
         /// <inheritdoc />
@@ -374,285 +234,6 @@ namespace Akka.Dispatch
         {
             return GetString(path.ToString(), @default);
         }
-
-        /// <inheritdoc />
-        public override decimal GetDecimal(string path)
-        {
-            return _config.GetDecimal(path);
-        }
-
-        /// <inheritdoc />
-        public override decimal GetDecimal(HoconPath path)
-        {
-            return _config.GetDecimal(path);
-        }
-
-        /// <inheritdoc />
-        public override decimal GetDecimal(string path, decimal @default = 0)
-        {
-            return _config.GetDecimal(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override decimal GetDecimal(HoconPath path, decimal @default = 0)
-        {
-            return _config.GetDecimal(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<bool> GetBooleanList(string path)
-        {
-            return _config.GetBooleanList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<bool> GetBooleanList(HoconPath path)
-        {
-            return _config.GetBooleanList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<bool> GetBooleanList(string path, IList<bool> @default)
-        {
-            return _config.GetBooleanList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<bool> GetBooleanList(HoconPath path, IList<bool> @default)
-        {
-            return _config.GetBooleanList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<byte> GetByteList(string path)
-        {
-            return _config.GetByteList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<byte> GetByteList(HoconPath path)
-        {
-            return _config.GetByteList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<byte> GetByteList(string path, IList<byte> @default)
-        {
-            return _config.GetByteList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<byte> GetByteList(HoconPath path, IList<byte> @default)
-        {
-            return _config.GetByteList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override long? GetByteSize(string path)
-        {
-            return _config.GetByteSize(path);
-        }
-
-        /// <inheritdoc />
-        public override long? GetByteSize(HoconPath path)
-        {
-            return _config.GetByteSize(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<decimal> GetDecimalList(string path)
-        {
-            return _config.GetDecimalList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<decimal> GetDecimalList(HoconPath path)
-        {
-            return _config.GetDecimalList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<decimal> GetDecimalList(string path, IList<decimal> @default)
-        {
-            return _config.GetDecimalList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<decimal> GetDecimalList(HoconPath path, IList<decimal> @default)
-        {
-            return _config.GetDecimalList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<double> GetDoubleList(string path)
-        {
-            return _config.GetDoubleList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<double> GetDoubleList(HoconPath path)
-        {
-            return _config.GetDoubleList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<double> GetDoubleList(string path, IList<double> @default)
-        {
-            return _config.GetDoubleList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<double> GetDoubleList(HoconPath path, IList<double> @default)
-        {
-            return _config.GetDoubleList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override float GetFloat(string path)
-        {
-            return _config.GetFloat(path);
-        }
-
-        /// <inheritdoc />
-        public override float GetFloat(HoconPath path)
-        {
-            return _config.GetFloat(path);
-        }
-
-        /// <inheritdoc />
-        public override float GetFloat(string path, float @default)
-        {
-            return _config.GetFloat(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override float GetFloat(HoconPath path, float @default)
-        {
-            return _config.GetFloat(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<float> GetFloatList(string path)
-        {
-            return _config.GetFloatList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<float> GetFloatList(HoconPath path)
-        {
-            return _config.GetFloatList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<float> GetFloatList(string path, IList<float> @default)
-        {
-            return _config.GetFloatList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<float> GetFloatList(HoconPath path, IList<float> @default)
-        {
-            return _config.GetFloatList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<int> GetIntList(string path)
-        {
-            return _config.GetIntList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<int> GetIntList(HoconPath path)
-        {
-            return _config.GetIntList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<int> GetIntList(string path, IList<int> @default)
-        {
-            return _config.GetIntList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<int> GetIntList(HoconPath path, IList<int> @default)
-        {
-            return _config.GetIntList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<long> GetLongList(string path)
-        {
-            return _config.GetLongList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<long> GetLongList(HoconPath path)
-        {
-            return _config.GetLongList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<long> GetLongList(string path, IList<long> @default)
-        {
-            return _config.GetLongList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<long> GetLongList(HoconPath path, IList<long> @default)
-        {
-            return _config.GetLongList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<string> GetStringList(string path)
-        {
-            return _config.GetStringList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<string> GetStringList(HoconPath path)
-        {
-            return _config.GetStringList(path);
-        }
-
-        /// <inheritdoc />
-        public override IList<string> GetStringList(string path, IList<string> @default)
-        {
-            return _config.GetStringList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override IList<string> GetStringList(HoconPath path, IList<string> @default)
-        {
-            return _config.GetStringList(path, @default);
-        }
-
-        /// <inheritdoc />
-        public override TimeSpan GetTimeSpan(string path, bool allowInfinite = true)
-        {
-            return _config.GetTimeSpan(path, null, allowInfinite);
-        }
-
-        /// <inheritdoc />
-        public override TimeSpan GetTimeSpan(HoconPath path, bool allowInfinite = true)
-        {
-            return _config.GetTimeSpan(path, null, allowInfinite);
-        }
-
-        /// <inheritdoc />
-        public override TimeSpan GetTimeSpan(string path, TimeSpan? @default = null, bool allowInfinite = true)
-        {
-            return _config.GetTimeSpan(path, @default, allowInfinite);
-        }
-
-        /// <inheritdoc />
-        public override TimeSpan GetTimeSpan(HoconPath path, TimeSpan? @default = null, bool allowInfinite = true)
-        {
-            return _config.GetTimeSpan(path, @default, allowInfinite);
-        }
-
-        #endregion
-
     }
 }
 
