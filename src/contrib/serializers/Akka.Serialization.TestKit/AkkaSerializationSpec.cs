8583974e8f52aa3975b1f132757bf677c09747bd
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-02-13 05:49:40 +0700

    Remove Hocon project reference. Fix serialization spec unit test.

--- src/contrib/serializers/Akka.Serialization.TestKit/AkkaSerializationSpec.cs
+++ src/contrib/serializers/Akka.Serialization.TestKit/AkkaSerializationSpec.cs
@@ -329,7 +329,12 @@ akka.actor {
         [Fact]
         public void CanSerializeConfig()
         {
-            var message = ConfigurationFactory.Default();
+            var message = ConfigurationFactory.ParseString(@"
+my-settings{
+    a: 1
+    b: 2
+    c: 3
+}");
             var serializer = Sys.Serialization.FindSerializerFor(message);
             var serialized = serializer.ToBinary(message);
             var deserialized = (Config)serializer.FromBinary(serialized, typeof(Config));
