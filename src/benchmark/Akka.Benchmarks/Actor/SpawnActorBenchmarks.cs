e065d148f818ad95c40ac888a5d960bc731eebf2
Aaron Stannard <aaron@petabridge.com>
2020-02-17 12:03:43 -0600

    fixed SpawnActorBenchmark configuration

--- src/benchmark/Akka.Benchmarks/Actor/SpawnActorBenchmarks.cs
+++ src/benchmark/Akka.Benchmarks/Actor/SpawnActorBenchmarks.cs
@@ -14,8 +14,8 @@ using BenchmarkDotNet.Engines;
 
 namespace Akka.Benchmarks.Actor
 {
-    [Config(typeof(MonitoringConfig))]
-    [SimpleJob(RunStrategy.Monitoring, launchCount: 1, warmupCount: 1, targetCount: 1)]
+    [Config(typeof(MicroBenchmarkConfig))]
+    [SimpleJob(RunStrategy.Throughput, targetCount:20, warmupCount:5, invocationCount: ActorCount)]
     public class SpawnActorBenchmarks
     {
         public const int ActorCount = 100_000;
@@ -35,11 +35,10 @@ namespace Akka.Benchmarks.Actor
             system.Dispose();
         }
 
-        [Benchmark(OperationsPerInvoke = ActorCount)]
-        public async Task Actor_spawn()
+        [Benchmark]
+        public void Actor_spawn()
         {
             var parent = system.ActorOf(Parent.Props);
-            await parent.Ask(StartTest.Instance, timeout);
         }
 
         #region actors
