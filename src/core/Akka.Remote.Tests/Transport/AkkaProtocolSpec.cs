6043843f49a5cde138bc1e395def11e08ae2894b
Aaron Stannard <aaron@petabridge.com>
2020-02-26 15:45:43 -0600

    increased default timeout

--- src/core/Akka.Remote.Tests/Transport/AkkaProtocolSpec.cs
+++ src/core/Akka.Remote.Tests/Transport/AkkaProtocolSpec.cs
@@ -50,7 +50,7 @@ namespace Akka.Remote.Tests.Transport
         public AkkaProtocolSpec(ITestOutputHelper helper)
             : base(@"
                     akka.actor.provider = remote
-                    akka.test.default-timeout = 1.5 s", helper)
+                    akka.test.default-timeout = 4 s", helper)
         {
             codec = new AkkaPduProtobuffCodec(Sys);
             testEnvelope = codec.ConstructMessage(_localAkkaAddress, TestActor, testMsg);
