785d9681756b04284bcc29c9ff4ba083af289b12
Aaron Stannard <aaron@petabridge.com>
2020-02-26 10:34:23 -0600

    enabling debug configuration

--- src/contrib/cluster/Akka.Cluster.Sharding.Tests.MultiNode/ClusterShardingRememberEntitiesSpec.cs
+++ src/contrib/cluster/Akka.Cluster.Sharding.Tests.MultiNode/ClusterShardingRememberEntitiesSpec.cs
@@ -32,7 +32,7 @@ namespace Akka.Cluster.Sharding.Tests
             Second = Role("second");
             Third = Role("third");
 
-            CommonConfig = DebugConfig(false)
+            CommonConfig = DebugConfig(true)
                 .WithFallback(ConfigurationFactory.ParseString($@"
                     akka.actor {{
                         serializers {{
