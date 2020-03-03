0948525ff24f68fa939142f9d41f59744c0cd2ff
Aaron Stannard <aaron@petabridge.com>
2020-03-03 15:12:09 -0600

    updated copyright headers to 2020 (#4289)
    * updated copyright headers to 2020
    
    * fixed missing namespace issues
    
    * missed a couple of missing namespaces

--- src/core/Akka.Docs.Tests/Testkit/ProbeSampleTest.cs
+++ src/core/Akka.Docs.Tests/Testkit/ProbeSampleTest.cs
@@ -1,3 +1,10 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProbeSampleTest.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2020 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2020 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
 using Akka.Actor;
 using Akka.TestKit;
 using Akka.TestKit.Xunit2;
--- src/core/Akka.Docs.Tutorials/Tutorial3/DeviceGroupInProgress.cs
+++ src/core/Akka.Docs.Tutorials/Tutorial3/DeviceGroupInProgress.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections.Generic;
+﻿//-----------------------------------------------------------------------
+// <copyright file="DeviceGroupInProgress.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2020 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2020 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
 using Akka.Actor;
 using Akka.Event;
 using static Tutorials.Tutorial3.MainDeviceGroup;
