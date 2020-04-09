194f61ee353a41a697ebd2d30af18e0c2101fac0
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-04-09 21:58:26 +0700

    Add spec

--- src/core/Akka.Tests/Actor/ActorSelectionSpec.cs
+++ src/core/Akka.Tests/Actor/ActorSelectionSpec.cs
@@ -449,37 +449,43 @@ namespace Akka.Tests.Actor
             probe.ExpectMsg<ActorIdentity>().ShouldBeEquivalentTo(new ActorIdentity(7, null));
             probe.ExpectNoMsg(TimeSpan.FromMilliseconds(200));
 
-            // Double wildcard, recursively get all children below /user/a
-            var expecteds = new List<ActorIdentity>
-            {
-                new ActorIdentity(9, b1),
-                new ActorIdentity(9, b2),
-                new ActorIdentity(9, c),
-                new ActorIdentity(9, d)
-            };
-            Sys.ActorSelection("/user/a/**").Tell(new Identify(9), probe.Ref);
-            probe.ExpectMsg<ActorIdentity>().Should(i => EqualsOnce(i, expecteds), "");
-            probe.ExpectMsg<ActorIdentity>().Should(i => EqualsOnce(i, expecteds), "");
-            probe.ExpectMsg<ActorIdentity>().Should(i => EqualsOnce(i, expecteds), "");
-            probe.ExpectMsg<ActorIdentity>().Should(i => EqualsOnce(i, expecteds), "");
-            expecteds.Count.Should().Be(0);
-            probe.ExpectNoMsg(TimeSpan.FromMilliseconds(200));
-
             Sys.ActorSelection("/user/a/*/c/d/e").Tell(new Identify(8), probe.Ref);
             probe.ExpectNoMsg(TimeSpan.FromMilliseconds(500));
         }
 
-        private bool EqualsOnce(ActorIdentity source, List<ActorIdentity> expected)
+        [Fact]
+        public void An_ActorSelection_must_identify_actors_with_double_wildcard_selection_correctly()
         {
-            foreach(var e in expected)
-            {
-                if(source.Equals(e))
-                {
-                    expected.Remove(e);
-                    return true;
-                }
-            }
-            return false;
+            var creator = CreateTestProbe();
+            var top = Sys.ActorOf(Props, "a");
+            var b1 = top.Ask<IActorRef>(new Create("b1"), TimeSpan.FromSeconds(3)).Result;
+            var b2 = top.Ask<IActorRef>(new Create("b2"), TimeSpan.FromSeconds(3)).Result;
+            var b3 = top.Ask<IActorRef>(new Create("b3"), TimeSpan.FromSeconds(3)).Result;
+            var c1 = b2.Ask<IActorRef>(new Create("c1"), TimeSpan.FromSeconds(3)).Result;
+            var c2 = b2.Ask<IActorRef>(new Create("c2"), TimeSpan.FromSeconds(3)).Result;
+            var d = c1.Ask<IActorRef>(new Create("d"), TimeSpan.FromSeconds(3)).Result;
+
+            var probe = CreateTestProbe();
+
+            // grab everything below /user/a
+            Sys.ActorSelection("/user/a/**").Tell(new Identify(1), probe.Ref);
+            probe.ReceiveN(6)
+                .Cast<ActorIdentity>()
+                .Select(i => i.Subject)
+                .ShouldAllBeEquivalentTo(new[] { b1, b2, b3, c1, c2, d });
+            probe.ExpectNoMsg(TimeSpan.FromMilliseconds(500));
+
+            // grab everything below /user/a/b2
+            Sys.ActorSelection("/user/a/b2/**").Tell(new Identify(2), probe.Ref);
+            probe.ReceiveN(3)
+                .Cast<ActorIdentity>()
+                .Select(i => i.Subject)
+                .ShouldAllBeEquivalentTo(new[] { c1, c2, d });
+            probe.ExpectNoMsg(TimeSpan.FromMilliseconds(500));
+
+            // nothing under /user/a/b2/c1/d
+            Sys.ActorSelection("/user/a/b2/c1/d/**").Tell(new Identify(3), probe.Ref);
+            probe.ExpectNoMsg(TimeSpan.FromMilliseconds(500));
         }
 
         [Fact]
