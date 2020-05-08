de54377c2c851fe04a89d82acd569a868759b2ac
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-05-08 21:50:12 +0700

    Unroll recursion in SysMsgInvokeAll

--- src/core/Akka/Actor/ActorCell.DefaultMessages.cs
+++ src/core/Akka/Actor/ActorCell.DefaultMessages.cs
@@ -246,66 +246,76 @@ namespace Akka.Actor
 
         private void SysMsgInvokeAll(EarliestFirstSystemMessageList messages, int currentState)
         {
-            var rest = messages.Tail;
-            var message = messages.Head;
-            message.Unlink();
-
-            try
+            while (true)
             {
-                switch (message)
+                var rest = messages.Tail;
+                var message = messages.Head;
+                message.Unlink();
+
+                try
                 {
-                    case SystemMessage sm when ShouldStash(sm, currentState):
-                        Stash(message);
-                        break;
-                    case ActorTaskSchedulerMessage atsm:
-                        HandleActorTaskSchedulerMessage(atsm);
-                        break;
-                    case Failed f:
-                        HandleFailed(f);
-                        break;
-                    case DeathWatchNotification n:
-                        WatchedActorTerminated(n.Actor, n.ExistenceConfirmed, n.AddressTerminated);
-                        break;
-                    case Create c:
-                        Create(c.Failure);
-                        break;
-                    case Watch w:
-                        AddWatcher(w.Watchee, w.Watcher);
-                        break;
-                    case Unwatch uw:
-                        RemWatcher(uw.Watchee, uw.Watcher);
-                        break;
-                    case Recreate r:
-                        FaultRecreate(r.Cause);
-                        break;
-                    case Suspend _:
-                        FaultSuspend();
-                        break;
-                    case Resume r:
-                        FaultResume(r.CausedByFailure);
-                        break;
-                    case Terminate _:
-                        Terminate();
-                        break;
-                    case Supervise s:
-                        Supervise(s.Child, s.Async);
-                        break;
-                    default:
-                        throw new NotSupportedException($"Unknown message {message.GetType().Name}");
+                    switch (message)
+                    {
+                        case SystemMessage sm when ShouldStash(sm, currentState):
+                            Stash(message);
+                            break;
+                        case ActorTaskSchedulerMessage atsm:
+                            HandleActorTaskSchedulerMessage(atsm);
+                            break;
+                        case Failed f:
+                            HandleFailed(f);
+                            break;
+                        case DeathWatchNotification n:
+                            WatchedActorTerminated(n.Actor, n.ExistenceConfirmed, n.AddressTerminated);
+                            break;
+                        case Create c:
+                            Create(c.Failure);
+                            break;
+                        case Watch w:
+                            AddWatcher(w.Watchee, w.Watcher);
+                            break;
+                        case Unwatch uw:
+                            RemWatcher(uw.Watchee, uw.Watcher);
+                            break;
+                        case Recreate r:
+                            FaultRecreate(r.Cause);
+                            break;
+                        case Suspend _:
+                            FaultSuspend();
+                            break;
+                        case Resume r:
+                            FaultResume(r.CausedByFailure);
+                            break;
+                        case Terminate _:
+                            Terminate();
+                            break;
+                        case Supervise s:
+                            Supervise(s.Child, s.Async);
+                            break;
+                        default:
+                            throw new NotSupportedException($"Unknown message {message.GetType().Name}");
+                    }
                 }
-            }
-            catch (Exception cause)
-            {
-                HandleInvokeFailure(cause);
-            }
+                catch (Exception cause)
+                {
+                    HandleInvokeFailure(cause);
+                }
 
-            var newState = CalculateState();
-            // As each state accepts a strict subset of another state, it is enough to unstash if we "walk up" the state
-            // chain
-            var todo = newState < currentState ? rest + UnstashAll() : rest;
+                var nextState = CalculateState();
+                // As each state accepts a strict subset of another state, it is enough to unstash if we "walk up" the state
+                // chain
+                var todo = nextState < currentState ? rest + UnstashAll() : rest;
 
-            if (IsTerminated) SendAllToDeadLetters(todo);
-            else if (!todo.IsEmpty) SysMsgInvokeAll(todo, newState);
+                if (IsTerminated)
+                {
+                    SendAllToDeadLetters(todo);
+                    break;
+                }
+                else if (todo.IsEmpty) break;
+
+                currentState = nextState;
+                messages = todo;
+            }
         }
 
         /// <summary>
