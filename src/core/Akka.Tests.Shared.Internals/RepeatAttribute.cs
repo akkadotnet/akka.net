a056dc47a900fda1a4b4749460754dffe330767f
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-05-09 04:05:40 +0700

    Add RepeatAttribute to easily repeat a unit test multiple times to test for test flakiness

---
+++ src/core/Akka.Tests.Shared.Internals/RepeatAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit.Sdk;
+
+namespace Akka.Tests.Shared.Internals
+{
+    public sealed class RepeatAttribute : DataAttribute
+    {
+        private readonly int _count;
+
+        public RepeatAttribute(int count)
+        {
+            if (count < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count),
+                      "Repeat count must be greater than 0.");
+            }
+            _count = count;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            foreach(var x in Enumerable.Range(1, _count))
+            {
+                yield return new object[] { x };
+            }
+        }
+    }
+}
