683a858117a910abd498c94909f7e5fc20e651b0
IgorFedchenko <igor@fedchenko.pro>
2020-05-22 12:23:04 +0300

    Added WindowsForms project with actors in UI thread

---
+++ src/examples/WindowsForms.Sample/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Akka.Actor;
+
+namespace WindowsForms.Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <inheritdoc />
+        protected override async void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            // This is execured in UI thread
+            var system = ActorSystem.Create("WindowsFormSystem");
+            var helloActor = system.ActorOf(Props.Create<HelloActor>());
+            var response = await helloActor.Ask<string>("Hello");
+            MessageBox.Show($"Actor response: {response}");
+        }
+
+        public class HelloActor : ReceiveActor
+        {
+            public HelloActor()
+            {
+                Receive<string>(msg => Sender.Tell("Hello"));
+            }
+        }
+    }
+}
