8e76619b9318297a4fdedaf9d01f33acf9e12e7b
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-06-02 01:31:02 +0700

    Incomplete port of Control.scala

---
+++ src/core/Akka.Remote/Artery/Control.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// ARTERY: Incomplete implementation
+namespace Akka.Remote.Artery
+{
+    /// <summary>
+    /// INTERNAL API
+    /// 
+    /// marker trait for protobuf-serializable artery messages
+    /// </summary>
+    internal interface IArteryMessage
+    { 
+        // Empty
+    }
+
+    /// <summary>
+    /// INTERNAL API
+    /// 
+    /// Marker trait for reply messages
+    /// </summary>
+    internal interface IReply : IControlMessage
+    {
+        // Empty
+    }
+
+    /// <summary>
+    /// INTERNAL API
+    /// 
+    /// Marker trait for control messages that can be sent via the system message sub-channel
+    /// but don't need full reliable delivery. E.g. `HandshakeReq` and `Reply`.
+    /// </summary>
+    internal interface IControlMessage : IArteryMessage
+    {
+        // Empty
+    }
+}
