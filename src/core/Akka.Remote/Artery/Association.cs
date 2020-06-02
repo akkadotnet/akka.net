cee41440c73620d5be1384763c56a13d60a59f60
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-06-02 22:26:17 +0700

    Incomplete port of Association.scala

---
+++ src/core/Akka.Remote/Artery/Association.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using Akka.Actor;
+
+namespace Akka.Remote.Artery
+{
+    // ARTERY: Incomplete implementation
+    internal class Association
+    {
+        /// <summary>
+        /// Holds reference to shared state of Association - *access only via helper methods*
+        /// </summary>
+        private volatile AssociationState _sharedStateDoNotCallMeDirectly = new AssociationState();
+
+        public bool SwapState(AssociationState oldState, AssociationState newState)
+        {
+            // ARTERY: stub function
+            return false;
+        }
+
+        public AssociationState AssociationState => Volatile.Read(ref _sharedStateDoNotCallMeDirectly);
+
+        public int SendTerminationHint(IActorRef replyTo)
+        {
+            // ARTERY: stub function
+            return 0;
+        }
+    }
+}
