bffdf17cea09d216122cf88a4c13923620bd1ff9
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-06-06 00:15:18 +0700

    Port Akka.Remote.Artery settings files
    (cherry picked from commit 752f561d103de5f7e9deee14c57b29218a302dbe)

---
+++ src/core/Akka.Remote/Artery/Settings/CompressionSettings.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Configuration;
+
+namespace Akka.Remote.Artery.Settings
+{
+    internal class CompressionSettings
+    {
+        public bool Debug => false;
+        public bool Enabled => ActorRefs.Max > 0 || Manifests.Max > 0;
+        public ActorRefsSettings ActorRefs { get; }
+        public ManifestsSettings Manifests { get; }
+
+        public CompressionSettings(Config config)
+        {
+            var compressionConfig = config.GetConfig("compression");
+            if (compressionConfig.IsNullOrEmpty())
+                throw ConfigurationException.NullOrEmptyConfig<CompressionSettings>("akka.remote.artery.advanced.compression");
+
+            ActorRefs = new ActorRefsSettings(compressionConfig);
+            Manifests = new ManifestsSettings(compressionConfig);
+        }
+    }
+}
