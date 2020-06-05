312bd45faeb241516d27a2af1bc6446882b9b2e1
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-06-05 23:19:46 +0700

    Move all internal public interfaces into its own namespace

---
+++ src/core/Akka.Remote/Artery/Interfaces/IOutboundEnvelope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Akka.Util;
+
+namespace Akka.Remote.Artery.Interfaces
+{
+    /// <summary>
+    /// INTERNAL API
+    /// </summary>
+    internal interface IOutboundEnvelope : INoSerializationVerificationNeeded
+    {
+        Option<RemoteActorRef> Recipient { get; }
+        object Message { get; }
+        Option<IActorRef> Sender { get; }
+
+        IOutboundEnvelope WithMessage(object Message);
+        IOutboundEnvelope Copy();
+    }
+
+}
