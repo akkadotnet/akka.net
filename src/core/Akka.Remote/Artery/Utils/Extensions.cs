b89c18bd62985c943286408b40564754758c4ce3
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-06-27 08:24:38 +0700

    Add NextPowerOfTwo, DebugString for IDIctionary, and NonFatal exception checker to helper extensions

--- src/core/Akka.Remote/Artery/Utils/Extensions.cs
+++ src/core/Akka.Remote/Artery/Utils/Extensions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading;
+using Akka.Event;
+using Debug = System.Diagnostics.Debug;
 
 namespace Akka.Remote.Artery.Utils
 {
@@ -52,5 +54,50 @@ namespace Akka.Remote.Artery.Utils
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static uint RotateRight(this uint i, int distance)
             => (i >> distance) | (i << -distance);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int NextPowerOfTwo(this int value)
+        {
+            // Taken from https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+
+            Debug.Assert(value >= 0);
+
+            // If the number is already a power of 2, we want to round to itself.
+            value--;
+
+            // Propogate 1-bits right: if the highest bit set is @ position n,
+            // then all of the bits to the right of position n will become set.
+            value |= value >> 1;
+            value |= value >> 2;
+            value |= value >> 4;
+            value |= value >> 8;
+            value |= value >> 16;
+
+            // This yields a number of the form 2^N - 1.
+            // Add 1 to get a power of 2 with the bit set @ position n + 1.
+            return value + 1;
+        }
+
+        public static string DebugString<TKey, TValue>(this IDictionary<TKey, TValue> dict)
+        {
+            var sb = new StringBuilder($"[{Logging.SimpleName(dict.GetType())}]: {{");
+            foreach (var kvp in dict)
+            {
+                sb.AppendLine($"  {kvp.Key}: {kvp.Value},");
+            }
+
+            return sb.AppendLine("}}").ToString();
+        }
+
+        public static bool NonFatal(this Exception e)
+        {
+            switch (e)
+            {
+                case null: return true;
+                case SystemException _:
+                    return false;
+                default: return true;
+            }
+        }
     }
 }
