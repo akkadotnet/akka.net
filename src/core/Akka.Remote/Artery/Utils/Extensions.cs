7dcd5f785b6893b02631d886296cd47a84d06614
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-06-30 22:44:31 +0700

    Move dictionary extensions to its own static class

--- src/core/Akka.Remote/Artery/Utils/Extensions.cs
+++ src/core/Akka.Remote/Artery/Utils/Extensions.cs
@@ -11,31 +11,6 @@ namespace Akka.Remote.Artery.Utils
     internal static class Extensions
     {
         /// <summary>
-        /// Get a value for a given key, or if it does not exist then default the value via a
-        /// <see cref="Func{TKey, TResult}"/> and put it in the Dictionary.
-        /// </summary>
-        /// <typeparam name="TKey">key type of Dictionary</typeparam>
-        /// <typeparam name="TValue">value type of Dictionary</typeparam>
-        /// <param name="dict">the <see cref="Dictionary{TKey,TValue}"/> to search on.</param>
-        /// <param name="key">to search on.</param>
-        /// <param name="mappingFunction">to provide a value if <see cref="Dictionary{TKey,TValue}.TryGetValue"/> returns false.</param>
-        /// <returns>the value if found otherwise the default.</returns>
-        public static TValue ComputeIfAbsent<TKey, TValue>(
-            this Dictionary<TKey, TValue> dict, 
-            TKey key,
-            Func<TKey, TValue> mappingFunction)
-        {
-            if (dict.TryGetValue(key, out var value))
-                return value;
-
-            value = mappingFunction(key);
-            if(!value.Equals(default(TValue)))
-                dict.Add(key, value);
-
-            return value;
-        }
-
-        /// <summary>
         /// Bitwise rotate left an unsigned integer
         /// </summary>
         /// <param name="i"></param>
@@ -76,17 +51,6 @@ namespace Akka.Remote.Artery.Utils
             // This yields a number of the form 2^N - 1.
             // Add 1 to get a power of 2 with the bit set @ position n + 1.
             return value + 1;
-        }
-
-        public static string DebugString<TKey, TValue>(this IDictionary<TKey, TValue> dict)
-        {
-            var sb = new StringBuilder($"[{Logging.SimpleName(dict.GetType())}]: {{");
-            foreach (var kvp in dict)
-            {
-                sb.AppendLine($"  {kvp.Key}: {kvp.Value},");
-            }
-
-            return sb.AppendLine("}}").ToString();
         }
 
         public static bool NonFatal(this Exception e)
