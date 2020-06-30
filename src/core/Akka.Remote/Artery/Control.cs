5579c67eefaea74574336405f1d6cdb1a050c476
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-06-30 22:49:04 +0700

    Partial Control.cs implementation

---
+++ src/core/Akka.Remote/Artery/Control.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.Remote.Artery
+{
+    /// <summary>
+    /// INTERNAL API
+    /// marker trait for protobuf-serializable artery messages
+    /// </summary>
+    internal interface IArteryMessage 
+    { }
+
+    /// <summary>
+    /// INTERNAL API
+    /// Marker trait for control messages that can be sent via the system message sub-channel
+    /// but don't need full reliable delivery. E.g. `HandshakeReq` and `Reply`.
+    /// </summary>
+    internal interface IControlMessage : IArteryMessage
+    { }
+
+    /// <summary>
+    /// INTERNAL API
+    /// Marker trait for reply messages
+    /// </summary>
+    interface IReply : IControlMessage
+    { }
+
+    // ARTERY: Incomplete implementation
+}
