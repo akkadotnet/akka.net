07f712872d4cf5581552b48f10d090b2df95e7f2
Andrew El <laingas@gmail.com>
2020-10-17 14:24:34 -0400

    Make sure Protobuf bytearray hack won't blow up if protobuf internals change

--- src/core/Akka.Remote/Transport/Streaming/StreamingTcpTransport.cs
+++ src/core/Akka.Remote/Transport/Streaming/StreamingTcpTransport.cs
@@ -58,7 +58,6 @@ namespace Akka.Remote.Transport.Streaming
                     .OfferAsync(
                         IO.ByteString.FromBytes(
                             _getByteArrayUnsafeFunc(payload)
-                            //payload.ToByteArray()
                             ))
                     .Result is QueueOfferResult
                     .Enqueued;
@@ -70,11 +69,23 @@ namespace Akka.Remote.Transport.Streaming
 
         private static Func<Google.Protobuf.ByteString, byte[]> build()
         {
-            var p = Expression.Parameter(typeof(ByteString));
-            return Expression.Lambda<Func<ByteString, byte[]>>(Expression.Field(p,
-                    typeof(ByteString).GetField("bytes",
-                        BindingFlags.NonPublic | BindingFlags.Instance)), p)
-                .Compile();
+            //See if we can 'cheat' and grab the byte array directly.
+            //If for some reason we can't (CAS in Framework, change in protobuf)
+            //We instead fall-back to simple version which allocates
+            //but works.
+            try
+            {
+                var p = Expression.Parameter(typeof(ByteString));
+                return Expression.Lambda<Func<ByteString, byte[]>>(Expression.Field(p,
+                        typeof(ByteString).GetField("bytes",
+                            BindingFlags.NonPublic | BindingFlags.Instance)), p)
+                    .Compile();
+            }
+            catch (Exception e)
+            {
+                return s => s.ToByteArray();
+            }
+            
         }
 
 
