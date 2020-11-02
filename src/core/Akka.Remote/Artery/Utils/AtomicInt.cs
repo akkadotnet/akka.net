2005dbab0150761e378c286e3e5bc7d61984c899
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-11-03 02:53:53 +0700

    Implement LinkedBlockingQueue

---
+++ src/core/Akka.Remote/Artery/Utils/AtomicInt.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Akka.Remote.Artery.Utils
+{
+    internal class AtomicInt
+    {
+        private int _value;
+        private readonly object _lock = new object();
+
+        /// <summary>
+        /// Sets the initial value of this <see cref="AtomicLong"/> to <paramref name="initialValue"/>.
+        /// </summary>
+        /// <param name="initialValue">TBD</param>
+        public AtomicInt(int initialValue = 0)
+        {
+            _value = initialValue;
+        }
+
+        /// <summary>
+        /// The current value of this <see cref="AtomicInt"/>
+        /// </summary>
+        public int Value
+        {
+            get {
+                lock (_lock)
+                {
+                    return _value;
+                }
+            }
+            set => Interlocked.Exchange(ref _value, value);
+        }
+
+        /// <summary>
+        /// If <see cref="Value"/> equals <paramref name="expected"/>, then set the Value to
+        /// <paramref name="newValue"/>.
+        /// </summary>
+        /// <param name="expected">TBD</param>
+        /// <param name="newValue">TBD</param>
+        /// <returns><c>true</c> if <paramref name="newValue"/> was set</returns>
+        public bool CompareAndSet(int expected, int newValue)
+        {
+            var previous = Interlocked.CompareExchange(ref _value, newValue, expected);
+            return previous == expected;
+        }
+
+        /// <summary>
+        /// Atomically sets the <see cref="Value"/> to <paramref name="newValue"/> and returns the old <see cref="Value"/>.
+        /// </summary>
+        /// <param name="newValue">The new value</param>
+        /// <returns>The old value</returns>
+        public int GetAndSet(int newValue)
+            => Interlocked.Exchange(ref _value, newValue);
+
+        public int GetAndAdd(int delta)
+        {
+            lock (_lock)
+            {
+                var pref = _value;
+                _value += delta;
+                return pref;
+            }
+        }
+
+        public int GetAndIncrement()
+        {
+            lock (_lock)
+            {
+                var pref = _value;
+                ++_value;
+                return pref;
+            }
+        }
+
+        public int Increment()
+            => Interlocked.Increment(ref _value);
+
+        public int GetAndDecrement()
+        {
+            lock (_lock)
+            {
+                var pref = _value;
+                --_value;
+                return pref;
+            }
+        }
+
+        public int Decrement()
+            => Interlocked.Decrement(ref _value);
+    }
+}
---
+++ src/core/Akka.Remote/Artery/Utils/IQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.Remote.Artery.Utils
+{
+    /// <summary>
+    /// A wrapper/facade interface for C# Queue to emulate java.util.Queue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IQueue<T> : ICollection<T>
+    {
+        /// <summary>
+        /// Inserts the specified element into this queue if it is possible to do so
+        /// immediately without violating capacity restrictions, returning
+        /// {@code true} upon success and throwing an {@code IllegalStateException}
+        /// if no space is currently available.
+        /// </summary>
+        /// <param name="e">the element to add</param>
+        /// <returns>{@code true}</returns>
+        new bool Add(T e);
+
+        /// <summary>
+        /// Inserts the specified element into this queue if it is possible to do
+        /// so immediately without violating capacity restrictions.
+        /// When using a capacity-restricted queue, this method is generally
+        /// preferable to {@link #add}, which can fail to insert an element only
+        /// by throwing an exception.
+        /// </summary>
+        /// <param name="e">the element to add</param>
+        /// <returns>{@code true} if the element was added to this queue, else {@code false}</returns>
+        bool Offer(T e);
+
+        /// <summary>
+        /// Retrieves and removes the head of this queue.  This method differs
+        /// from {@link #poll() poll()} only in that it throws an exception if
+        /// this queue is empty.
+        /// </summary>
+        /// <returns>the head of this queue</returns>
+        T Remove();
+
+        /// <summary>
+        /// Retrieves and removes the head of this queue,
+        /// or returns {@code null} if this queue is empty.
+        /// </summary>
+        /// <returns>the head of this queue, or {@code null} if this queue is empty</returns>
+        T Poll();
+
+        /// <summary>
+        /// Retrieves, but does not remove, the head of this queue.  This method
+        /// differs from {@link #peek peek} only in that it throws an exception
+        /// if this queue is empty.
+        /// </summary>
+        /// <returns>the head of this queue</returns>
+        T Element();
+
+        /// <summary>
+        /// Retrieves, but does not remove, the head of this queue,
+        /// or returns {@code null} if this queue is empty.
+        /// </summary>
+        /// <returns>the head of this queue, or {@code null} if this queue is empty</returns>
+        T Peek();
+
+        // From here on out, declared function interface is based on java.util.Collection
+        bool AddAll(ICollection<T> c);
+        T[] ToArray();
+        T[] ToArray(T[] a);
+    }
+}
