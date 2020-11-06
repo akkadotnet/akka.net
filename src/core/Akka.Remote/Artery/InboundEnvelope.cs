9760dc9475e2686c71d023979f156ad5c45389f6
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-11-07 00:59:18 +0700

    Rebase fix - implement ObjectPool

--- src/core/Akka.Remote/Artery/InboundEnvelope.cs
+++ src/core/Akka.Remote/Artery/InboundEnvelope.cs
@@ -34,9 +34,8 @@ namespace Akka.Remote.Artery
 
     internal class ReusableInboundEnvelope : IInboundEnvelope
     {
-        // ARTERY: ObjectPool isn't implemented yet
-        // public static ObjectPool<ReusableInboundEnvelope> CreateObjectPool(int capacity)
-        //     => new ObjectPool<ReusableInboundEnvelope>(capacity, () => new ReusableInboundEnvelope(), env => env.Clear());
+        public static ObjectPool<ReusableInboundEnvelope> CreateObjectPool(int capacity)
+            => new ObjectPool<ReusableInboundEnvelope>(capacity, create: () => new ReusableInboundEnvelope(), clear: env => env.Clear());
 
         public Option<IInternalActorRef> Recipient { get; private set; }
 
---
+++ src/core/Akka.Remote/Artery/ObjectPool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.Remote.Artery
+{
+    internal class ObjectPool<T>
+    {
+        public int Capacity { get; }
+        public Func<T> Create { get; }
+        public Action<T> Clear { get; }
+
+        private readonly ConcurrentQueue<T> _storage = new ConcurrentQueue<T>();
+
+        public ObjectPool(int capacity, Func<T> create, Action<T> clear)
+        {
+            Capacity = capacity;
+            Create = create;
+            Clear = clear;
+        }
+
+        public T Acquire()
+        {
+            if (_storage.TryDequeue(out var obj))
+                return obj;
+            obj = Create();
+            return obj;
+        }
+
+        public bool Release(T obj)
+        {
+            if (_storage.Count == Capacity) return false;
+
+            Clear(obj);
+            _storage.Enqueue(obj);
+            return true;
+        }
+    }
+}
