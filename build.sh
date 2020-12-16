14dc58039215a747da77737aa7453b3b2ff0c136
eabanonu <eabanonu@yahoo.com>
2020-12-16 14:47:50 +0100

    Update Fake to Fake-CLI 5.20.3 and INCREMENTALIST to 0.3.0

--- build.sh
+++ build.sh
@@ -10,14 +10,14 @@ INCREMENTALIST_DIR=$TOOLS_DIR/incrementalist
 INCREMENTALIST_EXE=$INCREMENTALIST_DIR/Incrementalist.Cmd.exe
 NUGET_EXE=$TOOLS_DIR/nuget.exe
 NUGET_URL=https://dist.nuget.org/win-x86-commandline/v4.3.0/nuget.exe
-FAKE_VERSION=4.63.0
+FAKE_VERSION=5.20.3
 FAKE_EXE=$TOOLS_DIR/FAKE/tools/FAKE.exe
 DOTNET_EXE=$SCRIPT_DIR/.dotnet/dotnet
 DOTNET_VERSION=5.0.101
 DOTNET_INSTALLER_URL=https://dot.net/v1/dotnet-install.sh
 DOTNET_CHANNEL=LTS
 PROTOBUF_VERSION=3.4.0
-INCREMENTALIST_VERSION=0.2.2
+INCREMENTALIST_VERSION=0.3.0
 
 # Define default arguments.
 TARGET="Default"
@@ -80,7 +80,7 @@ fi
 ###########################################################################
 
 if [ ! -f "$FAKE_EXE" ]; then
-    mono "$NUGET_EXE" install Fake -ExcludeVersion -Version $FAKE_VERSION -OutputDirectory "$TOOLS_DIR"
+    mono "$NUGET_EXE" install fake-cli -ExcludeVersion -Version $FAKE_VERSION -OutputDirectory "$TOOLS_DIR"
     if [ $? -ne 0 ]; then
         echo "An error occured while installing Cake."
         exit 1
