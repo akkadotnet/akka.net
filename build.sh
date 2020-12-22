828a82254c3a3cb56d244ee8048b56002afaea13
eabanonu <eabanonu@yahoo.com>
2020-12-22 19:04:48 +0100

    Check directory

--- build.sh
+++ build.sh
@@ -48,20 +48,10 @@ fi
 ###########################################################################
 # INSTALL .NET CORE CLI
 ###########################################################################
-
-echo "Installing .NET 5 SDK"
+echo "Installing .NET CLI..."
 if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
   mkdir "$SCRIPT_DIR/.dotnet"
 fi
-curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" $DOTNET_INSTALLER_URL
-bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION --channel $DOTNET_CHANNEL --install-dir .dotnet --no-path
-export PATH="$SCRIPT_DIR/.dotnet":$PATH
-export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
-chmod -R 0755 ".dotnet"
-"$SCRIPT_DIR/.dotnet/dotnet" --info
-
-echo "Installing .NET CLI..."
 curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" $DOTNET_INSTALLER_URL
 bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNETCORE_VERSION --channel $DOTNET_CHANNEL --install-dir .dotnet --no-path
 export PATH="$SCRIPT_DIR/.dotnet":$PATH
