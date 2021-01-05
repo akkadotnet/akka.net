86f47bcddada73599973413f872cfc0a40a9719a
Aaron Stannard <aaron@petabridge.com>
2021-01-04 20:23:32 -0600

    added ability to mix scopes inside actor

--- src/contrib/dependencyinjection/Akka.DependencyInjection.Tests/ActorServiceProviderPropsWithScopesSpecs.cs
+++ src/contrib/dependencyinjection/Akka.DependencyInjection.Tests/ActorServiceProviderPropsWithScopesSpecs.cs
@@ -83,6 +83,41 @@ namespace Akka.DependencyInjection.Tests
             deps2.Dependencies.All(x => x.Disposed).Should().BeFalse();
         }
 
+        [Fact(DisplayName =
+            "DI: actors who receive a mix of dependencies via IServiceScope should dispose ONLY of their scoped dependencies and recreate upon restart")]
+        public void ActorsWithMixedDependenciesShouldDisposeAndRecreateScopedUponRestart()
+        {
+            var spExtension = ServiceProvider.For(Sys);
+            var props = spExtension.Props(sp => new MixedActor(sp.GetRequiredService<AkkaDiFixture.ISingletonDependency>(), sp.CreateScope()));
+
+            // create a scoped actor using the props from Akka.DependencyInjection
+            var scoped1 = Sys.ActorOf(props, "scoped1");
+            scoped1.Tell(new FetchDependencies());
+            var deps1 = ExpectMsg<CurrentDependencies>();
+            deps1.Dependencies.All(x => x.Disposed).Should().BeFalse();
+
+            // crash + restart it
+            EventFilter.Exception<ApplicationException>().ExpectOne(() =>
+            {
+                scoped1.Tell(new Crash());
+            });
+
+            // all previous SCOPED dependencies should be disposed
+            deps1.Dependencies.Where(x => !(x is AkkaDiFixture.ISingletonDependency)).All(x => x.Disposed).Should().BeTrue();
+
+            // singletons should not be disposed
+            deps1.Dependencies.Where(x => (x is AkkaDiFixture.ISingletonDependency)).All(x => x.Disposed).Should().BeFalse();
+
+            // actor should restart with totally new dependencies (minus the singleton)
+            scoped1.Tell(new FetchDependencies());
+            var deps2 = ExpectMsg<CurrentDependencies>();
+            deps2.Dependencies.All(x => x.Disposed).Should().BeFalse();
+            var deps1Single = deps1.Dependencies.Single(x => (x is AkkaDiFixture.ISingletonDependency));
+            var deps2Single = deps2.Dependencies.Single(x => (x is AkkaDiFixture.ISingletonDependency));
+
+            deps1Single.Should().Be(deps2Single);
+        }
+
         public class Crash { }
 
         public class FetchDependencies { }
@@ -110,6 +145,38 @@ namespace Akka.DependencyInjection.Tests
                 Receive<FetchDependencies>(_ =>
                 {
                     Sender.Tell(new CurrentDependencies(new AkkaDiFixture.IDependency[] { _transient, _scoped }));
+                });
+
+                Receive<Crash>(_ => throw new ApplicationException("crash"));
+            }
+
+            protected override void PreStart()
+            {
+                _scoped = _scope.ServiceProvider.GetService<AkkaDiFixture.IScopedDependency>();
+                _transient = _scope.ServiceProvider.GetRequiredService<AkkaDiFixture.ITransientDependency>();
+            }
+
+            protected override void PostStop()
+            {
+                _scope.Dispose();
+            }
+        }
+
+        public class MixedActor : ReceiveActor
+        {
+            private readonly AkkaDiFixture.ISingletonDependency _singleton;
+            private readonly IServiceScope _scope;
+            private AkkaDiFixture.ITransientDependency _transient;
+            private AkkaDiFixture.IScopedDependency _scoped;
+
+            public MixedActor(AkkaDiFixture.ISingletonDependency singleton, IServiceScope scope)
+            {
+                _singleton = singleton;
+                _scope = scope;
+
+                Receive<FetchDependencies>(_ =>
+                {
+                    Sender.Tell(new CurrentDependencies(new AkkaDiFixture.IDependency[] { _transient, _scoped, _singleton }));
                 });
 
                 Receive<Crash>(_ => throw new ApplicationException("crash"));
