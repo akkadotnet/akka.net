076d8de5b260a75c82b2a0151e56fce410d24cd4
Igor Fedchenko <igor@fedchenko.pro>
2021-02-09 17:18:36 +0300

    Make Expect(0) to throw when any event received (#4771)
    * Added failing spec
    
    * Fixed expecting code
    
    * Restart CI

--- src/core/Akka.TestKit.Tests/TestEventListenerTests/AllTestForEventFilterBase.cs
+++ src/core/Akka.TestKit.Tests/TestEventListenerTests/AllTestForEventFilterBase.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Threading.Tasks;
 using Akka.Event;
+using FluentAssertions;
 using Xunit;
 using Xunit.Sdk;
 
@@ -126,8 +127,6 @@ namespace Akka.TestKit.Tests.Xunit2.TestEventListenerTests
             TestSuccessful = true;
         }
 
-
-
         [Fact]
         public void Specified_numbers_of_messagesan_be_intercepted()
         {
@@ -139,6 +138,17 @@ namespace Akka.TestKit.Tests.Xunit2.TestEventListenerTests
             TestSuccessful = true;
         }
 
+        [Fact]
+        public void Expect_0_events_Should_work()
+        {
+            this.Invoking(_ =>
+            {
+                EventFilter.Error().Expect(0, () =>
+                {
+                    Log.Error("something");
+                });
+            }).Should().Throw<Exception>("Expected 0 events");
+        }
 
         [Fact]
         public void Messages_can_be_muted()
