d34e11a2aa28bcc39464c667d99522f2c7b9c181
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-02-20 02:21:54 +0700

    Implement a rudimentary port of java CountDownLatch

---
+++ src/core/Akka.Remote/Artery/Utils/CountDownLatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Akka.Remote.Artery.Utils
+{
+    // Best fit approximation of how java CountDownLatch works.
+    internal class CountDownLatch :IDisposable
+    {
+        private int _count;
+        private readonly CancellationTokenSource _cts;
+        private readonly TaskCompletionSource<object> _tcs;
+
+        public CountDownLatch(int count)
+        {
+            if (count < 1)
+                throw new ArgumentException("count < 1", nameof(count));
+
+            _count = count;
+            _cts = new CancellationTokenSource();
+            _tcs = new TaskCompletionSource<object>(
+                TaskCreationOptions.LongRunning | TaskCreationOptions.RunContinuationsAsynchronously);
+
+            _cts.Token.Register(() =>
+            {
+                _tcs.SetCanceled();
+            });
+        }
+
+        public Task Await()
+        {
+            return _tcs.Task;
+        }
+
+        public Task Await(TimeSpan timeout)
+        {
+            _cts.CancelAfter(timeout);
+            return _tcs.Task;
+        }
+
+        public void CountDown()
+        {
+            Interlocked.Decrement(ref _count);
+            if(_count == 0)
+                _tcs.SetResult(null);
+        }
+
+        public void Dispose()
+        {
+            _cts?.Dispose();
+        }
+    }
+}
