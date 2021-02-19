6678ed3095c79fcd151bf7dbe1efb31e62b354f5
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-02-19 21:46:32 +0700

    Add Contains<T>() QOL method in Option extension class

---
+++ src/core/Akka.Remote/Artery/Utils/OptionExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Util;
+
+namespace Akka.Remote.Artery.Utils
+{
+    public static class OptionExtension
+    {
+        public static bool Contains<T>(this Util.IOptionVal<T> option, T value)
+            => option.HasValue && option.Value.Equals(value);
+    }
+}
