896d0fb345ceab13d7825d624b2c438b721ea701
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-02-19 21:47:17 +0700

    add MessageDispatcher stub class

---
+++ src/core/Akka.Remote/Artery/MessageDispatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace Akka.Remote.Artery
+{
+    // ARTERY: Empty skeleton class. NEED PORTING
+    internal class MessageDispatcher
+    {
+        public MessageDispatcher(ExtendedActorSystem system, RemoteActorRefProvider provider)
+        {
+
+        }
+    }
+}
