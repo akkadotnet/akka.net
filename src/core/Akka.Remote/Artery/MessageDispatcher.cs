e419f82c3448d942261a5c5c33fb9c34af1f2b1f
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-02-20 04:10:13 +0700

    Add stub functions to MessageDispatcher.cs

--- src/core/Akka.Remote/Artery/MessageDispatcher.cs
+++ src/core/Akka.Remote/Artery/MessageDispatcher.cs
@@ -1,16 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using Akka.Actor;
+using Akka.Event;
+using Akka.Remote.Artery.Utils;
+using Akka.Util;
 
 namespace Akka.Remote.Artery
 {
-    // ARTERY: Empty skeleton class. NEED PORTING
     internal class MessageDispatcher
     {
+        private readonly ExtendedActorSystem _system;
+        private readonly RemoteActorRefProvider _provider;
+
+        private readonly IInternalActorRef _remoteDaemon;
+        private readonly ILoggingAdapter _log;
+        private readonly bool _debugLogEnabled;
+
         public MessageDispatcher(ExtendedActorSystem system, RemoteActorRefProvider provider)
         {
+            _system = system;
+            _provider = provider;
 
+            _remoteDaemon = provider.RemoteDaemon;
+            _log = Logging.GetLogger(system, GetType().Name); //Logging.WithMarker(system, GetType().Name);
+            _debugLogEnabled = _log.IsDebugEnabled;
+        }
+
+        public void Dispatch(IInboundEnvelope inboundEnvelope)
+        {
+            throw new NotImplementedException();
         }
     }
 }
