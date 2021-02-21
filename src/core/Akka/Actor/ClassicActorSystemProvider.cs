b5586d294c7d1dfe3e2758f0f92bc896e962f7f4
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-02-21 08:41:46 +0700

    add IClassicActorSystemProvider interface

---
+++ src/core/Akka/Actor/ClassicActorSystemProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Annotations;
+
+namespace Akka.Actor
+{
+    /// <summary>
+    /// Glue API introduced to allow minimal user effort integration between classic and typed for example for streams.
+    ///
+    /// Not for user extension.
+    /// </summary>
+    [InternalApi]
+    public interface IClassicActorSystemProvider
+    {
+        /// <summary>
+        /// Allows access to the classic `akka.actor.ActorSystem` even for `akka.actor.typed.ActorSystem[_]`s.
+        /// </summary>
+        ActorSystem ClassicSystem { get; }
+    }
+
+    /// <summary>
+    /// Glue API introduced to allow minimal user effort integration between classic and typed for example for streams.
+    ///
+    /// Not for user extension.
+    /// </summary>
+    [InternalApi]
+    public interface IClassicActorContextProvider
+    {
+        /// <summary>
+        /// INTERNAL API
+        /// </summary>
+        IActorContext ClassicActorContext { get; }
+    }
+}
