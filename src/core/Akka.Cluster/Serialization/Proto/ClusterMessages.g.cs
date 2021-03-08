cf608684c18d7cfacdf6dfb7b9183e18fac5c5d0
zbynek001 <zbynek001@gmail.com>
2021-03-09 00:09:33 +0100

    protobuf generated

--- src/core/Akka.Cluster/Serialization/Proto/ClusterMessages.g.cs
+++ src/core/Akka.Cluster/Serialization/Proto/ClusterMessages.g.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="ClusterMessages.g.cs" company="Akka.NET Project">
 //     Copyright (C) 2009-2021 Lightbend Inc. <http://www.lightbend.com>
 //     Copyright (C) 2013-2021 .NET Foundation <https://github.com/akkadotnet/akka.net>
