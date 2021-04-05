dc2c12027f9b9844d83fad3b502b2197aa2aa874
Aaron Stannard <aaron@petabridge.com>
2021-04-05 08:53:34 -0500

    fixed up copyright headers (#4898)

--- src/benchmark/SerializationBenchmarks/Program.cs
+++ src/benchmark/SerializationBenchmarks/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2021 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2021 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Runtime.CompilerServices;
 using Akka.Actor;
 using Akka.Configuration;
