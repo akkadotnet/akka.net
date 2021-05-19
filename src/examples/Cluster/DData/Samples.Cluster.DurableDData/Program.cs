931690727ddef82be75c7e0418171ff482637c48
Aaron Stannard <aaron@petabridge.com>
2021-05-19 16:38:23 -0500

    added code sample

---
+++ src/examples/Cluster/DData/Samples.Cluster.DurableDData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Configuration;
+
+namespace Samples.Cluster.DurableDData
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+        }
+
+        public static readonly Config BaseConfig = @"
+            akka{
+                loglevel = DEBUG
+                actor.provider = cluster
+                cluster{
+                    seed-nodes = [""akka.tcp://ClusterSys@localhost:11000""]
+                    roles = [ddata]
+                    distributed-data{
+                        durable.keys = [*]
+                    }
+                }
+            }
+        ";
+
+        public static Config ConfigureNode(string lmdbFolder, int port)
+        {
+
+        }
+    }
+}
