61d7e4980aec447bdca7fbc56476705e0b929f49
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-06-02 02:43:42 +0700

    Sort ORSet elements on serialization to address inconsistent digest problem

---
+++ src/contrib/cluster/Akka.DistributedData/Serialization/Proto/OtherMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.DistributedData.Serialization.Proto.Msg
+{
+    internal sealed partial class OtherMessage : IComparable<OtherMessage>
+    {
+        public int CompareTo(OtherMessage other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+
+            var aByteString = EnclosedMessage.Span;
+            var bByteString = other.EnclosedMessage.Span;
+            var aSize = aByteString.Length;
+            var bSize = bByteString.Length;
+            if (aSize < bSize) return -1;
+            if (aSize > bSize) return 1;
+
+            for (var i = 0; i < aSize; i++)
+            {
+                var aByte = aByteString[i];
+                var bByte = bByteString[i];
+                if (aByte < bByte) return -1;
+                if (aByte > bByte) return 1;
+            }
+
+            return 0;
+        }
+    }
+}
