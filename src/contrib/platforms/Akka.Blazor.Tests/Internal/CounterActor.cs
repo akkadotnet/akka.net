0f6b01e224d3b4ac9ef818d6a9f3b7b945f5003f
Aaron Stannard <aaron@petabridge.com>
2021-06-09 14:11:13 -0500

    added Akka.NET integration test

---
+++ src/contrib/platforms/Akka.Blazor.Tests/Internal/CounterActor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace Akka.Blazor.Tests.Internal
+{
+    public sealed class CounterActor : ReceiveActor
+    {
+        public static Props Props { get; } = Props.Create(() => new CounterActor(0));
+
+        public sealed class Hit
+        {
+            public static readonly Hit Instance = new Hit();
+            private Hit(){}
+        }
+
+        private int _count;
+
+        public CounterActor(int count)
+        {
+            _count = count;
+
+            Receive<Hit>(_ =>
+            {
+                _count++;
+                Sender.Tell(_count);
+            });
+        }
+    }
+}
