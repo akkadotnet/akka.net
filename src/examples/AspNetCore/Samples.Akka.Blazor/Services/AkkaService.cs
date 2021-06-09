62992b88a3d697e60ff71f6dd43f59610efc7fa9
Aaron Stannard <aaron@petabridge.com>
2021-06-09 14:41:22 -0500

    added Blazor WASM sample

---
+++ src/examples/AspNetCore/Samples.Akka.Blazor/Services/AkkaService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Samples.Akka.Blazor.Actors;
+
+namespace Samples.Akka.Blazor.Services
+{
+    public class AkkaService : IAsyncDisposable, ICounterService
+    {
+        private readonly ActorSystem _actorSystem;
+        private readonly IActorRef _countActor;
+
+        public AkkaService(ActorSystem actorSystem)
+        {
+            _actorSystem = actorSystem;
+            _countActor = actorSystem.ActorOf(CounterActor.Props, "counter");
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _actorSystem.Terminate();
+        }
+
+        public async Task<int> GetCount(CancellationToken token)
+        {
+            return await _countActor.Ask<int>(CounterActor.Get.Instance, token).ConfigureAwait(false);
+        }
+
+        public async Task<int> Increment(CancellationToken token)
+        {
+            return await _countActor.Ask<int>(CounterActor.Hit.Instance, token).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/examples/AspNetCore/Samples.Akka.Blazor/Services/ICounterService.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ICounterService.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2021 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2021 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Samples.Akka.Blazor.Services
+{
+    public interface ICounterService
+    {
+        Task<int> GetCount(CancellationToken token);
+        Task<int> Increment(CancellationToken token);
+    }
+}
