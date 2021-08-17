634feeed175be789da8025b01fc24933d9063ddf
Aaron Stannard <aaron@petabridge.com>
2021-08-17 07:43:14 -0500

    introduced new project for Akka.Cluster.Benchmarks

---
+++ src/benchmark/Akka.Cluster.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Akka.Cluster.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
