5d1a136cf93099f4063f9b47ad95400983f2ccda
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-09-17 03:04:59 +0700

    Add Timeout property to make attribute compatible to Xunit

--- src/core/Akka.Remote.TestKit/MultiNodeFact.cs
+++ src/core/Akka.Remote.TestKit/MultiNodeFact.cs
@@ -49,6 +49,14 @@ namespace Akka.Remote.TestKit
         /// null, which will cause the fully qualified test name to be used.
         /// </summary>
         public virtual string DisplayName { get; set; }
+
+        /// <summary>
+        /// Marks the test as having a timeout, and gets or sets the timeout (in milliseconds).
+        /// WARNING: Using this with parallelization turned on will result in undefined behavior.
+        /// Timeout is only supported when parallelization is disabled, either globally or with
+        /// a parallelization-disabled test collection.
+        /// </summary>
+        public virtual int Timeout { get; set; }
     }
 }
 
