3497ef639cb9f9e3eb3d49d6631e1f2ffcf7ba57
Daniel James <daniel@thzinc.com>
2021-11-10 14:19:15 -0800

    Add new benchmarks to exercise large messages that may allocate more memory during serialization/deserialization
    Also included separate benchmarks to identify performance differences between serialization and deserialization

--- src/benchmark/Akka.Benchmarks/Serialization/SerializationBenchmarks.cs
+++ src/benchmark/Akka.Benchmarks/Serialization/SerializationBenchmarks.cs
@@ -6,6 +6,8 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Akka.Actor;
 using Akka.Benchmarks.Configurations;
 using Akka.Serialization;
@@ -17,10 +19,12 @@ namespace Akka.Benchmarks.Serialization
     public class SerializationBenchmarks
     {
         public const int Operations = 10_000;
+        public const int LargeOperations = 1_000;
 
         private ExtendedActorSystem system;
         private NewtonSoftJsonSerializer json;
         private Message message;
+        private LargeMessage largeMessage;
 
         [GlobalSetup]
         public void Setup()
@@ -29,6 +33,12 @@ namespace Akka.Benchmarks.Serialization
             json = new NewtonSoftJsonSerializer(system);
             var actorRef = system.ActorOf<Echo>();
             message = new Message(123, Guid.NewGuid().ToString("D"), actorRef);
+
+            var largeNumbers = new List<int>(Enumerable.Range(0, 1_000));
+            var tail = Enumerable.Range(0, 2)
+                .Select(_ => new LargeMessage(largeNumbers, new List<LargeMessage>()))
+                .ToList();
+            largeMessage = new LargeMessage(largeNumbers, tail);
         }
 
         [GlobalCleanup]
@@ -44,6 +54,54 @@ namespace Akka.Benchmarks.Serialization
             {
                 var bytes = json.ToBinary(message);
                 json.FromBinary(bytes, typeof(Message));
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = Operations)]
+        public void Json_serializer_message_to_binary()
+        {
+            for (int i = 0; i < Operations; i++)
+            {
+                json.ToBinary(message);
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = Operations)]
+        public void Json_serializer_message_from_binary()
+        {
+            var bytes = json.ToBinary(message);
+            for (int i = 0; i < Operations; i++)
+            {
+                json.FromBinary(bytes, typeof(Message));
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = LargeOperations)]
+        public void Json_serializer_large_message_roundtrip()
+        {
+            for (int i = 0; i < LargeOperations; i++)
+            {
+                var bytes = json.ToBinary(largeMessage);
+                json.FromBinary(bytes, typeof(LargeMessage));
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = LargeOperations)]
+        public void Json_serializer_large_message_to_binary()
+        {
+            for (int i = 0; i < LargeOperations; i++)
+            {
+                json.ToBinary(largeMessage);
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = LargeOperations)]
+        public void Json_serializer_large_message_from_binary()
+        {
+            var bytes = json.ToBinary(largeMessage);
+            for (int i = 0; i < LargeOperations; i++)
+            {
+                json.FromBinary(bytes, typeof(LargeMessage));
             }
         }
 
@@ -69,6 +127,18 @@ namespace Akka.Benchmarks.Serialization
                 Name = name;
                 ActorRef = actorRef;
             }
+        }
+
+        public sealed class LargeMessage
+        {
+            public LargeMessage(List<int> numbers, List<LargeMessage> tail)
+            {
+                Numbers = numbers;
+                Tail = tail;
+            }
+
+            public List<int> Numbers { get; }
+            public List<LargeMessage> Tail { get; }
         }
 
         #endregion
