1e802c2984f6d87ebe60c74ce3565f5624d14013
Aaron Stannard <aaron@petabridge.com>
2021-12-01 14:15:48 -0600

    Upgraded DocFx to 2.58.9, dropped `TargetFramework`
    close https://github.com/akkadotnet/akka.net/issues/5406

--- build.ps1
+++ build.ps1
@@ -33,7 +33,7 @@ $FakeVersion = "4.63.0"
 $NugetVersion = "5.8.0";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/v$NugetVersion/nuget.exe"
 $ProtobufVersion = "3.13.0"
-$DocfxVersion = "2.48.1"
+$DocfxVersion = "2.58.9"
 
 $IncrementalistVersion = "0.6.0";
 
