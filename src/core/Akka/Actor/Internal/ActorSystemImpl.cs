92f24cff3927918e2e61e9e14f119a63eacd5802
zetanova <dirnberger@cwtech.at>
2021-12-16 23:53:49 +0100

    add startup error log output

--- src/core/Akka/Actor/Internal/ActorSystemImpl.cs
+++ src/core/Akka/Actor/Internal/ActorSystemImpl.cs
@@ -245,23 +245,14 @@ namespace Akka.Actor.Internal
                 {
                     _log.Info(Settings.ToString());
                 }
-
-                //HACK: ensure provider started
-                //{
-                //    if (_provider.DefaultAddress is null)
-                //        Thread.Yield();
-                //    var i = 1;
-                //    while (i < 10 && _provider.DefaultAddress is null)
-                //        Thread.Sleep(i++ * 16);
-                //    if (i == 10 && _provider.DefaultAddress is null)
-                //        throw new TimeoutException($"Provider '{_provider.GetType()}' startup timeout");
-                //}
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Log.Debug(ex, "System startup failed");
+
                 try
                 {
-                    Terminate();
+                    _ = Terminate();
                 }
                 catch (Exception)
                 {
