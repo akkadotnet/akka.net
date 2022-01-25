a5ef30f30ba9fb39e178cd5f2f7ded8e0cfa1d16
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-01-25 21:14:33 +0700

    Add custom persistence provider documentation (#5516)
    * Add initial documentation proposal
    
    * Add custom provider sample project for documentation code linking
    
    * Fix linter error
    
    * Improve code formatting so it can be read better in the documentation
    
    * Documentation improvements
    
    * Add TODO tags for things that still need to be written
    * Use code linking for code examples, linking to the sample project
    * Improve WriteMessagesAsync documentation, shuffle paragraphs around for readability, add headers to separate the explanations, change long paragraphs into bullet points
    
    * Rename file name to custom-persistence-provider, add docs to TOC
    
    * Fix linter problems
    
    * Add missing PostStop method to SqliteJournal
    
    * Update documentation
    
    * Add SnapshotStore, extension, and unit testing documentation.
    
    * Fix linter errors
    
    * Fix bad word wrapping
    
    Co-authored-by: Aaron Stannard <aaron@petabridge.com>

---
+++ src/examples/Akka.Persistence.Custom.Tests/SqliteSnapshotStoreSerializationSpec.cs
@@ -0,0 +1,39 @@
+﻿// //-----------------------------------------------------------------------
+// // <copyright file="SqliteSnapshotStoreSerializationSpec.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+using System;
+using Akka.Configuration;
+using Akka.Persistence.TCK.Serialization;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Custom.Tests
+{
+    public class SqliteSnapshotStoreSerializationSpec: SnapshotStoreSerializationSpec
+    {
+        public SqliteSnapshotStoreSerializationSpec(ITestOutputHelper output)
+            : base(CreateSpecConfig("Filename=file:serialization-snapshot-" + Guid.NewGuid() + ".db"), "SqliteSnapshotStoreSerializationSpec", output)
+        {
+        }
+
+        private static Config CreateSpecConfig(string connectionString)
+        {
+            return ConfigurationFactory.ParseString(@"
+                akka.persistence {
+                    publish-plugin-commands = on
+                    snapshot-store {
+                        plugin = ""akka.persistence.snapshot-store.custom-sqlite""
+                        custom-sqlite {
+                            class = ""Akka.Persistence.Custom.Snapshot.SqliteSnapshotStore, Akka.Persistence.Custom""
+                            plugin-dispatcher = ""akka.actor.default-dispatcher""
+                            auto-initialize = on
+                            connection-string = """ + connectionString + @"""
+                        }
+                    }
+                }");
+        }
+    }
+}
