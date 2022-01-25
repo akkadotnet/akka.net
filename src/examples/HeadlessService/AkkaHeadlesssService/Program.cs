c5f79a8cf198837e713d40ad754d0181a6bfedfe
Ebere Abanonu <eabanonu@yahoo.com>
2022-01-25 15:41:15 +0100

    Added Headless Service

---
+++ src/examples/HeadlessService/AkkaHeadlesssService/Program.cs
@@ -0,0 +1,25 @@
+﻿#region headless-service-program
+
+// See https://aka.ms/new-console-template for more information
+using AkkaHeadlesssService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                    services.AddHostedService<AkkaService>();
+
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+await host.RunAsync();
+#endregion
