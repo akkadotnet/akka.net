cbaed69cb93f7be2ffd98cb8555880f852bbb046
Ebere Abanonu <eabanonu@yahoo.com>
2022-01-27 17:38:52 +0100

    https://github.com/akkadotnet/akka.net/issues/5541

---
+++ src/core/Akka.Tests/Dispatch/ChannelExecutorSpec.cs
@@ -0,0 +1,22 @@
+﻿
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.TestKit;
+using Xunit;
+
+namespace Akka.Tests.Dispatch
+{
+    public class ChannelExecutorSpec : AkkaSpec
+    {
+        [Fact]
+        public void Should_Successfully_Create_Channel_Executor_ActorSystem()
+        {
+            var config = @"
+                          akka.actor.default-dispatcher = { 
+                              executor = channel-executor
+                              channel-executor.priority = normal
+                          }";
+            var actorSytem = ActorSystem.Create("ce", config);
+        }
+    }
+}
