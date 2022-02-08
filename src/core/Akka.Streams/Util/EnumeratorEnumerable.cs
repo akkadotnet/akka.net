c8b1ad67a6ea9602e22ce2ba21bf1f61533092f9
Ebere Abanonu <eabanonu@yahoo.com>
2022-02-08 15:08:43 +0100

    retain the `exception` tag

--- src/core/Akka.Streams/Util/EnumeratorEnumerable.cs
+++ src/core/Akka.Streams/Util/EnumeratorEnumerable.cs
@@ -60,7 +60,9 @@ namespace Akka.Streams.Util
            
             public void Dispose() => _enumeratorFactory = null;
 
-            
+            /// <exception cref="ArgumentException">
+            /// This exception is thrown when the enumerator has passed the end of an enumerable.
+            /// </exception>
             public bool MoveNext()
             {
                 if (_current == null || !_current.MoveNext())
