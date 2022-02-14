8d13f1a5ed6ea7e10964b5045e2908804914844f
Ebere Abanonu <eabanonu@yahoo.com>
2022-02-14 18:29:06 +0100

    Channels implementation

--- src/core/Akka.TestKit/Internal/ChannelQueue.cs
+++ src/core/Akka.TestKit/Internal/ChannelQueue.cs
@@ -10,7 +10,9 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Channels;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Akka.TestKit.Internal
 {
@@ -20,33 +22,41 @@ namespace Akka.TestKit.Internal
     /// <remarks>Note! Part of internal API. Breaking changes may occur without notice. Use at own risk.</remarks>
     /// </summary>
     /// <typeparam name="T">The type of item to store.</typeparam>
-    public class BlockingQueue<T>
+    public class ChannelQueue<T>
     {
-        private readonly BlockingCollection<Positioned> _collection = new BlockingCollection<Positioned>(new QueueWithAddFirst());
+        private readonly Channel<Positioned> _channel = Channel.CreateBounded<Positioned>(2000);
+
 
         /// <summary>
         /// The number of items that are currently in the queue.
         /// </summary>
-        public int Count { get { return _collection.Count; } }
+        public int Count { get { return _channel.Reader.Count; } }
 
         /// <summary>
         /// Adds the specified item to the end of the queue.
         /// </summary>
         /// <param name="item">The item to add to the queue.</param>
-        public void Enqueue(T item)
+        public async ValueTask Enqueue(T item)
         {
-            if (!_collection.TryAdd(new Positioned(item)))
-                throw new InvalidOperationException("Failed to enqueue item into the queue.");
+            var writer = _channel.Writer;
+            await writer.WriteAsync(new Positioned(item));
+
+            //if (!writer. _channel.TryAdd(new Positioned(item)))
+            // 
+                //throw new InvalidOperationException("Failed to enqueue item into the queue.");
         }
 
         /// <summary>
         /// Adds the specified item to the front of the queue. 
         /// </summary>
         /// <param name="item">The item to add to the queue.</param>
-        public void AddFirst(T item)
+        public async ValueTask AddFirst(T item)
         {
-            if(!_collection.TryAdd(new Positioned(item, first:true)))
-                throw new InvalidOperationException("Failed to enqueue item into the head of the queue.");
+            var writer = _channel.Writer;
+            await writer.WriteAsync(new Positioned(item, first: true));
+
+            //if (!_channel.TryAdd(new Positioned(item, first:true)))
+                //throw new InvalidOperationException("Failed to enqueue item into the head of the queue.");
         }
 
         /// <summary>
@@ -57,25 +67,25 @@ namespace Akka.TestKit.Internal
         /// <param name="millisecondsTimeout">The number of milliseconds to wait for the add to complete.</param>
         /// <param name="cancellationToken">The cancellation token that can be used to cancel the operation.</param>
         /// <returns><c>true</c> if the add completed within the specified timeout; otherwise, <c>false</c>.</returns>
-        public bool TryEnqueue(T item, int millisecondsTimeout, CancellationToken cancellationToken)
+        public async ValueTask<bool> TryEnqueue(T item, int millisecondsTimeout, CancellationToken cancellationToken)
         {
-            return _collection.TryAdd(new Positioned(item), millisecondsTimeout, cancellationToken);
+            var result = _channel.Writer.TryWrite(new Positioned(item));
+            return await Task.FromResult(result);
+            //return _channel.TryAdd(new Positioned(item), millisecondsTimeout, cancellationToken);
         }
 
         /// <summary>
         /// Tries to remove the specified item from the queue.
         /// </summary>
-        /// <param name="item">The item to remove from the queue.</param>
-        /// <returns><c>true</c> if the item was removed; otherwise, <c>false</c>.</returns>
-        public bool TryTake(out T item)
+        /// <returns><c>(true, T)</c> if the item was removed; otherwise, <c>(false, default)</c>.</returns>
+        public async ValueTask<(bool Success, T Item)> TryTake()
         {
-            if(_collection.TryTake(out var p))
+            var reader = _channel.Reader;   
+            if(reader.TryRead(out var p))
             {
-                item = p.Value;
-                return true;
+                return await Task.FromResult((true, p.Value));
             }
-            item = default;
-            return false;
+            return await Task.FromResult((false, default(T)));
         }
 
         /// <summary>
@@ -86,15 +96,14 @@ namespace Akka.TestKit.Internal
         /// <param name="millisecondsTimeout">The number of milliseconds to wait for the remove to complete.</param>
         /// <param name="cancellationToken">The cancellation token that can be used to cancel the operation.</param>
         /// <returns><c>true</c> if the remove completed within the specified timeout; otherwise, <c>false</c>.</returns>
-        public bool TryTake(out T item, int millisecondsTimeout, CancellationToken cancellationToken)
+        public async ValueTask<(bool Success, T Item)> TryTake(int millisecondsTimeout, CancellationToken cancellationToken)
         {
-            if(_collection.TryTake(out var p, millisecondsTimeout, cancellationToken))
-            {
-                item = p.Value;
-                return true;
-            }
-            item = default;
-            return false;
+            var reader = _channel.Reader;
+            var p = await reader.ReadAsync(cancellationToken); 
+            if(p != null)
+                return (true, p.Value);
+
+            return (false, default(T)); 
         }
 
         /// <summary>
@@ -105,19 +114,20 @@ namespace Akka.TestKit.Internal
         /// This exception is thrown when the operation is canceled.
         /// </exception>
         /// <returns>The item removed from the collection.</returns>
-        public T Take(CancellationToken cancellationToken)
+        public async ValueTask<T> Take(CancellationToken cancellationToken)
         {
-            var p = _collection.Take(cancellationToken);
+            var reader = _channel.Reader;
+            var p = await reader.ReadAsync(cancellationToken);
             return p.Value;
         }
 
         /// <summary>
-        /// Copies the items from the <see cref="BlockingQueue{T}"/> instance into a new <see cref="List{T}"/>.
+        /// Copies the items from the <see cref="ChannelQueue{T}"/> instance into a new <see cref="List{T}"/>.
         /// </summary>
         /// <returns>A <see cref="List{T}"/> containing copies of the elements of the collection</returns>
         public List<T> ToList()
         {
-            var positionArray = _collection.ToArray();
+            var positionArray = _channel.Reader.ToArray();
             return positionArray.Select(positioned => positioned.Value).ToList();
         }
 
