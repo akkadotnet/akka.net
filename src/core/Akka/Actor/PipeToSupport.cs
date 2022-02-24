83ebaae0fa8cf8931cb8661deccdf452ab36d8bd
Gregorius Soedharmo <gregorius.soedharmo@petabridge.com>
2022-02-25 00:30:10 +0700

    Add function overload for backward compatibility

--- src/core/Akka/Actor/PipeToSupport.cs
+++ src/core/Akka/Actor/PipeToSupport.cs
@@ -27,15 +27,34 @@ namespace Akka.Actor
         /// <param name="sender">The IActorRef that will be used as the sender of the result. Defaults to <see cref="ActorRefs.Nobody"/> </param>
         /// <param name="success">A callback function that will be called on Task success. Defaults to <c>null</c> for no callback</param>
         /// <param name="failure">A callback function that will be called on Task failure. Defaults to <c>null</c> for no callback</param>
+        /// <returns>A detached task</returns>
+        public static Task PipeTo<T>(
+            this Task<T> taskToPipe,
+            ICanTell recipient,
+            IActorRef sender = null,
+            Func<T, object> success = null,
+            Func<Exception, object> failure = null)
+            => PipeTo(taskToPipe, recipient, true, sender, success, failure);
+        
+        /// <summary>
+        /// Pipes the output of a Task directly to the <paramref name="recipient"/>'s mailbox once
+        /// the task completes
+        /// </summary>
+        /// <typeparam name="T">The type of result of the Task</typeparam>
+        /// <param name="taskToPipe">The Task that result needs to be piped to an actor</param>
+        /// <param name="recipient">The actor that will receive the Task result</param>
+        /// <param name="sender">The IActorRef that will be used as the sender of the result. Defaults to <see cref="ActorRefs.Nobody"/> </param>
+        /// <param name="success">A callback function that will be called on Task success. Defaults to <c>null</c> for no callback</param>
+        /// <param name="failure">A callback function that will be called on Task failure. Defaults to <c>null</c> for no callback</param>
         /// <param name="useConfigureAwait">If set to true, <c>taskToPipe</c> will be awaited using <c>ConfigureAwait(false)</c></param>
         /// <returns>A detached task</returns>
         public static async Task PipeTo<T>(
             this Task<T> taskToPipe,
             ICanTell recipient,
+            bool useConfigureAwait,
             IActorRef sender = null, 
             Func<T, object> success = null,
-            Func<Exception, object> failure = null,
-            bool useConfigureAwait = true)
+            Func<Exception, object> failure = null)
         {
             sender = sender ?? ActorRefs.NoSender;
             
@@ -66,15 +85,33 @@ namespace Akka.Actor
         /// <param name="sender">The IActorRef that will be used as the sender of the result. Defaults to <see cref="ActorRefs.Nobody"/> </param>
         /// <param name="success">A callback function that will be called on Task success. Defaults to <c>null</c> for no callback</param>
         /// <param name="failure">A callback function that will be called on Task failure. Defaults to <c>null</c> for no callback</param>
+        /// <returns>A detached task</returns>
+        public static Task PipeTo(
+            this Task taskToPipe,
+            ICanTell recipient,
+            IActorRef sender = null,
+            Func<object> success = null,
+            Func<Exception, object> failure = null)
+            => PipeTo(taskToPipe, recipient, true, sender, success, failure);
+        
+        /// <summary>
+        /// Pipes the output of a Task directly to the <paramref name="recipient"/>'s mailbox once
+        /// the task completes.  As this task has no result, only exceptions will be piped to the <paramref name="recipient"/>
+        /// </summary>
+        /// <param name="taskToPipe">The Task that result needs to be piped to an actor</param>
+        /// <param name="recipient">The actor that will receive the Task result</param>
+        /// <param name="sender">The IActorRef that will be used as the sender of the result. Defaults to <see cref="ActorRefs.Nobody"/> </param>
+        /// <param name="success">A callback function that will be called on Task success. Defaults to <c>null</c> for no callback</param>
+        /// <param name="failure">A callback function that will be called on Task failure. Defaults to <c>null</c> for no callback</param>
         /// <param name="useConfigureAwait">If set to true, <c>taskToPipe</c> will be awaited using <c>ConfigureAwait(false)</c></param>
         /// <returns>A detached task</returns>
         public static async Task PipeTo(
             this Task taskToPipe,
             ICanTell recipient,
+            bool useConfigureAwait,
             IActorRef sender = null,
             Func<object> success = null,
-            Func<Exception, object> failure = null,
-            bool useConfigureAwait = true)
+            Func<Exception, object> failure = null)
         {
             sender = sender ?? ActorRefs.NoSender;
             
