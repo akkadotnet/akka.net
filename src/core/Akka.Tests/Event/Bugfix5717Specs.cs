b6214ef81ab5e798ecf4e52e6b70ea9e602feb8c
Aaron Stannard <aaron@petabridge.com>
2022-03-15 16:14:09 -0500

    Fixed `IActorRef` leak inside `EventStream` (#5720)
    * reproduced #5717
    
    Reproduced `IActorRef` leak inside the `EventStream`
    
    * cleaned up the `EventBusUnsubscriber`
    
    * close #5719 - cleaned up `EventStream` subscription management
    
    * added API approval
    
    For `Obsolete` attribute.
    
    * need to capture more data on why failures happen
    
    * harden bugfix5717specs

---
+++ src/core/Akka.Tests/Event/Bugfix5717Specs.cs
@@ -0,0 +1,66 @@
+﻿// //-----------------------------------------------------------------------
+// // <copyright file="Bugfix5717Specs.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Akka.Configuration;
+using Akka.TestKit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Tests.Event
+{
+    public class Bugfix5717Specs : AkkaSpec
+    {
+        public Bugfix5717Specs(ITestOutputHelper output) : base(Config.Empty, output){}
+        
+        /// <summary>
+        /// Reproduction for https://github.com/akkadotnet/akka.net/issues/5717
+        /// </summary>
+        [Fact]
+        public async Task Should_unsubscribe_from_all_topics_on_Terminate()
+        {
+            var es = Sys.EventStream;
+            var tm1 = 1;
+            var tm2 = "FOO";
+            var a1 = CreateTestProbe();
+            var a2 = CreateTestProbe();
+
+            es.Subscribe(a1.Ref, typeof(int));
+            es.Subscribe(a2.Ref, typeof(int));
+            es.Subscribe(a2.Ref, typeof(string));
+            es.Publish(tm1);
+            es.Publish(tm2);
+            a1.ExpectMsg(tm1);
+            a2.ExpectMsg(tm1);
+            a2.ExpectMsg(tm2);
+
+            // kill second test probe
+            Watch(a2);
+            Sys.Stop(a2);
+            ExpectTerminated(a2);
+
+            /*
+             * It's possible that the `Terminate` message may not have been processed by the
+             * Unsubscriber yet, so we want to try this operation more than once to see if it
+             * eventually executes the unsubscribe on the EventStream.
+             *
+             * If it still fails after multiple attempts, the issue is that the unsub was never
+             * executed in the first place.
+             */
+            await AwaitAssertAsync(async () =>
+            {
+                await EventFilter.DeadLetter().ExpectAsync(0, () =>
+                {
+                    es.Publish(tm1);
+                    es.Publish(tm2);
+                    a1.ExpectMsg(tm1);
+                });
+            }, interval:TimeSpan.FromSeconds(250));
+        }       
+    }
+}
