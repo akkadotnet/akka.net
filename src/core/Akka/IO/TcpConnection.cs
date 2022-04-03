c1d286f4c8f58fbd7abe682ce411155ff8567f66
Ismael Hamed <1279846+ismaelhamed@users.noreply.github.com>
2022-04-03 08:39:06 +0200

    Fix TcpConnection error handling

--- src/core/Akka/IO/TcpConnection.cs
+++ src/core/Akka/IO/TcpConnection.cs
@@ -108,14 +108,14 @@ namespace Akka.IO
         protected TcpConnection(TcpExt tcp, Socket socket, bool pullMode, Option<int> writeCommandsBufferMaxSize)
         {
             if (socket == null) throw new ArgumentNullException(nameof(socket));
-            
+
             _pullMode = pullMode;
             _writeCommandsQueue = new PendingSimpleWritesQueue(Log, writeCommandsBufferMaxSize);
             _traceLogging = tcp.Settings.TraceLogging;
-            
+
             Tcp = tcp;
             Socket = socket;
-            
+
             if (pullMode) SetStatus(ConnectionStatus.ReadingSuspended);
         }
 
@@ -164,12 +164,11 @@ namespace Akka.IO
 
                         var registerInfo = new ConnectionInfo(register.Handler, register.KeepOpenOnPeerClosed, register.UseResumeWriting);
 
-                        // if we have resumed reading from pullMode while waiting for Register then read
-                        if (_pullMode && !HasStatus(ConnectionStatus.ReadingSuspended)) ResumeReading();
-                        else if (!_pullMode) ReceiveAsync();
-
                         Context.SetReceiveTimeout(null);
                         Context.Become(Connected(registerInfo));
+
+                        // if we are in push mode or already have resumed reading in pullMode while waiting for Register then read
+                        if (!_pullMode || !HasStatus(ConnectionStatus.ReadingSuspended)) ResumeReading();
 
                         // If there is something buffered before we got Register message - put it all to the socket
                         var bufferedWrite = GetNextWrite();
@@ -177,8 +176,8 @@ namespace Akka.IO
                         {
                             SetStatus(ConnectionStatus.Sending);
                             DoWrite(registerInfo, bufferedWrite.Value);
-                        } 
-                        
+                        }
+
                         return true;
                     case ResumeReading _: ClearStatus(ConnectionStatus.ReadingSuspended); return true;
                     case SuspendReading _: SetStatus(ConnectionStatus.ReadingSuspended); return true;
@@ -205,7 +204,7 @@ namespace Akka.IO
                             Log.Warning("Received Write command before Register command. " +
                                         "It will be buffered until Register will be received (buffered write size is {0} bytes)", commandSize);
                         }
-                        
+
                         return true;
                     default: return false;
                 }
@@ -268,7 +267,7 @@ namespace Akka.IO
                         AcknowledgeSent();
                         if (IsWritePending)
                             DoWrite(info, GetAllowedPendingWrite());
-                        else 
+                        else
                             HandleClose(info, closeCommander, closedEvent);
                         return true;
                     case UpdatePendingWriteAndThen updatePendingWrite:
@@ -277,7 +276,7 @@ namespace Akka.IO
 
                         if (nextWrite.HasValue)
                             DoWrite(info, nextWrite);
-                        else 
+                        else
                             HandleClose(info, closeCommander, closedEvent);
                         return true;
                     case WriteFileFailed fail: HandleError(info.Handler, fail.Cause); return true;
@@ -312,7 +311,7 @@ namespace Akka.IO
                     case SocketSent _:
                         // Send ack to sender
                         AcknowledgeSent();
-                        
+
                         // If there is something to send - send it
                         var pendingWrite = GetAllowedPendingWrite();
                         if (pendingWrite.HasValue)
@@ -320,14 +319,14 @@ namespace Akka.IO
                             SetStatus(ConnectionStatus.Sending);
                             DoWrite(info, pendingWrite);
                         }
-                        
+
                         // If message is fully sent, notify sender who sent ResumeWriting command
                         if (!IsWritePending && _interestedInResume != null)
                         {
                             _interestedInResume.Tell(WritingResumed.Instance);
                             _interestedInResume = null;
                         }
-                        
+
                         return true;
                     case WriteCommand write:
                         if (HasStatus(ConnectionStatus.WritingSuspended))
@@ -356,7 +355,7 @@ namespace Akka.IO
                                     DropWrite(info, write);
                                     return true;
                                 }
-                                
+
                                 nextWrite = GetNextWrite(headCommands: new []{ (simpleWriteCommand, Sender) });
                             }
                             else
@@ -364,15 +363,15 @@ namespace Akka.IO
                                 _writeCommandsQueue.EnqueueSimpleWrites(write, Sender);
                                 nextWrite = GetNextWrite();
                             }
-                            
+
                             // If there is something to send and we are allowed to, lets put the next command on the wire
                             if (nextWrite.HasValue)
                             {
                                 SetStatus(ConnectionStatus.Sending);
                                 DoWrite(info, nextWrite.Value);
-                            } 
+                            }
                         }
-                        
+
                         return true;
                     case ResumeWriting _:
                         /*
@@ -396,7 +395,7 @@ namespace Akka.IO
                     case UpdatePendingWriteAndThen updatePendingWrite:
                         var updatedWrite = updatePendingWrite.RemainingWrite;
                         updatePendingWrite.Work();
-                        if (updatedWrite.HasValue) 
+                        if (updatedWrite.HasValue)
                             DoWrite(info, updatedWrite.Value);
                         return true;
                     case WriteFileFailed fail:
@@ -461,7 +460,7 @@ namespace Akka.IO
             {
                 ackInfo.Commander.Tell(ackInfo.Ack);
             }
-           
+
             ClearStatus(ConnectionStatus.Sending);
         }
 
@@ -538,7 +537,7 @@ namespace Akka.IO
             {
                 _pendingAcks.Enqueue(pendingAck);
             }
-            
+
             write.Value.DoWrite(info);
         }
 
@@ -672,7 +671,7 @@ namespace Akka.IO
                         throw new NotSupportedException($"Socket operation {e.LastOperation} is not supported");
                 }
             }
-            
+
             var args = new SocketAsyncEventArgs();
             args.UserToken = onCompleteNotificationsReceiver;
             args.Completed += (sender, e) =>
@@ -684,7 +683,7 @@ namespace Akka.IO
 
             return args;
         }
-        
+
         protected void ReleaseSocketEventArgs(SocketAsyncEventArgs e)
         {
             e.UserToken = null;
@@ -700,7 +699,7 @@ namespace Akka.IO
             catch (InvalidOperationException) { }
 
             e.Dispose();
-            
+
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -810,7 +809,7 @@ namespace Akka.IO
             {
                 return CreatePendingBufferWrite(writeCommands);
             }
-            
+
             // No more writes out there
             return Option<PendingWrite>.None;
         }
@@ -986,7 +985,7 @@ namespace Akka.IO
             {
                 return EnqueueSimpleWrites(command, sender, out _);
             }
-            
+
             /// <summary>
             /// Adds all <see cref="SimpleWriteCommand"/> subcommands stored in provided command.
             /// Performs buffer size checks
@@ -997,7 +996,7 @@ namespace Akka.IO
             public bool EnqueueSimpleWrites(WriteCommand command, IActorRef sender, out int bufferedSize)
             {
                 bufferedSize = 0;
-                
+
                 foreach (var writeInfo in ExtractFromCommand(command))
                 {
                     var sizeAfterAppending = _totalSizeInBytes + writeInfo.DataSize;
@@ -1013,10 +1012,10 @@ namespace Akka.IO
                     _queue.Enqueue((writeInfo.Command, sender, writeInfo.DataSize));
                     bufferedSize += writeInfo.DataSize;
                 }
-                
+
                 return true;
             }
-            
+
             /// <summary>
             /// Adds all <see cref="SimpleWriteCommand"/> subcommands stored in provided command.
             /// Performs buffer size checks for all, except first one, that is not buffered
@@ -1037,7 +1036,7 @@ namespace Akka.IO
                         first = writeInfo.Command;
                         continue;
                     }
-                    
+
                     var sizeAfterAppending = _totalSizeInBytes + writeInfo.DataSize;
                     if (_maxQueueSizeInBytes.HasValue && _maxQueueSizeInBytes.Value < sizeAfterAppending)
                     {
@@ -1061,7 +1060,7 @@ namespace Akka.IO
             {
                 if (_queue.Count == 0)
                     throw new InvalidOperationException("Write commands queue is empty");
-                
+
                 var (command, sender, size) = _queue.Dequeue();
                 _totalSizeInBytes -= size;
                 return (command, sender);
@@ -1076,7 +1075,7 @@ namespace Akka.IO
                 while (TryGetNext(out var command))
                     yield return command;
             }
-            
+
             /// <summary>
             /// Gets next command from the queue, if any
             /// </summary>
