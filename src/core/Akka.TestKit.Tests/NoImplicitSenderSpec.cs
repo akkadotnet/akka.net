f92b8b4286e3281c7d41ed8577d913e3327d87ae
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-04-20 02:10:04 +0700

    Convert Akka.TestKit.Tests to async - NoImplicitSenderSpec TestSchedulerSpec

--- src/core/Akka.TestKit.Tests/NoImplicitSenderSpec.cs
+++ src/core/Akka.TestKit.Tests/NoImplicitSenderSpec.cs
@@ -5,21 +5,21 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Akka.Actor;
-using Akka.TestKit;
 using Akka.Actor.Dsl;
 using Xunit;
 
-namespace Akka.Testkit.Tests
+namespace Akka.TestKit.Tests
 {
     public class NoImplicitSenderSpec : AkkaSpec, INoImplicitSender
     {
         [Fact(Skip = "Type assertion on null message causes NullReferenceException")]
-        public void When_Not_ImplicitSender_then_testActor_is_not_sender()
+        public async Task When_Not_ImplicitSender_then_testActor_is_not_sender()
         {
             var echoActor = Sys.ActorOf(c => c.ReceiveAny((m, ctx) => TestActor.Tell(ctx.Sender)));
             echoActor.Tell("message");
-            ExpectMsg<IActorRef>(actorRef => Equals(actorRef, ActorRefs.NoSender));
+            await ExpectMsgAsync<IActorRef>(actorRef => Equals(actorRef, ActorRefs.NoSender));
         }
 
     }
@@ -27,35 +27,35 @@ namespace Akka.Testkit.Tests
     public class ImplicitSenderSpec : AkkaSpec
     {
         [Fact]
-        public void ImplicitSender_should_have_testActor_as_sender()
+        public async Task ImplicitSender_should_have_testActor_as_sender()
         {
             var echoActor = Sys.ActorOf(c => c.ReceiveAny((m, ctx) => TestActor.Tell(ctx.Sender)));
             echoActor.Tell("message");
-            ExpectMsg<IActorRef>(actorRef => Equals(actorRef, TestActor));
+            await ExpectMsgAsync<IActorRef>(actorRef => Equals(actorRef, TestActor));
 
             //Test that it works after we know that context has been changed
             echoActor.Tell("message");
-            ExpectMsg<IActorRef>(actorRef => Equals(actorRef, TestActor));
+            await ExpectMsgAsync<IActorRef>(actorRef => Equals(actorRef, TestActor));
 
         }
 
 
         [Fact]
-        public void ImplicitSender_should_not_change_when_creating_Testprobes()
+        public async Task ImplicitSender_should_not_change_when_creating_Testprobes()
         {
             //Verifies that bug #459 has been fixed
             var testProbe = CreateTestProbe();
             TestActor.Tell("message");
-            ReceiveOne();
+            await ReceiveOneAsync();
             LastSender.ShouldBe(TestActor);
         }
 
         [Fact]
-        public void ImplicitSender_should_not_change_when_creating_TestActors()
+        public async Task ImplicitSender_should_not_change_when_creating_TestActors()
         {
             var testActor2 = CreateTestActor("test2");
             TestActor.Tell("message");
-            ReceiveOne();
+            await ReceiveOneAsync();
             LastSender.ShouldBe(TestActor);
         }
     }
