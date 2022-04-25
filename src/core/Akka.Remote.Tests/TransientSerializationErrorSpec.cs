dc999fd2a80fe2955053ec5f7acbc464267cbd04
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-04-26 00:50:11 +0700

    Convert Akka.Remote.Tests to async - TransientSerializationErrorSpec

--- src/core/Akka.Remote.Tests/TransientSerializationErrorSpec.cs
+++ src/core/Akka.Remote.Tests/TransientSerializationErrorSpec.cs
@@ -116,7 +116,7 @@ namespace Akka.Remote.Tests
             }
         }
 
-        internal class EchoActor : ActorBase
+        private class EchoActor : ActorBase
         {
             public static Props Props()
             {
@@ -130,16 +130,16 @@ namespace Akka.Remote.Tests
             }
         }
 
-        private readonly ActorSystem system2;
-        private readonly Address system2Address;
+        private readonly ActorSystem _system2;
+        private readonly Address _system2Address;
 
-        public AbstractTransientSerializationErrorSpec(Config config)
+        protected AbstractTransientSerializationErrorSpec(Config config)
             : base(config.WithFallback(ConfigurationFactory.ParseString(GetConfig())))
         {
             var port = ((ExtendedActorSystem)Sys).Provider.DefaultAddress.Port;
 
-            system2 = ActorSystem.Create(Sys.Name, Sys.Settings.Config);
-            system2Address = ((ExtendedActorSystem)system2).Provider.DefaultAddress;
+            _system2 = ActorSystem.Create(Sys.Name, Sys.Settings.Config);
+            _system2Address = ((ExtendedActorSystem)_system2).Provider.DefaultAddress;
         }
 
         private static string GetConfig()
@@ -168,21 +168,22 @@ namespace Akka.Remote.Tests
             ";
         }
 
-        protected override async Task AfterTerminationAsync()
+        protected override async Task AfterAllAsync()
         {
-            await ShutdownAsync(system2);
+            await base.AfterAllAsync();
+            await ShutdownAsync(_system2);
         }
 
 
         [Fact]
-        public void The_transport_must_stay_alive_after_a_transient_exception_from_the_serializer()
+        public async Task The_transport_must_stay_alive_after_a_transient_exception_from_the_serializer()
         {
-            system2.ActorOf(EchoActor.Props(), "echo");
+            _system2.ActorOf(EchoActor.Props(), "echo");
 
-            var selection = Sys.ActorSelection(new RootActorPath(system2Address) / "user" / "echo");
+            var selection = Sys.ActorSelection(new RootActorPath(_system2Address) / "user" / "echo");
 
             selection.Tell("ping", this.TestActor);
-            ExpectMsg("ping");
+            await ExpectMsgAsync("ping");
 
             // none of these should tear down the connection
             selection.Tell(ManifestIllegal.Instance, this.TestActor);
@@ -194,7 +195,7 @@ namespace Akka.Remote.Tests
 
             // make sure we still have a connection
             selection.Tell("ping", this.TestActor);
-            ExpectMsg("ping");
+            await ExpectMsgAsync("ping");
         }
     }
 
