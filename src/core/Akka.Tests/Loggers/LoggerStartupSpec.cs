780b353508e3e77abd9ca8b9fad276fce1d0eeca
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-07-02 02:05:41 +0700

    Simplify unit test

--- src/core/Akka.Tests/Loggers/LoggerStartupSpec.cs
+++ src/core/Akka.Tests/Loggers/LoggerStartupSpec.cs
@@ -27,47 +27,35 @@ namespace Akka.Tests.Loggers
             XUnitOutLogger.Helper = helper;
         }
         
-        [Fact]
-        public void ActorSystem_should_start_with_loggers_timing_out()
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void ActorSystem_should_start_with_loggers_timing_out(bool useAsync)
         {
             var slowLoggerConfig = ConfigurationFactory.ParseString($@"
 akka.stdout-logger-class = ""{typeof(XUnitOutLogger).FullName}, {typeof(XUnitOutLogger).Assembly.GetName().Name}""
 akka.loggers = [""{typeof(SlowLoggerActor).FullName}, {typeof(SlowLoggerActor).Assembly.GetName().Name}""]
 akka.logger-startup-timeout = 100ms").WithFallback(DefaultConfig);
-            
-            var loggerAsyncStartDisabledConfig = ConfigurationFactory.ParseString("akka.logger-async-start = false")
-                .WithFallback(slowLoggerConfig);
-            var loggerAsyncStartEnabledConfig = ConfigurationFactory.ParseString("akka.logger-async-start = true")
+
+            var config = ConfigurationFactory.ParseString($"akka.logger-async-start = {(useAsync ? "true" : "false")}")
                 .WithFallback(slowLoggerConfig);
 
-            ActorSystem sys1 = null;
-            ActorSystem sys2 = null;
+            ActorSystem sys = null;
             try
             {
-                this.Invoking(_ => sys1 = ActorSystem.Create("handing", loggerAsyncStartDisabledConfig)).Should()
+                this.Invoking(_ => sys = ActorSystem.Create("test", config)).Should()
                     .NotThrow("System should not fail to start when a logger timed out when logger is created synchronously");
 
                 // Logger actor should die
-                var probe1 = CreateTestProbe(sys1);
-                SlowLoggerActor.Probe = probe1;
-                Logging.GetLogger(sys1, this).Error("TEST");
-                probe1.ExpectNoMsg(1.Seconds());
-
-                this.Invoking(_ => sys2 = ActorSystem.Create("working", loggerAsyncStartEnabledConfig)).Should()
-                    .NotThrow("System should not fail to start when a logger timed out when logger is created asynchronously (setting should be ignored internally)");
-
-                // Logger actor should die
-                var probe2 = CreateTestProbe(sys2);
-                SlowLoggerActor.Probe = probe2;
-                Logging.GetLogger(sys2, this).Error("TEST");
-                probe2.ExpectNoMsg(1.Seconds());
+                var probe = CreateTestProbe(sys);
+                SlowLoggerActor.Probe = probe;
+                Logging.GetLogger(sys, this).Error("TEST");
+                probe.ExpectNoMsg(200.Milliseconds());
             }
             finally
             {
-                if(sys1 != null)
-                    Shutdown(sys1);
-                if(sys2 != null)
-                    Shutdown(sys2);
+                if(sys != null)
+                    Shutdown(sys);
             }
         }
 
