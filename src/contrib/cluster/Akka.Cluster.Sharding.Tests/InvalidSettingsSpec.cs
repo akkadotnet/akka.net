dcba034ec3a00f58af4bdc78ef614aa3e0ae10e8
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-10-11 19:20:48 +0700

    Improve error/exception message for misconfigured cluster provider (#6167)
    * Improve error/exception message for misconfigured cluster provider
    
    * Remove pattern matching
    
    * Improve validation to also catch other invalid configuration scenario
    
    * Simplify code
    
    * Update API Verify list
    
    (cherry picked from commit 5f8710d9ce016227b1e73e9c2dc1957f74508b19)

---
+++ src/contrib/cluster/Akka.Cluster.Sharding.Tests/InvalidSettingsSpec.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="InvalidSettingsSpec.cs" company="Akka.NET Project">
+//      Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+//      Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Configuration;
+using Akka.TestKit;
+using FluentAssertions;
+using Xunit;
+using static FluentAssertions.FluentActions;
+
+namespace Akka.Cluster.Sharding.Tests
+{
+    public class InvalidSettingsSpec: AkkaSpec
+    {
+        [Fact(DisplayName = "ClusterSharding started with invalid actor provider should raise a user friendly exception")]
+        public void InvalidActorProviderTest()
+        {
+            Invoking(() => ClusterSharding.Get(Sys)) // throws here
+                .Should().ThrowExactly<ConfigurationException>()
+                .WithMessage("*Did you forgot*");
+        }
+    }
+}
