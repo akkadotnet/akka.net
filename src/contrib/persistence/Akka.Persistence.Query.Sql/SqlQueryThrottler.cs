15fe2d436fbb9ce7507c483d3a05e8ddc754fb74
Aaron Stannard <aaron@petabridge.com>
2023-02-20 14:05:14 -0600

    working on throttler

---
+++ src/contrib/persistence/Akka.Persistence.Query.Sql/SqlQueryThrottler.cs
@@ -0,0 +1,45 @@
+﻿// //-----------------------------------------------------------------------
+// // <copyright file="SqlQueryThrottler.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+using Akka.Actor;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+
+namespace Akka.Persistence.Query.Sql
+{
+    /// <summary>
+    /// Rate limits the number of active Akka.Persistence.Query instances in order to prevent the journal
+    /// from opening too many concurrent connections.
+    /// </summary>
+    internal sealed class SqlQueryThrottler : ReceiveActor
+    {
+        private readonly int _maxConcurrentQueries;
+        private readonly IActorRef _journalRef;
+
+        private IActorRef _throttler;
+
+        public SqlQueryThrottler(IActorRef journalRef, int maxConcurrentQueries = 30)
+        {
+            _journalRef = journalRef;
+            _maxConcurrentQueries = maxConcurrentQueries;
+        }
+        
+        protected override void PreStart()
+        {
+            var materializer = Context.Materializer();
+            var (actor, source) = Source.ActorRef<IJournalRequest>(1000, OverflowStrategy.DropHead)
+                .PreMaterialize(materializer);
+            _throttler = actor;
+            
+            source
+                .SelectAsync(_maxConcurrentQueries, async request =>
+                {
+                    
+                })
+        }
+    }
+}
