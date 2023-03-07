d703632393475a488f280bedcb96ea944141b2a4
Sergey Popov <sergppv@gmail.com>
2023-03-07 03:59:51 +0300

    Realization of Persistence Query for InMemory Read Journal (#6409)
    * Started with InMemoryReadJournal.
    
    * Update API Verify list
    
    * Update csproj to use common.props and xunitsettings.props
    
    * ICurrentEventsByPersistenceIdQuery and IEventsByPersistenceIdQuery has been implemented for InMemoryReadJournal.
    
    * ICurrentEventsByTagQuery and IEventsByTagQuery has been implemented.
    
    * ICurrentAllEventsQuery and IAllEventsQuery has been implemented.
    
    * IPersistenceIds has been implemented.
    
    * Filesystem dependent test has been fixed.
    
    * Style and documentation fix.
    
    * Moved from long to int part 1.
    
    * Moved from long to int part 2.
    
    * Move from long to int part 3.
    
    * Refactoring.
    
    * Refactoring.
    
    * Update API Verify list
    
    * Revert changes to TCK and fix unit tests
    
    * Remove obsolete query messages
    
    * Update Akka.Persistence.Query.InMemory.Tests.csproj
    
    ---------
    
    Co-authored-by: Gregorius Soedharmo <arkatufus@yahoo.com>
    Co-authored-by: Aaron Stannard <aaron@petabridge.com>

---
+++ src/contrib/persistence/Akka.Persistence.Query.InMemory.Tests/InMemoryCurrentAllEventsSpec.cs
@@ -0,0 +1,28 @@
+// //-----------------------------------------------------------------------
+// // <copyright file="InMemoryCurrentAllEventsSpec.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+using Akka.Configuration;
+using Akka.Persistence.TCK.Query;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Query.InMemory.Tests
+{
+    public class InMemoryCurrentAllEventsSpec : CurrentAllEventsSpec
+    {
+        private static Config Config() => ConfigurationFactory.ParseString($@"
+            akka.loglevel = INFO
+            akka.persistence.query.journal.inmem.refresh-interval = 1s
+            akka.persistence.journal.plugin = ""akka.persistence.journal.inmem""
+            akka.persistence.snapshot-store.plugin = ""akka.persistence.snapshot-store.inmem""")
+            .WithFallback(InMemoryReadJournal.DefaultConfiguration());
+
+        public InMemoryCurrentAllEventsSpec(ITestOutputHelper output) : base(Config(), nameof(InMemoryCurrentAllEventsSpec), output)
+        {
+            ReadJournal = Sys.ReadJournalFor<InMemoryReadJournal>(InMemoryReadJournal.Identifier);
+        }
+    }
+}
---
+++ src/contrib/persistence/Akka.Persistence.Query.InMemory.Tests/InMemoryEventsByPersistenceIdSpec.cs
@@ -0,0 +1,29 @@
+// //-----------------------------------------------------------------------
+// // <copyright file="InMemoryEventsByPersistenceIdSpec.cs" company="Akka.NET Project">
+// //     Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+// //     Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+using Akka.Configuration;
+using Akka.Persistence.TCK.Query;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Query.InMemory.Tests
+{
+    public class InMemoryEventsByPersistenceIdSpec : EventsByPersistenceIdSpec
+    {
+        private static Config Config() => ConfigurationFactory.ParseString($@"
+            akka.loglevel = INFO
+            akka.persistence.query.journal.inmem.refresh-interval = 1s
+            akka.persistence.journal.plugin = ""akka.persistence.journal.inmem""
+            akka.persistence.snapshot-store.plugin = ""akka.persistence.snapshot-store.inmem""")
+            .WithFallback(InMemoryReadJournal.DefaultConfiguration());
+
+        public InMemoryEventsByPersistenceIdSpec(ITestOutputHelper output) :
+            base(Config(), nameof(InMemoryCurrentPersistenceIdsSpec), output)
+        {
+            ReadJournal = Sys.ReadJournalFor<InMemoryReadJournal>(InMemoryReadJournal.Identifier);
+        }
+    }
+}
