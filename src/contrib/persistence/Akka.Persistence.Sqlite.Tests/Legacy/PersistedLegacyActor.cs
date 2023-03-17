17289cd875c6db9f14db177a533bec2797469f09
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-03-17 20:27:42 +0700

    Add legacy data test

---
+++ src/contrib/persistence/Akka.Persistence.Sqlite.Tests/Legacy/PersistedLegacyActor.cs
@@ -0,0 +1,129 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="PersistedActor.cs" company="Akka.NET Project">
+//      Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using Akka.Actor;
+
+namespace Akka.Persistence.Sqlite.Tests
+{
+    public class PersistedLegacyActor: ReceivePersistentActor
+    {
+        private Persisted _savedState;
+        private readonly List<Persisted> _events = new List<Persisted>();
+        private bool _recoveryCompleted;
+        private bool _stateRequested;
+        private int _persistCount;
+        private IActorRef _probe;
+
+        public PersistedLegacyActor(string id, IActorRef probe)
+        {
+            PersistenceId = id;
+            _probe = probe;
+            
+            Recover<SnapshotOffer>(msg => _savedState = (Persisted) msg.Snapshot);
+            Recover<Persisted>(msg => _events.Add(msg));
+            
+            Command<RecoveryCompleted>(_ =>
+            {
+                _recoveryCompleted = true;
+                if(_stateRequested)
+                {
+                    _probe.Tell(new CurrentState(PersistenceId, _savedState, _events));
+                    _stateRequested = false;
+                }
+            });
+            
+            Command<GetState>(_ =>
+            {
+                _stateRequested = true;
+                if(_recoveryCompleted)
+                {
+                    _probe.Tell(new CurrentState(PersistenceId, _savedState, _events));
+                    _stateRequested = false;
+                }
+            });
+            
+            Command<Persisted>(msg =>
+            {
+                Persist(msg, persisted =>
+                {
+                    _persistCount++;
+                    _events.Add(persisted);
+                    _probe.Tell(PersistAck.Instance);
+                    if(_persistCount == 5)
+                        SaveSnapshot(persisted);
+                });
+            });
+            
+            Command<SaveSnapshotSuccess>(msg =>
+            {
+                _savedState = _events.Last();
+                DeleteMessages(msg.Metadata.SequenceNr);
+            });
+            
+            Command<DeleteMessagesSuccess>(msg =>
+            {
+                var deleted = _events.Where(e => e.Payload <= msg.ToSequenceNr).ToArray();
+                foreach (var e in deleted)
+                    _events.Remove(e);
+                _probe.Tell(new SaveSnapshotAck(_savedState, _events));
+            });
+        }
+        
+        public override string PersistenceId { get; }
+        
+        public sealed class Persisted
+        {
+            public Persisted(int payload)
+            {
+                Payload = payload;
+            }
+
+            public int Payload { get; }
+        }
+        
+        public sealed class PersistAck
+        {
+            public static readonly PersistAck Instance = new PersistAck();
+            private PersistAck() { }
+        }
+        
+        public sealed class SaveSnapshotAck
+        {
+            public SaveSnapshotAck(Persisted state, List<Persisted> events)
+            {
+                State = state;
+                Events = events;
+            }
+
+            public Persisted State { get; }
+            public List<Persisted> Events { get; }
+        }
+        
+        public sealed class GetState
+        {
+            public static readonly GetState Instance = new GetState();
+            private GetState() { }
+        }
+        
+        public sealed class CurrentState
+        {
+            public CurrentState(string pid, Persisted state, List<Persisted> events)
+            {
+                PersistenceId = pid;
+                State = state;
+                Events = events;
+            }
+
+            public string PersistenceId { get; }
+            public Persisted State { get; }
+            public List<Persisted> Events { get; }
+        }
+    }
+
+}
---
+++ src/contrib/persistence/Akka.Persistence.Sqlite.Tests/Legacy/SqliteLegacyJournalSpec.cs
@@ -0,0 +1,121 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="SqliteLegacyJournalSpec.cs" company="Akka.NET Project">
+//      Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.TestKit;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Sqlite.Tests.Legacy
+{
+    public class SqliteLegacyJournalSpec: Akka.TestKit.Xunit2.TestKit
+    {
+        private Dictionary<string, IActorRef> _actors = new Dictionary<string, IActorRef>();
+        private readonly TestProbe _probe;
+
+        public SqliteLegacyJournalSpec(ITestOutputHelper output)
+            : base(CreateSpecConfig("Filename=file:.\\\\data\\\\Sqlite.v1.3.0.db"), nameof(SqliteLegacyJournalSpec), output)
+        {
+            SqlitePersistence.Get(Sys);
+            _probe = CreateTestProbe();
+        }
+        
+        private static Config CreateSpecConfig(string connectionString)
+        {
+            return ConfigurationFactory.ParseString($@"
+akka.persistence {{
+    publish-plugin-commands = on
+    journal {{
+        plugin = akka.persistence.journal.sqlite
+        sqlite {{
+            auto-initialize = on
+            connection-string = ""{connectionString}""
+        }}
+    }}
+    snapshot-store {{
+        plugin = akka.persistence.snapshot-store.sqlite
+        sqlite {{
+            auto-initialize = on
+            connection-string = ""{connectionString}""
+        }}
+    }}
+}}").WithFallback(SqlitePersistence.DefaultConfiguration());
+        }
+
+        [Fact(DisplayName = "Legacy v1.3.0 and below data (null serializer_id) should be read correctly")]
+        public void ReadLegacyDataTest()
+        {
+            var stateDict = new Dictionary<string, PersistedLegacyActor.CurrentState>();
+            
+            _actors["A"] = Sys.ActorOf(Props.Create(() => new PersistedLegacyActor("A", _probe)));
+            _actors["B"] = Sys.ActorOf(Props.Create(() => new PersistedLegacyActor("B", _probe)));
+            _actors["C"] = Sys.ActorOf(Props.Create(() => new PersistedLegacyActor("C", _probe)));
+
+            _actors["A"].Tell(PersistedLegacyActor.GetState.Instance);
+            _actors["B"].Tell(PersistedLegacyActor.GetState.Instance);
+            _actors["C"].Tell(PersistedLegacyActor.GetState.Instance);
+            
+            for (var i = 0; i < 3; i++)
+            {
+                var state = _probe.ExpectMsg<PersistedLegacyActor.CurrentState>();
+                stateDict[state.PersistenceId] = state;
+            }
+
+            stateDict.Keys.ToArray().Should().BeEquivalentTo("A", "B", "C");
+
+            foreach (var state in stateDict.Values)
+            {
+                state.State.Should().Be(5);
+                state.Events.Count.Should().Be(5);
+                state.Events.Should().BeEquivalentTo(6, 7, 8, 9, 10);
+            }
+        }
+
+        // This method is not byte-rot, this documents how the legacy data are being generated. 
+        private void Generate()
+        {
+            _actors["A"] = Sys.ActorOf(Props.Create(() => new PersistedLegacyActor("A", _probe)));
+            _actors["B"] = Sys.ActorOf(Props.Create(() => new PersistedLegacyActor("B", _probe)));
+            _actors["C"] = Sys.ActorOf(Props.Create(() => new PersistedLegacyActor("C", _probe)));
+            
+            foreach (var i in Enumerable.Range(1, 5))
+            {
+                _actors["A"].Tell(new PersistedLegacyActor.Persisted(i));
+                _probe.ExpectMsg<PersistedLegacyActor.PersistAck>();
+                _actors["B"].Tell(new PersistedLegacyActor.Persisted(i));
+                _probe.ExpectMsg<PersistedLegacyActor.PersistAck>();
+                _actors["C"].Tell(new PersistedLegacyActor.Persisted(i));
+                _probe.ExpectMsg<PersistedLegacyActor.PersistAck>();
+            }
+            
+            var a = _probe.ExpectMsg<PersistedLegacyActor.SaveSnapshotAck>();
+            var b = _probe.ExpectMsg<PersistedLegacyActor.SaveSnapshotAck>();
+            var c = _probe.ExpectMsg<PersistedLegacyActor.SaveSnapshotAck>();
+            new [] { a.State.Payload, b.State.Payload, c.State.Payload }.Should().BeEquivalentTo(5, 5, 5);
+            a.Events.Count.Should().Be(0);
+            b.Events.Count.Should().Be(0);
+            c.Events.Count.Should().Be(0);
+            
+            foreach (var i in Enumerable.Range(6, 5))
+            {
+                _actors["A"].Tell(new PersistedLegacyActor.Persisted(i));
+                _probe.ExpectMsg<PersistedLegacyActor.PersistAck>();
+                _actors["B"].Tell(new PersistedLegacyActor.Persisted(i));
+                _probe.ExpectMsg<PersistedLegacyActor.PersistAck>();
+                _actors["C"].Tell(new PersistedLegacyActor.Persisted(i));
+                _probe.ExpectMsg<PersistedLegacyActor.PersistAck>();
+            }
+        }
+
+    }
+}
