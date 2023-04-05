b6a4ab5547ee385fe1597a385f49fe2f43d16c08
Aaron Stannard <aaron@petabridge.com>
2023-04-05 08:55:53 -0500

    disable SBR in all clustering specs

--- src/core/Akka.Cluster.TestKit/MultiNodeClusterSpec.cs
+++ src/core/Akka.Cluster.TestKit/MultiNodeClusterSpec.cs
@@ -59,6 +59,7 @@ namespace Akka.Cluster.TestKit
                         retry-interval = 200ms
                         waiting-for-state-timeout = 200ms
                     }
+                    downing-provider-class = """" # disable default SBR
                 }
                 akka.loglevel = INFO
                 akka.log-dead-letters = off
