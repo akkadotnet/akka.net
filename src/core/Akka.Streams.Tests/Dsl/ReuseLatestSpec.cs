3b76b12038342ed18aaf3898efed6527fcedb979
Aaron Stannard <aaron@petabridge.com>
2023-04-14 12:04:11 -0500

    updated all file headers to 2023 (#6649)

--- src/core/Akka.Streams.Tests/Dsl/ReuseLatestSpec.cs
+++ src/core/Akka.Streams.Tests/Dsl/ReuseLatestSpec.cs
@@ -1,6 +1,7 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="RepeatPreviousSpec.cs" company="Akka.NET Project">
-//     Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// <copyright file="ReuseLatestSpec.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
 // </copyright>
 //-----------------------------------------------------------------------
 
--- src/core/Akka/Util/FastLazy.cs
+++ src/core/Akka/Util/FastLazy.cs
@@ -1,7 +1,7 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="FastLazy.cs" company="Akka.NET Project">
-//     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
-//     Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//     Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
 // </copyright>
 //-----------------------------------------------------------------------
 
