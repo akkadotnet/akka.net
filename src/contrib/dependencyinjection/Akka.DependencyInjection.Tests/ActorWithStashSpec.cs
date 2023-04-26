02a17b778d1a3c9e92fa83d55135b71aa6f82592
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-04-27 00:18:42 +0700

    Add child actor unit tests

--- src/contrib/dependencyinjection/Akka.DependencyInjection.Tests/ActorWithStashSpec.cs
+++ src/contrib/dependencyinjection/Akka.DependencyInjection.Tests/ActorWithStashSpec.cs
@@ -26,7 +26,7 @@ public class ActorWithStashSpec: AkkaSpec, IClassFixture<AkkaDiFixture>
     [Fact(DisplayName = "DependencyInjection should create actor with IWithStash interface")]
     public void WithStashActorTest()
     {
-        var stashActor = Sys.ActorOf(DependencyResolver.For(Sys).Props<WithStashActor>());
+        var stashActor = GetActorOf<WithStashActor>(Sys);
         
         stashActor.Tell(GetName.Instance, TestActor);
         ExpectNoMsg(0.3.Seconds());
@@ -42,7 +42,7 @@ public class ActorWithStashSpec: AkkaSpec, IClassFixture<AkkaDiFixture>
     [Fact(DisplayName = "DependencyInjection should create actor with IWithUnboundedStash interface")]
     public void WithUnboundedStashActorTest()
     {
-        var stashActor = Sys.ActorOf(DependencyResolver.For(Sys).Props<WithUnboundedStashActor>());
+        var stashActor = GetActorOf<WithUnboundedStashActor>(Sys);
         
         stashActor.Tell(GetName.Instance, TestActor);
         ExpectNoMsg(0.3.Seconds());
@@ -55,6 +55,44 @@ public class ActorWithStashSpec: AkkaSpec, IClassFixture<AkkaDiFixture>
         ExpectNoMsg(0.3.Seconds());
     }
     
+    [Fact(DisplayName = "DependencyInjection should create child actor with IWithStash interface")]
+    public void WithStashChildActorTest()
+    {
+        var parentActor = Sys.ActorOf(Props.Create(() => new ParentActor<WithStashActor>()));
+        
+        parentActor.Tell(GetName.Instance, TestActor);
+        ExpectNoMsg(0.3.Seconds());
+        parentActor.Tell(GetName.Instance, TestActor);
+        ExpectNoMsg(0.3.Seconds());
+        
+        parentActor.Tell(StartProcessing.Instance, TestActor);
+        ExpectMsg<string>().Should().StartWith("s");
+        ExpectMsg<string>().Should().StartWith("s");
+        ExpectNoMsg(0.3.Seconds());
+    }
+    
+    [Fact(DisplayName = "DependencyInjection should create child actor with IWithUnboundedStash interface")]
+    public void WithUnboundedStashChildActorTest()
+    {
+        var parentActor = Sys.ActorOf(Props.Create(() => new ParentActor<WithUnboundedStashActor>()));
+        
+        parentActor.Tell(GetName.Instance, TestActor);
+        ExpectNoMsg(0.3.Seconds());
+        parentActor.Tell(GetName.Instance, TestActor);
+        ExpectNoMsg(0.3.Seconds());
+        
+        parentActor.Tell(StartProcessing.Instance, TestActor);
+        ExpectMsg<string>().Should().StartWith("s");
+        ExpectMsg<string>().Should().StartWith("s");
+        ExpectNoMsg(0.3.Seconds());
+    }
+    
+    private static IActorRef GetActorOf<T>(ActorSystem actorSystem) where T: ActorBase
+        => actorSystem.ActorOf(DependencyResolver.For(actorSystem).Props<T>());
+    
+    private static IActorRef GetActorOf<T>(IActorContext actorContext, ActorSystem actorSystem) where T: ActorBase
+        => actorContext.ActorOf(DependencyResolver.For(actorSystem).Props<T>());
+    
     private sealed class WithStashActor: StashingActor, IWithStash
     {
         public WithStashActor(AkkaDiFixture.IScopedDependency scoped) : base(scoped)
@@ -66,6 +104,15 @@ public class ActorWithStashSpec: AkkaSpec, IClassFixture<AkkaDiFixture>
     {
         public WithUnboundedStashActor(AkkaDiFixture.IScopedDependency scoped) : base(scoped)
         {
+        }
+    }
+    
+    private sealed class ParentActor<T>: ReceiveActor where T: StashingActor
+    {
+        public ParentActor()
+        {
+            var child = GetActorOf<T>(Context, Context.System);
+            ReceiveAny(msg => child.Forward(msg));
         }
     }
     
