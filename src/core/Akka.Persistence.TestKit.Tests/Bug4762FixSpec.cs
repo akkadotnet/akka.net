df576338038951715f6a41f939793bb76ce4ff67
Aaron Stannard <aaron@petabridge.com>
2024-01-04 16:07:19 -0600

    attempting to debug Akka.Persistence.TestKit.Tests

--- src/core/Akka.Persistence.TestKit.Tests/Bug4762FixSpec.cs
+++ src/core/Akka.Persistence.TestKit.Tests/Bug4762FixSpec.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Event;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Akka.Persistence.TestKit.Tests
 {
@@ -21,6 +22,11 @@ namespace Akka.Persistence.TestKit.Tests
     /// </summary>
     public class Bug4762FixSpec : PersistenceTestKit
     {
+        public Bug4762FixSpec(ITestOutputHelper outputHelper) : base(output: outputHelper)
+        {
+            
+        }
+        
         private class WriteMessage
         { }
 
@@ -65,10 +71,10 @@ namespace Akka.Persistence.TestKit.Tests
         }
 
         [Fact]
-        public async Task TestJournal_PersistAll_should_only_count_each_event_exceptions_once()
+        public Task TestJournal_PersistAll_should_only_count_each_event_exceptions_once()
         {
             var probe = CreateTestProbe();
-            await WithJournalWrite(write => write.Pass(), async () =>
+            return WithJournalWrite(write => write.Pass(), async () =>
             {
                 var actor = ActorOf(() => new TestActor2(probe));
                 Watch(actor);
--- src/core/Akka.Persistence.TestKit/Journal/JournalWriteBehaviorSetter.cs
+++ src/core/Akka.Persistence.TestKit/Journal/JournalWriteBehaviorSetter.cs
@@ -18,7 +18,7 @@ namespace Akka.Persistence.TestKit
     {
         internal JournalWriteBehaviorSetter(IActorRef journal)
         {
-            this._journal = journal;
+            _journal = journal;
         }
 
         private readonly IActorRef _journal;
