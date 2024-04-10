49fe208f61725b30f917ee6812e75d3779e723c4
Aaron Stannard <aaron@petabridge.com>
2024-04-10 14:38:20 -0500

    improve Akka.Cluster / Akka.Remote `DeadLetter` logging (#7149)
    * reformatted `DefaultMessageDispatcher`
    
    * added real XML-DOC comments for `ReliableDeliverySupervisor`
    
    * perf optimized `DeadLetterListener`
    
    * added realistic `ToString` method on `GossipEnvelope`
    
    * fixed multiple enumeration problem inside `ClusterDaemon`
    
    * solved another multiple enumeration problem
    
    * minor perf optimization
    
    * test and cluster event publishing clean up
    
    * clean up message extractor build warning
    
    * cleanup additional build warning related to `StartEntity`
    
    * harden failing `ClusterSpec`

--- src/core/Akka.Cluster.Tests/ClusterSpec.cs
+++ src/core/Akka.Cluster.Tests/ClusterSpec.cs
@@ -476,7 +476,7 @@ namespace Akka.Cluster.Tests
                 await probe.ExpectMsgAsync<ClusterEvent.MemberLeft>();
                 // MemberExited might not be published before MemberRemoved
                 var removed = (ClusterEvent.MemberRemoved)await probe.FishForMessageAsync(m => m is ClusterEvent.MemberRemoved);
-                removed.PreviousStatus.Should().BeEquivalentTo(MemberStatus.Exiting);
+                new [] {MemberStatus.Exiting, MemberStatus.Leaving}.Should().Contain(removed.PreviousStatus);
                 
                 await task.ShouldCompleteWithin(3.Seconds());
             }
