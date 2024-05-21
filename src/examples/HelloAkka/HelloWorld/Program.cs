236074b55541cc45151a2b92b6a87851f6ea6e99
Aaron Stannard <aaron@petabridge.com>
2024-05-21 14:19:04 -0500

    rewrote to use classless `main`

--- src/examples/HelloAkka/HelloWorld/Program.cs
+++ src/examples/HelloAkka/HelloWorld/Program.cs
@@ -5,35 +5,26 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-#region akka-hello-world-main
 
 using Akka.Actor;
+using HelloWorld;
 
-namespace HelloWorld;
+// ReSharper disable SuggestVarOrType_SimpleTypes
 
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        // create a new actor system (a container for actors)
-        ActorSystem system = ActorSystem.Create("the-universe");
+ActorSystem system = ActorSystem.Create("the-universe");
 
-        // create actor and get a reference to it.
-        // this will be an "ActorRef", which is not a 
-        // reference to the actual actor instance
-        // but rather a client or proxy to it
-        IActorRef greeter = system.ActorOf<GreetingActor>("greeter");
+// create actor and get a reference to it.
+// this will be an "ActorRef", which is not a 
+// reference to the actual actor instance
+// but rather a client or proxy to it
+IActorRef greeter = system.ActorOf<GreetingActor>("greeter");
 
-        // send a message to the actor
-        greeter.Tell(new Greet("World"));
+// send a message to the actor
+greeter.Tell(new Greet("World"));
 
-        //this is for demonstration purposes
-        await Task.Delay(TimeSpan.FromSeconds(1));
-        system.Stop(greeter);
+//this is for demonstration purposes
+await Task.Delay(TimeSpan.FromSeconds(1));
+system.Stop(greeter);
 
-        // prevent the application from exiting before message is handled            
-        await system.WhenTerminated;
-    }
-}
-
-#endregion
+// prevent the application from exiting before message is handled            
+await system.WhenTerminated;
