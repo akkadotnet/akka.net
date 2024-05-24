akkadotnet/akka.net

commit 04630daa191400c3a3815d2b28b278fd577ea419

Akka.Serialization: `INoSerializationVerificationNeeded` does not handle `IWrappedMessage` correctly (#7010)
* Added reproduction

`INoSerializationVerificationNeeded` does not handle `IWrappedMessage` correctly

* fixed issue

* added correct unwrapping method

* Fix dead letter behaviour change

* Fix implementation

---------

Co-authored-by: Gregorius Soedharmo <arkatufus@yahoo.com>

src/core/Akka/Actor/ActorCell.cs
@@ -516,11 +516,13 @@ namespace Akka.Actor
             return current != null ? current.Sender : ActorRefs.NoSender;
         }
 
+        #nullable enable
         private Envelope SerializeAndDeserialize(Envelope envelope)
         {
-            DeadLetter deadLetter;
-            var unwrapped = (deadLetter = envelope.Message as DeadLetter) != null ? deadLetter.Message : envelope.Message;
-
+            // recursively unwraps message, no need to check for DeadLetter because DeadLetter inherits IWrappedMessage
+            var unwrapped = WrappedMessage.Unwrap(envelope.Message);
+            
+            // don't do serialization verification if the underlying type doesn't require it
             if (unwrapped is INoSerializationVerificationNeeded)
                 return envelope;
 
@@ -534,11 +536,12 @@ namespace Akka.Actor
                 throw new SerializationException($"Failed to serialize and deserialize payload object [{unwrapped.GetType()}]. Envelope: [{envelope}], Actor type: [{Actor.GetType()}]", e);
             }
 
-            if (deadLetter != null)
-                return new Envelope(new DeadLetter(deserializedMsg, deadLetter.Sender, deadLetter.Recipient), envelope.Sender);
-            return new Envelope(deserializedMsg, envelope.Sender);
-
+            // special case handling for DeadLetters
+            return envelope.Message is DeadLetter deadLetter 
+                ? new Envelope(new DeadLetter(deserializedMsg, deadLetter.Sender, deadLetter.Recipient), envelope.Sender) 
+                : new Envelope(deserializedMsg, envelope.Sender);
         }
+        #nullable restore
 
         private object SerializeAndDeserializePayload(object obj)
         {

src/core/Akka.Tests/Serialization/SerializeAllMessagesSpec.cs
@@ -1,0 +1,76 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SerializeAllMessagesSpec.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Event;
+using Akka.TestKit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Tests.Serialization;
+
+public class SerializeAllMessagesSpec : AkkaSpec
+{
+    public SerializeAllMessagesSpec(ITestOutputHelper output) : base("akka.actor.serialize-messages = on", output)
+    {
+    }
+    
+    private class MyMessage : INoSerializationVerificationNeeded
+    {
+        public MyMessage(Action myAction)
+        {
+            MyAction = myAction;
+        }
+
+        // add an unserializable member, such as a delegate
+        public Action MyAction { get; }
+    }
+
+    private class MyWrappedMessage : IWrappedMessage
+    {
+        public MyWrappedMessage(object message)
+        {
+            Message = message;
+        }
+
+        public object Message { get; }
+    }
+    
+    // create an actor that will process a MyWrappedMessage and invoke the delegate
+    private class MyActor : ReceiveActor
+    {
+        public MyActor()
+        {
+            Receive<MyWrappedMessage>(msg =>
+            {
+                var myMessage = (MyMessage) msg.Message;
+                myMessage.MyAction();
+            });
+        }
+    }
+    
+    [Fact]
+    public async Task Should_not_serialize_WrappedMessage_with_INoSerializationVerificationNeeded()
+    {
+        // Arrange
+        var myProbe = CreateTestProbe();
+        var action = () => { myProbe.Tell("worked"); };
+        var message = new MyMessage(action);
+        var wrappedMessage = new MyWrappedMessage(message);
+        
+        var myActor = Sys.ActorOf(Props.Create(() => new MyActor()), "wrapped-message-actor");
+
+        await EventFilter.Error().ExpectAsync(0, async () =>
+        {
+            // Act
+            myActor.Tell(wrappedMessage);
+            await myProbe.ExpectMsgAsync("worked");
+        });
+    }
+}

src/core/Akka/Actor/BuiltInActors.cs
@@ -191,8 +191,10 @@ namespace Akka.Actor
     {
         public static object Unwrap(object message)
         {
-            if (message is IWrappedMessage wm)
-                return Unwrap(wm.Message);
+            while (message is IWrappedMessage wm)
+            {
+                message = wm.Message;
+            }
             return message;
         }
     }

commit 8100d3021e4ea109cb424f404ba4bd5add59e95e

Akka.Actor.Serialization: remember to serialize envelope AND underlying type when using `serialize-messages=on` (#7200)
* Fix bug introduced by https://github.com/akkadotnet/akka.net/pull/7010

need to serialize both the envelope and the underlying type when `serialize-messages=on`

* handle edge case with `IWrappedMessage` that implements `INoSerializationVerificationNeeded`

* Add unit test

* Fix pub-sub router spec

---------

Co-authored-by: Gregorius Soedharmo <arkatufus@yahoo.com>

src/core/Akka/Actor/ActorCell.cs
@@ -519,6 +519,10 @@ namespace Akka.Actor
         #nullable enable
         private Envelope SerializeAndDeserialize(Envelope envelope)
         {
+            // in case someone has an IWrappedMessage that ALSO implements INoSerializationVerificationNeeded
+            if(envelope.Message is INoSerializationVerificationNeeded)
+                return envelope;
+            
             // recursively unwraps message, no need to check for DeadLetter because DeadLetter inherits IWrappedMessage
             var unwrapped = WrappedMessage.Unwrap(envelope.Message);
             
@@ -529,7 +533,7 @@ namespace Akka.Actor
             object deserializedMsg;
             try
             {
-                deserializedMsg = SerializeAndDeserializePayload(unwrapped);
+                deserializedMsg = SerializeAndDeserializePayload(envelope.Message);
             }
             catch (Exception e)
             {

src/contrib/cluster/Akka.Cluster.Tools.Tests/PublishSubscribe/DistributedPubSubMediatorRouterSpec.cs
@@ -13,23 +13,51 @@ using Akka.Event;
 using Akka.Routing;
 using Akka.TestKit;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Akka.Cluster.Tools.Tests.PublishSubscribe
 {
-    public class WrappedMessage : RouterEnvelope
+    public class WrappedMessage : RouterEnvelope, IEquatable<WrappedMessage>
     {
         public WrappedMessage(string message) : base(message)
         {
         }
+
+        public bool Equals(WrappedMessage other) => other is not null && other.Message.Equals(Message);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is WrappedMessage wrapped && Equals(wrapped);
+        }
+
+        public override int GetHashCode() => Message != null ? Message.GetHashCode() : 0;
     }
 
-    public class UnwrappedMessage
+    public class UnwrappedMessage: IEquatable<UnwrappedMessage>
     {
         public string Message { get; }
 
         public UnwrappedMessage(string message)
         {
             Message = message;
+        }
+
+        public bool Equals(UnwrappedMessage other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            return other is not null && Message.Equals(other.Message);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is WrappedMessage wrapped && Equals(wrapped);
+        }
+
+        public override int GetHashCode()
+        {
+            return Message != null ? Message.GetHashCode() : 0;
         }
     }
 
@@ -38,13 +66,13 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
         private IActorRef mediator;
         private string path;
 
-        public DistributedPubSubMediatorRouterSpec(Config config) : base(config)
+        public DistributedPubSubMediatorRouterSpec(Config config, ITestOutputHelper output) : base(config, output)
         {
             mediator = DistributedPubSub.Get(Sys).Mediator;
             path = TestActor.Path.ToStringWithoutAddress();
         }
 
-        protected void NonUnwrappingPubSub(object msg)
+        protected void NonUnwrappingPubSub<T>(T msg)
         {
             Keep_the_RouterEnvelope_when_sending_to_local_logical_path(msg);
             Keep_the_RouterEnvelope_when_sending_to_logical_path(msg);
@@ -54,7 +82,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
             Send_message_to_dead_letters_if_no_recipients_available(msg);
         }
 
-        private void Keep_the_RouterEnvelope_when_sending_to_local_logical_path(object msg)
+        private void Keep_the_RouterEnvelope_when_sending_to_local_logical_path<T>(T msg)
         {
             mediator.Tell(new Put(TestActor));
             mediator.Tell(new Send(path, msg, localAffinity: true));
@@ -62,7 +90,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
             mediator.Tell(new Remove(path));
         }
 
-        private void Keep_the_RouterEnvelope_when_sending_to_logical_path(object msg)
+        private void Keep_the_RouterEnvelope_when_sending_to_logical_path<T>(T msg)
         {
             mediator.Tell(new Put(TestActor));
             mediator.Tell(new Send(path, msg, localAffinity: false));
@@ -70,7 +98,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
             mediator.Tell(new Remove(path));
         }
 
-        private void Keep_the_RouterEnvelope_when_sending_to_all_actors_on_logical_path(object msg)
+        private void Keep_the_RouterEnvelope_when_sending_to_all_actors_on_logical_path<T>(T msg)
         {
             mediator.Tell(new Put(TestActor));
             mediator.Tell(new SendToAll(path, msg));
@@ -78,7 +106,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
             mediator.Tell(new Remove(path));
         }
 
-        private void Keep_the_RouterEnvelope_when_sending_to_topic(object msg)
+        private void Keep_the_RouterEnvelope_when_sending_to_topic<T>(T msg)
         {
             mediator.Tell(new Subscribe("topic", TestActor));
             ExpectMsg<SubscribeAck>();
@@ -90,7 +118,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
             ExpectMsg<UnsubscribeAck>();
         }
 
-        private void Keep_the_RouterEnvelope_when_sending_to_topic_for_group(object msg)
+        private void Keep_the_RouterEnvelope_when_sending_to_topic_for_group<T>(T msg)
         {
             mediator.Tell(new Subscribe("topic", TestActor, "group"));
             ExpectMsg<SubscribeAck>();
@@ -102,7 +130,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
             ExpectMsg<UnsubscribeAck>();
         }
 
-        private void Send_message_to_dead_letters_if_no_recipients_available(object msg)
+        private void Send_message_to_dead_letters_if_no_recipients_available<T>(T msg)
         {
             var probe = CreateTestProbe();
             Sys.EventStream.Subscribe(probe.Ref, typeof(DeadLetter));
@@ -114,7 +142,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
 
     public class DistributedPubSubMediatorWithRandomRouterSpec : DistributedPubSubMediatorRouterSpec
     {
-        public DistributedPubSubMediatorWithRandomRouterSpec() : base(DistributedPubSubMediatorRouterConfig.GetConfig("random"))
+        public DistributedPubSubMediatorWithRandomRouterSpec(ITestOutputHelper output) : base(DistributedPubSubMediatorRouterConfig.GetConfig("random"), output)
         {
         }
 
@@ -135,7 +163,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
 
     public class DistributedPubSubMediatorWithRoundRobinRouterSpec : DistributedPubSubMediatorRouterSpec
     {
-        public DistributedPubSubMediatorWithRoundRobinRouterSpec() : base(DistributedPubSubMediatorRouterConfig.GetConfig("round-robin"))
+        public DistributedPubSubMediatorWithRoundRobinRouterSpec(ITestOutputHelper output) : base(DistributedPubSubMediatorRouterConfig.GetConfig("round-robin"), output)
         {
         }
 
@@ -156,7 +184,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
 
     public class DistributedPubSubMediatorWithBroadcastRouterSpec : DistributedPubSubMediatorRouterSpec
     {
-        public DistributedPubSubMediatorWithBroadcastRouterSpec() : base(DistributedPubSubMediatorRouterConfig.GetConfig("broadcast"))
+        public DistributedPubSubMediatorWithBroadcastRouterSpec(ITestOutputHelper output) : base(DistributedPubSubMediatorRouterConfig.GetConfig("broadcast"), output)
         {
         }
 
@@ -213,6 +241,7 @@ namespace Akka.Cluster.Tools.Tests.PublishSubscribe
             return ConfigurationFactory.ParseString($@"
                 akka.loglevel = INFO
                 akka.actor.provider = cluster
+                akka.actor.serialize-messages = on
                 akka.remote.dot-netty.tcp.port = 0
                 akka.remote.log-remote-lifecycle-events = off
                 akka.cluster.pub-sub.routing-logic = {routingLogic}

src/core/Akka.Tests/Actor/ActorCellSerializeMessagesTests.cs
@@ -1,0 +1,58 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ActorCellSerializeMessagesTests.cs" company="Akka.NET Project">
+//      Copyright (C) 2009-2024 Lightbend Inc. <http://www.lightbend.com>
+//      Copyright (C) 2013-2024 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Actor;
+using Akka.TestKit;
+using Xunit;
+using Xunit.Abstractions;
+using FluentAssertions;
+
+namespace Akka.Tests.Actor;
+
+public class ActorCellSerializeMessagesTests: AkkaSpec
+{
+    private class InnerMessage
+    {
+        public InnerMessage(string payload)
+        {
+            Payload = payload;
+        }
+
+        public string Payload { get; }
+    }
+    
+    private class MessageWrapper: IWrappedMessage
+    {
+        public MessageWrapper(object message)
+        {
+            Message = message;
+        }
+
+        public object Message { get; }
+    }
+    
+    private class EchoActor: ReceiveActor
+    {
+        public EchoActor()
+        {
+            ReceiveAny(msg => Sender.Tell(msg));
+        }
+    }
+    
+    public ActorCellSerializeMessagesTests(ITestOutputHelper output) : base("akka.actor.serialize-messages=on", output)
+    {
+    }
+
+    [Fact(DisplayName = "Wrapped message should be received by actors intact")]
+    public void WrappedMessageTest()
+    {
+        var actor = Sys.ActorOf(Props.Create(() => new EchoActor()));
+        actor.Tell(new MessageWrapper(new InnerMessage("payload")));
+        var receivedMsg = ExpectMsg<MessageWrapper>();
+        receivedMsg.Message.Should().BeOfType<InnerMessage>().Which.Payload.Should().Be("payload");
+    }
+}