6e0a8501fe316a17ca5cf7ebd89f52ddf428ef3f
Aaron Stannard <aaron@petabridge.com>
2024-08-15 14:01:29 -0500

    added 2024 headers

--- src/core/Akka.Tests/Actor/ActorCellSerializeMessagesTests.cs
+++ src/core/Akka.Tests/Actor/ActorCellSerializeMessagesTests.cs
@@ -1,9 +1,9 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="ActorCellSerializeMessagesTests.cs" company="Akka.NET Project">
-//      Copyright (C) 2009-2024 Lightbend Inc. <http://www.lightbend.com>
-//      Copyright (C) 2013-2024 .NET Foundation <https://github.com/akkadotnet/akka.net>
-//  </copyright>
-// -----------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------
+// <copyright file="ActorCellSerializeMessagesTests.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2024 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2024 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
 
 using Akka.Actor;
 using Akka.TestKit;
--- src/core/Akka.Tests/Delivery/TestProducer.cs
+++ src/core/Akka.Tests/Delivery/TestProducer.cs
@@ -1,10 +1,10 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="TestProducer.cs" company="Akka.NET Project">
-//      Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
-//      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
-//  </copyright>
-// -----------------------------------------------------------------------
-#nullable enable
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestProducer.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2024 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2024 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
 using System;
 using Akka.Actor;
 using Akka.Delivery;
