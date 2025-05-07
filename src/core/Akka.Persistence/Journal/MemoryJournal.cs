704731a240e453c45c561e37ae4a6820b9be2b67
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-05-08 03:45:20 +0700

    cleanup obsolete warnings

--- src/core/Akka.Persistence/Journal/MemoryJournal.cs
+++ src/core/Akka.Persistence/Journal/MemoryJournal.cs
@@ -32,6 +32,7 @@ namespace Akka.Persistence.Journal
         
         protected virtual ConcurrentDictionary<string, LinkedList<IPersistentRepresentation>> Messages { get { return _messages; } }
         
+        [Obsolete("Use WriteMessagesAsync() that takes a CancellationToken argument instead. Since 1.5.42")]
         protected override Task<IImmutableList<Exception>> WriteMessagesAsync(IEnumerable<AtomicWrite> messages)
             => WriteMessagesAsync(messages, CancellationToken.None);
         
@@ -63,6 +64,7 @@ namespace Akka.Persistence.Journal
             return Task.FromResult<IImmutableList<Exception>>(null); // all good
         }
 
+        [Obsolete("Use ReadHighestSequenceNrAsync() that takes a CancellationToken argument instead. Since 1.5.42")]
         public override Task<long> ReadHighestSequenceNrAsync(string persistenceId, long fromSequenceNr)
             => ReadHighestSequenceNrAsync(persistenceId, fromSequenceNr, CancellationToken.None);
         
@@ -80,6 +82,7 @@ namespace Akka.Persistence.Journal
             return Task.CompletedTask;
         }
         
+        [Obsolete("Use DeleteMessagesToAsync() that takes a CancellationToken argument instead. Since 1.5.42")]
         protected override Task DeleteMessagesToAsync(string persistenceId, long toSequenceNr)
             => DeleteMessagesToAsync(persistenceId, toSequenceNr, CancellationToken.None);
         
