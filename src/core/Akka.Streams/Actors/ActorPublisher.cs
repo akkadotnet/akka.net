20849dcf623de832857ecc098fa924863c273f48
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-06-20 00:01:19 +0700

    Cleanup Akka.Streams build warnings

--- src/core/Akka.Streams/Actors/ActorPublisher.cs
+++ src/core/Akka.Streams/Actors/ActorPublisher.cs
@@ -583,7 +583,13 @@ namespace Akka.Streams.Actors
             if (SubscriptionTimeout != Timeout.InfiniteTimeSpan)
             {
                 _scheduledSubscriptionTimeout = new Cancelable(Context.System.Scheduler);
+                // AK1004: Hard to replace this with IWithTimers implementation because SubscriptionTimeoutExceeded is
+                // being handled inside AroundReceive which interferes with IWithTimers internal message handling.
+                //
+                // Naive IWithTimers implementation will break this actor behavior.
+#pragma warning disable AK1004 
                 Context.System.Scheduler.ScheduleTellOnce(SubscriptionTimeout, Self, SubscriptionTimeoutExceeded.Instance, Self, _scheduledSubscriptionTimeout);
+#pragma warning restore AK1004
             }
         }
 
@@ -622,6 +628,7 @@ namespace Akka.Streams.Actors
         /// </summary>
         public override void AroundPostStop()
         {
+            _scheduledSubscriptionTimeout.Cancel();
             _state.Remove(Self);
             try
             {
