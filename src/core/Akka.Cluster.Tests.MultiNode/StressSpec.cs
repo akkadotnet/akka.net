5aa9ceb2f4f38e9497306ee51792abfa6a986c65
Aaron Stannard <aaron@petabridge.com>
2025-08-12 12:31:26 -0500

    Replace blocking .Wait() calls in StressSpec
    Replaced .Wait() calls with .GetAwaiter().GetResult() to avoid potential
    deadlocks while maintaining synchronous execution model. This is a
    transitional step before fully converting StressSpec to async.
    
    Changes:
    - TestConductor.Exit().Wait() -> GetAwaiter().GetResult()
    - TestConductor.Blackhole().Wait() -> GetAwaiter().GetResult()
    
    This eliminates the blocking wait calls that were causing thread pool
    starvation in CI environments.

--- src/core/Akka.Cluster.Tests.MultiNode/StressSpec.cs
+++ src/core/Akka.Cluster.Tests.MultiNode/StressSpec.cs
@@ -1002,7 +1002,7 @@ akka.remote.default-remote-dispatcher {
                                     Log.Info("Shutting down [{0}]", removeAddress);
                                 }
 
-                                TestConductor.Exit(removeRole, 0).Wait();
+                                TestConductor.Exit(removeRole, 0).GetAwaiter().GetResult();
                             }
                         }, Roles.First());
 
@@ -1060,7 +1060,7 @@ akka.remote.default-remote-dispatcher {
                                     {
                                         if (Settings.Infolog)
                                             Log.Info("Shutting down [{0}]", GetAddress(role));
-                                        TestConductor.Exit(role, 0).Wait(RemainingOrDefault);
+                                        TestConductor.Exit(role, 0).GetAwaiter().GetResult();
                                     }
                                 }
                             }, Roles.First());
@@ -1093,7 +1093,7 @@ akka.remote.default-remote-dispatcher {
                         {
                             foreach (var y in removeRoles)
                             {
-                                TestConductor.Blackhole(x, y, ThrottleTransportAdapter.Direction.Both).Wait();
+                                TestConductor.Blackhole(x, y, ThrottleTransportAdapter.Direction.Both).GetAwaiter().GetResult();
                             }
                         }
                     }, Roles.First());
