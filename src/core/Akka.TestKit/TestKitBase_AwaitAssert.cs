akkadotnet/akka.net

commit 6a290659997e35035f1dce75ba0c7b86bead5a45

Added async API to Akka.TestKit (#4075)

src/core/Akka.TestKit/TestKitBase_AwaitAssert.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Akka.TestKit.Internal;
 
 namespace Akka.TestKit
@@ -51,6 +52,45 @@ namespace Akka.TestKit
                         throw;
                 }
                 Thread.Sleep(t);
+                t = (stop - Now).Min(intervalValue);
+            }
+        }
+        
+        /// <summary>
+        /// <para>Await until the given assertion does not throw an exception or the timeout
+        /// expires, whichever comes first. If the timeout expires the last exception
+        /// is thrown.</para>
+        /// <para>The action is called, and if it throws an exception the thread sleeps
+        /// the specified interval before retrying.</para>
+        /// <para>If no timeout is given, take it from the innermost enclosing `within`
+        /// block.</para>
+        /// <para>Note that the timeout is scaled using <see cref="Dilated" />,
+        /// which uses the configuration entry "akka.test.timefactor".</para>
+        /// </summary>
+        /// <param name="assertion">The action.</param>
+        /// <param name="duration">The timeout.</param>
+        /// <param name="interval">The interval to wait between executing the assertion.</param>
+        public async Task AwaitAssertAsync(Action assertion, TimeSpan? duration=null, TimeSpan? interval=null)
+        {
+            var intervalValue = interval.GetValueOrDefault(TimeSpan.FromMilliseconds(800));
+            if(intervalValue == Timeout.InfiniteTimeSpan) intervalValue = TimeSpan.MaxValue;
+            intervalValue.EnsureIsPositiveFinite("interval");
+            var max = RemainingOrDilated(duration);
+            var stop = Now + max;
+            var t = max.Min(intervalValue);
+            while(true)
+            {
+                try
+                {
+                    assertion();
+                    return;
+                }
+                catch(Exception)
+                {
+                    if(Now + t >= stop)
+                        throw;
+                }
+                await Task.Delay(t);
                 t = (stop - Now).Min(intervalValue);
             }
         }

src/core/Akka.TestKit/EventFilter/IEventFilterApplier.cs
@@ -6,6 +6,7 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Threading.Tasks;
 
 namespace Akka.TestKit
 {
@@ -24,6 +25,26 @@ namespace Akka.TestKit
         /// </summary>
         /// <param name="action">The action.</param>
         void ExpectOne(Action action);
+        
+        /// <summary>
+        /// Executes <paramref name="action"/> and
+        /// expects one event to be logged during the execution.
+        /// This method fails and throws an exception if more than one event is logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        Task ExpectOneAsync(Action action);
+        
+        /// <summary>
+        /// Executes <paramref name="actionAsync"/> and
+        /// expects one event to be logged during the execution.
+        /// This method fails and throws an exception if more than one event is logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <param name="actionAsync">The action.</param>
+        Task ExpectOneAsync(Func<Task> actionAsync);
 
         /// <summary>
         /// Executes <paramref name="action"/> and
@@ -34,7 +55,17 @@ namespace Akka.TestKit
         /// <param name="timeout">The time to wait for a log event after executing <paramref name="action"/></param>
         /// <param name="action">The action.</param>
         void ExpectOne(TimeSpan timeout, Action action);
-
+        
+        /// <summary>
+        /// Executes <paramref name="action"/> and
+        /// expects one event to be logged during the execution.
+        /// This method fails and throws an exception if more than one event is logged,
+        /// or if a timeout occurs.
+        /// </summary>
+        /// <param name="timeout">The time to wait for a log event after executing <paramref name="action"/></param>
+        /// <param name="action">The action.</param>
+        Task ExpectOneAsync(TimeSpan timeout, Action action);
+        
         /// <summary>
         /// Executes <paramref name="action"/> and expects the specified number
         /// of events to be logged during the execution.
@@ -45,6 +76,28 @@ namespace Akka.TestKit
         /// <param name="expectedCount">The expected number of events</param>
         /// <param name="action">The action.</param>
         void Expect(int expectedCount, Action action);
+        
+        /// <summary>
+        /// Executes <paramref name="action"/> and expects the specified number
+        /// of events to be logged during the execution.
+        /// This method fails and throws an exception if more events than expected are logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <param name="expectedCount">The expected number of events</param>
+        /// <param name="action">The action.</param>
+        Task ExpectAsync(int expectedCount, Action action);
+        
+        /// <summary>
+        /// Executes <paramref name="actionAsync"/> task and expects the specified number
+        /// of events to be logged during the execution.
+        /// This method fails and throws an exception if more events than expected are logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <param name="expectedCount">The expected number of events</param>
+        /// <param name="actionAsync">The async action.</param>
+        Task ExpectAsync(int expectedCount, Func<Task> actionAsync);
 
         /// <summary>
         /// Executes <paramref name="action"/> and expects the specified number
@@ -57,6 +110,18 @@ namespace Akka.TestKit
         /// <param name="expectedCount">The expected number of events</param>
         /// <param name="action">The action.</param>
         void Expect(int expectedCount, TimeSpan timeout, Action action);
+        
+        /// <summary>
+        /// Executes <paramref name="action"/> and expects the specified number
+        /// of events to be logged during the execution.
+        /// This method fails and throws an exception if more events than expected are logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <param name="timeout">The time to wait for log events after executing <paramref name="action"/></param>
+        /// <param name="expectedCount">The expected number of events</param>
+        /// <param name="action">The action.</param>
+        Task ExpectAsync(int expectedCount, TimeSpan timeout, Action action);
 
         /// <summary>
         /// Executes <paramref name="func"/> and
@@ -69,6 +134,18 @@ namespace Akka.TestKit
         /// <param name="func">The function.</param>
         /// <returns>The returned value from <paramref name="func"/>.</returns>
         T ExpectOne<T>(Func<T> func);
+        
+        /// <summary>
+        /// Executes <paramref name="func"/> and
+        /// expects one event to be logged during the execution.
+        /// This function fails and throws an exception if more than one event is logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <typeparam name="T">The return value of the function</typeparam>
+        /// <param name="func">The function.</param>
+        /// <returns>The returned value from <paramref name="func"/>.</returns>
+        Task<T> ExpectOneAsync<T>(Func<T> func);
 
         /// <summary>
         /// Executes <paramref name="func"/> and
@@ -81,6 +158,18 @@ namespace Akka.TestKit
         /// <param name="func">The function.</param>
         /// <returns>The returned value from <paramref name="func"/>.</returns>
         T ExpectOne<T>(TimeSpan timeout, Func<T> func);
+        
+        /// <summary>
+        /// Executes <paramref name="func"/> and
+        /// expects one event to be logged during the execution.
+        /// This function fails and throws an exception if more than one event is logged,
+        /// or if a timeout occurs.
+        /// </summary>
+        /// <typeparam name="T">The return value of the function</typeparam>
+        /// <param name="timeout">The time to wait for a log event after executing <paramref name="func"/></param>
+        /// <param name="func">The function.</param>
+        /// <returns>The returned value from <paramref name="func"/>.</returns>
+        Task<T> ExpectOneAsync<T>(TimeSpan timeout, Func<T> func);
 
         /// <summary>
         /// Executes <paramref name="func"/> and expects the specified number
@@ -94,6 +183,19 @@ namespace Akka.TestKit
         /// <param name="func">The function.</param>
         /// <returns>The returned value from <paramref name="func"/>.</returns>
         T Expect<T>(int expectedCount, Func<T> func);
+        
+        /// <summary>
+        /// Executes <paramref name="func"/> and expects the specified number
+        /// of events to be logged during the execution.
+        /// This function fails and throws an exception if more events than expected are logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <typeparam name="T">The return value of the function</typeparam>
+        /// <param name="expectedCount">The expected number of events</param>
+        /// <param name="func">The function.</param>
+        /// <returns>The returned value from <paramref name="func"/>.</returns>
+        Task<T> ExpectAsync<T>(int expectedCount, Func<T> func);
 
         /// <summary>
         /// Executes <paramref name="func"/> and expects the specified number
@@ -108,6 +210,20 @@ namespace Akka.TestKit
         /// <param name="func">The function.</param>
         /// <returns>The returned value from <paramref name="func"/>.</returns>
         T Expect<T>(int expectedCount, TimeSpan timeout, Func<T> func);
+        
+        /// <summary>
+        /// Executes <paramref name="func"/> and expects the specified number
+        /// of events to be logged during the execution.
+        /// This function fails and throws an exception if more events than expected are logged,
+        /// or if a timeout occurs. The timeout is taken from the config value
+        /// "akka.test.filter-leeway", see <see cref="TestKitSettings.TestEventFilterLeeway"/>.
+        /// </summary>
+        /// <typeparam name="T">The return value of the function</typeparam>
+        /// <param name="timeout">The time to wait for log events after executing <paramref name="func"/></param>
+        /// <param name="expectedCount">The expected number of events</param>
+        /// <param name="func">The function.</param>
+        /// <returns>The returned value from <paramref name="func"/>.</returns>
+        Task<T> ExpectAsync<T>(int expectedCount, TimeSpan timeout, Func<T> func);
 
         /// <summary>
         /// Executes <paramref name="func"/> and prevent events from being logged during the execution.
@@ -116,6 +232,14 @@ namespace Akka.TestKit
         /// <param name="func">The function.</param>
         /// <returns>The returned value from <paramref name="func"/>.</returns>
         T Mute<T>(Func<T> func);
+        
+        /// <summary>
+        /// Executes <paramref name="func"/> and prevent events from being logged during the execution.
+        /// </summary>
+        /// <typeparam name="T">The return value of the function</typeparam>
+        /// <param name="func">The function.</param>
+        /// <returns>The returned value from <paramref name="func"/>.</returns>
+        Task<T> MuteAsync<T>(Func<T> func);
 
         /// <summary>
         /// Executes <paramref name="action"/> and prevent events from being logged during the execution.
@@ -123,6 +247,13 @@ namespace Akka.TestKit
         /// <param name="action">The function.</param>
         /// <returns>The returned value from <paramref name="action"/>.</returns>
         void Mute(Action action);
+        
+        /// <summary>
+        /// Executes <paramref name="action"/> and prevent events from being logged during the execution.
+        /// </summary>
+        /// <param name="action">The function.</param>
+        /// <returns>The returned value from <paramref name="action"/>.</returns>
+        Task MuteAsync(Action action);
 
         /// <summary>
         /// Prevents events from being logged from now on. To allow events to be logged again, call 

src/core/Akka.TestKit/EventFilter/Internal/EventFilterApplier.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Event;
 using Akka.TestKit.TestEvent;
@@ -45,6 +46,20 @@ namespace Akka.TestKit.Internal
             InternalExpect(action, _actorSystem, 1);
         }
 
+        public Task ExpectOneAsync(Func<Task> actionAsync)
+        {
+            return InternalExpectAsync(actionAsync, _actorSystem, 1);
+        }
+
+        /// <summary>
+        /// Async version of <see cref="ExpectOne(System.Action)"/>
+        /// </summary>
+        /// <param name="action"></param>
+        public async Task ExpectOneAsync(Action action)
+        {
+            await InternalExpectAsync(action, _actorSystem, 1);
+        }
+
         /// <summary>
         /// TBD
         /// </summary>
@@ -53,6 +68,15 @@ namespace Akka.TestKit.Internal
         public void ExpectOne(TimeSpan timeout, Action action)
         {
             InternalExpect(action, _actorSystem, 1, timeout);
+        }
+        
+        /// <summary>
+        /// Async version of <see cref="ExpectOne(System.TimeSpan,System.Action) "/>
+        /// </summary>
+        /// <returns></returns>
+        public async Task ExpectOneAsync(TimeSpan timeout, Action action)
+        {
+            await InternalExpectAsync(action, _actorSystem, 1, timeout);
         }
 
         /// <summary>
@@ -66,6 +90,22 @@ namespace Akka.TestKit.Internal
         }
 
         /// <summary>
+        /// Async version of Expect
+        /// </summary>
+        public Task ExpectAsync(int expectedCount, Func<Task> actionAsync)
+        {
+            return InternalExpectAsync(actionAsync, _actorSystem, expectedCount, null);
+        }
+
+        /// <summary>
+        /// Async version of <see cref="Expect(int,System.Action)"/>
+        /// </summary>
+        public async Task ExpectAsync(int expectedCount, Action action)
+        {
+            await InternalExpectAsync(action, _actorSystem, expectedCount, null);
+        }
+
+        /// <summary>
         /// TBD
         /// </summary>
         /// <param name="expectedCount">TBD</param>
@@ -74,6 +114,14 @@ namespace Akka.TestKit.Internal
         public void Expect(int expectedCount, TimeSpan timeout, Action action)
         {
             InternalExpect(action, _actorSystem, expectedCount, timeout);
+        }
+        
+        /// <summary>
+        /// Async version of <see cref="Expect(int,System.TimeSpan,System.Action)"/>
+        /// </summary>
+        public async Task ExpectAsync(int expectedCount, TimeSpan timeout, Action action)
+        {
+            await InternalExpectAsync(action, _actorSystem, expectedCount, timeout);
         }
 
         /// <summary>
@@ -86,6 +134,14 @@ namespace Akka.TestKit.Internal
         {
             return Intercept(func, _actorSystem, null, 1);
         }
+        
+        /// <summary>
+        /// Async version of ExpectOne
+        /// </summary>
+        public async Task<T> ExpectOneAsync<T>(Func<T> func)
+        {
+            return await InterceptAsync(func, _actorSystem, null, 1);
+        }
 
         /// <summary>
         /// TBD
@@ -97,6 +153,14 @@ namespace Akka.TestKit.Internal
         public T ExpectOne<T>(TimeSpan timeout, Func<T> func)
         {
             return Intercept(func, _actorSystem, timeout, 1);
+        }
+        
+        /// <summary>
+        /// Async version of ExpectOne
+        /// </summary>
+        public async Task<T> ExpectOneAsync<T>(TimeSpan timeout, Func<T> func)
+        {
+            return await InterceptAsync(func, _actorSystem, timeout, 1);
         }
 
         /// <summary>
@@ -112,6 +176,14 @@ namespace Akka.TestKit.Internal
         }
 
         /// <summary>
+        /// Async version of Expect
+        /// </summary>
+        public async Task<T> ExpectAsync<T>(int expectedCount, Func<T> func)
+        {
+            return await InterceptAsync(func, _actorSystem, null, expectedCount);
+        }
+
+        /// <summary>
         /// TBD
         /// </summary>
         /// <typeparam name="T">TBD</typeparam>
@@ -122,6 +194,14 @@ namespace Akka.TestKit.Internal
         public T Expect<T>(int expectedCount, TimeSpan timeout, Func<T> func)
         {
             return Intercept(func, _actorSystem, timeout, expectedCount);
+        }
+        
+        /// <summary>
+        /// Async version of Expect
+        /// </summary>
+        public async Task<T> ExpectAsync<T>(int expectedCount, TimeSpan timeout, Func<T> func)
+        {
+            return await InterceptAsync(func, _actorSystem, timeout, expectedCount);
         }
 
         /// <summary>
@@ -134,6 +214,14 @@ namespace Akka.TestKit.Internal
         {
             return Intercept(func, _actorSystem, null, null);
         }
+        
+        /// <summary>
+        /// Async version of Mute
+        /// </summary>
+        public async Task<T> MuteAsync<T>(Func<T> func)
+        {
+            return await InterceptAsync(func, _actorSystem, null, null);
+        }
 
         /// <summary>
         /// TBD
@@ -142,6 +230,14 @@ namespace Akka.TestKit.Internal
         public void Mute(Action action)
         {
             Intercept<object>(() => { action(); return null; }, _actorSystem, null, null);
+        }
+        
+        /// <summary>
+        /// Async version of Mute
+        /// </summary>
+        public async Task MuteAsync(Action action)
+        {
+            await InterceptAsync<object>(() => { action(); return null; }, _actorSystem, null, null);
         }
 
         /// <summary>
@@ -228,6 +324,69 @@ namespace Akka.TestKit.Internal
         }
 
         /// <summary>
+        /// Async version of <see cref="Intercept{T}"/>
+        /// </summary>
+        protected Task<T> InterceptAsync<T>(Func<T> func, ActorSystem system, TimeSpan? timeout, int? expectedOccurrences, MatchedEventHandler matchedEventHandler = null)
+        {
+            return InterceptAsync(() => Task.FromResult(func()), system, timeout, expectedOccurrences, matchedEventHandler);
+        }
+        
+        /// <summary>
+        /// Async version of <see cref="Intercept{T}"/>
+        /// </summary>
+        protected async Task<T> InterceptAsync<T>(Func<Task<T>> func, ActorSystem system, TimeSpan? timeout, int? expectedOccurrences, MatchedEventHandler matchedEventHandler = null)
+        {
+            var leeway = system.HasExtension<TestKitSettings>()
+                ? TestKitExtension.For(system).TestEventFilterLeeway
+                : _testkit.TestKitSettings.TestEventFilterLeeway;
+
+            var timeoutValue = timeout.HasValue ? _testkit.Dilated(timeout.Value) : leeway;
+            matchedEventHandler = matchedEventHandler ?? new MatchedEventHandler();
+            system.EventStream.Publish(new Mute(_filters));
+            try
+            {
+                foreach(var filter in _filters)
+                {
+                    filter.EventMatched += matchedEventHandler.HandleEvent;
+                }
+                var result = await func();
+
+                if(!await AwaitDoneAsync(timeoutValue, expectedOccurrences, matchedEventHandler))
+                {
+                    var actualNumberOfEvents = matchedEventHandler.ReceivedCount;
+                    string msg;
+                    if(expectedOccurrences.HasValue)
+                    {
+                        var expectedNumberOfEvents = expectedOccurrences.Value;
+                        if(actualNumberOfEvents < expectedNumberOfEvents)
+                            msg = string.Format("Timeout ({0}) while waiting for messages. Only received {1}/{2} messages that matched filter [{3}]", timeoutValue, actualNumberOfEvents, expectedNumberOfEvents, string.Join(",", _filters));
+                        else
+                        {
+                            var tooMany = actualNumberOfEvents - expectedNumberOfEvents;
+                            msg = string.Format("Received {0} {1} too many. Expected {2} {3} but received {4} that matched filter [{5}]", tooMany, GetMessageString(tooMany), expectedNumberOfEvents, GetMessageString(expectedNumberOfEvents), actualNumberOfEvents, string.Join(",", _filters));
+                        }
+                    }
+                    else
+                        msg = string.Format("Timeout ({0}) while waiting for messages that matched filter [{1}]", timeoutValue, _filters);
+
+                    var assertionsProvider = system.HasExtension<TestKitAssertionsProvider>()
+                        ? TestKitAssertionsExtension.For(system)
+                        : TestKitAssertionsExtension.For(_testkit.Sys);
+                    assertionsProvider.Assertions.Fail(msg);
+                }
+                return result;
+            }
+            finally
+            {
+                foreach(var filter in _filters)
+                {
+                    filter.EventMatched -= matchedEventHandler.HandleEvent;
+                }
+                system.EventStream.Publish(new Unmute(_filters));
+            }
+        }
+
+        /// <summary>
         /// TBD
         /// </summary>
         /// <param name="timeout">TBD</param>
@@ -240,6 +399,20 @@ namespace Akka.TestKit.Internal
             {
                 var expected = expectedOccurrences.GetValueOrDefault();
                 _testkit.AwaitConditionNoThrow(() => matchedEventHandler.ReceivedCount >= expected, timeout);
+                return matchedEventHandler.ReceivedCount == expected;
+            }
+            return true;
+        }
+        
+        /// <summary>
+        /// Async version of <see cref="AwaitDone"/>
+        /// </summary>
+        protected async Task<bool> AwaitDoneAsync(TimeSpan timeout, int? expectedOccurrences, MatchedEventHandler matchedEventHandler)
+        {
+            if(expectedOccurrences.HasValue)
+            {
+                var expected = expectedOccurrences.GetValueOrDefault();
+                await _testkit.AwaitConditionNoThrowAsync(() => matchedEventHandler.ReceivedCount >= expected, timeout);
                 return matchedEventHandler.ReceivedCount == expected;
             }
             return true;
@@ -258,6 +431,22 @@ namespace Akka.TestKit.Internal
         private void InternalExpect(Action action, ActorSystem actorSystem, int expectedCount, TimeSpan? timeout = null)
         {
             Intercept<object>(() => { action(); return null; }, actorSystem, timeout, expectedCount);
+        }
+        
+        /// <summary>
+        /// Async version of <see cref="InternalExpect"/>
+        /// </summary>
+        private async Task InternalExpectAsync(Func<Task> actionAsync, ActorSystem actorSystem, int expectedCount, TimeSpan? timeout = null)
+        {
+            await InterceptAsync<object>(() => { actionAsync(); return Task.FromResult<object>(null); }, actorSystem, timeout, expectedCount);
+        }
+        
+        /// <summary>
+        /// Async version of <see cref="InternalExpect"/>
+        /// </summary>
+        private async Task InternalExpectAsync(Action action, ActorSystem actorSystem, int expectedCount, TimeSpan? timeout = null)
+        {
+            await InterceptAsync<object>(() => { action(); return Task.FromResult<object>(null); }, actorSystem, timeout, expectedCount);
         }
 
         /// <summary>

commit 5a234b60011de127b2d3316aa05de2245cfef848

Fix timing race condition in AwaitAssertAsync causing premature timeouts
Fixed a check-then-act race condition in TestKit's AwaitAssertAsync that
could cause tests to timeout prematurely with only 1 retry attempt instead
of the expected number of retries within the timeout window.

The bug was introduced in PR #4075 (Dec 2019) when async API was added to
TestKit. The timeout check occurred BEFORE Task.Delay(), creating a timing
window where thread scheduling delays, GC pauses, or system load could
cause the actual elapsed time to exceed the timeout even though the
pre-check indicated a retry should occur.

Changes:
- Move timeout check to AFTER Task.Delay() in both AwaitAssertAsync overloads
- Changed boundary condition from >= to > to allow final retry when at exact timeout
- Re-run assertion on timeout to propagate the actual exception instead of generic timeout
- Added explanatory comments documenting the race condition

This fixes flaky test failures in cluster tests where initialization under
load would take longer than expected, causing false timeout failures.

Fixes StartEntitySpec.StartEntity_while_the_entity_is_waiting_for_restart_should_restart_it_immediately

src/core/Akka.TestKit/TestKitBase_AwaitAssert.cs
@@ -62,16 +62,19 @@ namespace Akka.TestKit
                 }
                 catch(Exception)
                 {
-                    var stopped = Now + t;
-                    if (stopped >= stop)
-                    {
-                        Sys.Log.Warning("AwaitAssert failed, timeout [{0}] is over after [{1}] attempts and [{2}] elapsed time", max, attempts, stopped - start);
-                        throw;
-                    }
-                        
+                    // Check timeout after delay to avoid check-then-act race condition
                 }
                 attempts++;
                 await Task.Delay(t, cancellationToken);
+
+                // Check if we've exceeded the timeout AFTER sleeping
+                if (Now > stop)
+                {
+                    Sys.Log.Warning("AwaitAssert failed, timeout [{0}] is over after [{1}] attempts and [{2}] elapsed time", max, attempts, Now - start);
+                    // Re-run the assertion one final time to get the actual exception
+                    assertion();
+                }
+
                 t = (stop - Now).Min(intervalValue);
             }
         }
@@ -96,9 +99,11 @@ namespace Akka.TestKit
             var intervalValue = interval.GetValueOrDefault(TimeSpan.FromMilliseconds(100));
             if(intervalValue == Timeout.InfiniteTimeSpan) intervalValue = TimeSpan.MaxValue;
             intervalValue.EnsureIsPositiveFinite("interval");
+            var start = Now;
             var max = RemainingOrDilated(duration);
             var stop = Now + max;
             var t = max.Min(intervalValue);
+            var attempts = 0;
             while(true)
             {
                 cancellationToken.ThrowIfCancellationRequested();
@@ -109,10 +114,19 @@ namespace Akka.TestKit
                 }
                 catch(Exception)
                 {
-                    if(Now + t >= stop)
-                        throw;
+                    // Check timeout after delay to avoid check-then-act race condition
                 }
+                attempts++;
                 await Task.Delay(t, cancellationToken);
+
+                // Check if we've exceeded the timeout AFTER sleeping
+                if (Now > stop)
+                {
+                    Sys.Log.Warning("AwaitAssert failed, timeout [{0}] is over after [{1}] attempts and [{2}] elapsed time", max, attempts, Now - start);
+                    // Re-run the assertion one final time to get the actual exception
+                    await assertion();
+                }
+
                 t = (stop - Now).Min(intervalValue);
             }
         }