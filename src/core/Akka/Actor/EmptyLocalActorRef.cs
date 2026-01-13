683384454420a1ecfab0b50d2e18529428a9aa07
Gregorius Soedharmo <arkatufus@yahoo.com>
2026-01-14 01:03:50 +0700

    [Nullable] Convert EmptyLocalActorRef and Exceptions

--- src/core/Akka/Actor/EmptyLocalActorRef.cs
+++ src/core/Akka/Actor/EmptyLocalActorRef.cs
@@ -10,100 +10,98 @@ using Akka.Dispatch;
 using Akka.Dispatch.SysMsg;
 using Akka.Event;
 
-namespace Akka.Actor
+#nullable enable
+namespace Akka.Actor;
+
+/// <summary>
+/// TBD
+/// </summary>
+public class EmptyLocalActorRef : MinimalActorRef
 {
+    private readonly EventStream _eventStream;
+
     /// <summary>
     /// TBD
     /// </summary>
-    public class EmptyLocalActorRef : MinimalActorRef
+    /// <param name="provider">TBD</param>
+    /// <param name="path">TBD</param>
+    /// <param name="eventStream">TBD</param>
+    public EmptyLocalActorRef(IActorRefProvider provider, ActorPath path, EventStream eventStream)
     {
-        private readonly IActorRefProvider _provider;
-        private readonly ActorPath _path;
-        private readonly EventStream _eventStream;
+        Provider = provider;
+        Path = path;
+        _eventStream = eventStream;
+    }
 
-        /// <summary>
-        /// TBD
-        /// </summary>
-        /// <param name="provider">TBD</param>
-        /// <param name="path">TBD</param>
-        /// <param name="eventStream">TBD</param>
-        public EmptyLocalActorRef(IActorRefProvider provider, ActorPath path, EventStream eventStream)
-        {
-            _provider = provider;
-            _path = path;
-            _eventStream = eventStream;
-        }
+    /// <summary>
+    /// TBD
+    /// </summary>
+    public override ActorPath Path { get; }
 
-        /// <summary>
-        /// TBD
-        /// </summary>
-        public override ActorPath Path { get { return _path; } }
+    /// <summary>
+    /// TBD
+    /// </summary>
+    public override IActorRefProvider Provider { get; }
 
-        /// <summary>
-        /// TBD
-        /// </summary>
-        public override IActorRefProvider Provider { get { return _provider; } }        
-
-        /// <summary>
-        /// TBD
-        /// </summary>
-        [Obsolete("Use Context.Watch and Receive<Terminated> [1.1.0]")]
+    /// <summary>
+    /// TBD
+    /// </summary>
+    [Obsolete("Use Context.Watch and Receive<Terminated> [1.1.0]")]
 #pragma warning disable CS0809
-        public override bool IsTerminated { get { return true; } }
+    public override bool IsTerminated => true;
 #pragma warning restore CS0809
-        /// <summary>
-        /// TBD
-        /// </summary>
-        /// <param name="message">TBD</param>
-        /// <param name="sender">TBD</param>
-        /// <exception cref="InvalidMessageException">This exception is thrown if the given <paramref name="message"/> is undefined.</exception>
-        protected override void TellInternal(object message, IActorRef sender)
-        {
-            if (message == null) throw new InvalidMessageException("Message is null");
-            if (message is DeadLetter d) SpecialHandle(d.Message, d.Sender);
-            else if (!SpecialHandle(message, sender))
-            {
-                _eventStream.Publish(new DeadLetter(message, sender.IsNobody() ? _provider.DeadLetters : sender, this));
-            }
-        }
 
-        /// <summary>
-        /// TBD
-        /// </summary>
-        /// <param name="message">TBD</param>
-        public override void SendSystemMessage(ISystemMessage message)
+    /// <summary>
+    /// TBD
+    /// </summary>
+    /// <param name="message">TBD</param>
+    /// <param name="sender">TBD</param>
+    /// <exception cref="InvalidMessageException">This exception is thrown if the given <paramref name="message"/> is undefined.</exception>
+    protected override void TellInternal(object message, IActorRef sender)
+    {
+        if (message is null) throw new InvalidMessageException("Message is null");
+        if (message is DeadLetter d) SpecialHandle(d.Message, d.Sender);
+        else if (!SpecialHandle(message, sender))
         {
-            Mailbox.DebugPrint("EmptyLocalActorRef {0} having enqueued {1}", Path, message);
-            SpecialHandle(message, _provider.DeadLetters);
+            _eventStream.Publish(new DeadLetter(message, sender.IsNobody() ? Provider.DeadLetters : sender, this));
         }
+    }
 
-        /// <summary>
-        /// TBD
-        /// </summary>
-        /// <param name="message">TBD</param>
-        /// <param name="sender">TBD</param>
-        /// <returns>TBD</returns>
-        protected virtual bool SpecialHandle(object message, IActorRef sender)
+    /// <summary>
+    /// TBD
+    /// </summary>
+    /// <param name="message">TBD</param>
+    public override void SendSystemMessage(ISystemMessage message)
+    {
+        Mailbox.DebugPrint("EmptyLocalActorRef {0} having enqueued {1}", Path, message);
+        SpecialHandle(message, Provider.DeadLetters);
+    }
+
+    /// <summary>
+    /// TBD
+    /// </summary>
+    /// <param name="message">TBD</param>
+    /// <param name="sender">TBD</param>
+    /// <returns>TBD</returns>
+    protected virtual bool SpecialHandle(object message, IActorRef sender)
+    {
+        switch (message)
         {
-            if (message is Watch watch)
-            {
+            case Watch watch:
                 if (watch.Watchee.Equals(this) && !watch.Watcher.Equals(this))
                 {
                     watch.Watcher.SendSystemMessage(new DeathWatchNotification(watch.Watchee, existenceConfirmed: false, addressTerminated: false));
                 }
                 return true;
-            }
-            if (message is Unwatch)
+            
+            case Unwatch:
                 return true;    //Just ignore
-
-            if (message is Identify identify)
-            {
+            
+            case Identify identify:
                 sender.Tell(new ActorIdentity(identify.MessageId, null));
                 return true;
-            }
-
-            if (message is ActorSelectionMessage actorSelectionMessage)
-            {
+            
+            case ActorSelectionMessage actorSelectionMessage:
                 if (actorSelectionMessage.Message is Identify selectionIdentify)
                 {
                     if (!actorSelectionMessage.WildCardFanOut)
@@ -117,24 +115,23 @@ namespace Akka.Actor
                     }
                     else
                     {
-                        _eventStream.Publish(new DeadLetter(actorSelectionMessage.Message, sender.IsNobody() ? _provider.DeadLetters : sender, this));
+                        _eventStream.Publish(new DeadLetter(actorSelectionMessage.Message, sender.IsNobody() ? Provider.DeadLetters : sender, this));
                     }
                 }
                 return true;
-            }
-
-            if (WrappedMessage.IsDeadLetterSuppressedAnywhere(message))
-            {
-                PublishSupressedDeadLetter(message, sender);
-                return true;
-            }
-
-            return false;
         }
 
-        private void PublishSupressedDeadLetter(object msg, IActorRef sender)
+        if (WrappedMessage.IsDeadLetterSuppressedAnywhere(message))
         {
-            _eventStream.Publish(new SuppressedDeadLetter(msg, sender.IsNobody() ? _provider.DeadLetters : sender, this));
+            PublishSupressedDeadLetter(message, sender);
+            return true;
         }
+
+        return false;
+    }
+
+    private void PublishSupressedDeadLetter(object msg, IActorRef sender)
+    {
+        _eventStream.Publish(new SuppressedDeadLetter(msg, sender.IsNobody() ? Provider.DeadLetters : sender, this));
     }
 }
