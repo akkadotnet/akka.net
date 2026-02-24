4b095136eff5f2a0f7dd8a55b14ca773ae6549ad
Gregorius Soedharmo <arkatufus@yahoo.com>
2026-02-25 01:23:05 +0700

    Implement xUnit 3 compatible Akka.Tests.Shared.Internals

---
+++ src/core/Akka.Tests.Shared.Internals.Xunit3/Helpers/FSharpDelegateHelper.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FSharpDelegateHelper.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Microsoft.FSharp.Core;
+
+// ReSharper disable once CheckNamespace
+namespace Akka.Tests.Shared.Internals.Helpers;
+
+/// <summary>
+/// Maps F# methods to C# delegates
+/// </summary>
+public static class FsharpDelegateHelper
+{
+    public static FSharpFunc<T2, TResult> Create<T2, TResult>(Func<T2, TResult> func)
+    {
+        return FSharpFunc<T2, TResult>.FromConverter(Conv);
+        
+        TResult Conv(T2 input) => func(input);
+    }
+
+    public static FSharpFunc<T1, FSharpFunc<T2, TResult>> Create<T1, T2, TResult>(Func<T1, T2, TResult> func)
+    {
+        return FSharpFunc<T1, FSharpFunc<T2, TResult>>.FromConverter(Conv);
+        
+        FSharpFunc<T2, TResult> Conv(T1 value1)
+        {
+            return Create<T2, TResult>(value2 => func(value1, value2));
+        }
+    }
+
+    public static FSharpFunc<T1, FSharpFunc<T2, FSharpFunc<T3, TResult>>> Create<T1, T2, T3, TResult>(
+        Func<T1, T2, T3, TResult> func)
+    {
+        return FSharpFunc<T1, FSharpFunc<T2, FSharpFunc<T3, TResult>>>.FromConverter(Conv);
+        
+        FSharpFunc<T2, FSharpFunc<T3, TResult>> Conv(T1 value1)
+        {
+            return Create<T2, T3, TResult>((value2, value3) => func(value1, value2, value3));
+        }
+    }
+}
---
+++ src/core/Akka.Tests.Shared.Internals.Xunit3/TestReceiveActor.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestReceiveActor.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Akka.Actor;
+
+// ReSharper disable once CheckNamespace
+namespace Akka.TestKit;
+
+/// <summary>
+/// Just like <see cref="ReceiveActor"/>. Adds a Receive-overload that allows you to write code like:
+/// <code>Receive("the message", m => ... );</code>
+/// </summary>
+public class TestReceiveActor : ReceiveActor
+{
+    public void Receive<T>(T value, Action<T> handler) where T : IEquatable<T>
+    {
+        Receive(m => Equals(value, m), handler);
+    }
+}
