5b05104a96cb249825d8adff211be95edea047fb
Gregorius Soedharmo <arkatufus@yahoo.com>
2026-03-27 23:13:20 +0700

    [xUnit 3] Convert Akka.Cluster.TestKit

--- src/core/Akka/Properties/AssemblyInfo.cs
+++ src/core/Akka/Properties/AssemblyInfo.cs
@@ -45,6 +45,7 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Akka.MultiNodeTestRunner.Shared.Tests")]
 [assembly: InternalsVisibleTo("Akka.Cluster.Tests.MultiNode")]
 [assembly: InternalsVisibleTo("Akka.Cluster.TestKit")]
+[assembly: InternalsVisibleTo("Akka.Cluster.TestKit.Xunit2")]
 [assembly: InternalsVisibleTo("Akka.Cluster.Tools")]
 [assembly: InternalsVisibleTo("Akka.DistributedData")]
 [assembly: InternalsVisibleTo("Akka.Persistence")]
